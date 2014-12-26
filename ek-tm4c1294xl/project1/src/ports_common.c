5c85a4e4a0b83c945ace8c217908862c490a5a8f
TI Cortex M4 <ti.cortex.m4@gmail.com>
2014-12-26 21:20:32 +0300

    add new file

---
+++ ek-tm4c1294xl/project1/src/ports_common.c
@@ -0,0 +1,52 @@
+/*------------------------------------------------------------------------------
+PORTS_COMMON.C
+
+
+------------------------------------------------------------------------------*/
+
+#include        "main.h"
+#include        "mem_serial0.h"
+#include        "serial0.h"
+#include        "crc-16.h"
+#include        "ports.h"
+
+
+
+uchar   InBuff(uint  i) {
+  switch (ibPort) {
+    case 0: return mpbInBuff0[i];
+  }
+
+  return 0; // TODO
+}
+
+
+void    Answer(uint  wSize, serial  seT) {
+  switch (ibPort) {
+    case 0:	Answer0(wSize, seT); break;
+  }
+}
+
+
+uint    IndexInBuff(void) {
+  switch (ibPort) {
+    case 0: return iwInBuff0;
+  }
+
+  return 0; // TODO
+}
+
+
+
+void    MakeCRC16OutBuff(uchar  bOffset, uint  wSize) {
+  switch (ibPort) {
+    case 0: MakeCRC16(mpbOutBuff0+bOffset, wSize); break;
+  }
+}
+
+
+void    MakeCRC16InBuff(uchar  bOffset, uint  wSize) {
+  switch (ibPort) {
+    case 0: MakeCRC16(mpbInBuff0+bOffset, wSize); break;
+  }
+}
