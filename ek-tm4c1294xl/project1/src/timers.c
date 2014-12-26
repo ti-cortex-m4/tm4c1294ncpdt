27b11832c27066ec21fa88ea6f6d87ecf5c1283f
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2014-12-26 17:00:36 +0300

    update timer

--- ek-tm4c1294xl/project1/src/timers.c
+++ ek-tm4c1294xl/project1/src/timers.c
@@ -1,34 +1,33 @@
-//*****************************************************************************
-// timers.c
-//*****************************************************************************
+/*------------------------------------------------------------------------------
+TIMERS.C
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "inc/hw_ints.h"
-#include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
-#include "driverlib/debug.h"
-#include "driverlib/fpu.h"
-#include "driverlib/gpio.h"
-#include "driverlib/interrupt.h"
-#include "driverlib/pin_map.h"
-#include "driverlib/rom.h"
-#include "driverlib/rom_map.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/timer.h"
-#include "driverlib/uart.h"
 
-void InitTimers(uint32_t ui32SysClock)
-{
+------------------------------------------------------------------------------*/
+
+#include 		"main.h"
+#include 		"inc/hw_memmap.h"
+#include 		"inc/hw_ints.h"
+//#include 		"driverlib/debug.h"
+//#include 		"driverlib/fpu.h"
+#include 		"driverlib/gpio.h"
+#include 		"driverlib/interrupt.h"
+//#include 		"driverlib/pin_map.h"
+//#include 		"driverlib/rom.h"
+//#include 		"driverlib/rom_map.h"
+#include 		"driverlib/sysctl.h"
+#include 		"driverlib/timer.h"
+//#include 		"driverlib/uart.h"
+#include 		"serial0.h"
+
+
+
+void InitTimers(uint32_t ui32SysClock) {
     // Enable the peripherals.
     SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
 
     // Configure the 32-bit periodic timer.
     TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
     TimerLoadSet(TIMER0_BASE, TIMER_A, ui32SysClock / 1);
-
-    // Enable processor interrupts.
-    IntMasterEnable();
 
     // Setup the interrupts for the timer timeouts.
     IntEnable(INT_TIMER0A);
@@ -38,9 +37,12 @@ void InitTimers(uint32_t ui32SysClock)
     TimerEnable(TIMER0_BASE, TIMER_A);
 }
 
-void Timer0IntHandler(void)
-{
+
+
+void Timer0IntHandler(void) {
 	TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+	InDelay0();
 
     GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
     SysCtlDelay(5000);
