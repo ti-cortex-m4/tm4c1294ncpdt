63cfaf6dcd2ebbe10c6f2088ea9bf2cc64603e66
TI Cortex M4 <ti.cortex.m4@gmail.com>
2014-12-26 14:38:54 +0300

    edit some files

--- ek-tm4c1294xl/project1/src/mem_ports.h
+++ ek-tm4c1294xl/project1/src/mem_ports.h
@@ -6,6 +6,8 @@ MEM_PORTS.H
 
 extern  port                mppoPorts[bPORTS];
 
+extern	boolean				boSendAT;
+
 extern  uchar               mpcbSendAT[bPORTS];
 
 extern  answer              mpanSendAT[bPORTS];
@@ -15,3 +17,17 @@ extern  uint                mpwInDelay[bPORTS];
 extern  uint                mpwMinInDelay[bPORTS], mpwMajInDelay[bPORTS];
 
 extern  boolean             mpboLocal[bPORTS];
+
+extern	uchar               bInBuff0,
+                            bInBuff1,
+                            bInBuff2,
+                            bInBuff3,
+                            bInBuff4,
+                            bInBuff5,
+                            bInBuff6,
+                            bInBuff7,
+                            bInBuff8,
+                            bInBuff9,
+                            bInBuffA;
+
+extern	uchar               bQuery;
