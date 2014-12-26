beccb25d7c4d1b6b0b6e9103fd9895982057fdff
TI Cortex M4 <ti.cortex.m4@gmail.com>
2014-12-26 21:13:02 +0300

    add new files

--- ek-tm4c1294xl/project1/src/ports.h
+++ ek-tm4c1294xl/project1/src/ports.h
@@ -4,4 +4,19 @@ PORTS.H
 
 ------------------------------------------------------------------------------*/
 
-extern  serial              mpSerial[bPORTS];
+#include        "main.h"
+#include        "ports_stack.h"
+#include        "ports_common.h"
+
+
+
+
+extern  serial                  mpSerial[bPORTS];
+
+extern	uchar                   ibPort;
+
+
+
+void    Output(uint  wSize);
+void    Result(uchar  bT);
+void    LongResult(uchar  bT);
