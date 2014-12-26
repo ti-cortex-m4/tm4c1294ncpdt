198584849678347bfa315114250b13565b921891
TI Cortex M4 <ti.cortex.m4@gmail.com>
2014-12-26 14:09:15 +0300

    add new files

---
+++ ek-tm4c1294xl/project1/src/queries.h
@@ -0,0 +1,48 @@
+/*------------------------------------------------------------------------------
+QUERIES.H
+
+ Запросы
+------------------------------------------------------------------------------*/
+
+
+#ifndef __INQUIRYS
+#define __INQUIRYS
+
+
+#define bINQ_GETCURRTIME               1
+
+#define bINQ_SETKEY                    246
+#define bINQ_GETDISPLAY                247
+
+
+// ???
+#define bESC_OK                        'O'
+#define bESC_BADCOMMAND                'C'
+#define bESC_BADADDRESS                'A'
+#define bESC_BADDATA                   'D'
+#define bESC_BADSIZE                   'S'
+#define bESC_BADMODE                   'M'
+#define bESC_BADFLASH                  'F'
+#define bESC_BADDIGITAL                'D'
+
+
+// ???
+#define bRES_OK                        0
+#define bRES_BADCOMMAND                1
+#define bRES_BADADDRESS                2
+#define bRES_BADDATA                   3
+#define bRES_BADSIZE                   4
+#define bRES_BADMODE                   5
+#define bRES_BADFLASH                  6
+#define bRES_BADDIGITAL                7
+#define bRES_BADPORT                   8
+#define bRES_BADACCESS                 9
+#define bRES_BADCORRECTION             10
+#define bRES_BUSY                      11
+#define bRES_OUTOVERFLOW               99
+#define bRES_NEEDPROGRAM               100
+#define bRES_NEEDWORK                  101
+#define bRES_NEEDREPROGRAM             102
+
+
+#endif
