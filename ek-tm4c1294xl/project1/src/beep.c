087ba10af5ccf129db38a6b94d7716794ba02fd0
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2014-12-28 18:56:35 +0300

    add new file

---
+++ ek-tm4c1294xl/project1/src/beep.c
@@ -0,0 +1,35 @@
+/*------------------------------------------------------------------------------
+BEEP.C
+
+
+------------------------------------------------------------------------------*/
+
+#include        "delay.h"
+
+
+
+void    SoundOn(void) {
+}
+
+
+void    SoundOff(void) {
+}
+
+
+
+void    InitBeep(void) {
+  SoundOff();       
+}
+
+
+
+void    Beep(void) {
+  SoundOn();  Delay(50);
+  SoundOff(); Delay(50);
+}
+
+
+void    LongBeep(void) {
+  Beep(); Beep(); Beep();
+}
+
