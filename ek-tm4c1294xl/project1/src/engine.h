b87c2d0d1248fe58ca6843e4afd431abb4e16b1a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2014-12-28 19:16:04 +0300

    add new file

---
+++ ek-tm4c1294xl/project1/src/engine.h
@@ -0,0 +1,19 @@
+/*------------------------------------------------------------------------------
+ENGINE.H
+
+
+------------------------------------------------------------------------------*/
+
+#include        "main.h"
+
+
+
+extern  uchar                   ibCan,ibGrp,ibDig;
+
+extern  uchar                   ibDay,ibMon;
+
+extern  uint                    iwHou;
+
+extern  ulong                   dwBuffC;
+
+extern  uint                    wBuffD;
