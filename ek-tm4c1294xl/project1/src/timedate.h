726b3bb8bc04963344aca5df6b85a0f5e182c103
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2014-12-28 20:57:53 +0300

    add time/date routines

--- ek-tm4c1294xl/project1/src/timedate.h
+++ ek-tm4c1294xl/project1/src/timedate.h
@@ -10,5 +10,18 @@ TIMEDATE.H
 
 extern  time                    tiAlt;
 
+
+
+bool    TrueTimeDate(void);
+uchar   Weekday(void);
+
+uchar   DaysInMonth(void);
+uchar   DaysInMonthSpec(uchar  bYear, uchar  bMonth);
+uint    DaysInYearSpec(uchar  bYear);
+
 ulong   DateToSecIndex(void);
 void    SecIndexToDate(ulong  dwT);
+
+void    ShowTime(void);
+void    ShowDate(void);
+void    ShowTimeDate(void);
