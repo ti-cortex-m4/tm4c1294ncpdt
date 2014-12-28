4188e595e719d64c6dc117bc3d729567642a6bbb
TI Cortex M4 <ti.cortex.m4@gmail.com>
2014-12-28 20:57:53 +0300

    add time/date routines

--- ek-tm4c1294xl/project1/src/timedate.c
+++ ek-tm4c1294xl/project1/src/timedate.c
@@ -5,14 +5,72 @@ TIMEDATE.H
 ------------------------------------------------------------------------------*/
 
 #include        "main.h"
+#include        "display.h"
+#include        "lines.h"
+#include        "engine.h"
 
 
 
 time                    tiAlt;
-ulong                   dwBuffC;
 
-static const uchar      mpbDaysInMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+uchar const             mpbDaysInMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
+
+
+bool     TrueTimeDate(void)
+{
+  if (tiAlt.bSecond > 59) return false;
+  if (tiAlt.bMinute > 59) return false;
+  if (tiAlt.bHour   > 23) return false;
+
+  if ((tiAlt.bDay == 0) || (tiAlt.bDay > DaysInMonth()))
+    return false;
+
+  if ((tiAlt.bMonth == 0) || (tiAlt.bMonth > 12 ))
+    return false;
+
+  if ((tiAlt.bYear < 2) || (tiAlt.bYear > 99))
+    return false;
+
+  return true;
+}
+
+
+
+uchar   Weekday(void)
+{
+uchar   i;
+
+  wBuffD = 5 - 1 + tiAlt.bDay;
+
+  for (i=1; i<tiAlt.bMonth; i++)
+  {
+    if ((tiAlt.bYear % 4 == 0) && (i == 2))
+      wBuffD += 29;
+    else
+      wBuffD += mpbDaysInMonth[i - 1];
+  }
+
+  for (i=0; i<tiAlt.bYear; i++)
+  {
+    if (i % 4 == 0)
+      wBuffD += 366;
+    else
+      wBuffD += 365;
+  }
+
+  return(wBuffD % 7);
+}
+
+
+
+uchar   DaysInMonth(void)
+{
+  if ((tiAlt.bYear % 4 == 0) && (tiAlt.bMonth == 2))
+    return(29);
+  else
+    return( mpbDaysInMonth[tiAlt.bMonth - 1] );
+}
 
 
 uchar   DaysInMonthSpec(uchar  bYear, uchar  bMonth)
@@ -79,4 +137,36 @@ void    SecIndexToDate(ulong  dwT)
 
   tiAlt.bMinute = dwT/60;  dwT -= (ulong)60*tiAlt.bMinute;
   tiAlt.bSecond = dwT;
+}
+
+
+
+void    ShowTime(void)
+{
+  sprintf(szLo+4,"%02u:%02u:%02u",
+                 tiAlt.bHour,
+                 tiAlt.bMinute,
+                 tiAlt.bSecond);
+}
+
+
+void    ShowDate(void)
+{
+  sprintf(szLo+4,"%02u.%02u.%02u",
+                 tiAlt.bDay,
+                 tiAlt.bMonth,
+                 tiAlt.bYear);
+
+  szLo[14] = szDigits[Weekday() + 1];
+}
+
+
+void    ShowTimeDate(void)
+{
+  sprintf(szLo+1,"%02u:%02u %02u.%02u.%02u",
+                 tiAlt.bHour,
+                 tiAlt.bMinute,
+                 tiAlt.bDay,
+                 tiAlt.bMonth,
+                 tiAlt.bYear);
 }
