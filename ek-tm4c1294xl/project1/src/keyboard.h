e10311e146eb2528f379183a533173a961eb7822
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2014-12-28 11:59:57 +0300

    add keyboard handler

--- ek-tm4c1294xl/project1/src/keyboard.h
+++ ek-tm4c1294xl/project1/src/keyboard.h
@@ -1,8 +1,50 @@
 /*------------------------------------------------------------------------------
 KEYBOARD.H
 
-
+ Главная функция обработки нажатий клавиатуры
 ------------------------------------------------------------------------------*/
+
+#include        "main.h"
+/*
+#include        "timer0.h"
+#include        "lines.h"
+*/
 
 extern  uchar                   bKey;
 extern  bool                    fKey;
+/*
+extern  uchar           code    mpbKeys[14];
+
+extern  key             data    enKeyboard;
+
+extern  global          data    enGlobal;
+
+extern  uchar           data    ibX,ibY,ibZ;
+
+extern  uchar           idata   ibXmin,ibXmax,
+                                ibYmin,ibYmax;
+
+extern  uint            idata   iwA,iwAmin,iwAmax;
+
+extern  uchar           data    bProgram;
+
+extern  bit                     fUpdateA,
+                                fUpdateB;
+
+
+
+void    InitKeyboard(void);
+void    ResetKeyboard(void);
+
+void    Keyboard(void);
+
+void    NoProgram(void);
+void    BlockProgram(uchar  bA);
+void    BlockProgramExt(uchar  bA);
+void    BlockPrograms(uchar  bA, uchar  bB);
+void    NeedProgram(uchar  bA);
+void    NeedPrograms(uchar  bA, uchar  bB);
+
+void    ShowLogin(void);
+uchar   GetMaximum(void);
+*/
