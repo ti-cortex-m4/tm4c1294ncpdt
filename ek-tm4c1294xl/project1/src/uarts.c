9c0247c88e7cd6af011b69ff8add911def3626cb
TI Cortex M4 <ti.cortex.m4@gmail.com>
2014-12-28 10:35:46 +0300

    minor fix

--- ek-tm4c1294xl/project1/src/uarts.c
+++ ek-tm4c1294xl/project1/src/uarts.c
@@ -23,6 +23,13 @@ void UARTSend(const uint8_t *pui8Buffer) {
 }
 
 
+void UARTSend2(const uint8_t *pui8Buffer, uint8_t wSize) {
+    while (wSize-- > 0) {
+        UARTCharPut(UART0_BASE, *pui8Buffer++);
+    }
+}
+
+
 void InitUARTs(uint32_t ui32SysClock) {
     // Enable the GPIO port that is used for the on-board LED.
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
@@ -39,6 +46,10 @@ void InitUARTs(uint32_t ui32SysClock) {
     GPIOPinConfigure(GPIO_PA1_U0TX);
     ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
+    UARTFIFOLevelSet(UART0_BASE, UART_FIFO_TX1_8,UART_FIFO_RX1_8);
+    UARTTxIntModeSet(UART0_BASE, UART_TXINT_MODE_EOT) ;
+    //UARTFIFOEnable(INT_UART0);
+
     // Configure the UART for 115,200, 8-N-1 operation.
     ROM_UARTConfigSetExpClk(UART0_BASE, ui32SysClock, 9600, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
 
@@ -46,12 +57,12 @@ void InitUARTs(uint32_t ui32SysClock) {
 
     // Enable the UART interrupt.
     ROM_IntEnable(INT_UART0);
-    ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+    ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT | UART_INT_TX);
 
-    UARTSend((uint8_t *)"Start");
+    //UARTSend((uint8_t *)"Start");
 }
 
-
+/*
 void UART0IntHandler2(void) {
 uint32_t ui32Status;
 
@@ -66,3 +77,4 @@ uint32_t ui32Status;
         GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0);
     }
 }
+*/
