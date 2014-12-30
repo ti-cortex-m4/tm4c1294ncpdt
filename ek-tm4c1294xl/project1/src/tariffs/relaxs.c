06de54590f01112418a4dea62bc24538bf15262c
ti-cortex-m4 <ti.cortex.m4@gmail.com>
2014-12-30 10:41:23 +0300

    add tariffs routines

---
+++ ek-tm4c1294xl/project1/src/tariffs/relaxs.c
@@ -0,0 +1,160 @@
+/*------------------------------------------------------------------------------
+RELAXS.C
+
+ Подпрограммы управления праздниками
+------------------------------------------------------------------------------*/
+
+#include        "../main.h"
+#include        "../mem_tariffs.h"
+#include        "../memory_program.h"
+#include        "../keyboard.h"
+#include        "../display.h"
+#include        "../timedate.h"
+#include        "../rtc.h"
+#include        "tariffs.h"
+
+
+
+//                                           0123456789ABCDEF
+static char const       szNoRelaxs[]      = " нет праздников ",
+                        szRelaxHoliday[]  = " выходной",
+                        szRelaxWeekday[]  = " рабочий ",
+                        szRelaxError[]    = " ошибка !";
+
+
+
+// читает количество дней в списке праздников
+uchar   GetRelaxSize(void)
+{
+  return( mpreRelaxs.bSize );
+}
+
+
+// записывает количество дней в списке праздников
+void    SetRelaxSize(uchar  bSize)
+{
+  mpreRelaxs.bSize = bSize;
+}
+
+
+
+// читает день из списка праздников
+void    GetRelaxDate(uchar  ibRelax)
+{
+  tiRelax = mpreRelaxs.mptiDate[ibRelax];
+}
+
+
+// записывает день в списов праздников
+void    SetRelaxDate(uchar  ibRelax)
+{
+  mpreRelaxs.mptiDate[ibRelax] = tiRelax;
+}
+
+
+
+// установка списка праздников по умолчанию
+void    DefaultRelaxs(void)
+{
+uchar const     mpRelaxs[8*3] =
+{ 1,  1,  2,
+  2,  1,  2,
+  7,  1,  2,
+  8,  3,  2,
+  1,  5,  2,
+  9,  5,  2,
+  7, 11,  2,
+ 25, 12,  2 };
+
+  SetRelaxSize(0);
+
+  ibZ = 0;
+  for (ibX=0; ibX<sizeof(mpRelaxs)/3; ibX++)
+  {
+    tiRelax.bDay    = mpRelaxs[ibZ++];
+    tiRelax.bMonth  = mpRelaxs[ibZ++];
+    tiRelax.bSecond = mpRelaxs[ibZ++];  // тип праздника: будни, суббота, воскресенье
+
+    SetRelaxDate(ibX);
+    SetRelaxSize(GetRelaxSize() + 1);
+  }
+
+  boSetRelaxs = boTrue; //boFalse;
+}
+
+
+
+// сброс списка праздников
+void    ResetRelaxs(void)
+{
+  SetRelaxSize(0);
+  boSetRelaxs = boTrue; //boFalse;
+
+  boRelaxsFlag = boFalse;
+  ibRelaxsTariff = 1;
+}
+
+
+
+// определяет тип дня недели
+uchar   GetModeAlt(void)
+{
+uchar  i;
+
+  if (bOldMode == 0)                    // без учёта выходных и праздничных дней
+    return(0);                          // будни
+
+  for (i=0; i<GetRelaxSize(); i++)
+  {
+    GetRelaxDate(i);
+
+    if ((tiRelax.bDay   == tiAlt.bDay) &&
+        (tiRelax.bMonth == tiAlt.bMonth)) 
+      return(tiRelax.bSecond);          // тип текущего дня определяется по списку праздников
+  }
+
+  i = Weekday();                        // тип текущего дня определяется по календарю
+
+  if (i < 5)  return(0);                // будни
+  else 
+  if (i == 5) return(1);                // суббота
+  else        return(2);                // воскресенье
+}
+// требует предварительной установки переменной tiAlt
+
+
+
+// показывает тип текущего режима
+void    ShowModeName(uchar  ibMode)
+{
+  switch (ibMode)
+  {
+    case 1:  strcpy(szLo+7, szRelaxHoliday);  break;
+    case 2:  strcpy(szLo+7, szRelaxWeekday);  break;
+    default: strcpy(szLo+7, szRelaxError);    break;
+  }
+}
+
+
+
+// показывает дату и тип текущего праздника, его номер и общее количество праздников
+void    ShowRelax(uchar  ibRelax)
+{
+  if (GetRelaxSize() > 0)
+  {
+    GetRelaxDate(ibRelax);
+   
+    sprintf(szLo," %2u.%02u",
+                 tiRelax.bDay,
+                 tiRelax.bMonth);
+
+    ShowModeName(tiRelax.bSecond);
+
+    sprintf(szHi+11,"%2u/%-2u",
+                    ibRelax+1,
+                    GetRelaxSize());    
+  }
+  else ShowLo(szNoRelaxs);    
+}
+
+
---
+++ ek-tm4c1294xl/project1/src/tariffs/zones.h
@@ -0,0 +1,40 @@
+/*------------------------------------------------------------------------------
+ZONES.H
+
+ Подпрограммы управления суточными тарифными графиками
+------------------------------------------------------------------------------*/
+
+#include        "../main.h"
+
+
+
+extern  breaks                  brZero;
+
+
+
+uchar   GetZoneAltSize(void);
+void    SetZoneAltSize(uchar  bSize);
+
+void    GetZoneAltBreak(uchar  ibBreak);
+void    SetZoneAltBreak(uchar  ibBreak);
+
+uchar   GetZoneKeySize(void);
+void    SetZoneKeySize(uchar  bSize);
+
+void    GetZoneKeyBreak(uchar  ibBreak);
+void    SetZoneKeyBreak(uchar  ibBreak);
+
+uchar   GetBreakAltIndex(uchar  ibBreak);
+uchar   GetBreakKeyIndex(uchar  ibBreak);
+
+void    ResetZones(void);
+
+void    ShowZoneAltBreak(uchar  ibBreak);
+void    ShowZoneKeyBreak(uchar  ibBreak);
+
+bool    TrueZone(void);
+
+void    SetKeyZonePow(void);
+void    SetKeyZoneEng(void);
+
+
