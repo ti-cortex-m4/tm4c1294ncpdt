310f9e567a27dcf18bcf93cafa4a9207d925ba44
TI Cortex M4 <ti.cortex.m4@gmail.com>
2014-12-30 08:16:30 +0300

    update

--- ek-tm4c1294xl/project1/src/main.h
+++ ek-tm4c1294xl/project1/src/main.h
@@ -6,6 +6,7 @@
 #include        <stdbool.h>
 #include        "system.h"
 #include        "main_keyboard.h"
+#include        "main_serial.h"
 
 
 typedef unsigned char   uchar;
@@ -67,17 +68,6 @@ typedef enum
 } global;
 
 
-// тип 'режим работы порта'
-typedef enum 
-{ 
-  STR_SLAVEESC          = 0,
-  STR_SLAVECRC          = 1,
-  STR_MASTERDIRECT      = 2,
-  STR_MASTERMODEM       = 3,
-  STR_SLAVEUNI          = 4
-} stream;
-
-
 // тип 'настройки порта'
 typedef struct
 {
@@ -85,55 +75,6 @@ typedef struct
   uchar         ibParity;               // индекс контроля чётности
   stream        enStream;               // режим работы порта
 } port;
-
-
-
-// тип 'состояние порта'
-typedef enum 
-{ 
-  SER_BEGIN             = 0,            // начальное состояние
-
-  SER_OUTPUT_SLAVE      = 1,            // передача
-  SER_INPUT_SLAVE       = 2,            // приём
-  SER_POSTINPUT_SLAVE   = 3,            // приём завершён
-
-  SER_HEADER            = 4,            // передача заголовка
-  SER_POINTER           = 5,            // передача данных по указателю
-  SER_CRCLO             = 6,            // передача младнего байта CRC
-  SER_CRCHI             = 7,            // передача старшего байта CRC
-  SER_CLOSE             = 8,            // передача завершена
-
-  SER_ESC               = 9,            // приём Esc
-  SER_CTRL_Z            = 10,           // приём Ctrl Z
-  SER_CHAR              = 11,           // приём символа
-
-  SER_OUTPUT_MASTER     = 20,           // передача
-  SER_INPUT_MASTER      = 21,           // приём
-  SER_POSTINPUT_MASTER  = 22,           // приём завершён
-
-  SER_ANSWER_MODEM      = 23,           // приём от модема
-  SER_POSTANSWER_MODEM  = 24,           // приём от модема завершён
-
-  SER_GOODCHECK         = 30,           // контрольная сумма совпадает
-  SER_BADCHECK          = 31,           // ошибка: плохая контрольная сумма
-  SER_BADLINK           = 32,           // ошибка: нет ответа
-  SER_OVERFLOW          = 33,           // ошибка: переполнение входного буфера
-
-  SER_PAUSE             = 40,           // пауза отработана
-
-  SER_PACKET_HEADER     = 50,
-  SER_PACKET_BODY       = 51,
-
-  SER_LOCAL             = 60,
-  SER_BULK              = 61,
-
-  SER_INPUT_SLAVE2      = 62,           // приём
-  SER_POSTINPUT_SLAVE2  = 63,           // приём завершён
-} serial;        
-
-
-#define SERIAL_MODEM    0xFF00
-#define SERIAL_LOCAL    0xFF01
 
 
 #endif
