7bd9e07bb558010935d7586fd0388a5d5c3ce68f
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2014-12-30 13:02:25 +0300

    add start routines

--- ek-tm4c1294xl/project1/src/mem_program.h
+++ ek-tm4c1294xl/project1/src/mem_program.h
@@ -6,5 +6,6 @@ MEMORY_PROGRAM.H
 
 extern boolean                 boSetTime,
                                boSetDate,
+                               boSetGroups,
                                boSetPassword,
                                boSetRelaxs;
