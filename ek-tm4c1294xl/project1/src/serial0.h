5eb005fe45c097803ca7507fe8836fb7297472d9
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-01-17 10:37:42 +0300

    update timer0

--- ek-tm4c1294xl/project1/src/serial0.h
+++ ek-tm4c1294xl/project1/src/serial0.h
@@ -9,6 +9,6 @@ SERIAL0.H
 
 
 void    InitSerial0(void);
-void    InDelay0(void);
+void    InDelay0_Timer0(void);
 void    Query0(uint  cwIn, uchar  cbOut, bool  fMinInDelay);
 void    Answer0(uint  wSize, serial  seT);
