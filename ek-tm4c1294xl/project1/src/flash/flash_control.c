fbcd225a13c5c84078d7cf0ca4432fec2aeb34ab
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-01-23 21:34:00 +0300

    minor update

---
+++ ek-tm4c1294xl/project1/src/flash/flash_control.c
@@ -0,0 +1,38 @@
+/*------------------------------------------------------------------------------
+FLASH_CONTROL.C
+
+
+------------------------------------------------------------------------------*/
+
+#include        <string.h>
+#include        "../main.h"
+#include        "../mem_flash.h"
+#include        "../mem_ports.h"
+#include        "../ports.h"
+
+
+
+void    IncFlashControl(void) 
+{
+  mpcwFlashControl[wPageOut]++;
+}
+
+// TODO OutFlashControl
+void    OutFlashControl(void) 
+{
+uint    i;
+
+  if (500*(bInBuff6+1) < wPAGES)
+  {
+    InitPushCRC();
+    Push(&mpcwFlashControl[500*bInBuff6], sizeof(uint)*500);
+    Output(sizeof(uint)*500);
+  }
+  else 
+  {
+    InitPushCRC();
+    Push(&mpcwFlashControl[500*bInBuff6], sizeof(uint)*(wPAGES-500*bInBuff6));
+    for (i=0; i<500*(bInBuff6+1)-wPAGES; i++) PushInt(0xFFFF);
+    Output(sizeof(uint)*500);
+  }
+}
