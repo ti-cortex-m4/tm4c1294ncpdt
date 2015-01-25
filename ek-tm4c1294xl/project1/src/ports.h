6d8e2aa617ca76ca7ce2cf4f0ee0da7fbda8a851
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-01-25 14:05:59 +0300

    update ports

--- ek-tm4c1294xl/project1/src/ports.h
+++ ek-tm4c1294xl/project1/src/ports.h
@@ -20,4 +20,5 @@ extern	uchar                   ibPort;
 void    Output(uint  wSize);
 void    Result(uchar  bT);
 void    LongResult(uchar  bT);
+void    Outptr(void  *pData, uint  wSize);
 void    Common(void  *pbData, uint  wSize);
