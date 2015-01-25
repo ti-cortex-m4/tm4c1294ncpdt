dde52722d6f450f4233bd679bb28a7fb36b4ea7f
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-01-25 17:38:06 +0300

    update energy

--- ek-tm4c1294xl/project1/src/sensors.h
+++ ek-tm4c1294xl/project1/src/sensors.h
@@ -4,5 +4,11 @@ SENSORS.H
 
 ------------------------------------------------------------------------------*/
 
-ulong   GetMajDelay(uchar  i);
-ulong   GetMinDelay(uchar  i);
+ulong   GetMajDelay(uchar  ibPort);
+ulong   GetMinDelay(uchar  ibPort);
+
+uchar   GetDigitalPort(uchar  ibCanal);
+uchar   GetDigitalPhone(uchar  ibCanal);
+uchar   GetDigitalDevice(uchar  ibCanal);
+uchar   GetDigitalAddress(uchar  ibCanal);
+uchar   GetDigitalLine(uchar  ibCanal);
