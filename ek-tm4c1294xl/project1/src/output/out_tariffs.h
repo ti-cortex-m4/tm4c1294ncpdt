68399e9f2ca3f7beb9c80d85391d8c13f7ea4d0f
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-01-27 21:15:23 +0300

    update tariffs

--- ek-tm4c1294xl/project1/src/output/out_tariffs.h
+++ ek-tm4c1294xl/project1/src/output/out_tariffs.h
@@ -4,4 +4,28 @@ OUT_TARIFFS.H
 
 ------------------------------------------------------------------------------*/
 
+void    OutGetPublic(void);
+void    OutSetPublic(void);
 
+void    OutGetOldMode(void);
+void    OutSetOldMode(void);
+
+void    OutGetOldPowTariffs(void);
+void    OutSetOldPowTariffs(void);
+
+void    OutGetOldEngTariffs(void);
+void    OutSetOldEngTariffs(void);
+
+void    OutGetOldPubTariffs(void);
+void    OutSetOldPubTariffs(void);
+
+void    OutGetNewPowTariffs(void);
+void    OutSetNewPowTariffs(void);
+
+void    OutGetNewEngTariffs(void);
+void    OutSetNewEngTariffs(void);
+
+void    OutGetNewPubTariffs(void);
+void    OutSetNewPubTariffs(void);
+
+void    OutGetTariffsDay(void);
