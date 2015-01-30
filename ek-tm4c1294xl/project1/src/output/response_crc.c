0ac5a368687a923c01be1300f20b3f190fcca870
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-01-30 21:01:42 +0300

    update answers

--- ek-tm4c1294xl/project1/src/output/response_crc.c
+++ ek-tm4c1294xl/project1/src/output/response_crc.c
@@ -13,13 +13,7 @@ RESPONSE_CRC.C
 #include        "../display.h"
 #include        "../keyboard.h"
 #include        "../ports.h"
-#include        "out_rtc.h"
-#include        "out_groups.h"
-#include        "out_tariffs.h"
-#include        "out_flash.h"
-#include        "out_delay.h"
-#include        "out_console.h"
-#include        "response2_crc.h"
+#include        "response1_crc.h"
 
 
 
@@ -82,72 +76,7 @@ void    ResponseCRC(void) {
     }
 
     ShowCommandCRC(bSTA_OK);
-
-    switch (bInBuff4)
-    {
-      case bINQ_GETCURRTIME:
-        OutGetCurrTimeDate();
-        break;
-
-      case bINQ_GETGROUP:
-        OutGetGroup();
-        break;
-
-      case bINQ_SETGROUP:
-        OutSetGroup();
-        break;
-
-      case bINQ_GETPUBLIC: OutGetPublic(); break;
-      case bINQ_SETPUBLIC: OutSetPublic(); break;
-
-      case bINQ_GETOLDMODE: OutGetOldMode(); break;
-      case bINQ_SETOLDMODE: OutSetOldMode(); break;
-
-      case bINQ_GETOLDPOWTARIFFS: OutGetOldPowTariffs(); break;
-      case bINQ_SETOLDPOWTARIFFS: OutSetOldPowTariffs(); break;
-
-      case bINQ_GETOLDENGTARIFFS: OutGetOldEngTariffs(); break;
-      case bINQ_SETOLDENGTARIFFS: OutSetOldEngTariffs(); break;
-
-      case bINQ_GETOLDPUBTARIFFS: OutGetOldPubTariffs(); break;
-      case bINQ_SETOLDPUBTARIFFS: OutSetOldPubTariffs(); break;
-
-      case bINQ_GETNEWPOWTARIFFS: OutGetNewPowTariffs(); break;
-      case bINQ_SETNEWPOWTARIFFS: OutSetNewPowTariffs(); break;
-
-      case bINQ_GETNEWENGTARIFFS: OutGetNewEngTariffs(); break;
-      case bINQ_SETNEWENGTARIFFS: OutSetNewEngTariffs(); break;
-
-      case bINQ_GETNEWPUBTARIFFS: OutGetNewPubTariffs(); break;
-      case bINQ_SETNEWPUBTARIFFS: OutSetNewPubTariffs(); break;
-
-      case bINQ_GETTARIFFSDAY: OutGetTariffsDay(); break;
-
-      case bINQ_GETFLASHPAGE:
-        OutFlashPage();
-        break;
-
-      case bINQ_SETDELAY:
-        OutSetDelay();
-        break;
-
-      case bINQ_SETKEY:
-        OutSetKey();
-        break;
-
-      case bINQ_GETDISPLAY:
-        OutGetDisplay();
-        break;
-
-      case bINQ_RESPONSE2:
-      	Response2_CRC();
-        break;
-
-      default:
-        ShowCommandCRC(bSTA_BADCOMMAND);
-        Result(bRES_BADCOMMAND);
-        break;
-    } 
+    Response1_CRC();
   }
 }
 
