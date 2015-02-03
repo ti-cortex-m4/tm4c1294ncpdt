b37ec9cb3b02353447207bedab2d12a8ef5939fc
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-02-03 08:21:24 +0300

    mapped flash pages

--- ek-tm4c1294xl/project1/src/include/flash.h
+++ ek-tm4c1294xl/project1/src/include/flash.h
@@ -31,9 +31,19 @@ FLASH.H
 #define wFLA_PRIVATE    (uint)(wFLA_BEGIN)
 #define wFLA_GLOBAL     (uint)(wFLA_PRIVATE + 1)
 #define wFLA_LOGICAL    (uint)(wFLA_GLOBAL + 1)
+
 #define wFLA_GROUPS     (uint)(wFLA_LOGICAL + 1)
 
-#define wFLA_END        (uint)(wFLA_GROUPS + 10)
+#define wFLA_TRANS_ENG  (uint)(wFLA_GROUPS + sizeof(group)*bGROUPS/wFREEPAGE_SIZE + 1)
+#define wFLA_TRANS_CNT  (uint)(wFLA_TRANS_ENG + sizeof(real)*bCANALS/wFREEPAGE_SIZE + 1)
+#define wFLA_PULSE_HOU  (uint)(wFLA_TRANS_CNT + sizeof(real)*bCANALS/wFREEPAGE_SIZE + 1)
+#define wFLA_PULSE_MNT  (uint)(wFLA_PULSE_HOU + sizeof(real)*bCANALS/wFREEPAGE_SIZE + 1)
+
+#define wFLA_COUNT      (uint)(wFLA_PULSE_MNT + sizeof(real)*bCANALS/wFREEPAGE_SIZE + 1)
+#define wFLA_LOSSE      (uint)(wFLA_COUNT + sizeof(real)*bCANALS/wFREEPAGE_SIZE + 1)
+#define wFLA_LEVEL      (uint)(wFLA_LOSSE + sizeof(real)*bCANALS/wFREEPAGE_SIZE + 1)
+
+#define wFLA_END        (uint)(wFLA_LEVEL + sizeof(real)*bCANALS/wFREEPAGE_SIZE + 1)
 
 
 #endif
