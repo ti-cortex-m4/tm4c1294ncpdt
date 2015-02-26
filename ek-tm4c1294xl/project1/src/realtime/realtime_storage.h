64cd2f929ec035dbf222d234b1492c199654cf73
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-02-26 10:23:05 +0300

    update storage

--- ek-tm4c1294xl/project1/src/realtime/realtime_storage.h
+++ ek-tm4c1294xl/project1/src/realtime/realtime_storage.h
@@ -4,3 +4,14 @@ REALTIME_STORAGE.H
 
 ------------------------------------------------------------------------------*/
 
+boolean SavePointersMnt(void);
+boolean LoadPointersMnt(void);
+
+boolean SavePointersHou(void);
+boolean LoadPointersHou(void);
+
+boolean SavePointersDay(void);
+boolean LoadPointersDay(void);
+
+boolean SavePointersMon(void);
+boolean LoadPointersMon(void);
