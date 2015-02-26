b794711f001bd4acc455ed24d12651db5cafaaa2
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-02-26 15:57:15 +0300

    update realtime

--- ek-tm4c1294xl/project1/src/realtime/realtime.c
+++ ek-tm4c1294xl/project1/src/realtime/realtime.c
@@ -7,14 +7,8 @@ TODO атомарные операции
 #include        "../main.h"
 #include        "../memory/mem_realtime.h"
 #include        "../memory/mem_settings.h"
-#include        "../memory/mem_factors.h"
-#include        "../memory/mem_energy.h"
 #include        "../rtc.h"
-#include        "../impulses/impulses.h"
-#include        "../energy.h"
 #include        "../health.h"
-#include        "../digitals/digitals.h"
-#include        "../digitals/profile.h"
 #include        "next_second.h"
 #include        "next_minute1.h"
 #include        "next_minute3.h"
@@ -220,42 +214,6 @@ void    InitRealtime(void)
     LoadPointersMon();
 
     LoadTimeCurr();
-  }
-}
-
-
-void    StartRealtime(void)
-{
-uchar    c;
-real     re;
-
-  ibSoftMnt = 0;
-
-  ibSoftHou = 0;
-  iwHardHou = 0;
-
-  ibSoftDay = 0;
-  ibHardDay = 0;
-
-  ibSoftMon = 0;
-  ibHardMon = (GetCurrTimeDate()->bMonth) - 1;
-
-  cbSummer = 0;
-  cbWinter = 0;
-
-  DisableImpulses();
-  memset(&mpwImpMntCan[ibSoftMnt], 0, sizeof(uint)*bCANALS);
-  EnableImpulses(); // TODO EnableImpulses
-
-  MakeCurrHouCan();
-
-  for (c=0; c<bCANALS; c++)
-  {
-    if (GetDigitalDevice(c) == 0)
-    {
-    	re = GetCanReal(mpreCount,c);
-      SetCanReal(mpreCntMonCan[ ibSoftMon ], c, &re);
-    }
   }
 }
 
