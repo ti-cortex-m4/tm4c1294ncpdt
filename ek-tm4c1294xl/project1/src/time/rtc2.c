b4eb4eb65d009ce5f4edc2c24bad24e4613ad6d1
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-03-03 21:42:39 +0300

    add native RTC

---
+++ ek-tm4c1294xl/project1/src/time/rtc2.c
@@ -0,0 +1,44 @@
+/*------------------------------------------------------------------------------
+RTC2.C
+              
+
+------------------------------------------------------------------------------*/
+
+#include        "../main.h"
+#include        "../time/timedate.h"
+#include        "../display/lines.h"
+#include 				"inc/hw_sysctl.h"
+#include 				"inc/hw_gpio.h"
+#include 				"inc/hw_memmap.h"
+#include        "inc/hw_ssi.h"
+#include        "rtc.h"
+#include        "rtc2.h"
+
+
+#ifdef NATIVE_RTC
+
+
+
+#define HWREG(x) (*((volatile uint32_t *)(x)))
+
+//3 такта на запуск генераторов периферии
+static void RunClocking(void)
+{
+__asm("   nop\n"
+      "   nop\n"
+      "   nop\n");
+}
+
+
+void    InitGPIO_RTC() {
+  HWREG(SYSCTL_RCGCGPIO) |= SYSCTL_RCGCGPIO_R5;
+
+  RunClocking();
+
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DIR)   |= 0x000E;
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DIR)   &= 0xFFFE;
+
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DEN)   |= 0x000F;
+}
+
+#endif
---
+++ ek-tm4c1294xl/project1/src/time/rtc2.h
@@ -0,0 +1,23 @@
+/*------------------------------------------------------------------------------
+RTC1.H
+
+
+------------------------------------------------------------------------------*/
+
+#include        "rtc.h"
+
+#ifdef NATIVE_RTC
+
+//Биты управления SPI
+#define SPI_BIT_SCK   0x0008 //F3
+#define SPI_BIT_SI    0x0002 //F1
+#define SPI_BIT_SO    0x0001 //F0
+#define SPI_BIT_CS    0x0004 //F2
+
+//Адреса пинов управления SPI
+#define GPIO_DATABIT_SCK (GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0020)//F3
+#define GPIO_DATABIT_SI  (GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0008)//F1
+#define GPIO_DATABIT_SO  (GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0004)//F0
+#define GPIO_DATABIT_CS  (GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0010)//F2
+
+#endif
