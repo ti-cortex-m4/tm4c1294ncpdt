4e291628d928922bb0aaf04e2165ff7c6ca697a2
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-03-04 13:34:07 +0300

    add native flash

---
+++ ek-tm4c1294xl/project1/src/flash/flash2.c
@@ -0,0 +1,47 @@
+/*------------------------------------------------------------------------------
+FLASH2.C
+
+
+------------------------------------------------------------------------------*/
+
+#include        "../main.h"
+#include 				"inc/hw_gpio.h"
+#include 				"inc/hw_memmap.h"
+#include 				"inc/hw_sysctl.h"
+#include        "at45.h"
+#include        "flash1.h"
+
+
+
+#ifdef NATIVE_FLASH
+
+
+#define HWREG(x) (*((volatile uint32_t *)(x)))
+
+
+//3 такта на запуск генераторов периферии
+static void RunClocking(void)
+{
+__asm("   nop\n"
+      "   nop\n"
+      "   nop\n");
+}
+
+
+void Init_SPIhandAT45DB321(void)
+{
+	 //Включение периферии
+	 HWREG(SYSCTL_RCGCGPIO) |= 0x4080;//Запуск генераторов портов "H" и "Q"
+
+	 RunClocking();
+
+	 //Для порта "Q" (SPI)
+	 HWREG(GPIO_PORTQ_BASE + GPIO_O_DIR)   |= 0x0005;//пины на передачу (PQ3 на прием)
+	 HWREG(GPIO_PORTQ_BASE + GPIO_O_DEN)   |= 0x000D;//цифровой сигнал
+
+	 //Для порта "H" (CE)
+	 HWREG(GPIO_PORTH_AHB_BASE + GPIO_O_DIR) |= 0x0001;//пин на передачу
+	 HWREG(GPIO_PORTH_AHB_BASE + GPIO_O_DEN) |= 0x0001;//цифровой сигнал
+}
+
+#endif
