1a89e774af63db6a895cf15c9e932d1e8ae4a14a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-03-10 13:35:16 +0300

    update speeds

--- ek-tm4c1294xl/project1/src/serial/speeds.c
+++ ek-tm4c1294xl/project1/src/serial/speeds.c
@@ -6,32 +6,8 @@ SPEEDS.C
 
 #include        "../main.h"
 #include        "../memory/mem_ports.h"
-#include        "../keyboard/keyboard.h"
-#include        "../display/display.h"
 #include        "../serial/ports.h"
-#include        "../include/programs.h"
 #include        "speeds.h"
-
-
-
-//                                       0123456789ABCDEF        
-message                 msgStreams[bSTREAMS] =
-                                      { "Ведомый,Esc+CRC ",
-#ifndef MODBUS
-                                        "Ведомый,CRC     ",
-#else
-                                        "Ведомый,Modbus  ",
-#endif
-                                        "Ведущий,прямой  ",
-                                        "Ведущий,модемный",
-                                        "Ведомый,УПИО    "};
-
-static char const       szMasters[]   = " ведущие режимы ",
-                        szDirectYes[] = "прямая связь: 0 ",
-                        szDirectNo[]  = "телефонный номер",
-                        szFunction[]  = "Данная функция: ",
-                        szDirectOnly[]= "для прямой связи",
-                        szModemOnly[] = "   для модема   ";
 
 
 
@@ -66,9 +42,6 @@ static uint const       mpwInDelayMaster[bSPEEDS] =
   (uint)(wFREQUENCY_T0*0.3087),
   (uint)(wFREQUENCY_T0*0.3044)
 };
-
-// четности
-static char const       szParitys[]     = "neoms";
 
 
 
@@ -149,14 +122,14 @@ uint    w;
 
 void    ResetSpeeds(void)
 {  
-uchar   i;
+uchar   p;
 
   mppoPorts[0].enStream = STR_SLAVEESC;
   mppoPorts[1].enStream = STR_SLAVEESC;
   mppoPorts[2].enStream = STR_MASTERDIRECT;
   mppoPorts[3].enStream = STR_MASTERDIRECT;
 
-  for (i=0; p<bPORTS; p++)
+  for (p=0; p<bPORTS; p++)
   {
     mppoPorts[p].ibSpeed  = 3;
     mppoPorts[p].ibParity = 0;
@@ -205,144 +178,3 @@ uchar   p;
 
 
 
-void    ShowSpeeds(uchar  ibPrt, bool  fShowLocalDisable)
-{
-  Clear();
-  sprintf(szLo+1,"%-6lu",mpdwSpeeds[ mppoPorts[ibPrt].ibSpeed ]);
-  sprintf(szLo+15,"%u",ibPrt+1);
-
-  if (mppoPorts[ibPrt].ibParity < bPARITYS)
-    szLo[8] = szParitys[ mppoPorts[ibPrt].ibParity ];
-  else
-    szLo[8] = '?';
-
-  if (mppoPorts[ibPrt].enStream < bSTREAMS)
-    ShowHi(msgStreams[ mppoPorts[ibPrt].enStream ]);
-  else 
-    ShowHi(szError);
-
-  if (fShowLocalDisable)
-  {
-    if (IsMaster(ibPrt))
-      (mpboLocalDisable[ibPrt] == TRUE) ? (szLo[13] = '-') : (szLo[13] = '+');
-    else
-      szLo[13] = ' ';
-  }
-
-  if (enGlobal != GLB_WORK) 
-    szLo[0] = '.';
-}
-
-
-
-bool    StreamPort(uchar  ibPrt)
-{
-  if ((mppoPorts[ibPrt].enStream == STR_MASTERDIRECT) ||
-      (mppoPorts[ibPrt].enStream == STR_MASTERMODEM)) return(1);
-  else
-  {
-    SaveDisplay();
-
-    Clear();
-    sprintf(szHi,"Порт %u: нужны",ibPrt+1);
-    ShowLo(szMasters);
-    DelayMsg();
-
-    BlockProgram(bSET_SPEED);
-    DelayMsg();
-
-    LoadDisplay();
-    return(0);
-  }
-}
-
-
-
-void    ShowStreamPort(char const  *szT)
-{
-  SaveDisplay();
-
-  ShowHi(szFunction);
-  ShowLo(szT);
-  DelayMsg();
-
-  BlockProgram(bSET_SPEED);
-  DelayMsg();
-
-  LoadDisplay();
-}
-
-
-bool    StreamPortDirect(uchar  ibPrt)
-{
-  if (mppoPorts[ibPrt].enStream == STR_MASTERDIRECT) return(1);
-  else
-  {
-    ShowStreamPort(szDirectOnly);
-    return(0);
-  }
-}
-
-
-bool    StreamPortModem(uchar  ibPrt)
-{
-  if (mppoPorts[ibPrt].enStream == STR_MASTERMODEM) return(1);
-  else
-  {
-    ShowStreamPort(szModemOnly);
-    return(0);
-  }
-}
-
-
-
-void    ShowStreamPortCan(char const  *szT, uchar  ibPrt, uchar  ibCan)
-{
-  SaveDisplay();
-
-  ShowHi(szWarning);
-  Clear();
-  sprintf(szLo+4,"канала %u",ibCan+1);
-  DelayMsg();
-
-  Clear();
-  sprintf(szHi,"Порт %u: нужны",ibPrt+1);
-  ShowLo(szT);
-  DelayMsg();
-
-  BlockPrograms(bSET_SPEED,bSET_DIGITALS);
-  DelayMsg();
-
-  LoadDisplay();
-}
-
-
-
-bool    StreamPortCan(uchar  ibPrt, uchar  ibCan)
-{
-  if ((mppoPorts[ibPrt].enStream == STR_MASTERDIRECT) ||
-      (mppoPorts[ibPrt].enStream == STR_MASTERMODEM)) return(1);
-  else
-  {
-    ShowStreamPortCan(szMasters,ibPrt,ibCan);
-    return(0);
-  }
-}
-
-
-
-bool    StreamPortPhoneCan(uchar  ibPrt, uchar  ibPhn, uchar  ibCan)
-{
-  if ((mppoPorts[ibPrt].enStream == STR_MASTERDIRECT) && (ibPhn != 0)) 
-  {
-    ShowStreamPortCan(szDirectYes,ibPrt,ibCan);
-    return(0);
-  }
-  else
-  if ((mppoPorts[ibPrt].enStream == STR_MASTERMODEM) && (ibPhn == 0)) 
-  {
-    ShowStreamPortCan(szDirectNo,ibPrt,ibCan);
-    return(0);
-  }
-  else return(1);
-}
