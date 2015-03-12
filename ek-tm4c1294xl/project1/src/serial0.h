05d64255990f402ddca866364dbb6c6401d43e07
ti-cortex-m4 <ti.cortex.m4@google.com>
2015-03-12 16:00:45 +0300

    update UARTs

--- ek-tm4c1294xl/project1/src/serial0.h
+++ ek-tm4c1294xl/project1/src/serial0.h
@@ -4,10 +4,6 @@ SERIAL0.H
 
 ------------------------------------------------------------------------------*/
 
-#include 		"main.h"
-
-
-
 void    InitSerial0(void);
 void    InDelay0_Timer0(void);
 void    Query0(uint  cwIn, uchar  cbOut, bool  fMinInDelay);
--- ek-tm4c1294xl/project1/src/uarts.c
+++ ek-tm4c1294xl/project1/src/uarts.c
@@ -16,21 +16,9 @@ UARTS.Ñ
 #include        "serial0.h"
 
 
-void UARTSend(const uint8_t *pui8Buffer) {
-    while (*pui8Buffer) {
-        ROM_UARTCharPutNonBlocking(UART0_BASE, *pui8Buffer++);
-    }
-}
 
-
-void UARTSend2(const uint8_t *pui8Buffer, uint8_t wSize) {
-    while (wSize-- > 0) {
-        UARTCharPut(UART0_BASE, *pui8Buffer++);
-    }
-}
-
-
-void InitUARTs(uint32_t ui32SysClock) {
+void    InitUart0(uint32_t  ui32SysClock)
+{
     // Enable the GPIO port that is used for the on-board LED.
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
 
@@ -46,16 +34,38 @@ void InitUARTs(uint32_t ui32SysClock) {
     GPIOPinConfigure(GPIO_PA1_U0TX);
     ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
-    UARTFIFOLevelSet(UART0_BASE, UART_FIFO_TX1_8,UART_FIFO_RX1_8); // TODO FIFO
-    UARTTxIntModeSet(UART0_BASE, UART_TXINT_MODE_EOT) ;
-    //UARTFIFOEnable(INT_UART0);
-
-    // Configure the UART for 115,200, 8-N-1 operation.
-//    ROM_UARTConfigSetExpClk(UART0_BASE, ui32SysClock, 9600, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
-
-    InitSerial0();
+    UARTFIFOLevelSet(UART0_BASE, UART_FIFO_TX1_8,UART_FIFO_RX1_8);
+    UARTTxIntModeSet(UART0_BASE, UART_TXINT_MODE_EOT);
+    //TODO UARTFIFOEnable(INT_UART0);
 
     // Enable the UART interrupt.
     ROM_IntEnable(INT_UART0);
     ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT | UART_INT_TX);
+}
+
+
+void    InitUart1(uint32_t  ui32SysClock)
+{
+//    // Enable the GPIO port that is used for the on-board LED.
+//    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+//
+//    // Enable the GPIO pins for the LED (PN0).
+//    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+//
+//    // Enable the peripherals used by this example.
+//    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+//    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+//
+//    // Set GPIO A0 and A1 as UART pins.
+//    GPIOPinConfigure(GPIO_PA0_U0RX);
+//    GPIOPinConfigure(GPIO_PA1_U0TX);
+//    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+//
+//    UARTFIFOLevelSet(UART0_BASE, UART_FIFO_TX1_8,UART_FIFO_RX1_8);
+//    UARTTxIntModeSet(UART0_BASE, UART_TXINT_MODE_EOT);
+//    //TODO UARTFIFOEnable(INT_UART0);
+//
+//    // Enable the UART interrupt.
+//    ROM_IntEnable(INT_UART0);
+//    ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT | UART_INT_TX);
 }
