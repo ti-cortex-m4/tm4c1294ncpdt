2251b1e268e7cc8327d5b6492b90314bf93e14e1
ti-cortex-m4 <ti.cortex.m4@google.com>
2015-03-13 09:20:54 +0300

    add serial2 ISR

---
+++ ek-tm4c1294xl/project1/src/isr/serial2.h
@@ -0,0 +1,10 @@
+/*------------------------------------------------------------------------------
+SERIAL2.H
+
+
+------------------------------------------------------------------------------*/
+
+void    InitSerial0(void);
+void    InDelay0_Timer0(void);
+void    Query0(uint  cwIn, uchar  cbOut, bool  fMinInDelay);
+void    Answer0(uint  wSize, serial  seT);
