25a26fa3df9cbee48d88170bee20db81b806679c
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-03-13 17:06:53 +0300

    add files

---
+++ ek-tm4c1294xl/project1/src/devices/automatic2.h
@@ -0,0 +1,9 @@
+/*------------------------------------------------------------------------------
+_AUTOMATIC2.H
+              
+
+------------------------------------------------------------------------------*/
+
+bit     ReadSensors(uchar  ibCanal);
+bit     ReadTimeDate(uchar  ibCanal);
+bit     ReadCntMonCan(uchar  ibMonth, uchar  ibCanal);
