9834b736a3d4fb08e52935a4a74850aa6d5f040d
ti-cortex-m4 <ti.cortex.m4@google.com>
2015-03-13 17:12:32 +0300

    fix

--- ek-tm4c1294xl/project1/src/devices/automatic1.h
+++ ek-tm4c1294xl/project1/src/devices/automatic1.h
@@ -4,20 +4,20 @@ _AUTOMATIC1.H
 
 ------------------------------------------------------------------------------*/
 
-bit     ReadKoeffDeviceA(void);
-bit     ReadKoeffDeviceB(void);
-bit     ReadKoeffDeviceB_Special(void);
-bit     ReadKoeffDeviceC(void);
-bit     OpenDeviceD(void);
-bit     ReadAllEnergyD(void);
-bit     OpenDeviceE(void);
-bit     OpenDeviceG(void);
-bit     OpenDeviceH(void);
-bit     ReadKoeffDeviceH(void);
-bit     ReadKoeffDeviceG(void);
-bit     ReadSensorN(void);
-bit     OpenDeviceP(void);
-bit     ReadKoeffDeviceP(void);
-
-void    key_Automatic(void);
-void    key_Automatic2(void);
+//bit     ReadKoeffDeviceA(void);
+//bit     ReadKoeffDeviceB(void);
+//bit     ReadKoeffDeviceB_Special(void);
+//bit     ReadKoeffDeviceC(void);
+//bit     OpenDeviceD(void);
+//bit     ReadAllEnergyD(void);
+//bit     OpenDeviceE(void);
+//bit     OpenDeviceG(void);
+//bit     OpenDeviceH(void);
+//bit     ReadKoeffDeviceH(void);
+//bit     ReadKoeffDeviceG(void);
+//bit     ReadSensorN(void);
+//bit     OpenDeviceP(void);
+//bit     ReadKoeffDeviceP(void);
+//
+//void    key_Automatic(void);
+//void    key_Automatic2(void);
