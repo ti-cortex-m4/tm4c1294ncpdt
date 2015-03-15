ad0e0fbee2fd1186f144ee9f76f5a46d867ede09
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-03-15 21:18:25 +0300

    update digitals

---
+++ ek-tm4c1294xl/project1/src/digitals/digitals_status.c
@@ -0,0 +1,73 @@
+/*------------------------------------------------------------------------------
+DIGITALS_STATUS.C
+
+
+------------------------------------------------------------------------------*/
+/*
+#include        "main.h"
+#include        "xdata.h"
+
+
+
+bit             fStatus;
+
+
+extern  uint            xdata   wCurr,wPause,wNext;
+
+
+
+void    SetCurr(uint  w)
+{
+  fStatus = 1;
+  wCurr = w;
+  fStatus = 0;
+}
+
+
+uint    GetCurr(void)
+{
+uint  w;
+
+  fStatus = 1;
+  w = wCurr;
+  fStatus = 0;
+  return w;
+}
+
+
+
+void    SetPause(uint  w)
+{
+  fStatus = 1;
+  wPause = w;
+  fStatus = 0;
+}
+
+
+uint    GetPause(void)
+{
+  return wPause;
+}
+
+
+
+void    SetNext(uint  w)
+{
+  fStatus = 1;
+  wNext = w;
+  fStatus = 0;
+}
+
+
+uint    GetNext(void)
+{
+  return wNext;
+}
+
+
+
+bit     GetStatus(void)
+{
+  return fStatus;
+}
+*/
