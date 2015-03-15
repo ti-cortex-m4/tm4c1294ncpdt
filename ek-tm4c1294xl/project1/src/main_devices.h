0417915042200ee4fa46c5bec8d2ba135570aa9a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-03-15 19:18:53 +0300

    update current task

--- ek-tm4c1294xl/project1/src/main_devices.h
+++ ek-tm4c1294xl/project1/src/main_devices.h
@@ -9,7 +9,31 @@ MAIN_DEVICES.H
 
 
 #define SKIP_A
+//#define SKIP_B
 #define SKIP_C
+#define SKIP_D
+#define SKIP_E
+#define SKIP_F
+#define SKIP_G
+#define SKIP_H
+#define SKIP_I
+#define SKIP_J
+#define SKIP_K
+#define SKIP_L
+#define SKIP_M
+#define SKIP_N
+#define SKIP_O
+#define SKIP_P
+#define SKIP_Q
+#define SKIP_R
+#define SKIP_S
+#define SKIP_T
+#define SKIP_U
+#define SKIP_V
+#define SKIP_W
+#define SKIP_X
+#define SKIP_Y
+#define SKIP_Z
 
 
 #endif
