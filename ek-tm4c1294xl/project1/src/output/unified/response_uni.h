8bdf01e88b8f30608415d1976e44df519a3c0fe2
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-03-20 19:21:38 +0300

    fix

---
+++ ek-tm4c1294xl/project1/src/output/unified/response_uni.h
@@ -0,0 +1,20 @@
+/*------------------------------------------------------------------------------
+RESPONSE_UNI.H
+
+
+------------------------------------------------------------------------------*/
+
+//void    UniMaster(void);
+//
+//void    Output2(uint  wSize);
+//void    Output2_Code(uint  wSize, uchar  bCode, time  *mptiCode);
+//void    Result2(uchar  bT);
+//void    Result2_Info(uchar  bT, uchar  bInfo);
+//void    Common2(void  xdata  *pbData, uint  wSize);
+//
+//void    SetDelayUni(void);
+//
+//bit     GetDefCan(uchar  ibCanal);
+//bit     GetDefGrp(uchar  ibGroup);
+//
+//void    PushRealUni(status4  status, real  *preT);
