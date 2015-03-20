e28088bf618cdf3eefc1111834573b1b1c8d5e1e
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-03-20 14:03:26 +0300

    fix

--- ek-tm4c1294xl/project1/src/digitals/digitals.h
+++ ek-tm4c1294xl/project1/src/digitals/digitals.h
@@ -36,3 +36,6 @@ void    ShowDigital(uchar  ibCan);
 void    ShowDigitalNumber(uchar  ibDig);
 void    ShowCanalNumber(uchar  ibCan);
 void    ShowPort(uchar  ibPort);
+
+void    ShowPortDelayHi(uchar  ibPrt);
+void    ShowPortDelayLo(uchar  ibPrt);
