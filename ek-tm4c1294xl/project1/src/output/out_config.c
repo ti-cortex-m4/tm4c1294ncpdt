6525c0be9887fb8b8b10f2cc103c0e39ebcaa699
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-03-21 16:04:34 +0300

    update flash statistic

---
+++ ek-tm4c1294xl/project1/src/output/out_config.c
@@ -0,0 +1,36 @@
+/*------------------------------------------------------------------------------
+OUT_CONFIG.C
+                    
+
+------------------------------------------------------------------------------*/
+
+#include        "../main.h"
+#include        "../serial/ports.h"
+#include        "../flash/at45.h"
+
+
+
+void    OutGetConfig(void)
+{
+  InitPushPtr();
+
+  PushChar(bCANALS);
+  PushChar(bGROUPS);
+  PushChar(bTARIFFS);
+  PushChar(bMINUTES);
+  PushInt(wHOURS);
+  PushChar(bDAYS);
+  PushChar(bMONTHS);
+
+  PushChar(0xFF);
+
+  PushInt(GetFlashStatus());
+
+  PushChar(0);
+  PushChar(0);
+  PushChar(0);
+  PushChar(0);
+  PushChar(0);
+
+  OutptrOutBuff(16);
+}
