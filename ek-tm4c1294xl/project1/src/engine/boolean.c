3c40d61c7774add7306530eb42f63aefb1538213
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-03-29 18:21:53 +0300

    add boolean routines

---
+++ ek-tm4c1294xl/project1/src/engine/boolean.c
@@ -0,0 +1,20 @@
+/*------------------------------------------------------------------------------
+BOOLEAN.Ñ
+
+
+------------------------------------------------------------------------------*/
+
+#include        "main.h"
+
+
+
+boolean CharToBoolean(uchar  b)
+{
+  return (b == TRUE ? TRUE : FALSE);
+}
+
+
+boolean InvertBoolean(boolean  bo)
+{
+  return (bo == TRUE ? FALSE : TRUE);
+}
