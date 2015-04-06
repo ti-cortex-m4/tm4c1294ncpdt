190c71b5ccde6d7b4ba53a0e17d290ab9c614b5a
ti-cortex-m4 <ti.cortex.m4@google.com>
2015-04-06 17:54:22 +0400

    fix

--- ek-tm4c1294xl/project1/src/output/unified/response_uni.c
+++ ek-tm4c1294xl/project1/src/output/unified/response_uni.c
@@ -321,43 +321,4 @@ void    RunResponseUNI_All(void)
 }
 
 
-/*
-void    PushFFFFFF(void)
-{
-  PushChar(0xFF); PushChar(0xFF); PushChar(0xFF);
-}
-
-
-void    PushRealUni(status4  status, real  *preT)
-{
-  if (boStrictUni == TRUE)
-  {
-    switch (status)
-    {
-      case ST4_NONE:          PushFFFFFF(); PushChar(0xFF); break;
-      case ST4_NOTSUPPORTED:  PushFFFFFF(); PushChar(0xFF); break;
-      case ST4_NOTPRESENTED:  PushFFFFFF(); PushChar(0xFF); break;
-      case ST4_BADDIGITAL:    PushFFFFFF(); PushChar(0xFE); break;
-      case ST4_BADFLASH:      PushFFFFFF(); PushChar(0xFF); break;
-      case ST4_BADPORT:       PushFFFFFF(); PushChar(0xFE); break;
-      case ST4_BADENABLING:   PushFFFFFF(); PushChar(0xFF); break;
-      default:                Push(preT, sizeof(real)); break;
-    }
-  }
-  else
-  {
-    switch (status)
-    {
-      case ST4_NONE:          PushFFFFFF(); PushChar(0xE0); break;
-      case ST4_NOTSUPPORTED:  PushFFFFFF(); PushChar(0xE1); break;
-      case ST4_NOTPRESENTED:  PushFFFFFF(); PushChar(0xE2); break;
-      case ST4_BADDIGITAL:    PushFFFFFF(); PushChar(0xE3); break;
-      case ST4_BADFLASH:      PushFFFFFF(); PushChar(0xE4); break;
-      case ST4_BADPORT:       PushFFFFFF(); PushChar(0xE5); break;
-      case ST4_BADENABLING:   PushFFFFFF(); PushChar(0xE6); break;
-      default:                Push(preT, sizeof(real)); break;
-    }
-  }
-}
-*/
 #endif
