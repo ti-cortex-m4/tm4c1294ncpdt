664607039685157b12f2bcbd50f6e534b4b75380
ti-cortex-m4 <ti.cortex.m4@google.com>
2015-04-09 15:50:24 +0400

    rename procedures

---
+++ ek-tm4c1294xl/project1/src/devices/extended_4.c
@@ -0,0 +1,43 @@
+/*------------------------------------------------------------------------------
+EXTENDED_4.C
+
+ Значения счетчиков на НАЧАЛО месяца
+------------------------------------------------------------------------------*/
+
+#include        "../console.h"
+#include        "../memory/mem_realtime.h"
+#include        "../memory/mem_extended_4.h"
+#include        "../serial/ports.h"
+#include        "../digitals/digitals.h"
+#include        "../digitals/digitals_display.h"
+#include        "../digitals/digitals_messages.h"
+#include        "../devices/devices.h"
+#include        "../sensors/automatic_p.h"
+#include        "../sensors/automatic3.h"
+#include        "../time/rtc.h"
+#include        "../time/timedate.h"
+#include        "extended_4t.h"
+
+
+
+file const              flExt4Flag = {FLS_EXT_4_FLAG, &boExt4Flag, sizeof(boolean)};
+file const              flExt4Months = {FLS_EXT_4_MONTHS, &bExt4Months, sizeof(uchar)};
+}
+
+
+
+void    InitExtended4T(void) 
+{
+  LoadFileBoolean(&flExt4Flag, FALSE);
+  LoadFileChar(&flExt4Months, 0, 12, 4);
+}
+
+
+void    ResetExtended4T(void) 
+{ 
+  boExt4Flag = FALSE;
+  SaveFile(&flExt4Flag);
+
+  bExt4Months = 4;
+  SaveFile(&flExt4Months);
+}
--- ek-tm4c1294xl/project1/src/keyboard/devices/key_ext_4t_flag.c
+++ ek-tm4c1294xl/project1/src/keyboard/devices/key_ext_4t_flag.c
@@ -1,57 +1,27 @@
 /*------------------------------------------------------------------------------
-KEY_EXT_4T_FLAG.CS
+KEY_EXT_4T_FLAG.C
 
 
 ------------------------------------------------------------------------------*/
-/*
-#include        "main.h"
-#include        "xdata.h"
-#include        "display.h"
-#include        "beep.h"
-#include        "timer0.h"
-#include        "keyboard.h"
+
+#include        "../../../main.h"
+#include        "../../../devices/extended_4t.h"
+#include        "../../key_flag.h"
 
 
 
 //                                          0123456789ABCDEF
-message         code    szExt4TFlag1     = "   Загружать    ",
-                        szExt4TFlag2     = "знач. счетчиков ",
-                        szExt4TFlag3     = "на начало месяца",
-                        szExt4TFlag4     = "  по тарифам ?  ";
+static char const       szMessage1[]     = "   Загружать    ",
+                        szMessage2[]     = "знач. счетчиков ",
+                        szMessage3[]     = "   на начало    ",
+                        szMessage4[]     = "    месяцев     ",
+                        szMessage5[]     = "  по тарифам ?  ";
 
-uchar           *code   pszExt4TFlag[] = { szExt4TFlag1, szExt4TFlag2, szExt4TFlag3, szExt4TFlag4,  "" };
+static char const       *pszMessages[]   = { szMessage1, szMessage2, szMessage3, szMessage4, szMessage5, "" };
 
 
 
 void    key_SetExt4TFlag(void)
 {
-  if (bKey == bKEY_ENTER)
-  {
-    if (enKeyboard == KBD_ENTER)  
-    {
-      enKeyboard = KBD_INPUT1;
-      Clear();
-      
-      LoadSlide(pszExt4TFlag);    
-      ShowBoolean(boExt4TFlag);  
-    } 
-    else Beep();
-  }
-  
-
-  else if (bKey == bKEY_POINT)
-  {
-    if (enGlobal != GLB_WORK)
-    {
-      if ((enKeyboard == KBD_INPUT1) || (enKeyboard == KBD_POSTINPUT1))
-      {           
-        boExt4TFlag = ~boExt4TFlag;
-        ShowBoolean(boExt4TFlag);
-      }
-      else Beep(); 
-    }
-    else Beep(); 
-  } 
-  else Beep(); 
+  key_SetFlag(&flExt4TFlag, pszMessages);
 }
-*/
