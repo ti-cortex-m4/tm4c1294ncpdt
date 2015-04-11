2b14c54e1098c959ad9ed6ab78180655fb5178d0
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-04-11 13:55:36 +0300

    update extended 4

--- ek-tm4c1294xl/project1/src/devices/extended_4.h
+++ ek-tm4c1294xl/project1/src/devices/extended_4.h
@@ -16,3 +16,19 @@ extern  file const              flExt4EnblCan;
 
 void    InitExtended4(void);
 void    ResetExtended4(void);
+
+void    NextDayExtended4(void);
+void    NextMonExtended4(void);
+
+void    MakeExtended4(void);
+
+void    OutExtended40(void);
+void    OutExtended401(void);
+void    OutExtended41(void);
+void    OutExtended42(void);
+void    OutExtended43(void);
+
+bit     ReadCntMonCanFCurr(uchar  ibMonth, uchar  ibCanal);
+bit     ReadCntMonCanFBuff(uchar  ibMonth, uchar  ibCanal);
+void    ShowCntMonCanF2(void);
+void    ShowExtended4(uchar  ibCanal, uchar  ibMonth);
