6f026997f5caea64813f6685c52f4a6be437de37
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-04-13 16:50:35 +0400

    add extended 7

---
+++ ek-tm4c1294xl/project1/src/devices/extended_7.h
@@ -0,0 +1,13 @@
+/*------------------------------------------------------------------------------
+EXTENDED_7.H
+
+
+------------------------------------------------------------------------------*/
+
+void    ResetExtended7(void);
+
+void    NextDayExtended7(void);
+
+void    MakeExtended7(uchar  ibCan);
+
+void    OutExtended7(void);
---
+++ ek-tm4c1294xl/project1/src/memory/mem_extended_7.h
@@ -0,0 +1,9 @@
+/*------------------------------------------------------------------------------
+MEM_EXTENDED_6.H
+
+
+------------------------------------------------------------------------------*/
+
+extern  value6                  mpCntDayCan7Curr[bCANALS], mpCntDayCan7Buff[bCANALS];
+
+extern  uint                    cwDayCan7;
