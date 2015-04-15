edb203f243bf36e1ccda46a51187a2e3559fe0f5
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-04-15 18:55:11 +0300

    fix

--- ek-tm4c1294xl/project1/src/include/flash.h
+++ ek-tm4c1294xl/project1/src/include/flash.h
@@ -42,9 +42,10 @@ FLASH.H
 #define bPOWER_GRP          (uchar)(sizeof(power)   * bGROUPS/wFREEPAGE_SIZE + 1)
 #define REALCAN_PAGES       (uchar)(sizeof(real)    * bCANALS/wFREEPAGE_SIZE + 1)
 #define PARAMS_PAGES        (uchar)(sizeof(real)    * wPARAMS/wFREEPAGE_SIZE + 1)
+#define VALUE4_CAN_PAGES    (uchar)(sizeof(mpCntMonCan4)/wFREEPAGE_SIZE + 1)
+#define VALUE4T_CAN_PAGES   (uchar)(sizeof(mpCntMonCan4T)/wFREEPAGE_SIZE + 1)
 #define VALUE5_CAN_PAGES    (uchar)(sizeof(mpvaValue5)/wFREEPAGE_SIZE + 1)
-#define VALUE6_CAN_PAGES    (uchar)(sizeof(mpCntMonCan4)/wFREEPAGE_SIZE + 1)
-#define VALUE6T_CAN_PAGES   (uchar)(sizeof(mpCntMonCan4T)/wFREEPAGE_SIZE + 1)
+#define VALUE6_CAN_PAGES    (uchar)(sizeof(mpCntMonCan6)/wFREEPAGE_SIZE + 1)
 #define VALUE7_CAN_PAGES    (uchar)(sizeof(mpCntDayCan7)/wFREEPAGE_SIZE + 1)
 
 
