8ac3c5041f6edacad10a90b3e2716afc4a8e9f83
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-05-05 18:32:29 +0300

    final check

--- ek-tm4c1294xl/project1/src/serial/print.h
+++ ek-tm4c1294xl/project1/src/serial/print.h
@@ -1,16 +1,18 @@
 /*------------------------------------------------------------------------------
-PRINT.H
+PRINT,H
 
 
 ------------------------------------------------------------------------------*/
 
-//#define ENABLE_PRINT
+#define ENABLE_PRINT
 
 
 
 void    PrintStart(void);
+void    PrintStop(void);
+
 void    PrintString(const char  *psz);
 void    PrintChar(uchar  b);
 void    PrintInt(uint  w);
+void    PrintLong(ulong  dw);
 void    PrintTime(time  ti);
-void    PrintStop(void);
