fcf1dbb2847cba2391e79192a2d797ce7efb4716
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-05-08 20:24:05 +0300

    update ESC

--- ek-tm4c1294xl/project1/src/output/esc/response_esc.c
+++ ek-tm4c1294xl/project1/src/output/esc/response_esc.c
@@ -16,6 +16,7 @@ RESPONSE_ESC.C
 #include "esc.h"
 
 
+
 void    EscPtrReset(void)
 {
 }
@@ -24,7 +25,6 @@ void    EscPtrReset(void)
 void    Esc(uint  wSize)
 {
   InitPush(0);
-
   uchar bT = 0;
 
   uint i;
@@ -34,7 +34,6 @@ void    Esc(uint  wSize)
   }
 
   PushChar(bT);
-
   Answer(wSize+1, SER_OUTPUT_SLAVE);
 }
 
@@ -115,7 +114,7 @@ uchar   i;
     if (enGlobal == GLB_PROGRAM)
       return;
 
-    if (boBlockingEsc == TRUE)
+    if (boDsblEsc == TRUE)
       return;
 
     bQuery = InBuff(0);
@@ -182,7 +181,7 @@ uchar   i;
 
     ShowCommand();
 
-    if (boBlockingEsc == TRUE)
+    if (boDsblEsc == TRUE)
     {
       if (bQuery != 'R') 
       {
@@ -250,57 +249,7 @@ uchar   i;
 
         Esc(2);
         break;
-
-      case 'l':
-        i = mpbEsc_l[ ibMachineEsc ];
-
-        if (++i >= wHOURS/48) i = 0;
-
-        mpbEsc_l[ ibMachineEsc ] = i;
-
-        InitPush(0);
-        PushChar('l');
-        PushChar(i);
-        Esc(2);
-
-        ShowNumberEsc( mpbEsc_l[ ibMachineEsc ] );
-        break;
-
-      case 'v':
-        i = mpbEsc_v[ ibMachineEsc ];
-
-        if (++i >= 6) i = 0;
-
-        mpbEsc_v[ ibMachineEsc ] = i;
-
-        InitPush(0);
-        PushChar('v');
-        PushChar(i);
-        Esc(2);
-
-        ShowNumberEsc( mpbEsc_v[ ibMachineEsc ] );
-        break;
-
-      case '{':
-        InitPush(0);
-        for (i=0; i<16; i++)
-        {
-          tiAlt = *PGetCanTime(mptiEsc_S, i+16*ibMachineEsc);
-          Push(&tiAlt, sizeof(time));
-        }
-        Esc(16*sizeof(time));
-        break;
-
-      case '}':
-        InitPush(0);
-        for (i=0; i<16; i++)
-        {
-          tiAlt = *PGetCanTime(mptiEsc_V, i+16*ibMachineEsc);
-          Push(&tiAlt, sizeof(time));
-        }
-        Esc(16*sizeof(time));
-        break;
-
+*/
       case 'T':
         InitPush(0);
         PushChar( ToBCD(tiCurr.bSecond) );
@@ -312,200 +261,6 @@ uchar   i;
         Esc(6);
         break;
 
-      case 'W':
-        InitPush(0);
-
-        if (GetFlashStatus() == 0)
-          i = 0x07;                            // норма
-        else
-          i = 0x01;                            // ошибка флэш-памяти
-
-        if (cbPowerOn == 0)
-          PushChar(i | 0x08);
-        else
-        if (cbPowerOn == 1)
-          PushChar(i | 0x10);
-        else
-          PushChar(i);
-
-        Push(&tiPowerOff, sizeof(time));       // время последнего выключения питания
-        Push(&tiPowerOn,  sizeof(time));       // время последнего включения питания
-        PushChar(cbPowerOn);                   // количество выключений питания
-
-        PushChar(0);                           // время выключения за предыдущий месяц (дней, часов, минут)
-        PushChar(0);
-        PushChar(0);
-
-        PushChar(0);                           // время выключения за текущий месяц (дней, часов, минут)
-        PushChar(0);
-        PushChar(0);
-
-        Esc(20);
-        break;
-
-      case 'w':
-        InitPush(0);
-
-        // первый байт состояния
-        i = bOldMode;
-
-        tiAlt = *PGetCurrTimeDate();
-        if (GetModeAlt != 0)
-          i |= 0x04;
-
-        if (enGlobal == GLB_REPROGRAM)
-          i |= 0x20;
-
-        PushChar(i);
-
-        // второй байт состояния
-        if (bOldMode == 4)
-          PushChar(4);
-        else
-          PushChar(0);
-
-        // дата/время первого запуска
-        PushChar( ToBCD(tiStart.bSecond) );
-        PushChar( ToBCD(tiStart.bMinute) );
-        PushChar( ToBCD(tiStart.bHour  ) );
-        PushChar( ToBCD(tiStart.bDay   ) );
-        PushChar( ToBCD(tiStart.bMonth ) );
-        PushChar( ToBCD(tiStart.bYear  ) );
-
-        // тарифные зоны по кварталам
-        ibMode = 0;
-        for (ibMonth=12; ibMonth<16; ibMonth++)
-        {
-          zoAlt = *PGetZonePowMonthMode();
-          PushZone();
-        }
-
-        // тарифные зоны текущего месяца
-        ibMode  = 0;
-        ibMonth = tiCurr.bMonth - 1;
-
-        zoAlt = *PGetZonePowMonthMode();
-        PushZone();
-
-        // список праздников
-        for (i=0; i<GetRelaxSize(); i++)
-        {
-          if (i >= 20) break;
-
-          GetRelaxDate(i);
-          PushChar( ToBCD(tiRelax.bDay)   );
-          PushChar( ToBCD(tiRelax.bMonth) );
-        }
-
-        while (i++ < 20)
-        {
-          PushChar(0x55);
-          PushChar(0x55);
-        }
-
-        // соcтав групп: все каналы
-        for (i=bFRAMES*ibMachineEsc; i<bFRAMES*(1+ibMachineEsc); i++)
-        {
-          wBuffD = 0;
-
-          for (j=0; j<GetGroupsSize(i); j++)
-          {
-            if (j > 16) break;
-            wBuffD |= (uint)(0x01 << GetGroupsNodeCanal(i,j));
-          }
-
-          PushChar(wBuffD % 0x100);
-          PushChar(wBuffD / 0x100);
-        }
-
-        // соcтав групп: каналы с отрицательным знаком
-        for (i=bFRAMES*ibMachineEsc; i<bFRAMES*(1+ibMachineEsc); i++)
-        {
-          wBuffD = 0;
-
-          for (j=0; j<GetGroupsSize(i); j++)
-          {
-            if (j > 16) break;
-            if (GetGroupsNodeSign(i,j) == 1)
-              wBuffD |= (uint)(0x01 << GetGroupsNodeCanal(i,j));
-          }
-
-          PushChar(wBuffD % 0x100);
-          PushChar(wBuffD / 0x100);
-        }
-
-        // коэффициенты трансформации
-        for (i=0; i<16; i++)
-        {
-          reBuffA = *PGetCanReal(mpreTransEng, i+16*ibMachineEsc);
-          PushRealBCD();
-        }
-
-        // коэффициенты преобразования
-        for (i=0; i<16; i++)
-        {
-          reBuffA = *PGetCanReal(mprePulseHou, i+16*ibMachineEsc);
-          PushRealBCD();
-        }
-
-        // лимиты
-        for (i=0; i<18; i++)  PushChar(0);
-
-        // заводской номер
-        PushChar( ToBCD(wPrivate % 100)         );
-        PushChar( ToBCD((wPrivate % 10000)/100) );
-        PushChar( ToBCD(wPrivate / 10000)       );
-
-        // заводской номер
-        PushChar( ToBCD(wPrivate % 100)         );
-        PushChar( ToBCD((wPrivate % 10000)/100) );
-        PushChar( ToBCD(wPrivate / 10000)       );
-
-        // коэффициенты потерь
-        for (i=0; i<16; i++)
-        {
-          reBuffA = *PGetCanReal(mpreLosse, i+16*ibMachineEsc) * 1000000;
-          PushRealBCD();
-        }
-
-        Esc(300);
-        break;
-
-      case '*':
-        InitPush(0);
-
-        ibMode = 0;
-        for (ibMonth=0; ibMonth<12; ibMonth++)
-        {
-          zoAlt = *PGetZonePowMonthMode();
-          PushZone();
-        }
-
-        ibMode = 0;
-        for (ibMonth=0; ibMonth<12; ibMonth++)
-        {
-          zoAlt = *PGetZoneEngMonthMode();
-          PushZone();
-        }
-
-        PushChar(0);
-        PushChar(0);
-        PushChar(0);
-        PushChar(0);
-
-        if (boPublicCurr == FALSE)
-          PushChar(1);
-        else
-          PushChar(0);
-
-        if (boPublicPrev == FALSE)
-          PushChar(1);
-        else
-          PushChar(0);
-
-        Esc(300);
-        break;
-*/
       case 'R':
         InitPush(0);
         Push("CќЊ+2 V.06 10.10.08!",20);
@@ -542,22 +297,6 @@ uchar   i;
         EscDisplay();
         break;
 /*
-#ifdef  FLOW
-      case 0x1C:
-        InitPush(0);
-
-        for (i=0; i<16; i++)
-          PushChar(GetDigitalDevice(i+16*ibMachineEsc));
-
-        for (i=0; i<16; i++)
-        {
-          PushChar(GetDigitalAddress(i+16*ibMachineEsc));
-          PushChar(GetDigitalLine(i+16*ibMachineEsc)+1);
-        }
-
-        Esc(16+32);
-        break;
-
       case 0x1F:
         if (cbWaitQuery != 0)
         {
@@ -581,53 +320,6 @@ uchar   i;
           Push("Transit OK: 1 !",15);
           Esc(15);
         }
-        break;
-#endif
-
-      case 'Щ':
-        InitPush(0);
-        PushChar(bPortGPS);
-        PushChar(bStatusGPS);
-        PushChar(bVersionMaxGPS);
-        PushChar(bVersionMinGPS);
-        PushChar(bGMT);
-        Push(mpboGPSRun, sizeof(mpboGPSRun));
-        Push(mpcwGPSRun, sizeof(mpcwGPSRun));
-        Push(&tiPrevCorrect, sizeof(time));
-        Push(&tiPostCorrect, sizeof(time));
-        Push(&mpcwPosValueCurr, sizeof(mpcwPosValueCurr));
-        Push(&mpcwNegValueCurr, sizeof(mpcwNegValueCurr));
-        Push(&mpcwPosCountCurr, sizeof(mpcwPosCountCurr));
-        Push(&mpcwNegCountCurr, sizeof(mpcwNegCountCurr));
-        Push(&mpcwPosValuePrev, sizeof(mpcwPosValuePrev));
-        Push(&mpcwNegValuePrev, sizeof(mpcwNegValuePrev));
-        Push(&mpcwPosCountPrev, sizeof(mpcwPosCountPrev));
-        Push(&mpcwNegCountPrev, sizeof(mpcwNegCountPrev));
-        PushChar(SeasonCurr());
-        PushChar(boSeasonGPS);
-        Esc(345+2);
-        break;
-
-      case 'Ъ':
-        InitPush(0);
-        for (i=0; i<bCANALS; i++)
-        {
-          PushChar(mpboDefEscV[i]); PushInt(mpcwEscV_OK[i]); PushInt(mpcwEscV_Error[i]);
-          PushChar(mpboDefEscS[i]); PushInt(mpcwEscS_OK[i]); PushInt(mpcwEscS_Error[i]);
-          PushChar(mpboDefEscU[i]); PushInt(mpcwEscU_OK[i]); PushInt(mpcwEscU_Error[i]);          
-        }
-        Esc(bCANALS*15);
-        break;
-
-      case 'Ь':
-        InitPush(0);
-        for (i=0; i<bCANALS; i++)
-        {
-          PushInt(mpcwDigital_OK[i]);
-          PushInt(mpcwProfile_OK[i]); PushInt(mpcwProfile_Error[i]);
-          PushInt(mpcwCalcDig[i]);
-        }
-        Esc(bCANALS*8);
         break;
 
       case 'Ю':
