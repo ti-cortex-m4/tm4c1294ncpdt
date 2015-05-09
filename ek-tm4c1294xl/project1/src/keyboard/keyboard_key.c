20fb218b5f687b9fee6b9ff21db550d8d9258039
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-05-09 12:16:00 +0300

    update correct

--- ek-tm4c1294xl/project1/src/keyboard/keyboard_key.c
+++ ek-tm4c1294xl/project1/src/keyboard/keyboard_key.c
@@ -8,8 +8,9 @@ KEYBOARD_KEY.C
 #include "../include/programs.h"
 #include "keyboard.h"
 #include "key_speeds.h"
-#include "time/key_correct_time.h"
 #include "time/key_timedate.h"
+#include "time/key_correct_time.h"
+#include "time/key_correct.h"
 #include "key_password.h"
 #include "key_logical.h"
 #include "key_private.h"
@@ -283,9 +284,12 @@ void    Keyboard_Key(void)
     {
       case bGET_SPEED:          key_SetSpeeds();        break;
 
-      case bGET_CORRECT_TIME:   key_CorrectTime();      break;
       case bGET_CURRTIME:       key_SetCurrTime();      break;
       case bGET_CURRDATE:       key_SetCurrDate();      break;
+
+      case bGET_CORRECT_TIME:   key_CorrectTime();      break;
+      case bGET_CORRECTCURR:    key_GetCorrectCurr();   break;
+      case bGET_CORRECTPREV:    key_GetCorrectPrev();   break;
 
       case bGET_GROUPS:         key_GetGroups();        break;
 
