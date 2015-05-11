9536fc62df887b0463a3458a71acba0ebda6ec7a
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-05-11 20:58:39 +0300

    final check

--- ek-tm4c1294xl/project1/src/keyboard/digitals/params/key_mnt_params.c
+++ ek-tm4c1294xl/project1/src/keyboard/digitals/params/key_mnt_params.c
@@ -1,62 +1,26 @@
 /*------------------------------------------------------------------------------
-KEY_MNT_PARAMS.C
+KEY_MNT_PARAMS,C
 
 
 ------------------------------------------------------------------------------*/
 
 #include "../../../console.h"
 #include "../../../digitals/params/params.h"
-#include "../../key_bool.h"
+#include "../../key_flag.h"
 
 
 
 //                                         0123456789ABCDEF
-static char const       szMntParams[]   = "Параметры: время",
-                        szMinute30[]    = " 30 минут       ",
-                        szMinute3[]     = " 3 минуты       ";
+static char const       szMessage1[]    = "Время обновления",
+                        szMessage2[]    = " для параметров ",
+                        szTrue[]        = " 3 минуты       ",
+                        szFalse[]       = " 30 минут       ";
 
+static char const       *pszMessages[]  = { szMessage1, szMessage2, "" };
 
-
-static void Show(boolean  bo)
-{
-  if (bo == FALSE)
-    strcpy(szLo,szMinute30);
-  else
-    strcpy(szLo,szMinute3);
-
-  if (enGlobal != GLB_WORK)
-    szLo[0] = '.';
-}
 
 
 void    key_SetMntParams(void)
 {
-  if (bKey == bKEY_ENTER)
-  {
-    if (enKeyboard == KBD_ENTER)
-    {
-      enKeyboard = KBD_INPUT1;
-      Clear();
-
-      ShowHi(szMntParams);
-      Show(boMntParams);
-    }
-    else Beep();
-  }
-
-
-  else if (bKey == bKEY_POINT)
-  {
-    if (enGlobal != GLB_WORK)
-    {
-      if ((enKeyboard == KBD_INPUT1) || (enKeyboard == KBD_POSTINPUT1))
-      {
-        boMntParams = InvertBoolean(boMntParams);
-        Show(boMntParams);
-      }
-      else Beep();
-    }
-    else Beep();
-  }
-  else Beep();
+  key_SetFlag(&flMntParams, pszMessages, szTrue, szFalse);
 }
