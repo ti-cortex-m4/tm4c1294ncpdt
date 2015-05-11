25ac154c905576de5b3b4069b5bf7cb0257d19ca
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-05-11 07:27:30 +0300

    final check

--- ek-tm4c1294xl/project1/src/keyboard/digitals/key_enbl_keys.c
+++ ek-tm4c1294xl/project1/src/keyboard/digitals/key_enbl_keys.c
@@ -1,53 +1,24 @@
 /*------------------------------------------------------------------------------
-KEY_ENBL_KEYS.C
+KEY_ENBL_KEYS,C
 
 
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
 #include "../../memory/mem_digitals.h"
-#include "../keyboard.h"
-#include "../../display/display.h"
 #include "../../digitals/digitals.h"
-#include "../../flash/files.h"
+#include "../key_bool.h"
 
 
 
 //                                         0123456789ABCDEF
-static char const       szEnblKeys[]    = "Пароли ?        ";
+static char const       szMessage[]     = "Пароли ?        ";
+
+static char const       *pszMessages[]  = { szMessage, "" };
 
 
 
 void    key_SetEnblKeys(void)
 {
-  if (bKey == bKEY_ENTER)
-  {
-    if (enKeyboard == KBD_ENTER)  
-    {
-      enKeyboard = KBD_INPUT1;
-      Clear();
-      
-      ShowHi(szEnblKeys);
-      ShowBoolean(boEnblKeys);
-    }
-    else Beep();
-  }
-  
-
-  else if (bKey == bKEY_POINT)
-  {
-    if (enGlobal != GLB_WORK)
-    {
-      if ((enKeyboard == KBD_INPUT1) || (enKeyboard == KBD_POSTINPUT1))
-      {           
-        boEnblKeys = InvertBoolean(boEnblKeys);
-        SaveFile(&flEnblKeys);
-
-        ShowBoolean(boEnblKeys);
-      }
-      else Beep(); 
-    }
-    else Beep(); 
-  } 
-  else Beep(); 
+  key_SetBool(&flEnblKeys, pszMessages);
 }
