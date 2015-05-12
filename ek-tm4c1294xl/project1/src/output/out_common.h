4ebf6a4abf5b79c6833b9ca996c61b66ebe74d6f
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-05-12 19:06:34 +0300

    update

--- ek-tm4c1294xl/project1/src/output/out_common.h
+++ ek-tm4c1294xl/project1/src/output/out_common.h
@@ -8,3 +8,8 @@ void    OutGetFloatCan(float  *mfl);
 void    OutGetFloatCan_GlobalWork(float  *mfl);
 
 void    OutSetFloatCan(float  *mfl, file const  *pfl);
+
+void    OutRealCanExt(real  *mpe);
+void    OutIntCanExt(uint  *mpw);
+void    OutCharCanExt(uchar  *mpb);
+void    OutBoolCanExt(boolean  *mpf);
