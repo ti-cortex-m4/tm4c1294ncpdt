9f5db1c30ce982e638cbdbbfaf1b96c308c000fd
ti-cortex-m4 <ti.cortex.m4@google.com>
2015-05-15 17:23:34 +0400

    add new handlers

---
+++ ek-tm4c1294xl/project1/src/output/response253_crc.c
@@ -0,0 +1,24 @@
+/*------------------------------------------------------------------------------
+RESPONSE253_CRC,C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+#include "../memory/mem_ports.h"
+#include "../include/states.h"
+#include "../serial/ports.h"
+#include "../output/response_crc.h"
+
+
+
+void    Response253_CRC(void)
+{
+//  switch (bInBuff5)
+//  {
+//    default:
+//      ShowTestResponse(bSTA_BADCOMMAND);
+//      Result(bRES_BADCOMMAND);
+//      break;
+//  }
+}
