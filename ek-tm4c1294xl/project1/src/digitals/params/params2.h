cd708e671ac3fec80dfc2561ed0ef839a8e9a3ac
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-05-16 18:02:47 +0300

    extract to separate file

--- ek-tm4c1294xl/project1/src/digitals/params/params2.h
+++ ek-tm4c1294xl/project1/src/digitals/params/params2.h
@@ -4,37 +4,6 @@ PARAMS2.H
 
 ------------------------------------------------------------------------------*/
 
-extern  char const              szParamP[],
-                                szParamP1[],
-                                szParamP2[],
-                                szParamP3[],
-                                szParamQ[],
-                                szParamQ1[],
-                                szParamQ2[],
-                                szParamQ3[],
-                                szParamS[],
-                                szParamS1[],
-                                szParamS2[],
-                                szParamS3[],
-                                szParamU[],
-                                szParamU1[],
-                                szParamU2[],
-                                szParamU3[],
-                                szParamI[],
-                                szParamI1[],
-                                szParamI2[],
-                                szParamI3[],
-                                szParamC[],
-                                szParamC1[],
-                                szParamC2[],
-                                szParamC3[],
-                                szParamF[],
-                                szParamF1[],
-                                szParamF2[],
-                                szParamF3[],
-                                szBadParam[];
-
-
 extern  boolean                 boBeginParam;
 
 
