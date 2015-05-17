4c7eb611ee5bdca1a875376f6a3541b19f583076
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-05-17 18:20:03 +0300

    fix

--- ek-tm4c1294xl/project1/src/keyboard/key_char.h
+++ ek-tm4c1294xl/project1/src/keyboard/key_char.h
@@ -8,4 +8,4 @@ extern  char                    szCharLimits[bDISPLAY + bMARGIN];
 
 
 
-void    key_SetChar(file const  *pflFile, char const  *pszSlide[], uchar const  bMin, uchar const  bMax);
+void    key_SetChar(file const  *pflFile, char const  *pszSlide[], uchar  bMin, uchar  bMax);
