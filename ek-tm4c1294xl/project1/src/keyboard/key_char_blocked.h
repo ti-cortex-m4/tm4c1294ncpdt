1ffa146b877ec0d67d0192ab4a5895c391941cc4
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-05-17 18:09:07 +0300

    add

---
+++ ek-tm4c1294xl/project1/src/keyboard/key_char_blocked.h
@@ -0,0 +1,11 @@
+/*------------------------------------------------------------------------------
+KEY_CHAR_BLOCKED,H
+
+
+------------------------------------------------------------------------------*/
+
+extern  char                    szCharLimits[bDISPLAY + bMARGIN];
+
+
+
+void    key_SetCharBlocked(file const  *pflFile, char const  *pszSlide[], uchar const  bMin, uchar const  bMax);
