7c5db76a467f45e41de64b99b819379ea6e6668d
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-05-17 18:09:07 +0300

    add

---
+++ ek-tm4c1294xl/project1/src/keyboard/key_char_blocked.c
@@ -0,0 +1,78 @@
+/*------------------------------------------------------------------------------
+KEY_CHAR_BLOCKED,C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+#include "keyboard.h"
+#include "../display/display.h"
+#include "../flash/files.h"
+#include "key_char.h"
+
+
+
+//                                            0123456789ABCDEF
+static char const       szMaskChar[]       = "      ___       ";
+
+char                    szCharLimits[bDISPLAY + bMARGIN];
+
+
+
+void    key_SetCharBlocked(file const  *pflFile, char const  *pszSlide[], uchar const  bMin, uchar const  bMax)
+{
+  uchar *pbVal = (uchar *) pflFile->pbBuff;
+
+  if (bKey == bKEY_ENTER)
+  {
+    if (enKeyboard == KBD_ENTER)
+    {
+      enKeyboard = KBD_POSTENTER;
+      Clear();
+
+      strcpy(szCharLimits, szClear);
+      sprintf(szCharLimits+4, "%3u..%-3u", bMin, bMax);
+
+      LoadSlide(pszSlide);
+      ShowChar(*pbVal);
+    } 
+    else if (enKeyboard == KBD_POSTINPUT1)
+    {
+      enKeyboard = KBD_POSTENTER;
+
+      uchar b = GetCharLo(6,8);
+      if ((b >= bMin) && (b <= bMax))
+      {
+        *pbVal = b;
+        SaveFile(pflFile);
+
+        ShowChar(*pbVal);
+      }
+      else
+      {
+        enKeyboard = KBD_INPUT1;
+        LongBeep();
+
+        ShowLo(szMaskChar);
+      }
+    }
+    else Beep();
+  }
+
+
+  else if (bKey < 10)
+  {
+    if ((enGlobal != GLB_WORK) && (enKeyboard == KBD_POSTENTER))
+    {
+      enKeyboard = KBD_INPUT1;
+      ShowLo(szMaskChar);
+    }
+
+    if ((enKeyboard == KBD_INPUT1) || (enKeyboard == KBD_POSTINPUT1))
+    {
+      enKeyboard = KBD_POSTINPUT1;
+      ShiftLo(6,8);
+    }
+  }
+  else Beep();
+}
