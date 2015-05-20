96f04b0c701312726f8ccdd9860d7b268e519fa9
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-05-20 10:46:18 +0400

    fix

--- ek-tm4c1294xl/project1/src/keyboard/time/key_timedate.h
+++ ek-tm4c1294xl/project1/src/keyboard/time/key_timedate.h
@@ -5,11 +5,12 @@ KEY_TIMEDATE.H
 ------------------------------------------------------------------------------*/
 
 void    key_SetCurrTime(void);
-void    key_SetCurrDate(void);
-void    key_CorrectTime(void);
+void    auto_GetCurrTime(void);
 
-void    auto_GetCurrTime(void);   
-void    auto_GetCurrDate(void); 
+void    key_SetCurrDate(void);
+void    auto_GetCurrDate(void);
+
+void    key_CorrectTime(void);
 
 void    ShowCurrentTime(void);
 void    HideCurrentTime(bool  fClearProgram);
