864e90be6366470cb3ba36000b1da620f5f43f58
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-05-23 16:58:43 +0300

    remove from project2

--- ek-tm4c1294xl/project2/src/energy4.h
+++
@@ -1,8 +0,0 @@
-/*------------------------------------------------------------------------------
-ENERGY4.H
-
-
-------------------------------------------------------------------------------*/
-
-real    GetPowGrpHouCurr(uchar  ibGrp, uchar  bMul);
-real    GetPowCanHouCurr(uchar  ibCan, uchar  bMul);
--- ek-tm4c1294xl/project2/src/flash/files2.h
+++
@@ -1,8 +0,0 @@
-/*------------------------------------------------------------------------------
-FILES2.H
-
-
-------------------------------------------------------------------------------*/
-
-void    LoadFileBoolean(file const  *pfl, boolean  boDef);
-void    LoadFileChar(file const  *pfl, uchar  bMin, uchar  bMax, uchar  bDef);
