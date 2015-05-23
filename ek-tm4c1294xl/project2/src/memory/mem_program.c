b6fbb4931d5599fb8ad26999ff64bf5d740b70e4
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-05-23 17:04:00 +0300

    remove from project2

---
+++ ek-tm4c1294xl/project2/src/memory/mem_program.c
@@ -0,0 +1,18 @@
+/*------------------------------------------------------------------------------
+MEMORY_PROGRAM.C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+
+
+
+// флаги задания соответствующих программ
+boolean                 boSetTime,
+                        boSetDate,
+                        boSetGroups,
+                        boSetPassword;
+
+// TODO: SaveFile
+boolean                 boFirstReset;
