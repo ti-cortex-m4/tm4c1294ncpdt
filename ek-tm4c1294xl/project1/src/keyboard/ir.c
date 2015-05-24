6a4c2881da7b78d05e398734988174f8feca6208
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-05-24 20:05:30 +0300

    init PP0

--- ek-tm4c1294xl/project1/src/keyboard/ir.c
+++ ek-tm4c1294xl/project1/src/keyboard/ir.c
@@ -5,9 +5,24 @@ IR.C
 ------------------------------------------------------------------------------*/
 
 #include "../main.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_sysctl.h"
+#include "inc/hw_types.h"
 #include "keyboard.h"
+#include "../time/delay.h"
 #include "../../irmp/irmp.h"
 #include "ir.h"
+
+
+
+void    InitIR(void)
+{
+  HWREG(SYSCTL_RCGCGPIO) |= SYSCTL_RCGCGPIO_R13; // GPIO Port P Run Mode Clock Gating Control
+  DelayGPIO();
+  HWREG(GPIO_PORTP_BASE + GPIO_O_DIR) &= 0xFFFE; // GPIO Direction
+  HWREG(GPIO_PORTP_BASE + GPIO_O_DEN) |= 0x0001; // GPIO Digital Enable
+}
 
 
 
