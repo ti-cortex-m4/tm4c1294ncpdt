ebdb497a461e4257943adb935a88ba8a99671af6
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-05-26 07:39:36 +0300

    renaming

---
+++ ek-tm4c1294xl/project1/src/digitals/extended/extended_4_in.h
@@ -0,0 +1,11 @@
+/*------------------------------------------------------------------------------
+EXTENDED_4_OUTPUT.H
+
+
+------------------------------------------------------------------------------*/
+
+extern  uchar                   bStatus;
+
+
+bool    ReadCntMonCanF_Curr(uchar  ibMon, uchar  ibCan);
+bool    ReadCntMonCanF_Buff(uchar  ibMon, uchar  ibCan);
---
+++ ek-tm4c1294xl/project1/src/digitals/extended/extended_4_out.h
@@ -0,0 +1,11 @@
+/*------------------------------------------------------------------------------
+EXTENDED_4_OUTPUT.H
+
+
+------------------------------------------------------------------------------*/
+
+void    OutExtended40(void);
+void    OutExtended401(void);
+void    OutExtended41(void);
+void    OutExtended42(void);
+void    OutExtended43(void);
