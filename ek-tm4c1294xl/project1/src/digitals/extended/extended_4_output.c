e2b0b7e84f099e92fe5f39a87a400804a18be002
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-05-26 07:32:40 +0300

    add files

---
+++ ek-tm4c1294xl/project1/src/digitals/extended/extended_4_output.c
@@ -0,0 +1,335 @@
+/*------------------------------------------------------------------------------
+EXTENDED_4_OUTPUT.C
+
+ «начени€ счетчиков на конец мес€цев из буфера с дозапросом (oтчет є52 от 21.11.2009)
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../console.h"
+#include "../../memory/mem_realtime.h"
+#include "../../memory/mem_energy.h"
+#include "../../memory/mem_extended_6.h"
+#include "../../realtime/realtime.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports_devices.h"
+#include "../../digitals/digitals.h"
+#include "../../digitals/digitals_pause.h"
+#include "../../digitals/digitals_display.h"
+#include "../../digitals/digitals_messages.h"
+#include "../../devices/devices.h"
+#include "../../sensors/device_f.h"
+#include "../../sensors/automatic_p.h"
+#include "../../sensors/automatic2.h"
+#include "../../time/rtc.h"
+#include "../../time/timedate.h"
+#include "../../engine.h"
+#include "../../energy2.h"
+#include "extended_4.h"
+
+
+
+void    PushData4(uchar  ibCan, uchar  ibMon, bool  fDouble)
+{
+  if (GetDigitalDevice(ibCan) == 0)
+  {
+    PushChar(ST4_OK);
+    PushInt(0xFF);
+    PushInt(0xFF);
+    PushFloat(mpreCntMonCan[ PrevSoftMon() ][ibCan]);
+    PushTime(tiZero);
+  }
+  else
+  {
+  	value6 va = mpCntMonCan4[ibCan];
+    PushChar(va.bSelf);
+    PushInt(0xFF);
+    PushInt(0xFF);
+    PushFloat(va.reSelf);
+    PushTime(va.tiUpdate);
+  }
+}
+
+
+
+void    OutExtended40(void)
+{
+  if (enGlobal == GLB_PROGRAM)
+    Result(bRES_NEEDWORK);
+  else if (InBuff(6) >= bMONTHS)
+    Result(bRES_BADADDRESS);
+  else
+  {
+    LoadCntMon(InBuff(6));
+    LoadExt4Values(InBuff(6));
+
+    InitPushPtr();            
+    uint wSize = 0;
+
+    uchar c;
+    for (c=0; c<bCANALS; c++)
+    {
+      if ((InBuff(7 + c/8) & (0x80 >> c%8)) != 0) 
+      {
+        PushData4(c, InBuff(6), false);
+        wSize += (1+2+2+4+6);
+      }
+    }
+
+    OutptrOutBuff(wSize);
+  }
+}
+
+
+
+void    OutExtended401(void)
+{
+  if (enGlobal == GLB_PROGRAM)
+    Result(bRES_NEEDWORK);
+  else if (InBuff(6) >= bMONTHS)
+    Result(bRES_BADADDRESS);
+  else
+  {
+    LoadCntMon(InBuff(6));
+    LoadExt4Values(InBuff(6));
+
+    InitPushPtr();            
+    PushChar(boExt4Flag);
+    PushChar(bExt4Months);
+    PushInt(cwDayCan6);
+    PushInt(cwMonCan6);
+    uint wSize = 1+1+2+2;
+
+    uchar c;
+    for (c=0; c<bCANALS; c++)
+    {
+      if ((InBuff(7 + c/8) & (0x80 >> c%8)) != 0) 
+      {
+        PushData4(c, InBuff(6), false);
+        wSize += (1+2+2+4+6);
+      }
+    }
+
+    OutptrOutBuff(wSize);
+  }
+}
+
+
+
+void    OutExtended41(void)
+{
+  if (enGlobal == GLB_PROGRAM)
+    Result(bRES_NEEDWORK);
+  else if ((InBuff(6) >= bMONTHS) || (InBuff(7) >= bCANALS))
+    Result(bRES_BADADDRESS);
+  else
+  {
+    LoadCntMon(InBuff(6));
+    LoadExt4Values(InBuff(6));
+
+    InitPushPtr();            
+    PushData4(InBuff(7), InBuff(6), false);
+    OutptrOutBuff(1+2+2+4+6);
+  }
+}
+
+
+
+void    OutExtended42(void)
+{
+  if (enGlobal == GLB_PROGRAM)
+    Result(bRES_NEEDWORK);
+  else if ((InBuff(6) >= bMONTHS) || (InBuff(7) >= bCANALS))
+    Result(bRES_BADADDRESS);
+  else
+  {
+    uchar ibMon = InBuff(6);
+    uchar ibCan = InBuff(7);
+
+    value4 va;
+    memset(&va, 0, sizeof(va));
+    va.tiSelf = *GetCurrTimeDate();
+
+    if (GetDigitalDevice(ibCan) == 0)
+    {
+      if (LoadCntMon(ibMon) == 1)
+      {
+        va.bSelf = ST4_OK;
+        va.reSelf = mpreCntMonCan[ PrevSoftMon() ][ibCan];
+      }
+      else
+      {
+        va.bSelf = ST4_BADFLASH;
+        va.reSelf = 0;
+      }
+
+      InitPushPtr();            
+      Push(&va, sizeof(value4));
+      OutptrOutBuff(sizeof(value4));
+    }
+    else if (mpboEnblCan[ibCan] == FALSE)
+    {
+      va.bSelf = ST4_BADENABLING;
+      va.reSelf = 0;
+
+      InitPushPtr();            
+      Push(&va, sizeof(value4));
+      OutptrOutBuff(sizeof(value4));
+    }
+    else if (GetDigitalPhone(ibCan) != 0)
+    {
+      va.bSelf = ST4_BADPORT;
+      va.reSelf = 0;
+
+      InitPushPtr();            
+      Push(&va, sizeof(value4));
+      OutptrOutBuff(sizeof(value4));
+    }
+    else
+    {
+      SaveDisplay();
+
+      ShowHi(szClear); 
+      sprintf(szHi,"ћес€ц: %-2u",ibMon+1); sprintf(szHi+14,"%2u",ibCan+1);
+      Clear(); 
+
+      uchar p = ibPort;
+      bool f = ReadCntMonCan(ibMon,ibCan);
+      ibPort = p;
+
+      if (f == 1)
+      {
+        va.bSelf = ST4_OK;
+        va.reSelf = reBuffA;
+      }
+      else
+      {
+        va.bSelf = ST4_BADDIGITAL;
+        va.reSelf = 0;
+      }
+
+      InitPushPtr();            
+      Push(&va, sizeof(value4));
+      OutptrOutBuff(sizeof(value4));
+
+      LoadDisplay();
+      NextPause(); // внимание !
+    }     
+  }
+}
+
+
+
+void    OutExtended43(void)
+{
+  InitPushPtr();
+  uint wSize = 0;
+
+  uchar c;
+  for (c=0; c<bCANALS; c++)
+  {
+    if ((InBuff(6 + c/8) & (0x80 >> c%8)) != 0)
+    {
+      Push(&mpboExt4EnblCan[c], sizeof(uchar));
+      wSize += sizeof(uchar);
+    }
+  }
+
+  OutptrOutBuff(wSize);
+}
+
+
+
+bool    ReadCntMonCanF_Curr(uchar  ibMon, uchar  ibCan)
+{
+uchar   i;
+
+  Clear();
+
+  LoadCurrDigital(ibCan);
+  ibPort = diCurr.ibPort;
+
+  Clear(); 
+
+  for (i=0; i<bMINORREPEATS; i++)
+  {
+    QueryBreakF();
+
+    InitPushPck();
+    PushChar(diCurr.bAddress);          
+    PushChar(0);
+
+    PushChar((5+3+2) % 0x100);
+    PushChar((5+3+2) / 0x100);
+
+    PushChar(0xFF);
+    PushChar(bEXT_GETEXTENDED42);         
+    PushChar(ibMon);         
+    PushChar(ibCan);         
+
+    PckQueryIO(bHEADER+sizeof(value4)+2, 5+3+2);
+
+    if (Input() == SER_GOODCHECK) break;
+    if (fKey == true) return(0);
+  }
+
+  if (i == bMINORREPEATS) return(0);
+  ShowPercent(100);
+
+
+  InitPop(bHEADER);
+  bStatus = PopChar();
+  PopChar(); PopChar(); PopChar(); PopChar(); 
+  PopFloat(); 
+  Pop(&tiAlt, sizeof(time));   
+
+  return(1);
+}
+
+
+bool    ReadCntMonCanF_Buff(uchar  ibMon, uchar  ibCan)
+{
+uchar   i;
+
+  Clear();
+
+  LoadCurrDigital(ibCan);
+  ibPort = diCurr.ibPort;
+
+  Clear(); 
+
+  for (i=0; i<bMINORREPEATS; i++)
+  {
+    QueryBreakF();
+
+    InitPushPck();
+    PushChar(diCurr.bAddress);          
+    PushChar(0);
+
+    PushChar((5+10+2) % 0x100);
+    PushChar((5+10+2) / 0x100);
+
+    PushChar(0xFF);
+    PushChar(bEXT_GETEXTENDED40);         
+    PushChar(ibMon);         
+
+    PushChar(0xFF);
+    PushChar(0xFF);
+    PushChar(0xFF);
+    PushChar(0xFF);
+    PushChar(0xFF);
+    PushChar(0xFF);
+    PushChar(0xFF);
+    PushChar(0xFF);
+
+    PckQueryIO(bHEADER+sizeof(value4)*bCANALS+2, 5+10+2);
+
+    if (Input() == SER_GOODCHECK) break;
+    if (fKey == true) return(0);
+  }
+
+  if (i == bMINORREPEATS) return(0);
+  ShowPercent(100);
+
+
+  return(1);
+}
---
+++ ek-tm4c1294xl/project1/src/digitals/extended/extended_4_output.h
@@ -0,0 +1,18 @@
+/*------------------------------------------------------------------------------
+EXTENDED_4_OUTPUT.H
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../memory/mem_extended_4.h"
+
+
+
+void    OutExtended40(void);
+void    OutExtended401(void);
+void    OutExtended41(void);
+void    OutExtended42(void);
+void    OutExtended43(void);
+
+bool    ReadCntMonCanF_Curr(uchar  ibMon, uchar  ibCan);
+bool    ReadCntMonCanF_Buff(uchar  ibMon, uchar  ibCan);
