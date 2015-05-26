07fa010f43aeae7db37db20ad4923a0efdca0d3c
ti-cortex-m4 <ti.cortex.m4@google.com>
2015-05-26 15:43:09 +0400

    renamed

--- ek-tm4c1294xl/project1/src/digitals/extended/extended_4_out.h
+++ ek-tm4c1294xl/project1/src/digitals/extended/extended_4_out.h
@@ -5,7 +5,7 @@ EXTENDED_4_OUT.H
 ------------------------------------------------------------------------------*/
 
 void    OutExtended40(bool  fDouble);
-void    OutExtended401(bool  fDouble);
+void    OutExtended44(bool  fDouble);
 void    OutExtended41(bool  fDouble);
 void    OutExtended42(void);
 void    OutExtended43(void);
