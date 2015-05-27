f26d09c77f6c28ce65803cf60282e58564e6006f
ti-cortex-m4 <ti.cortex.m4@google.com>
2015-05-27 10:37:08 +0400

    use 'double float'

--- ek-tm4c1294xl/project1/src/digitals/extended/extended_4_in.c
+++ ek-tm4c1294xl/project1/src/digitals/extended/extended_4_in.c
@@ -40,12 +40,12 @@ bool    ReadCntMonCanF_Curr(uchar  ibMon, uchar  ibCan)
     PushChar((5+3+2) % 0x100);
     PushChar((5+3+2) / 0x100);
 
-    PushChar(0xFF);
+    PushChar(0xFD);
     PushChar(bEXT_GETEXTENDED42);         
     PushChar(ibMon);         
     PushChar(ibCan);         
 
-    PckQueryIO(bHEADER+(1+2+2+4+6)+2, 5+3+2);
+    PckQueryIO(bHEADER+(1+2+2+8+6)+2, 5+3+2);
 
     if (Input() == SER_GOODCHECK) break;
     if (fKey == true) return false;
@@ -59,7 +59,7 @@ bool    ReadCntMonCanF_Curr(uchar  ibMon, uchar  ibCan)
   bStatus4 = PopChar();
   PopChar(); PopChar();
   PopChar(); PopChar();
-  dbValue4 = PopFloat();
+  dbValue4 = PopDouble();
   Pop(&tiUpdate4, sizeof(time));
 
   return true;
