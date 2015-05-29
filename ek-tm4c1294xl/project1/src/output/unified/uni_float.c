fed223b64dcfda763236f3b3b44a6a782d82e30d
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-05-29 10:31:07 +0400

    renaming

--- ek-tm4c1294xl/project1/src/output/unified/uni_float.c
+++ ek-tm4c1294xl/project1/src/output/unified/uni_float.c
@@ -12,40 +12,62 @@ UNI_FLOAT,C
 
 
 
-static void PushFFFFFF(void)
+static void PushFF(bool  fDouble)
 {
-  PushChar(0xFF); PushChar(0xFF); PushChar(0xFF);
+  if (fDouble)
+  {
+    PushChar(0xFF);
+    PushChar(0xFF);
+    PushChar(0xFF);
+    PushChar(0xFF);
+    PushChar(0xFF);
+    PushChar(0xFF);
+    PushChar(0xFF);
+  }
+  else
+  {
+    PushChar(0xFF);
+    PushChar(0xFF);
+    PushChar(0xFF);
+  }
 }
 
 
-void    PushFloatUni(status  st, float  fl)
+void    PushFloatOrDoubleUni(status  bStatus, double  db, bool  fDouble)
 {
   if (boStrictUni == true)
   {
-    switch (st)
+    switch (bStatus)
     {
-      case ST4_NONE:          PushFFFFFF(); PushChar(0xFF); break;
-      case ST4_NOTSUPPORTED:  PushFFFFFF(); PushChar(0xFF); break;
-      case ST4_NOTPRESENTED:  PushFFFFFF(); PushChar(0xFF); break;
-      case ST4_BADDIGITAL:    PushFFFFFF(); PushChar(0xFE); break;
-      case ST4_BADFLASH:      PushFFFFFF(); PushChar(0xFF); break;
-      case ST4_MODEM_LINK:    PushFFFFFF(); PushChar(0xFE); break;
-      case ST4_DISABLED:      PushFFFFFF(); PushChar(0xFF); break;
-      default:                PushFloat(fl); break;
+      case ST4_NONE:          PushFF(fDouble); PushChar(0xFF); break;
+      case ST4_NOTSUPPORTED:  PushFF(fDouble); PushChar(0xFF); break;
+      case ST4_NOTPRESENTED:  PushFF(fDouble); PushChar(0xFF); break;
+      case ST4_BADDIGITAL:    PushFF(fDouble); PushChar(0xFE); break;
+      case ST4_BADFLASH:      PushFF(fDouble); PushChar(0xFF); break;
+      case ST4_MODEM_LINK:    PushFF(fDouble); PushChar(0xFE); break;
+      case ST4_DISABLED:      PushFF(fDouble); PushChar(0xFF); break;
+      default:                PushFloatOrDouble(db, fDouble); break;
     }
   }
   else
   {
-    switch (st)
+    switch (bStatus)
     {
-      case ST4_NONE:          PushFFFFFF(); PushChar(0xE0); break;
-      case ST4_NOTSUPPORTED:  PushFFFFFF(); PushChar(0xE1); break;
-      case ST4_NOTPRESENTED:  PushFFFFFF(); PushChar(0xE2); break;
-      case ST4_BADDIGITAL:    PushFFFFFF(); PushChar(0xE3); break;
-      case ST4_BADFLASH:      PushFFFFFF(); PushChar(0xE4); break;
-      case ST4_MODEM_LINK:    PushFFFFFF(); PushChar(0xE5); break;
-      case ST4_DISABLED:      PushFFFFFF(); PushChar(0xE6); break;
-      default:                PushFloat(fl); break;
+      case ST4_NONE:          PushFF(fDouble); PushChar(0xE0); break;
+      case ST4_NOTSUPPORTED:  PushFF(fDouble); PushChar(0xE1); break;
+      case ST4_NOTPRESENTED:  PushFF(fDouble); PushChar(0xE2); break;
+      case ST4_BADDIGITAL:    PushFF(fDouble); PushChar(0xE3); break;
+      case ST4_BADFLASH:      PushFF(fDouble); PushChar(0xE4); break;
+      case ST4_MODEM_LINK:    PushFF(fDouble); PushChar(0xE5); break;
+      case ST4_DISABLED:      PushFF(fDouble); PushChar(0xE6); break;
+      default:                PushFloatOrDouble(db, fDouble); break;
     }
   }
+}
+
+
+
+void    PushFloatUni(status  bStatus, float  fl)
+{
+  PushFloatOrDoubleUni(bStatus, fl, false);
 }
