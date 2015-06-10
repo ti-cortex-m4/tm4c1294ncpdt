d4f81d363c3a10515b760ccf2b9ac111249de81c
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-06-10 21:42:04 +0300

    add template

---
+++ ek-tm4c1294xl/project1/src/nvram/cache.c
@@ -0,0 +1,21 @@
+/*------------------------------------------------------------------------------
+CACHE.C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+#include "cache.h"
+
+
+
+bool    SaveCache(file const  *pfl)
+{
+  return false;
+}
+
+
+bool    LoadCache(file const  *pfl)
+{
+  return false;
+}
