901ae240d3a1d53d51e7190c7a1054e34ebde778
ti-cortex-m4 <ti.cortex.m4@google.com>
2015-06-18 10:48:53 +0400

    fix

--- ek-tm4c1294xl/project1/src/nvram/cache.h
+++ ek-tm4c1294xl/project1/src/nvram/cache.h
@@ -10,3 +10,9 @@ CACHE.H
 
 bool    SaveCache(cache const  *pfl);
 bool    LoadCache(cache const  *pfl);
+
+bool    SaveArrayX(ulong  dwAddr, uint  wSizeX, uchar  ibX, void  *pBuff);
+bool    LoadArrayX(ulong  dwAddr, uint  wSizeX, uchar  ibX, void  *pBuff);
+
+bool    SaveArrayXY(ulong  dwAddr, uint  wSizeX, uchar  ibX, uint  wSizeY, uchar  ibY, void  *pBuff);
+bool    LoadArrayXY(ulong  dwAddr, uint  wSizeX, uchar  ibX, uint  wSizeY, uchar  ibY, void  *pBuff);
