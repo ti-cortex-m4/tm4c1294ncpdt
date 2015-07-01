0f66b47cd80993463134fa39211fadce8e54ee35
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-07-01 17:57:53 +0400

    fix

--- ek-tm4c1294xl/project1/src/flash/records.c
+++ ek-tm4c1294xl/project1/src/flash/records.c
@@ -5,27 +5,30 @@ RECORDS.C
 ------------------------------------------------------------------------------*/
 
 #include "../main.h"
+#include "../memory/mem_flash.h"
 #include "../memory/mem_records.h"
+#include "../time/rtc.h"
 #include "files.h"
+#include "record3.h"
 #include "records.h"
 
 
 
-bool    AddKeyRecord(event  ev)
+bool    AddKeyRecord(event  evCode)
 {
   if (IsRecordDisabled(evCode)) return(1);
 
   uint i = (cdwKeyRecord % wRECORDS);
 
-  OpenIn(wFLA_KEYRECORD + i / bRECORD_BLOCK);
-  memcpy(mpbPageOut, mpbPageIn, wFREEPAGE_SIZE);
+  OpenIn(KEY_RECORD + i / bRECORD_BLOCK);
+  memcpy(mpbPageOut, mpbPageIn, wLEAF_BYTES);
 
   memset(&reCurr, 0, sizeof(record));
 
-  reCurr.tiNow     = *PGetCurrTimeDate();
+  reCurr.tiNow     = *GetCurrTimeDate();
   reCurr.cdwRecord = cdwKeyRecord++;
   reCurr.evCode    = evCode;
-
+/*
   switch (evCode)
   {
     case EVE_GPS_GOODLINK:  memcpy(&reCurr.mpbBuff+0, &tiGPS, sizeof(time)); break;
@@ -50,8 +53,8 @@ bool    AddKeyRecord(event  ev)
     case EVE_SMK_GOODLINK:  memcpy(&reCurr.mpbBuff+0, &tiSMK, sizeof(time)); break;
     case EVE_SMK_GOODSMK_1: memcpy(&reCurr.mpbBuff+0, &tiAlt, sizeof(time)); break;
   }
-
-  OpenOut(wFLA_KEYRECORD + i / bRECORD_BLOCK);
+*/
+  OpenOut(KEY_RECORD + i / bRECORD_BLOCK);
   memcpy(&mpbPageOut + (i % bRECORD_BLOCK)*sizeof(record), &reCurr, sizeof(record));
 
   return( CloseOut() );
@@ -81,3 +84,58 @@ bool AddImpRecord(event  ev) {
 bool AddModRecord(event  ev) {
 	return false;
 }
+
+
+
+bool    GetRecord(class  clT, uint  iwRecord)
+{
+  uint wPage;
+  switch (clT)
+  {
+    case CLA_KEYBOARD: wPage = KEY_RECORD; break;
+    case CLA_SYSTEM:   wPage = SYS_RECORD; break;
+    case CLA_DIGITAL:  wPage = DIG_RECORD; break;
+    case CLA_IMPULSE:  wPage = IMP_RECORD; break;
+    case CLA_MODEM:    wPage = MOD_RECORD; break;
+    case CLA_PHONES2:  wPage = PH2_RECORD; break;
+  }
+
+  uint i = (iwRecord % wRECORDS);
+
+  OpenIn(wPage + i / bRECORD_BLOCK);
+  memcpy(&reCurr, &mpbPageIn + (i % bRECORD_BLOCK)*sizeof(record), sizeof(record));
+
+  return(1);
+}
+
+
+ulong   GetRecordCount(class  clT)
+{
+  switch (clT)
+  {
+    case CLA_KEYBOARD: return(cdwKeyRecord);
+    case CLA_SYSTEM:   return(cdwSysRecord);
+    case CLA_DIGITAL:  return(cdwDigRecord);
+    case CLA_IMPULSE:  return(cdwImpRecord);
+    case CLA_MODEM:    return(cdwModRecord);
+    case CLA_PHONES2:  return(cdwPh2Record);
+  }
+}
+
+
+bool    GetRecordsBlock(class  clT, uchar  ibBlock)
+{
+  uint i = GetRecordCount(clT) % wRECORDS;
+  ibBlock = (bRECORD_SIZE + i/bRECORD_BLOCK - ibBlock) % bRECORD_PAGES;
+
+  switch (clT)
+  {
+    case CLA_KEYBOARD: return(OpenIn(KEY_RECORD + ibBlock));
+    case CLA_SYSTEM:   return(OpenIn(SYS_RECORD + ibBlock));
+    case CLA_DIGITAL:  return(OpenIn(DIG_RECORD + ibBlock));
+    case CLA_IMPULSE:  return(OpenIn(IMP_RECORD + ibBlock));
+    case CLA_MODEM:    return(OpenIn(MOD_RECORD + ibBlock));
+    case CLA_PHONES2:  return(OpenIn(PH2_RECORD + ibBlock));
+  }
+}
+
--- ek-tm4c1294xl/project1/src/memory/mem_records.h
+++ ek-tm4c1294xl/project1/src/memory/mem_records.h
@@ -6,4 +6,4 @@ MEM_RECORDS.H
 
 extern  record                  reCurr;
 
-extern  ulong                   cdwKeyRecord, cdwSysRecord, cdwDigRecord, cdwImpRecord;
+extern  ulong                   cdwKeyRecord, cdwSysRecord, cdwDigRecord, cdwImpRecord, cdwModRecord, cdwPh2Record;
