9d14c64025defcb23ae248457daac2115c6d9613
ti-cortex-m4 <ti.cortex.m4@google.com>
2015-07-06 15:30:46 +0400

    fix records

--- ek-tm4c1294xl/project1/src/memory/mem_records.h
+++ ek-tm4c1294xl/project1/src/memory/mem_records.h
@@ -7,3 +7,5 @@ MEM_RECORDS.H
 extern  record                  reCurr;
 
 extern  ulong                   cdwKeyRecord, cdwSysRecord, cdwDigRecord, cdwImpRecord, cdwModRecord, cdwPh2Record, cdwAuxRecord;
+
+extern  time                    tiRecordSetCurrTime;
