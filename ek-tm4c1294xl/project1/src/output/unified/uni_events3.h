66c0280bfca0142a059bafc46bd1178f529d18a4
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-07-07 22:09:57 +0300

    split large file

---
+++ ek-tm4c1294xl/project1/src/output/unified/uni_events3.h
@@ -0,0 +1,10 @@
+/*------------------------------------------------------------------------------
+U_EVENTS.H
+
+
+------------------------------------------------------------------------------*/
+
+void    GetEventsCountsUni(void); 
+void    GetEventsUni(void); 
+void    GetEventsUni_Fix(void);
+void    GetEventsMessagesUni(void); 
