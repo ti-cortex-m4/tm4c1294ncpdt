de007b1ad8c76c853ec17ce541c1ba873890feab
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-07-12 21:29:57 +0300

    fix

--- ek-tm4c1294xl/project1/src/flash/flash_define1.h
+++ ek-tm4c1294xl/project1/src/flash/flash_define1.h
@@ -1,15 +1,15 @@
 /*------------------------------------------------------------------------------
-FLASH_DEFINE1.H
+FLASH_DEFINE1,H
 
 
 ------------------------------------------------------------------------------*/
 
-#define SPI_bTT_SCK   0x0008 //PD3
-#define SPI_bTT_SI    0x0002 //PD1
-#define SPI_bTT_SO    0x0001 //PD0
-#define SPI_bTT_CS    0x0004 //PD2
+#define MASK_FLASH1_SCK 0x0008 // PD3
+#define MASK_FLASH1_SI  0x0002 // PD1
+#define MASK_FLASH1_SO  0x0001 // PD0
+#define MASK_FLASH1_CS  0x0004 // PD2
 
-#define GPIO_DATAbTT_SCK (GPIO_PORTD_AHB_BASE + GPIO_O_DATA + 0x0020) //PD3
-#define GPIO_DATAbTT_SI  (GPIO_PORTD_AHB_BASE + GPIO_O_DATA + 0x0008) //PD1
-#define GPIO_DATAbTT_SO  (GPIO_PORTD_AHB_BASE + GPIO_O_DATA + 0x0004) //PD0
-#define GPIO_DATAbTT_CS  (GPIO_PORTD_AHB_BASE + GPIO_O_DATA + 0x0010) //PD2
+#define GPIO_FLASH1_SCK (GPIO_PORTD_AHB_BASE + GPIO_O_DATA + 0x0020) // PD3
+#define GPIO_FLASH1_SI  (GPIO_PORTD_AHB_BASE + GPIO_O_DATA + 0x0008) // PD1
+#define GPIO_FLASH1_SO  (GPIO_PORTD_AHB_BASE + GPIO_O_DATA + 0x0004) // PD0
+#define GPIO_FLASH1_CS  (GPIO_PORTD_AHB_BASE + GPIO_O_DATA + 0x0010) // PD2
