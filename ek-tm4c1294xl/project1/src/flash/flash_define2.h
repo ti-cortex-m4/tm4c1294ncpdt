cffed1ad758afdac76f00f24dadd2c15fb46b61b
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-07-12 21:28:40 +0300

    fix

--- ek-tm4c1294xl/project1/src/flash/flash_define2.h
+++ ek-tm4c1294xl/project1/src/flash/flash_define2.h
@@ -1,15 +1,15 @@
 /*------------------------------------------------------------------------------
-FLASH_DEFINE2.H
+FLASH_DEFINE2,H
 
 
 ------------------------------------------------------------------------------*/
 
-#define SPI_bTT2_SCK   0x0020 //PB5
-#define SPI_bTT2_SI    0x0010 //PE4
-#define SPI_bTT2_SO    0x0020 //PE5
-#define SPI_bTT2_CS    0x0010 //PB4
+#define MASK_FLASH2_SCK 0x0020 // PB5
+#define MASK_FLASH2_SI  0x0010 // PE4
+#define MASK_FLASH2_SO  0x0020 // PE5
+#define MASK_FLASH2_CS  0x0010 // PB4
 
-#define GPIO_DATAbTT2_SCK (GPIO_PORTB_AHB_BASE + GPIO_O_DATA + 0x0080)//PB5
-#define GPIO_DATAbTT2_SI  (GPIO_PORTE_AHB_BASE + GPIO_O_DATA + 0x0040)//PE4
-#define GPIO_DATAbTT2_SO  (GPIO_PORTE_AHB_BASE + GPIO_O_DATA + 0x0080)//PE5
-#define GPIO_DATAbTT2_CS  (GPIO_PORTB_AHB_BASE + GPIO_O_DATA + 0x0040)//PB4
+#define GPIO_FLASH2_SCK (GPIO_PORTB_AHB_BASE + GPIO_O_DATA + 0x0080) // PB5
+#define GPIO_FLASH2_SI  (GPIO_PORTE_AHB_BASE + GPIO_O_DATA + 0x0040) // PE4
+#define GPIO_FLASH2_SO  (GPIO_PORTE_AHB_BASE + GPIO_O_DATA + 0x0080) // PE5
+#define GPIO_FLASH2_CS  (GPIO_PORTB_AHB_BASE + GPIO_O_DATA + 0x0040) // PB4
