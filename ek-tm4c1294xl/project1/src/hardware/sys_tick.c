08406b41386a31445eda421f4c1d066a2db2dbe5
ti cortex m4 <ti.cortex.m4@gmail.com>
2015-07-15 16:42:18 +0400

    fix

--- ek-tm4c1294xl/project1/src/hardware/sys_tick.c
+++ ek-tm4c1294xl/project1/src/hardware/sys_tick.c
@@ -5,9 +5,7 @@ SYS_TICK.C
 ------------------------------------------------------------------------------*/
 
 #include "../main.h"
-#include "inc/hw_gpio.h"
-#include "inc/hw_memmap.h"
-#include "inc/hw_sysctl.h"
+#include "inc/hw_nvic.h"
 #include "inc/hw_types.h"
 #include "driverlib/systick.h"
 #include "sys_tick.h"
