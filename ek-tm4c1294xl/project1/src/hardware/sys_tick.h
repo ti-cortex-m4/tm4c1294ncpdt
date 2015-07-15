81d9bc3c0e288eeb0405add166026994f13e7d41
ti cortex m4 <ti.cortex.m4@gmail.com>
2015-07-15 16:35:17 +0400

    fix

--- ek-tm4c1294xl/project1/src/hardware/sys_tick.h
+++ ek-tm4c1294xl/project1/src/hardware/sys_tick.h
@@ -4,3 +4,7 @@ SYS_TICK.H
 
 ------------------------------------------------------------------------------*/
 
+void    InitSysTick(uint32_t  ui32SysClock);
+
+void    StartSysTick(void);
+ulong   StopSysTick(void);
