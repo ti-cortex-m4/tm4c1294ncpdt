c67271ad8b531c47bc7eed41126af977bf787e35
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-07-15 16:35:17 +0400

    fix

--- ek-tm4c1294xl/project1/src/hardware/sys_tick.c
+++ ek-tm4c1294xl/project1/src/hardware/sys_tick.c
@@ -5,4 +5,32 @@ SYS_TICK.C
 ------------------------------------------------------------------------------*/
 
 #include "../main.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_sysctl.h"
+#include "inc/hw_types.h"
 #include "driverlib/systick.h"
+#include "sys_tick.h"
+
+
+
+void    InitSysTick(uint32_t  ui32SysClock)
+{
+  SysTickPeriodSet((ui32SysClock / 1000) - 1); // 1 ms
+  HWREG(NVIC_ST_CURRENT) = 0;
+  SysTickDisable();
+}
+
+
+void    StartSysTick(void)
+{
+  HWREG(NVIC_ST_CURRENT) = 0;
+  SysTickEnable();
+}
+
+
+ulong   StopSysTick(void)
+{
+  SysTickDisable();
+  return HWREG(NVIC_ST_CURRENT);
+}
