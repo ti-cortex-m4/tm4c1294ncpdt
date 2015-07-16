4e9146ce38c64b9eff61408526245ad451b08194
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-07-16 07:37:33 +0300

    add ASSERT

--- ek-tm4c1294xl/project1/src/kernel/array_mnt.c
+++ ek-tm4c1294xl/project1/src/kernel/array_mnt.c
@@ -15,18 +15,22 @@ ARRAY_MNT,C
 
 bool    SaveImpMnt(uint  iwMntTo, uchar  ibMntFrom)
 {
+  ASSERT(iwMntTo < bMINUTES);
+  ASSERT(ibMntFrom < 2);
   return SaveArrayX(IMPMNTCAN, sizeof(uint)*bCANALS, iwMntTo, &mpwImpMntCan[ ibMntFrom ]);
 }
 
 
 bool    FreeImpMnt(uint  iwMntTo)
 {
+  ASSERT(iwMntTo < bMINUTES);
   return FreeArrayX(IMPMNTCAN, sizeof(uint)*bCANALS, iwMntTo);
 }
 
 
 bool    LoadImpMnt(uint  iwMntFrom)
 {
+  ASSERT(iwMntFrom < bMINUTES);
   if (iwMntFrom == iwHardMnt)
   {
     memcpy(mpwImpMntCan[ PrevSoftMnt() ], mpwImpMntCan[ ibSoftMnt ], sizeof(uint)*bCANALS);
