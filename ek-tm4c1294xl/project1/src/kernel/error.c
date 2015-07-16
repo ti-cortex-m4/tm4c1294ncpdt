3e64f3d2216f7265bf7cc2002c0d6c4241d6030c
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-07-16 07:37:33 +0300

    add ASSERT

--- ek-tm4c1294xl/project1/src/kernel/error.c
+++ ek-tm4c1294xl/project1/src/kernel/error.c
@@ -4,10 +4,20 @@ ERROR.H
 
 ------------------------------------------------------------------------------*/
 
-#include        <stdint.h>
+#include <stdint.h>
+#include "../hardware/beep.h"
+
+
 
 // The error routine that is called if the driver library encounters an error.
 #ifdef DEBUG
-void __error__(char *pcFilename, uint32_t ui32Line) {
+
+void __error__(char *pcFilename, uint32_t ui32Line)
+{
+  Beep();
+  while(1)
+  {
+  }
 }
+
 #endif
