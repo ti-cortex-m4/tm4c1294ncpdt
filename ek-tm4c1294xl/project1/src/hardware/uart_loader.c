93ff9d1447bfa2c1b8da1f8b2294596082043731
ti cortex m4 <ti.cortex.m4@gmail.com>
2015-07-17 15:42:39 +0400

    fix

--- ek-tm4c1294xl/project1/src/hardware/uart_loader.c
+++ ek-tm4c1294xl/project1/src/hardware/uart_loader.c
@@ -5,22 +5,22 @@ UART_LOADER.C
 ------------------------------------------------------------------------------*/
 
 #include "../main.h"
-//#include "inc/hw_nvic.h"
 #include "inc/hw_types.h"
 #include "inc/hw_flash.h"
-//#include "driverlib/systick.h"
 #include "uart_loader.h"
 
 
 
 // bNumPort - номер порта (0-A, 1-B, 2-C, 3-D, 4-E, 5-F, 6-G, 7-H)
 // bNumPin - номер пина (0-Pin0, 1-Pin1, 2-Pin2, 3-Pin3, 4-Pin4, 5-Pin5, 6-Pin6, 7-Pin7)
-// bPolarity - пол€рность сигнала на пине порта (0-”ровень0, 1-”ровень1)
-// bEnableNegative - активаци€ запуска загрузчика при старте (0-загрузчик активен)
+// bPolarity - пол€рность сигнала на пине порта (0 - уровень 0, 1 - уровень 1)
+// bEnableNegative - активаци€ запуска загрузчика при старте (0 - загрузчик активен)
 void    BOOTCFG_Write(uchar bNumPort, uchar bNumPin, uchar bPolarity, uchar bEnableNegative)
 {
-  // проверка правильности значений
-  if((bNumPort > 7) || (bNumPin > 7) || (bPolarity > 1) || (bEnableNegative > 1) ) return;
+  ASSERT(bNumPort <= 7);
+  ASSERT(bNumPin <= 7);
+  ASSERT(bPolarity <= 1);
+  ASSERT(bEnableNegative <= 1);
 
   // данные
   HWREG(FLASH_FMD) = 0xFFFF00FE + (bNumPort << 13) + (bNumPin << 10) + (bPolarity << 9 ) + (bEnableNegative << 8 );
@@ -38,7 +38,7 @@ void    BOOTCFG_Write(uchar bNumPort, uchar bNumPin, uchar bPolarity, uchar bEna
 
 void    InitUartLoader(void)
 {
-  if(HWREG(FLASH_BOOTCFG) == 0xFFFFFFFE)
+  if (HWREG(FLASH_BOOTCFG) == 0xFFFFFFFE)
   {
     BOOTCFG_Write(2, 4, 0, 0);
   }
