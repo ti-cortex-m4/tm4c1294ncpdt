ace145360e0f1818a770b169e596f0cec4ccfc50
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-08-11 12:25:04 +0300

    the file moved

---
+++ ek-tm4c1294xl/project1/src/main.c
@@ -0,0 +1,177 @@
+/*------------------------------------------------------------------------------
+MAIN,C
+
+
+------------------------------------------------------------------------------*/
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+#include "hardware/watchdog.h"
+#include "hardware/system_clock.h"
+#include "hardware/sys_tick.h"
+#include "hardware/beep.h"
+#include "display/lcd.h"
+#include "time/rtc.h"
+#include "keyboard/infrared.h"
+#include "keyboard/key.h"
+#include "keyboard/keyboard.h"
+#include "nvram/23x1024.h"
+#include "flash/flash.h"
+#include "display/display.h"
+#include "isr/timer0.h"
+#include "isr/timer1.h"
+#include "isr/timer2.h"
+#include "isr/serial0.h"
+#include "isr/serial1.h"
+#include "isr/serial2.h"
+#include "isr/serial3.h"
+#include "hardware/memory.h"
+#include "hardware/power_off.h"
+#include "hardware/uart_loader.h"
+#include "uarts.h"
+#include "serial/modems.h"
+#include "serial/print.h"
+#include "flash/flash1.h"
+#include "flash/flash2.h"
+#include "flash/storage.h"
+#include "flash/records.h"
+#include "settings.h"
+#include "impulses/impulses.h"
+#include "impulses/factors.h"
+#include "groups.h"
+#include "tariffs/zones.h"
+#include "tariffs/tariffs.h"
+#include "tariffs/relaxs.h"
+#include "tariffs/gaps.h"
+#include "digitals/digitals.h"
+#include "digitals/limits.h"
+#include "digitals/params/params.h"
+#include "digitals/profile/profile_core.h"
+#include "devices/devices_init.h"
+#include "devices/devices.h"
+#include "digitals/phones.h"
+#include "special/defects.h"
+#include "serial/speeds.h"
+#include "serial/flow.h"
+#include "output/esc/esc.h"
+#include "output/esc/response_esc.h"
+#include "output/unified/uni.h"
+#include "output/unified/response_uni.h"
+#include "time/decret.h"
+#include "time/correct1.h"
+#include "time/correct2.h"
+#include "special/recalc.h"
+#include "realtime/realtime.h"
+#include "realtime/realtime_init.h"
+#include "output/response_crc.h"
+
+
+
+int main(void)
+{
+  ui32SysClock = GetSystemClockFrequency();
+
+  InitSysTick(ui32SysClock);
+  InitUart0(ui32SysClock);
+  InitUart2(ui32SysClock);
+  InitUart3(ui32SysClock);
+  InitUart4(ui32SysClock);
+
+  DisableWDT();
+
+  InitBeep();
+  InitLCD();
+  InitNvram();
+  InitFlash1();
+  InitFlash2();
+  InitRTC();
+  InitCODE();
+
+  InitRecords();
+  InitSettings();
+  InitInfrared();
+  InitKey();
+  InitKeyboard();
+  InitImpulses();
+  InitFactors();
+  InitGroups();
+  InitZones();
+  InitTariffs();
+  InitRelaxs();
+  InitGaps();
+  InitDecret();
+  InitCorrect1();
+  InitCorrect2();
+  InitDigitals();
+  InitLimits();
+  InitDevices1();
+  InitParams();
+  InitPhones();
+  InitProfile();
+  InitSpeeds();
+  InitEsc();
+  InitUni();
+  InitFlow();
+  InitPowerOff();
+
+  InitSlide();
+
+  PrintStart();
+  InitStorage();
+  InitDefects();
+  InitRealtime();
+  PrintStop();
+
+  InitDisplay();
+
+//  InitCurrent();
+//  InitProfiles();
+  InitDevices2();
+
+  InitSerial0();
+  InitSerial1();
+  InitSerial2();
+  InitSerial3();
+
+  InitTimer0(ui32SysClock);
+  InitTimer1(ui32SysClock);
+  InitTimer2(ui32SysClock);
+
+  IntMasterEnable();
+
+  InitRecalc();
+  InitUartLoader();
+  LongBeep();
+
+  InitWDT();
+
+  while (1)
+  {
+    ASSERT((enGlobal == GLB_PROGRAM) || (enGlobal == GLB_WORK) || (enGlobal == GLB_REPROGRAM));
+
+    ResetWDT();
+
+    RunRealtime();
+    RunDevices();
+    RunInfrared();
+    RunKeyboard();
+    RunLocal();
+
+    RunResponseCRC_All();
+    RunResponseEsc_All();
+    RunResponseUni_All();
+    RunResponseFlow_All();
+  }
+}
