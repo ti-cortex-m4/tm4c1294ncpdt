b3d20233b5b8619570a1df65f3a2a2e926183c6e
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-08-12 12:11:04 +0300

    renaming

---
+++ ek-tm4c1294xl/project1/self/flash/flash_define1.h
@@ -0,0 +1,15 @@
+/*------------------------------------------------------------------------------
+FLASH_DEFINE1,H
+
+
+------------------------------------------------------------------------------*/
+
+#define MASK_FLASH2_SCK 0x0020 // PB5
+#define MASK_FLASH2_SI  0x0010 // PE4
+#define MASK_FLASH2_SO  0x0020 // PE5
+#define MASK_FLASH2_CS  0x0010 // PB4
+
+#define GPIO_FLASH2_SCK (GPIO_PORTB_AHB_BASE + GPIO_O_DATA + 0x0080) // PB5
+#define GPIO_FLASH2_SI  (GPIO_PORTE_AHB_BASE + GPIO_O_DATA + 0x0040) // PE4
+#define GPIO_FLASH2_SO  (GPIO_PORTE_AHB_BASE + GPIO_O_DATA + 0x0080) // PE5
+#define GPIO_FLASH2_CS  (GPIO_PORTB_AHB_BASE + GPIO_O_DATA + 0x0040) // PB4
