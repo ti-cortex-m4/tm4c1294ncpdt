5a9c3c99ba3722bb223f8c2147953fa107bbefc1
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-08-12 12:14:13 +0300

    renaming

--- ek-tm4c1294xl/project1/self/flash/flash1.h
+++ ek-tm4c1294xl/project1/self/flash/flash1.h
@@ -4,8 +4,8 @@ FLASH1,H
 
 ------------------------------------------------------------------------------*/
 
-void    InitFlash2(void);
+void    InitFlash1(void);
 
-bool    PageErase2(uint const  wPageOut);
-bool    PageRead2(uint const  wPageIn);
-bool    PageWrite2(uint const  wPageOut);
+bool    PageErase1(uint const  wPageOut);
+bool    PageRead1(uint const  wPageIn);
+bool    PageWrite1(uint const  wPageOut);
