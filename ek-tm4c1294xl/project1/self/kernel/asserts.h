6e14b7723458df54a038f11647b9046ccb350ad4
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-08-13 14:16:14 +0300

    final check

--- ek-tm4c1294xl/project1/self/kernel/asserts.h
+++ ek-tm4c1294xl/project1/self/kernel/asserts.h
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------------
-ASSERTS.H
+ASSERTS,H
 
 
 ------------------------------------------------------------------------------*/
@@ -11,20 +11,23 @@ ASSERTS.H
 #include "../main.h"
 
 
+#define ASSERTS_SIZE    16
+#define FILE_NAME_SIZE  128
+
 
 typedef struct
 {
   uint          iwEvent;
   time          tiEvent;
   ulong         dwLine;
-  char          szFileName[128];
+  char          szFileName[FILE_NAME_SIZE];
 } assert;
 
 
 #endif
 
 
-extern  assert                  mpAsserts[16];
+extern  assert                  mpAsserts[ASSERTS_SIZE];
 
 
 
