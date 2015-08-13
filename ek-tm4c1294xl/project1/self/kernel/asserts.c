3e79717405fa92087302c59b85fcfaded3e78736
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-08-13 13:28:55 +0300

    fix

--- ek-tm4c1294xl/project1/self/kernel/asserts.c
+++ ek-tm4c1294xl/project1/self/kernel/asserts.c
@@ -12,20 +12,26 @@ ASSERTS.C
 
 
 
+ulong                   dwAsserts;
 assert                  mpAsserts[16];
 
+cache const             chAssertsIdx = {ASSERTS_IDX, &dwAsserts, sizeof(ulong)};
 cache const             chAsserts = {ASSERTS, &mpAsserts, sizeof(mpAsserts)};
 
 
 
 void    InitAsserts(void)
 {
+  LoadCache(&chAssertsIdx);
   LoadCache(&chAsserts);
 }
 
 
 void    ResetAsserts(void)
 {
+	dwAsserts = 0;
+  SaveCache(&chAssertsIdx);
+
   memset(&mpAsserts, 0, sizeof(mpAsserts));
   SaveCache(&chAsserts);
 }
@@ -34,7 +40,10 @@ void    ResetAsserts(void)
 
 void    AddAssert(char  *pcFileName, ulong  dwLine)
 {
-  uchar i = 0;
+  uchar i = dwAsserts % 16;
+
+  dwAsserts++;
+  SaveCache(&chAssertsIdx);
 
   mpAsserts[i].iwEvent = i;
   mpAsserts[i].tiEvent = *GetCurrTimeDate();
@@ -52,6 +61,8 @@ void    OutAsserts(void)
 {
   InitPushCRC();
 
+  PushLong(dwAsserts);
+
   uchar i;
   for (i=0; i<16; i++)
   {
@@ -61,5 +72,5 @@ void    OutAsserts(void)
     Push(mpAsserts[i].szFileName, 128);
   }
 
-  Output(sizeof(mpAsserts));
+  Output(4+sizeof(mpAsserts));
 }
