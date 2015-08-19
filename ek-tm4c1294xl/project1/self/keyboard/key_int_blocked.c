a3ea7c47d23edf52e3f17dd68d2eb3e00143463d
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-08-19 10:25:20 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/keyboard/key_int_blocked.c
@@ -0,0 +1,86 @@
+/*------------------------------------------------------------------------------
+KEY_CHAR_BLOCKED,C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+#include "../console.h"
+#include "key_char_blocked.h"
+
+
+
+//                                            0123456789ABCDEF
+static char const       szMaskChar[]       = "      ___       ",
+                        szBlocked[]        = "Запрещено:      ";
+
+extern  char                    szCharLimits[bDISPLAY + bMARGIN];
+
+
+
+void    key_SetIntBlocked(cache const  *pch, char const  *pszSlide[], uint  bMin, uint  bMax, bool  fBlocked, uint  wProgram)
+{
+  uchar *pbVal = (uchar *) pch->pbBuff;
+
+  if (bKey == bKEY_ENTER)
+  {
+    if (enKeyboard == KBD_ENTER)
+    {
+      if (fBlocked == true)
+      {
+        enKeyboard = KBD_POSTENTER;
+        Clear();
+
+        strcpy(szCharLimits, szClear);
+        sprintf(szCharLimits+4, "%3u..%-3u", bMin, bMax);
+
+        LoadSlide(pszSlide);
+        ShowChar(*pbVal);
+      }
+      else
+      {
+        ShowHi(szBlocked);
+        Clear();
+        sprintf(szLo+1,"программой %u",wProgram);
+      }
+    } 
+    else if (enKeyboard == KBD_POSTINPUT1)
+    {
+      enKeyboard = KBD_POSTENTER;
+
+      uchar b = GetCharLo(6,8);
+      if ((b >= bMin) && (b <= bMax))
+      {
+        *pbVal = b;
+        SaveCache(pch);
+
+        ShowChar(*pbVal);
+      }
+      else
+      {
+        enKeyboard = KBD_INPUT1;
+        LongBeep();
+
+        ShowLo(szMaskChar);
+      }
+    }
+    else Beep();
+  }
+
+
+  else if (bKey < 10)
+  {
+    if ((enGlobal != GLB_WORK) && (enKeyboard == KBD_POSTENTER))
+    {
+      enKeyboard = KBD_INPUT1;
+      ShowLo(szMaskChar);
+    }
+
+    if ((enKeyboard == KBD_INPUT1) || (enKeyboard == KBD_POSTINPUT1))
+    {
+      enKeyboard = KBD_POSTINPUT1;
+      ShiftLo(6,8);
+    }
+  }
+  else Beep();
+}
--- ek-tm4c1294xl/project1/self/keyboard/sensors/key_plc_u_flag.c
+++ ek-tm4c1294xl/project1/self/keyboard/sensors/key_plc_u_flag.c
@@ -4,18 +4,17 @@ KEY_PLC_U_FLAG,C
 
 ------------------------------------------------------------------------------*/
 
-#include "../../../main.h"
-#include "../../../devices/devices_init.h"
-#include "../../key_bool.h"
+#include "../../main.h"
+#include "../../devices/devices_init.h"
+#include "../key_bool.h"
 
 
 
 //                                          0123456789ABCDEF
 static char const       szMessage1[]     = " Интерфейс PLC  ",
-                        szMessage2[]     = " для счетчиков  ",
-                        szMessage3[]     = "    CE301 ?     ";
+                        szMessage2[]     = "  для CE301 ?   ";
 
-static char const       *pszMessages[] = { szMessage1, szMessage2, szMessage3, "" };
+static char const       *pszMessages[] = { szMessage1, szMessage2, "" };
 
 
 
