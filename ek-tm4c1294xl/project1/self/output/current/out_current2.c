9dfd4e5cfcc14d4fc1a0c38b71721aca17f4498b
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-08-22 13:57:15 +0300

    final check

--- ek-tm4c1294xl/project1/self/output/current/out_current2.c
+++ ek-tm4c1294xl/project1/self/output/current/out_current2.c
@@ -4,40 +4,41 @@ OUT_CURRENT2,C
 
 ------------------------------------------------------------------------------*/
 
-#include "../main.h"
-#include "../memory/mem_factors.h"
-#include "../memory/mem_current.h"
-#include "../memory/mem_extended_1.h"
-#include "../memory/mem_ports.h"
-#include "../digitals/sensors.h"
-#include "../serial/ports.h"
+#include "../../main.h"
+#include "../../memory/mem_factors.h"
+#include "../../memory/mem_current.h"
+#include "../../memory/mem_realtime.h"
+#include "../../digitals/sensors.h"
+#include "../../digitals/digitals.h"
+#include "../../serial/ports.h"
+#include "../../energy.h"
 #include "out_current2.h"
 
 
 
 void    OutEng3MinDataExt(void)
 {
-uchar   i;
-uint    wSize;
-
   InitPushPtr();
+  uint wSize = 0;
+
+  uchar i;
   for (i=0; i<2+bCANALS/8; i++) PushChar(InBuff(4+i));
 
-  wSize = 0;
-
-  for (i=0; i<bCANALS; i++)
+  uchar c;
+  for (c=0; c<bCANALS; c++)
   {
-    if ((InBuff(6 + i/8) & (0x80 >> i%8)) != 0)
+    if ((InBuff(6 + c/8) & (0x80 >> c%8)) != 0)
     {
-      if (GetDigitalDevice(i) == 0)
-        reBuffA = *PGetCounterOld(i);
-      else if (mpboEnblCan[i] == boFalse)
-        reBuffA = 0;
-      else
-        reBuffA = *PGetCanReal(mpreValueCntHou,i) * mpdwBase[i];
+      float fl;
 
-      PushReal();
-      wSize += sizeof(real);
+      if (GetDigitalDevice(c) == 0)
+        fl = GetCntCurrImp(c);
+      else if (mpboEnblCan[c] == false)
+        fl = 0;
+      else
+        fl = mpdbValueCntHou[c] * mpdwBase[c];
+
+      wSize += PushFloat(fl);
     }
   }
 
@@ -47,27 +48,27 @@ uint    wSize;
 
 void    OutEng3MinTimeExt(void)
 {
-uchar   i;
-uint    wSize;
 
   InitPushPtr();
+  uint wSize = 0;
+
+  uchar i;
   for (i=0; i<2+bCANALS/8; i++) PushChar(InBuff(4+i));
 
-  wSize = 0;
-
-  for (i=0; i<bCANALS; i++)
+  uchar c;
+  for (c=0; c<bCANALS; c++)
   {
-    if ((InBuff(6 + i/8) & (0x80 >> i%8)) != 0)
+    if ((InBuff(6 + c/8) & (0x80 >> c%8)) != 0)
     {
-      if (GetDigitalDevice(i) == 0)
+      if (GetDigitalDevice(c) == 0)
       {
         Push(&tiCurr, sizeof(time));
         Push(&tiCurr, sizeof(time));
       }
       else
       {
-        Push(&mptiBase[i], sizeof(time));
-        Push(&mptiOffs[i], sizeof(time));
+        Push(&mptiBase[c], sizeof(time));
+        Push(&mptiOffs[c], sizeof(time));
       }
 
       wSize += 2*sizeof(time);
