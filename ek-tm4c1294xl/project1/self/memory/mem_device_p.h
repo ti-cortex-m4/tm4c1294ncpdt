b5ec2abcef6c95fdeff2cff790967cc98a8cbccc
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-08-27 13:33:07 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/memory/mem_device_p.h
@@ -0,0 +1,21 @@
+/*------------------------------------------------------------------------------
+MEM_DEVICE_P.H
+
+
+------------------------------------------------------------------------------*/
+
+extern  uchar                   mpbPasswordEls[8], mpbRandomEls[16], mpbResultEls[16];
+
+extern  uchar                   mpbEls1[8], mpbEls2[16];
+
+extern  uchar                   mpbBuffEls[128];
+
+extern  uchar                   pbPutEls, pbGetEls, cbSizeEls;
+
+extern  uchar                   bFlagElsCurr, bFlagElsPrev;
+
+extern  uchar                   bCodeElsCurr, bCodeElsPrev;
+
+extern  ulong                   dwStampElsCurr, dwStampElsPrev;
+
+extern  uchar                   bBreakEls, bFirstEls;
