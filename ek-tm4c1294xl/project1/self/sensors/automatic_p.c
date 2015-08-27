f8fb22994664fdcb610631f2bcb0a37ef16cd4f8
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-08-27 13:28:01 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/automatic_p.c
+++ ek-tm4c1294xl/project1/self/sensors/automatic_p.c
@@ -5,6 +5,17 @@ AUTOMATIC_P.C
 ------------------------------------------------------------------------------*/
 
 #include "../main.h"
+#include "../memory/mem_digitals.h"
+#include "../memory/mem_factors.h"
+#include "../display/display.h"
+#include "../keyboard/keyboard.h"
+#include "../time/delay.h"
+#include "../time/timedate.h"
+#include "../serial/ports.h"
+#include "../serial/ports_devices.h"
+#include "../devices/devices.h"
+#include "../digitals/digitals.h"
+//#include "automatic1.h"
 #include "device_p.h"
 #include "automatic_p.h"
 
