f07a303ace18f0f40bc441c1ee12d5fb7f6b90cf
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-08-29 10:51:46 +0300

    fix

--- ek-tm4c1294xl/project1/self/keyboard/devices/key_ext_6_flag.c
+++ ek-tm4c1294xl/project1/self/keyboard/devices/key_ext_6_flag.c
@@ -14,10 +14,9 @@ KEY_EXT_6_FLAG,C
 static char const       szMessage1[]     = "  Прямой опрос  ",
                         szMessage2[]     = "знач. счетчиков ",
                         szMessage3[]     = "на начало суток ",
-                        szMessage4[]     = " ABB ЕвроАльфа, ",
-                        szMessage5[]     = " Elster A1140 ? ";
+                        szMessage4[]     = " ABB ЕвроАльфа  ";
 
-static char const       *pszMessages[]   = { szMessage1, szMessage2, szMessage3, szMessage4, szMessage5, "" };
+static char const       *pszMessages[]   = { szMessage1, szMessage2, szMessage3, szMessage4, "" };
 
 
 
