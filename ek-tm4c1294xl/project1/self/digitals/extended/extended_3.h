490921aca9116cece9ae2e35a31be5e4046008df
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-09-03 11:19:22 +0300

    add extended_3

---
+++ ek-tm4c1294xl/project1/self/digitals/extended/extended_3.h
@@ -0,0 +1,17 @@
+/*------------------------------------------------------------------------------
+EXTENDED_3.H
+
+
+------------------------------------------------------------------------------*/
+
+void    ShowEventsA(uchar  ibEvent);
+void    ShowEventsB(uchar  ibEvent);
+
+void    ResetExtended3(void);
+void    ResetExtended3_Full(void);
+
+void    MakeExtended3(void);
+
+void    OutExtended30(void);
+void    OutExtended31(void);
+
