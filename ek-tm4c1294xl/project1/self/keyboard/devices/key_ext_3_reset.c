ab4f7511dbfedc89b09c08f545706da2e142b707
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-09-04 09:48:24 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/keyboard/devices/key_ext_3_reset.c
@@ -0,0 +1,58 @@
+/*------------------------------------------------------------------------------
+KEY_EXT_3_RESET.C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../console.h"
+#include "../../digitals/extended/extended_3.h"
+
+
+
+//                                         0123456789ABCDEF
+static char const       szResetWDT[]    = "—брос по WDT ?  ";
+
+
+
+void    key_SetExt3Reset(void)
+{
+  if (bKey == bKEY_ENTER)
+  {
+    if (enKeyboard == KBD_ENTER)  
+    {
+      enKeyboard = KBD_INPUT1;
+
+      ShowHi(szResetWDT);
+
+      Clear();
+      ShowAnswer();
+    } 
+    else if (enKeyboard == KBD_POSTINPUT1)
+    {
+      ResetExtended3_Full();
+      OK();
+    }
+    else Beep();
+  }
+  
+
+  else if (bKey == bKEY_POINT)
+  {
+    if (enGlobal != GLB_WORK)
+    {
+      if ((enKeyboard == KBD_INPUT1) || (enKeyboard == KBD_POSTINPUT1))
+      {           
+        if (enKeyboard == KBD_INPUT1)
+          enKeyboard = KBD_POSTINPUT1;
+        else
+          enKeyboard = KBD_INPUT1;
+
+        ShowAnswer();
+      }
+      else Beep(); 
+    }
+    else Beep(); 
+  } 
+  else Beep(); 
+}
