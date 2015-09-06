f2473fa42f7d27495e1989f1da915daf547fd70b
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-09-06 10:41:20 +0300

    fix

--- ek-tm4c1294xl/project1/self/output/out_minute30.h
+++ ek-tm4c1294xl/project1/self/output/out_minute30.h
@@ -4,4 +4,6 @@ OUT_MINUTE30,H
 
 ------------------------------------------------------------------------------*/
 
+void    OutPowHouGrp(bool  fAllGroups, uint  iwHou, uchar  bMul);
+
 void    OutImpCanHouExt(void);
