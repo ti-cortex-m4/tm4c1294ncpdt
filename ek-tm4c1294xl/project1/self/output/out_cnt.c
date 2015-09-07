6e41570f95266849feacb8758b90f15fbddf0e70
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-09-07 13:29:18 +0300

    fix

--- ek-tm4c1294xl/project1/self/output/out_cnt.c
+++ ek-tm4c1294xl/project1/self/output/out_cnt.c
@@ -5,90 +5,89 @@ OUT_CNT.C
 ------------------------------------------------------------------------------*/
 
 #include "../main.h"
+#include "../memory/mem_settings.h"
 #include "../serial/ports.h"
+#include "../digitals/digitals.h"
+#include "../energy.h"
 #include "out_cnt.h"
 
 
 
 void    OutCntCanOld(void)
 {
-        if (enGlobal != GLB_PROGRAM)
-        {
-          if (bInBuff5 < bCANALS)
-          {
-            InitPushPtr();
-
-            PGetCounterOld( bInBuff5 );
-            PushReal();
-
-            OutptrOutBuff(sizeof(real));
-          }
-          else Result(bRES_BADADDRESS);
-        }
-        else Result(bRES_NEEDWORK);
+  if (enGlobal != GLB_PROGRAM)
+  {
+    if (bInBuff5 < bCANALS)
+    {
+      InitPushPtr();
+      OutptrOutBuff(PushFloat(GetCntCurrImp(bInBuff5)));
+    }
+    else Result(bRES_BADADDRESS);
+  }
+  else Result(bRES_NEEDWORK);
 }
 
 
 void    OutCntCanNew(void)
 {
-        if (enGlobal != GLB_PROGRAM)
-        {
-          if (bInBuff5 < bCANALS)
-          {
-            InitPushPtr();
-
-            PGetCounterOld( bInBuff5 );
-            PushReal();
-
-            OutptrOutBuff(sizeof(real));
-          }
-          else Result(bRES_BADADDRESS);
-        }
-        else Result(bRES_NEEDWORK);
+  if (enGlobal != GLB_PROGRAM)
+  {
+    if (bInBuff5 < bCANALS)
+    {
+      InitPushPtr();
+      OutptrOutBuff(PushFloat(GetCntCurrImp(bInBuff5)));
+    }
+    else Result(bRES_BADADDRESS);
+  }
+  else Result(bRES_NEEDWORK);
 }
 
 
 
 void    OutCntCanOldAll(void)
 {
-        if (enGlobal != GLB_PROGRAM)
-        {
-          InitPushPtr();
-          for (i=0; i<bCANALS; i++)
-          {
-            PGetCounterOld(i);
-            PushReal();
-          }
-          OutptrOutBuff(sizeof(real)*bCANALS);
-        }
-        else Result(bRES_NEEDWORK);
+  if (enGlobal != GLB_PROGRAM)
+  {
+    InitPushPtr();
+    uint wSize = 0;
+
+    uchar c;
+    for (c=0; c<bCANALS; c++)
+    {
+      wSize += PushFloat(GetCntCurrImp(c));
+    }
+
+    OutptrOutBuff(wSize);
+  }
+  else Result(bRES_NEEDWORK);
 }
 
 
 void    OutCntCanNewAll(void)
 {
-        if (enGlobal != GLB_PROGRAM)
-        {
-          InitPushPtr();
-          for (i=0; i<bCANALS; i++)
-          {
-            if (GetDigitalDevice(i) == 0)
-              reBuffA = *PGetCounterOld(i);
-            else
-            if ((GetDigitalDevice(i) == 5) ||
-                (GetDigitalDevice(i) == 6))
-              reBuffA = *PGetCanReal(mpreEsc_S,i);
-            else
-            {
-              if (boTrue == boLoadMnt)
-                reBuffA = *PGetCanReal(mpreValueCntHou,i) * mpdwBase[i];
-              else
-                reBuffA = *PGetCanReal(mpreEsc_S,i);
-            }
+  if (enGlobal != GLB_PROGRAM)
+  {
+    InitPushPtr();
 
-            PushReal();
-          }
-          OutptrOutBuff(sizeof(real)*bCANALS);
-        }
-        else Result(bRES_NEEDWORK);
+    uchar c;
+    for (c=0; c<bCANALS; c++)
+    {
+      if (GetDigitalDevice(c) == 0)
+        reBuffA = *GetCntCurrImp(c);
+      else
+      if ((GetDigitalDevice(c) == 5) || (GetDigitalDevice(c) == 6))
+        reBuffA = *PGetCanReal(mpreEsc_S,c);
+      else
+      {
+        if (boLoadMnt == true)
+          reBuffA = *PGetCanReal(mpreValueCntHou,c) * mpdwBase[c];
+        else
+          reBuffA = *PGetCanReal(mpreEsc_S,c);
+      }
+
+      PushFloat();
+    }
+    OutptrOutBuff(sizeof(real)*bCANALS);
+  }
+  else Result(bRES_NEEDWORK);
 }
