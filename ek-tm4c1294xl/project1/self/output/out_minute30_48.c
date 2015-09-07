eceeba8d7f65f93346d9f124c787bf5dce7411a2
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-09-07 17:05:55 +0300

    fix

--- ek-tm4c1294xl/project1/self/output/out_minute30_48.c
+++ ek-tm4c1294xl/project1/self/output/out_minute30_48.c
@@ -31,19 +31,13 @@ void    OutPowGrpHou48(void)
   for (h=0; h<48; h++)
   {
     if (LoadImpHou(iwHou) == false) { Result(bRES_BADFLASH); return; }
-    else
-    {
-      if ((bInBuff6 == 0) && (h > GetCurrHouIndex()))
-      {
-        PushFloat(0);
-      }
-      else
-      {
-        PushFloat(GetGrpHouInt2Real(mpwImpHouCan[ PrevSoftHou() ], bInBuff5, 2));
-      }
 
-      wSize += sizeof(float);
-    }
+    if ((bInBuff6 == 0) && (h > GetCurrHouIndex()))
+      PushFloat(0);
+    else
+      PushFloat(GetGrpHouInt2Real(mpwImpHouCan[ PrevSoftHou() ], bInBuff5, 2));
+
+    wSize += sizeof(float);
 
     if (++iwHou >= wHOURS) iwHou = 0;
   }
@@ -63,15 +57,13 @@ void    OutImpCanHou48(void)
   for (h=0; h<48; h++)
   {
     if (LoadImpHou(iwHou) == false) { Result(bRES_BADFLASH); return; }
-    else
-    {
-      if ((bInBuff6 == 0) && (h > GetCurrHouIndex()))
-        PushInt(0);
-      else
-        PushInt( mpwImpHouCan[ PrevSoftHou() ][ bInBuff5 ] );
 
-      wSize += sizeof(uint);
-    }
+    if ((bInBuff6 == 0) && (h > GetCurrHouIndex()))
+      PushInt(0);
+    else
+      PushInt( mpwImpHouCan[ PrevSoftHou() ][ bInBuff5 ] );
+
+    wSize += sizeof(uint);
 
     if (++iwHou >= wHOURS) iwHou = 0;
   }
@@ -94,23 +86,21 @@ float   re;
   for (h=0; h<48; h++)
   {
     if (LoadImpHou(iwHou) == false) { Result(bRES_BADFLASH); return; }
-    else
+
+    uchar g;
+    for (g=0; g<bGROUPS; g++)
     {
-      uchar g;
-      for (g=0; g<bGROUPS; g++)
+      if ((InBuff(7 + g/8) & (0x80 >> g%8)) != 0)
       {
-        if ((InBuff(7 + g/8) & (0x80 >> g%8)) != 0)
-        {
-          if ((bInBuff6 == 0) && (h > GetCurrHouIndex()))
-            re = 0;
-          else
-            re = GetGrpHouInt2Real(mpwImpHouCan[ PrevSoftHou() ], g, 2);
+        if ((bInBuff6 == 0) && (h > GetCurrHouIndex()))
+          re = 0;
+        else
+          re = GetGrpHouInt2Real(mpwImpHouCan[ PrevSoftHou() ], g, 2);
 
-          PushFloat(re);
+        PushFloat(re);
 
-          wSize += sizeof(float);
-          if (wSize >= (wOUTBUFF_SIZE-0x40)) { Result(bRES_OUTOVERFLOW); return; }
-        }
+        wSize += sizeof(float);
+        if (wSize >= (wOUTBUFF_SIZE-0x40)) { Result(bRES_OUTOVERFLOW); return; }
       }
     }
 
@@ -139,21 +129,19 @@ void    OutImpCanHou48Ext(void)
   for (h=0; h<48; h++)
   {
     if (LoadImpHouFree(iwHou) == false) { Result(bRES_BADFLASH); return; }
-    else
-    {
-      uchar c;
-      for (c=0; c<bCANALS; c++)
-      {
-        if ((InBuff(7 + c/8) & (0x80 >> c%8)) != 0)
-        {
-          if ((bInBuff6 == 0) && (h > GetCurrHouIndex()))
-            PushInt(0);
-          else
-            PushImpHouCan( mpwImpHouCan[ PrevSoftHou() ][ c ], c );
 
-          wSize += sizeof(uint);
-          if (wSize >= (wOUTBUFF_SIZE-0x40)) { Result(bRES_OUTOVERFLOW); return; }
-        }
+    uchar c;
+    for (c=0; c<bCANALS; c++)
+    {
+      if ((InBuff(7 + c/8) & (0x80 >> c%8)) != 0)
+      {
+        if ((bInBuff6 == 0) && (h > GetCurrHouIndex()))
+          PushInt(0);
+        else
+          PushImpHouCan( mpwImpHouCan[ PrevSoftHou() ][ c ], c );
+
+        wSize += sizeof(uint);
+        if (wSize >= (wOUTBUFF_SIZE-0x40)) { Result(bRES_OUTOVERFLOW); return; }
       }
     }
 
