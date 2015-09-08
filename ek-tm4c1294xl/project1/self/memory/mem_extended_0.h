900136748bef2a36066c84217b348cdd9625f721
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-09-08 13:15:57 +0300

    fix

--- ek-tm4c1294xl/project1/self/memory/mem_extended_0.h
+++ ek-tm4c1294xl/project1/self/memory/mem_extended_0.h
@@ -6,7 +6,7 @@ MEM_EXTENDED_0,H
 
 extern  bool                    boExt0Flag;
 
-extern  uchar                   bExt0Counter;
+extern  uchar                   bExt0Limit, bExt0Counter;
 
 extern  ulong                   mpdwExt0RepeatsPH[bPORTS][48];
 
