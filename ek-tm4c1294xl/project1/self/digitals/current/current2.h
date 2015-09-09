c465d3f22dfd149d753b1c888d9ae603c18cab89
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-09-09 11:18:34 +0300

    fix

--- ek-tm4c1294xl/project1/self/digitals/current/current2.h
+++ ek-tm4c1294xl/project1/self/digitals/current/current2.h
@@ -4,9 +4,10 @@ CURRENT2.H
 
 ------------------------------------------------------------------------------*/
 
-void    ResetCurrent2(void);
-void    NextMntCurrent2(void);
 void    InitCurrent2(void);
+void    ResetCurrent2(void);
+
+void    NextMntCurrent2(void);
 void    StartCurrent2(void);
 void    MakeCurrent2(void);
 void    StopCurrent2(void);
