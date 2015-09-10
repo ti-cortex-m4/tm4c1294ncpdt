c5a509ba04c80132bb167a4baf4bb70340d7ab2b
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-09-10 14:22:38 +0300

    add new files

---
+++ ek-tm4c1294xl/project1/self/keyboard/serial/key_input_mode_control.c
@@ -0,0 +1,86 @@
+/*------------------------------------------------------------------------------
+KEY_INPUT_MODE_CONTROL.C
+
+
+------------------------------------------------------------------------------*/
+/*
+#include        "main.h"
+#include        "xdata.h"
+#include        "display.h"
+#include        "beep.h"
+#include        "timer0.h"
+#include        "keyboard.h"
+
+
+
+//                                         0123456789ABCDEF
+message         code    szIOControl1    = " Автоматическое ",
+                        szIOControl2    = "  переключение  ",
+                        szIOControl3    = "на прием ведомых",
+                        szIOControl4    = "   портов 3,4   ";
+                        
+uchar           *code   pszIOControl[]  = { szIOControl1, szIOControl2, szIOControl3, szIOControl4, "" };
+
+
+
+void    key_SetInputModeControl(void)
+{
+  Clear();
+  switch (bIOControl)
+  {
+    case 0:  strcpy(szLo+1,"нет");      break;
+    case 1:  strcpy(szLo+1,"30 минут"); break;
+    case 6:  strcpy(szLo+1,"3 часа");   break;
+    case 12: strcpy(szLo+1,"6 часов");  break;
+    case 24: strcpy(szLo+1,"12 часов"); break;
+    case 48: strcpy(szLo+1,"24 часа");  break;
+    default: strcpy(szLo+1,"?");        break;
+  }
+
+  if (enGlobal != GLB_WORK)
+    szLo[0] = '.';
+}
+
+
+void    key_SetIOControl(void)
+{
+  if (bKey == bKEY_ENTER)
+  {
+    if (enKeyboard == KBD_ENTER)  
+    {
+      enKeyboard = KBD_POSTENTER;
+      Clear();
+      
+      LoadSlide(pszIOControl);    
+      ShowIOControl();  
+    } 
+    else Beep();
+  }
+  
+
+  else if (bKey == bKEY_POINT)
+  {
+    if (enGlobal != GLB_WORK)
+    {
+      if (enKeyboard == KBD_POSTENTER)
+      {           
+        switch (bIOControl)
+        {
+          case 0:  bIOControl = 1;  break;
+          case 1:  bIOControl = 6;  break;
+          case 6:  bIOControl = 12; break;
+          case 12: bIOControl = 24; break;
+          case 24: bIOControl = 48; break;
+          case 48: bIOControl = 0;  break;
+          default: bIOControl = 0;  break;
+        }
+        
+        ShowIOControl();
+      }
+      else Beep(); 
+    }
+    else Beep(); 
+  } 
+  else Beep(); 
+}
+*/
---
+++ ek-tm4c1294xl/project1/self/serial/input_mode_control.c
@@ -0,0 +1,22 @@
+/*------------------------------------------------------------------------------
+INPUT_MODE_CONTROL.C
+
+
+------------------------------------------------------------------------------*/
+
+#include    "../main.h"
+
+
+/*
+void    InputModeControl(void)
+{
+  if ((fActive == 0) || (bIOControl == 0)) return;
+  if (GetHouIndex() % bIOControl != 0) return;
+
+  tiIOControl = tiCurr;
+  dwIOControl++;
+
+  if (IsSlave(2)) InputMode2();
+  if (IsSlave(3)) InputMode3();
+}
+*/
