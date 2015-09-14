185a29b28e477e2313eae4cb9a56606c41e30307
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-09-14 13:53:34 +0300

    formatted

--- ek-tm4c1294xl/project1/self/memory/mem_ports.h
+++ ek-tm4c1294xl/project1/self/memory/mem_ports.h
@@ -4,74 +4,74 @@ MEM_PORTS.H
 
 ------------------------------------------------------------------------------*/
 
-extern  port                mppoPorts[bPORTS];
+extern  port                    mppoPorts[bPORTS];
 
-extern  uint                mpwInDelay[bPORTS];
+extern  uint                    mpwInDelay[bPORTS];
 
-extern  uint                mpwMinorInDelay[bPORTS], mpwMajorInDelay[bPORTS];
+extern  uint                    mpwMinorInDelay[bPORTS], mpwMajorInDelay[bPORTS];
 
-extern  bool                mpboLocal[bPORTS];
+extern  bool                    mpboLocal[bPORTS];
 
-extern  bool                mpboLocalDisable[bPORTS];
+extern  bool                    mpboLocalDisable[bPORTS];
 
-extern  uint                mpwOutputDelay[bPORTS];
+extern  uint                    mpwOutputDelay[bPORTS];
 
-extern  uchar               mpbCorrectLimit[bPORTS];
+extern  uchar                   mpbCorrectLimit[bPORTS];
 
-extern  uchar               bInBuff0,
-                            bInBuff1,
-                            bInBuff2,
-                            bInBuff3,
-                            bInBuff4,
-                            bInBuff5,
-                            bInBuff6,
-                            bInBuff7,
-                            bInBuff8,
-                            bInBuff9,
-                            bInBuffA,
-                            bInBuffB,
-                            bInBuffC,
-                            bInBuffD,
-                            bInBuffE,
-                            bInBuffF;
+extern  uchar                   bInBuff0,
+                                bInBuff1,
+                                bInBuff2,
+                                bInBuff3,
+                                bInBuff4,
+                                bInBuff5,
+                                bInBuff6,
+                                bInBuff7,
+                                bInBuff8,
+                                bInBuff9,
+                                bInBuffA,
+                                bInBuffB,
+                                bInBuffC,
+                                bInBuffD,
+                                bInBuffE,
+                                bInBuffF;
 
-extern	uchar               bQuery;
+extern	uchar                   bQuery;
 
-extern	uchar               ibPortActive;
-
-
-
-extern  uint                iwPopSave;
-
-extern  uint                iwInBuffSave;
-
-extern  uint                cwInBuffSave;
-
-extern  uchar               mpbInBuffSave[wINBUFF_SIZE];
+extern	uchar                   ibPortActive;
 
 
 
-extern  uchar               cbHeaderBcc;
+extern  uint                    iwPopSave;
 
-extern  uint                cwInBuffBcc;
+extern  uint                    iwInBuffSave;
 
+extern  uint                    cwInBuffSave;
 
-
-extern  uchar               bInputModeReset;
-
-extern  time                tiInputModeReset;
-
-extern  ulong               dwInputModeReset;
+extern  uchar                   mpbInBuffSave[wINBUFF_SIZE];
 
 
 
-extern  bool                fSlaveModem;
+extern  uchar                   cbHeaderBcc;
 
-extern  bool                fSendAT;
+extern  uint                    cwInBuffBcc;
 
-extern  uchar               mpcbSendAT[bPORTS];
 
-extern  send_at             mpstSendAT[bPORTS];
+
+extern  uchar                   bInputModeReset;
+
+extern  time                    tiInputModeReset;
+
+extern  ulong                   dwInputModeReset;
+
+
+
+extern  bool                    fSlaveModem;
+
+extern  bool                    fSendAT;
+
+extern  uchar                   mpcbSendAT[bPORTS];
+
+extern  send_at                 mpstSendAT[bPORTS];
 
 
 
