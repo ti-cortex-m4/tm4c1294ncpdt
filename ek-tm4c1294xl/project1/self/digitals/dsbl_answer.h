7e9d12c3033ba762ca86368303bb04822f3e292a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-09-16 16:41:33 +0300

    fix

--- ek-tm4c1294xl/project1/self/digitals/dsbl_answer.h
+++ ek-tm4c1294xl/project1/self/digitals/dsbl_answer.h
@@ -4,9 +4,15 @@ DSBL_ANSWER.H
 
 ------------------------------------------------------------------------------*/
 
+extern  cache const             chDsblAnswer;
+
+
+
 void    InitDisableAnswer(void);
 void    ResetDisableAnswer(void);
 
 void    EnableAnswer(void);
 void    DisableAnswer(void);
 bool    AnswerDisabled(void);
+
+void    OutDsblAnswer(void);
