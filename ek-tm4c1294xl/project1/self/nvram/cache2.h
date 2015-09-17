5c47455342530f0a5b92bcfead7c2ad543e9c6bc
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-09-17 09:43:59 +0300

    update cache routines

--- ek-tm4c1294xl/project1/self/nvram/cache2.h
+++ ek-tm4c1294xl/project1/self/nvram/cache2.h
@@ -4,9 +4,11 @@ CACHE2.H
 
 ------------------------------------------------------------------------------*/
 
-void    SaveCacheBool(cache const  *pch, bool  fVal);
+void    SaveCacheBool(cache const  *pch, bool  f);
 void    LoadCacheBool(cache const  *pch, bool  fDef);
 
+void    SaveCacheChar(cache const  *pch, uchar  b);
 void    LoadCacheChar(cache const  *pch, uchar  bMin, uchar  bMax, uchar  bDef);
 
+void    SaveCacheInt(cache const  *pch, uint  w);
 void    LoadCacheInt(cache const  *pch, uint  wMin, uint  wMax, uint  wDef);
