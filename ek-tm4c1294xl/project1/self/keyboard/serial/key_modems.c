d945d59036b25e90a32e9d1166c8a5914e0ebbcf
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-09-17 10:09:51 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/keyboard/serial/key_modems.c
@@ -0,0 +1,64 @@
+/*------------------------------------------------------------------------------
+_MODEMS2.C
+
+
+------------------------------------------------------------------------------*/
+/*
+#include        "main.h"
+#include        "xdata.h"
+#include        "display.h"
+#include        "beep.h"
+#include        "keyboard.h"
+#include        "speed.h"
+#include        "ports.h"
+#include        "status.h"
+#include        "modems2.h"
+
+
+//                                         0123456789ABCDEF
+message         code    szModemsQuality = "Качество сигнала";
+
+
+void    key_GetModemsQuality(void)
+{
+  if (bKey == bKEY_ENTER)
+  {
+    if (enKeyboard == KBD_ENTER)
+    {
+      ShowHi(szModemsQuality);
+
+      enKeyboard = KBD_INPUT1;
+      Port();
+    }
+    else if (enKeyboard == KBD_POSTINPUT1)
+    {
+      ibX = GetChar(10,11) - 1;
+      if (ibX < bPORTS)
+      {
+        enKeyboard = KBD_SHOW;
+
+        ShowModemQuality(ibX);
+        SetCurr(DEV_BEGIN);
+      }
+      else Beep();
+    }
+    else if (enKeyboard == KBD_SHOW)
+    {   
+      ShowModemQuality(ibX);
+      SetCurr(DEV_BEGIN);
+    } 
+    else Beep();
+  }
+
+
+  else if (bKey < 10)
+  {        
+    if ((enKeyboard == KBD_INPUT1) || (enKeyboard == KBD_POSTINPUT1))
+    {
+      enKeyboard = KBD_POSTINPUT1;
+      ShiftLo(10,11);
+    }
+  }
+  else Beep();
+}
+*/
