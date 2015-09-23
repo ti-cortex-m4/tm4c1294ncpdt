483aa2f2d5d221fd6fcec40352b1b6f2d1531b23
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-09-23 12:49:07 +0300

    fix

--- ek-tm4c1294xl/project1/self/realtime/next_minute1.c
+++ ek-tm4c1294xl/project1/self/realtime/next_minute1.c
@@ -9,6 +9,7 @@ NEXT_MINUTE1.C
 #include "../memory/mem_settings.h"
 #include "../memory/mem_ports.h"
 #include "realtime.h"
+#include "throughput.h"
 #include "../keyboard/time/key_timedate.h"
 
 
@@ -17,7 +18,11 @@ void    NextMinute1(void)
 {
   Realtime1.cdwMinutes1++;
 
-  if (fActive == true) fSendAT = 1;
+  if ((fActive == true) && (enGlobal != GLB_PROGRAM))
+  {
+    fSendAT = 1;
+    EveryMinuteThroughput();
+  }
 
   if ((fActive == true) && (enGlobal == GLB_WORK))
   {
