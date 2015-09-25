7c5de44b89a7e762821074883878b9a6d3c92434
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-09-25 13:10:30 +0300

    fix

--- ek-tm4c1294xl/project1/self/keyboard/test/key_test_power_off.c
+++ ek-tm4c1294xl/project1/self/keyboard/test/key_test_power_off.c
@@ -1,24 +1,23 @@
 /*------------------------------------------------------------------------------
 KEY_TEST_POWER_OFF.H
 
-
+Тест датчика напряжения
 ------------------------------------------------------------------------------*/
 
+#include "inc/hw_gpio.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_sysctl.h"
+#include "inc/hw_types.h"
 #include "../../main.h"
-#include "../keyboard.h"
-#include "../../display/display.h"
+#include "../../console.h"
 #include "../../hardware/power_off.h"
-#include "../../health.h"
-#include       "inc/hw_gpio.h"
-#include       "inc/hw_memmap.h"
-#include       "inc/hw_sysctl.h"
-#include       "inc/hw_types.h"
+#include "key_test_power_off.h"
 
 
 
 //                                         0123456789ABCDEF
-static char const       szTestRTC1[]    = "Тест часов      ",
-                        szTestRTC2[]    = "метка    время  ";
+static char const       szMessageHi[]   = "Тест датчика    ",
+                        szMessageLo[]   = "напряжения      ";
 
 
 
@@ -30,31 +29,18 @@ void    key_TestPowerOff(void)
     {
       enKeyboard = KBD_POSTENTER;
 
-      ShowHi(szTestRTC1);
-      ShowLo(szTestRTC2);
+      ShowHi(szMessageHi);
+      ShowLo(szMessageLo);
     }
   }
 }
 
-
-/*
-static uchar Show(bool  f)
-{
-  switch (f)
-  {
-    case true:      return '+';
-    case false:     return '-';
-    case UNDEFINED: return '?';
-    default:        return ' ';
-  }
-}
-*/
 
 
 void    auto_TestPowerOff(void)
 {
   if (enKeyboard == KBD_POSTENTER)
   {
-    szLo[15] = IsPowerOff() ? '+' : '-';
+    szLo[13] = IsPowerOff() ? '-' : '+';
   }
 }
