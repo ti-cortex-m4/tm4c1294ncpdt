3434f9ec023452c400dd6faec78266610d2898ed
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-10-02 16:49:04 +0300

    fix

--- ek-tm4c1294xl/project1/self/devices/decompress_v.c
+++ ek-tm4c1294xl/project1/self/devices/decompress_v.c
@@ -21,12 +21,12 @@ DECOMPRESS_V!C
 
 #ifndef SKIP_V
 
-static uchar RepackV_0xDB(uchar  bCode)
+static uchar RepackV(uchar  bCode1, uchar  bCode2)
 {
 uchar   i;
 
   for (i=1; i<=IndexInBuff()-2; i++)
-    if ((GetInBuff(i) == 0xDB) && (GetInBuff(i+1) == bCode)) return i;
+    if ((GetInBuff(i) == bCode1) && (GetInBuff(i+1) == bCode2)) return i;
 
   return 0;
 }
@@ -39,10 +39,10 @@ uchar   i,j;
   if (mpSerial[ibPort] != SER_INPUT_MASTER) return;
 
   j = 0;
-  for (i=0; i<IndexInBuff(); i++) if (GetInBuff(i) == 0xC0) j++;
+  for (i=0; i<IndexInBuff(); i++) if (GetInBuff(i) == 0x55) j++;
   if (j != 2) return;
 
-  if ((GetInBuff(0) != 0xC0) || (GetInBuff(IndexInBuff()-1) != 0xC0))
+  if ((GetInBuff(0) != 0x73) || (GetInBuff(1) != 0x55) || (GetInBuff(IndexInBuff()-1) != 0x55))
     return;
 
   if ((GetInBuff(6) & 0xF0) != 0x50)
@@ -58,7 +58,7 @@ uchar   i,j;
   {
     f = 0;
 
-    j = RepackV_0xDB(0xDD);
+    j = RepackV(0xDB,0xDD);
     if (j != 0)
     {
       SetInBuff(j, 0xDB);
@@ -68,7 +68,7 @@ uchar   i,j;
       continue;
     }
 
-    j = RepackV_0xDB(0xDC);
+    j = RepackV(0xDB,0xDC);
     if (j != 0)
     {
       SetInBuff(j, 0xC0);
@@ -88,20 +88,21 @@ static uchar CheckV(void)
 {
 uint    i;
 
-  if (GetInBuff(0) != 0xC0) return 1;
-  if (GetInBuff(IndexInBuff()-1) != 0xC0) return 2;
+  if (GetInBuff(0) != 0x73) return 1;
+  if (GetInBuff(1) != 0x55) return 2;
+  if (GetInBuff(IndexInBuff()-1) != 0x55) return 3;
 
-  if (GetInBuff(1) != 0x48) return 3;
+  if (GetInBuff(1) != 0x48) return 4;
 
   i = mpdwAddress1[diCurr.bAddress-1] % 0x10000;
-  if (GetInBuff(4) != (i % 0x100)) return 4;
-  if (GetInBuff(5) != (i / 0x100)) return 5;
+  if (GetInBuff(4) != (i % 0x100)) return 5;
+  if (GetInBuff(5) != (i / 0x100)) return 6;
 
   i = wPrivate;
-  if (GetInBuff(2) != (i % 0x100)) return 6;
-  if (GetInBuff(3) != (i / 0x100)) return 7;
+  if (GetInBuff(2) != (i % 0x100)) return 7;
+  if (GetInBuff(3) != (i / 0x100)) return 8;
 
-  MakeCrcSInBuff(1, IndexInBuff()-2); if (bCrcS != 0) return 8;
+  if (MakeCrcVInBuff(2, IndexInBuff()-2) != 0) return 9;
 
   return 0;
 }
@@ -109,9 +110,7 @@ uint    i;
 
 uchar   ChecksumV(void)
 {
-uchar   i;
-
-  i = CheckV();
+  uchar i = CheckV();
   if (i != 0)
   {
     Clear(); sprintf(szLo+1,"код ошибки: %u",i);
