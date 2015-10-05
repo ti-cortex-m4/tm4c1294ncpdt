a9ad52f3009740556655381febbe105320f30bdd
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-10-05 12:57:42 +0300

    fix

--- ek-tm4c1294xl/project1/self/serial/monitor.h
+++ ek-tm4c1294xl/project1/self/serial/monitor.h
@@ -10,12 +10,21 @@ MONITOR.H
 
 void    InitMonitor(void);
 
+bool    UseMonitor(void);
+
 void    MonitorOpen(uchar  ibPrt);
 void    MonitorClose(void);
 
 void    DelayMonitor_1Hz(void);
 
-void    MonitorOut(uint  cwIn, uchar  cbOut);
+void    MonitorString(const char  *psz);
+void    MonitorCharDec(uchar  b);
+void    MonitorCharHex(uchar  b);
+void    MonitorIntDec(uint  w);
+void    MonitorLongDec(ulong  dw);
+void    MonitorTime(time  ti);
+
+void    MonitorOut(uint  cwIn, uint  cwOut);
 void    MonitorIn(void);
 
 uint    GetMonitorDelay(void);
