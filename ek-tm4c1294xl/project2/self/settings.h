92c1ab905422b92b33f1ed3db6b2105034dc857a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-10-29 15:18:01 +0300

    add settings

---
+++ ek-tm4c1294xl/project2/self/settings.h
@@ -0,0 +1,14 @@
+/*------------------------------------------------------------------------------
+SETTINGS.H
+
+
+------------------------------------------------------------------------------*/
+
+extern uint32_t dwIP;
+extern uint32_t dwGateway;
+extern uint32_t dwNetmask;
+
+
+
+uint32_t SaveSettings(void);
+uint32_t LoadSettings(void);
