3508e0d8a01a79833a3c742ef376e558776fad43
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-11-02 15:37:41 +0300

    add activity flag

--- ek-tm4c1294xl/project2/self/udp/udp_output.h
+++ ek-tm4c1294xl/project2/self/udp/udp_output.h
@@ -4,7 +4,13 @@ UDP_OUTPUT.H
 
 ------------------------------------------------------------------------------*/
 
+void Init_UDPOutput(void);
+bool IsActive_UDPOutput(void);
+
 err_t UDPOutputX(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port);
 
 err_t UDPOutput_GetLong(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, ulong dw);
 err_t UDPOutput_SetLong(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, ulong *pdw);
+
+err_t UDPOutputW(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port);
+err_t UDPOutputO(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port);
