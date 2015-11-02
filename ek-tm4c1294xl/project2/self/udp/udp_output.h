d862913214c6c1ed8631c2908a1bd57caa00f9a8
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-11-02 17:32:47 +0300

    fix

--- ek-tm4c1294xl/project2/self/udp/udp_output.h
+++ ek-tm4c1294xl/project2/self/udp/udp_output.h
@@ -7,7 +7,7 @@ UDP_OUTPUT.H
 void Init_UDPOutput(void);
 bool IsActive_UDPOutput(void);
 
-err_t UDPOutputM(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port);
+err_t UDPOutput_MAC(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port);
 
 err_t UDPOutput_GetLong(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, ulong dw);
 err_t UDPOutput_SetLong(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, ulong *pdw);
