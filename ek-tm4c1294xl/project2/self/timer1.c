09705ffacb7a74b5d74504956ca68e914d3435a2
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-11-09 10:52:29 +0300

    fix

--- ek-tm4c1294xl/project2/self/timer1.c
+++ ek-tm4c1294xl/project2/self/timer1.c
@@ -1,18 +1,14 @@
 /*------------------------------------------------------------------------------
-TIMER.C
+TIMER1.C
 
 ------------------------------------------------------------------------------*/
 
 #include "main.h"
 #include "uart/log.h"
-//#include "serial/serial4.h"
 #include "inc/hw_memmap.h"
-//#include "inc/hw_memmap.h"
-//#include "self/main.h"
 #include "inc/hw_ints.h"
 #include "inc/hw_types.h"
 #include "inc/hw_timer.h"
-//#include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/timer.h"
@@ -28,20 +24,17 @@ TIMER.C
 
 void    InitTimer1(ulong dwSysClockFreq)
 {
-  // Enable the peripherals.
   SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
 
-  // Configure the 32-bit periodic timer.
   TimerConfigure(TIMER1_BASE, TIMER_CFG_PERIODIC);
   TimerLoadSet(TIMER1_BASE, TIMER_A, dwSysClockFreq / TIMER1_FREQ);
 
-  // Setup the interrupts for the timer timeouts.
   IntEnable(INT_TIMER1A);
   TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
 
-  // Enable the timer.
   TimerEnable(TIMER1_BASE, TIMER_A);
 }
+
 
 
 void Timer1IntHandler(void)
@@ -49,5 +42,5 @@ void Timer1IntHandler(void)
   HWREG(TIMER1_BASE + TIMER_O_ICR) = TIMER_TIMA_TIMEOUT;
 
   LogTimer_1000Hz();
-  uart_timer();
+  Serial_1000Hz();
 }
