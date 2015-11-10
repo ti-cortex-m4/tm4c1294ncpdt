62ebb3eef7548a8c0a5f165b50a164d941f3fde2
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-11-10 11:17:09 +0300

    rename

--- ek-tm4c1294xl/project2/self/udp/udp_out.h
+++ ek-tm4c1294xl/project2/self/udp/udp_out.h
@@ -11,10 +11,10 @@ UDP_OUT.H
 void InitUDP_Out(void);
 bool IsActive_UDPOutput(void);
 
-err_t UDP_OutInfo(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast, ulong dw1);
+err_t UDP_OutInfo(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast);
 
-err_t UDP_OutGetLong3(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast, ulong dw1, ulong dw2, ulong dw3);
-err_t UDP_OutSetLong3(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast, ulong *pdw1, ulong *pdw2, ulong *pdw3);
+err_t UDP_OutGetSettings(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast);
+err_t UDP_OutSetSettings(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast);
 
 err_t UDP_OutBegin(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast);
 err_t UDP_OutEnd(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast);
