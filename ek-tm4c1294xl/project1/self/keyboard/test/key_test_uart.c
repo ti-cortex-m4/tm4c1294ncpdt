a102aa57c61682a87734133ae9d4f4458cc0ebdd
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-11-11 17:37:09 +0300

    update tests

--- ek-tm4c1294xl/project1/self/keyboard/test/key_test_uart.c
+++ ek-tm4c1294xl/project1/self/keyboard/test/key_test_uart.c
@@ -1,19 +1,19 @@
 /*------------------------------------------------------------------------------
-KEY_TEST_UART!C
 
+ KEY_TEST_UART.C
 
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
 #include "../../console.h"
-//#include "../../memory/mem_realtime.h"
-//#include "../../memory/mem_digitals.h"
-//#include "../keyboard.h"
-//#include "../../display/display.h"
-//#include "../../digitals/digitals.h"
-//#include "../../digitals/digitals_messages.h"
-//#include "../../serial/ports.h"
-//#include "../../serial/modems.h"
+#include "../../display/lines.h"
+#include "../../hardware/watchdog.h"
+#include "../../isr/serial2.h"
+#include "../../isr/serial3.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/uart.h"
 #include "key_test_uart.h"
 
 
@@ -23,25 +23,131 @@ static char const       szMessage1[]    = "Тест UART       ",
                         szMessage2[]    = "    тест ?__    ";
 
 
+#define TEST_CHAR       0x4B
+
+
+static uchar            bOut0, bIn0,
+                        bOut1, bIn1,
+                        bOut2, bIn2,
+                        bOut3, bIn3;
+
+
+
+static void Test(ulong  dwBaseOut, ulong  dwBaseIn, uchar  *pbOut, uchar  *pbIn)
+{
+  if (UARTCharPutNonBlocking(dwBaseOut, TEST_CHAR))
+    (*pbOut)++;
+
+  if (UARTCharGetNonBlocking(dwBaseIn) == TEST_CHAR)
+    (*pbIn)++;
+}
+
+
+
 static void GetTest0(void)
 {
+  IntDisable(INT_UART0);
+  IntDisable(INT_UART2);
+  IntDisable(INT_UART3);
+  IntDisable(INT_UART4);
+
+  bOut0 = 0; bIn0 = 0;
+  bOut1 = 0; bIn1 = 0;
+  bOut2 = 0; bIn2 = 0;
+  bOut3 = 0; bIn3 = 0;
+
+  while (1)
+  {
+    Test(UART0_BASE, UART0_BASE, &bOut0, &bIn0);
+    Test(UART2_BASE, UART2_BASE, &bOut1, &bIn1);
+    Test(UART3_BASE, UART3_BASE, &bOut2, &bIn2);
+    Test(UART4_BASE, UART4_BASE, &bOut3, &bIn3);
+
+    Lo(0, bOut0); Lo(2, bIn0);
+    Lo(5, bOut1); Lo(7, bIn1);
+    Lo(10, bOut2); Lo(12, bIn2);
+    Hi(10, bOut3); Hi(12, bIn3);
+
+    if (fKey == true) {fKey = false; break; }
+
+    Delay(50);
+    ResetWatchdog();
+  }
+
+  IntEnable(INT_UART0);
+  IntEnable(INT_UART2);
+  IntEnable(INT_UART3);
+  IntEnable(INT_UART4);
 }
 
 
 static void GetTest1(void)
 {
+  IntDisable(INT_UART3);
+  IntDisable(INT_UART4);
+
+  bOut2 = 0; bIn2 = 0;
+  bOut3 = 0; bIn3 = 0;
+
+  sprintf(szLo,"3:     > 4:");
+
+  OutputMode2();
+  InputMode3();
+
+  while (1)
+  {
+    Test(UART3_BASE, UART4_BASE, &bOut2, &bIn3);
+
+    Lo(2, bOut2); Lo(4, bIn2);
+    Lo(11, bOut3); Lo(13, bIn3);
+
+    if (fKey == true) {fKey = false; break; }
+
+    Delay(50);
+    ResetWatchdog();
+  }
+
+  IntEnable(INT_UART3);
+  IntEnable(INT_UART4);
 }
 
 
 static void GetTest2(void)
 {
+  IntDisable(INT_UART3);
+  IntDisable(INT_UART4);
+
+  bOut2 = 0; bIn2 = 0;
+  bOut3 = 0; bIn3 = 0;
+
+  sprintf(szLo,"3:     < 4:");
+
+  InputMode2();
+  OutputMode3();
+
+  while (1)
+  {
+    Test(UART4_BASE, UART3_BASE, &bOut3, &bIn2);
+
+    Lo(2, bOut2); Lo(4, bIn2);
+    Lo(11, bOut3); Lo(13, bIn3);
+
+    if (fKey == true) {fKey = false; break; }
+
+    Delay(50);
+    ResetWatchdog();
+  }
+
+  IntEnable(INT_UART3);
+  IntEnable(INT_UART4);
 }
 
 
-static void GetTest(uchar  ibX)
+
+static void GetTest(uchar  i)
 {
-  sprintf(szHi+15,"%u",ibX+1); Clear();
-  switch (ibX)
+  sprintf(szHi+15,"%u",i+1); Clear();
+  switch (i)
   {
     case 0:  GetTest0();  break;
     case 1:  GetTest1();  break;
@@ -72,7 +178,7 @@ static uchar i;
     }
     else if (enKeyboard == KBD_POSTINPUT1)
     {
-      if ((i = GetChar(10,11) - 1) < 3)
+      if ((i = GetCharLo(10,11) - 1) < 3)
       {
         enKeyboard = KBD_POSTENTER;
         GetTest(i);
