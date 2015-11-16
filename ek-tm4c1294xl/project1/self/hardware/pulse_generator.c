b3799eccfd0de42cc01d57123a2fdcbebca1b80c
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-11-16 09:31:30 +0300

    update

--- ek-tm4c1294xl/project1/self/hardware/pulse_generator.c
+++ ek-tm4c1294xl/project1/self/hardware/pulse_generator.c
@@ -5,10 +5,35 @@ PULSE_GENERATOR,C
 ------------------------------------------------------------------------------*/
 
 #include "../main.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
 #include "pulse_generator.h"
 
 
+uchar   bPulseGenerator;
 
-void    InitPulsegenerator(void)
+
+void    PulseGeneratorIntHandler(void)
 {
+  bPulseGenerator++;
+
+  GPIOIntClear(GPIO_PORTP_BASE, GPIO_PIN_5);
+}
+
+
+
+void    InitPulseGenerator(void)
+{
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOP);
+
+  GPIOPinTypeGPIOInput(GPIO_PORTP_BASE, GPIO_PIN_5);
+  GPIOPadConfigSet(GPIO_PORTP_BASE, GPIO_PIN_5, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+
+  GPIOIntDisable(GPIO_PORTP_BASE, GPIO_PIN_5);
+  GPIOIntClear(GPIO_PORTP_BASE, GPIO_PIN_5);
+  GPIOIntRegister(GPIO_PORTP_BASE, PulseGeneratorIntHandler);
+  GPIOIntTypeSet(GPIO_PORTP_BASE, GPIO_PIN_5, GPIO_FALLING_EDGE);
+  GPIOIntEnable(GPIO_PORTP_BASE, GPIO_PIN_5);
+
 }
