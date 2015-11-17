eb2846b2fe1cbbdbb985e1534076a2a00360d0da
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-11-17 09:54:54 +0300

    raw commit

--- ek-tm4c1294xl/project1/self/sensors/automatic_k.c
+++ ek-tm4c1294xl/project1/self/sensors/automatic_k.c
@@ -61,3 +61,242 @@ uchar   i;
 
   return GetTime2(ti, true);
 }
+
+
+
+bit     ReadEnergyDayDatesK_Full(void)
+{
+  for (ibZ=0; ibZ<bMINORREPEATS; ibZ++)
+  {
+    QueryCloseK();
+    QueryEnergyDayDatesK();
+
+    if (BccInput() == SER_GOODCHECK) break;
+    if (fKey == 1) return(0);
+  }
+
+  if (ibZ == bMINORREPEATS) return(0);
+  ShowPercent(60);
+
+  ReadEnergyDayDatesK();
+
+  return(1);
+}
+
+
+bit     ReadEnergyMonDatesK_Full(void)
+{
+  for (ibZ=0; ibZ<bMINORREPEATS; ibZ++)
+  {
+    QueryCloseK();
+    QueryEnergyMonDatesK();
+
+    if (BccInput() == SER_GOODCHECK) break;
+    if (fKey == 1) return(0);
+  }
+
+  if (ibZ == bMINORREPEATS) return(0);
+  ShowPercent(60);
+
+  ReadEnergyMonDatesK();
+
+  return(1);
+}
+
+
+
+bit     ReadEnergyDayK_Full(uchar  bDay)
+{
+  for (ibCan=0; ibCan<4; ibCan++)
+  {
+    for (ibZ=0; ibZ<bMINORREPEATS; ibZ++)
+    {
+      QueryCloseK();
+      QueryEnergyDayK(ibCan,bDay);
+
+      if (BccInput() == SER_GOODCHECK) break;
+      if (fKey == 1) return(0);
+    }
+
+    if (ibZ == bMINORREPEATS) return(0);
+    ShowPercent(70+ibCan);
+
+    ReadEnergyK(ibCan);
+  }
+
+  return(1);
+}
+
+
+bit     ReadEnergyMonK_Full(uchar  bMon)
+{
+  for (ibCan=0; ibCan<4; ibCan++)
+  {
+    for (ibZ=0; ibZ<bMINORREPEATS; ibZ++)
+    {
+      QueryCloseK();
+      QueryEnergyMonK(ibCan,bMon);
+
+      if (BccInput() == SER_GOODCHECK) break;
+      if (fKey == 1) return(0);
+    }
+
+    if (ibZ == bMINORREPEATS) return(0);
+    ShowPercent(70+ibCan);
+
+    ReadEnergyK(ibCan);
+  }
+
+  return(1);
+}
+
+
+bit     ReadEnergyMonTariffK_Full(uchar  bMon, uchar  ibTariff)
+{
+  for (ibCan=0; ibCan<4; ibCan++)
+  {
+    for (ibZ=0; ibZ<bMINORREPEATS; ibZ++)
+    {
+      QueryCloseK();
+      QueryEnergyMonK(ibCan,bMon);
+
+      if (BccInput() == SER_GOODCHECK) break;
+      if (fKey == 1) return(0);
+    }
+
+    if (ibZ == bMINORREPEATS) return(0);
+    ShowPercent(70+ibCan);
+
+    ReadEnergyTariffK(ibCan,ibTariff);
+  }
+
+  return(1);
+}
+
+
+
+bit     ReadCntMonCanK2(uchar  ibMonth) // на конец мес€ца
+{
+  if (ReadTimeDateK() == 0) return(0);
+
+  if (tiAlt.bMonth == ibMonth+1)
+  {
+    if (tiAlt.bDay > 1)
+      tiAlt.bDay--;
+    else
+    {
+      if (tiAlt.bMonth > 1)
+        tiAlt.bMonth--;
+      else
+      {
+        tiAlt.bMonth = 12;
+        tiAlt.bYear--;
+      }
+
+      tiAlt.bDay = DaysInMonth();
+    }
+
+	  if (ReadEnergyDayDatesK_Full() == 0) return (0);
+
+    daAlt.bDay   = tiAlt.bDay;
+    daAlt.bMonth = tiAlt.bMonth;
+    daAlt.bYear  = tiAlt.bYear;
+	  ibGrp = IsDayAddedK();
+
+    if (ibGrp == 0)
+    {
+      sprintf(szLo, "сутки %02bu.%02bu.%02bu ?",tiAlt.bDay,tiAlt.bMonth,tiAlt.bYear);
+      Delay(1000);
+      return(0);
+    }
+	  if (ReadEnergyDayK_Full(ibGrp) == 0) return (0);
+
+    QueryCloseK();
+  }
+  else
+  {
+    tiAlt.bYear = (ibMonth+1 > tiAlt.bMonth) ? tiAlt.bYear-1 : tiAlt.bYear;
+    tiAlt.bMonth = ibMonth+1;
+
+	  if (ReadEnergyMonDatesK_Full() == 0) return (0);
+
+    daAlt.bDay   = tiAlt.bDay;
+    daAlt.bMonth = tiAlt.bMonth;
+    daAlt.bYear  = tiAlt.bYear;
+	  ibGrp = IsMonAddedK();
+
+    if (ibGrp == 0)
+    {
+      sprintf(szLo, " мес€ц %02bu.%02bu ?  ",tiAlt.bMonth,tiAlt.bYear);
+      Delay(1000);
+      return(0);
+    }
+	  if (ReadEnergyMonK_Full(ibGrp) == 0) return (0);
+
+    QueryCloseK();
+  }
+
+
+  reBuffB = mpreTransCnt[ibDig];
+
+  for (ibCan=0; ibCan<4; ibCan++)
+  {
+    reBuffA = mpreChannelsB[ibCan] * reBuffB;
+    mpreChannelsB[ibCan] = reBuffA;
+    mpboChannelsA[ibCan] = boTrue;
+  }
+
+  reBuffA = mpreChannelsB[diCurr.ibLine];
+
+  return(1);
+}
+
+
+/*
+status4 ReadCntMonCanTariffK(uchar  ibMonth, uchar  ibTariff) // на начало мес€ца
+{
+  if (ReadTimeDateK() == 0) return(ST4_BADDIGITAL);
+
+  if (ibMonth == 0)
+  {
+    tiAlt.bMonth = 12;
+    tiAlt.bYear--;
+  }
+  else
+  {
+    if (ibMonth+1 > tiAlt.bMonth) tiAlt.bYear--;
+    tiAlt.bMonth = ibMonth;
+  }
+
+  if (ReadEnergyMonDatesK_Full() == 0) return (ST4_BADDIGITAL);
+
+  daAlt.bDay   = tiAlt.bDay;
+  daAlt.bMonth = tiAlt.bMonth;
+  daAlt.bYear  = tiAlt.bYear;
+  ibGrp = IsMonAddedK();
+
+  if (ibGrp == 0)
+  {
+    sprintf(szLo, " мес€ц %02bu.%02bu ?  ",tiAlt.bMonth,tiAlt.bYear);
+    Delay(1000);
+    return(ST4_NOTPRESENTED);
+  }
+  if (ReadEnergyMonTariffK_Full(ibGrp,ibTariff) == 0) return (ST4_BADDIGITAL);
+
+  QueryCloseK();
+
+
+  reBuffB = mpreTransCnt[ibDig];
+
+  for (ibCan=0; ibCan<4; ibCan++)
+  {
+    reBuffA = mpreChannelsB[ibCan] * reBuffB;
+    mpreChannelsB[ibCan] = reBuffA;
+    mpboChannelsA[ibCan] = boTrue;
+  }
+
+  reBuffA = mpreChannelsB[diCurr.ibLine];
+
+  return(ST4_OK);
+}
+*/
--- ek-tm4c1294xl/project1/self/sensors/automatic_k.h
+++ ek-tm4c1294xl/project1/self/sensors/automatic_k.h
@@ -5,3 +5,5 @@ AUTOMATIC_K!H
 ------------------------------------------------------------------------------*/
 
 time2   ReadTimeCanK(void);
+//bit     ReadCntMonCanK2(uchar  ibMonth);
+//status4 ReadCntMonCanTariffK(uchar  ibMonth, uchar  ibTariff);
