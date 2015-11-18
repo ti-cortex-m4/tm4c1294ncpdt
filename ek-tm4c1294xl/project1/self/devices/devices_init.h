2e74a1e7dd3559b74ea27118a4eb9dd49c37b30a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2015-11-18 12:10:32 +0300

    fix

--- ek-tm4c1294xl/project1/self/devices/devices_init.h
+++ ek-tm4c1294xl/project1/self/devices/devices_init.h
@@ -13,6 +13,7 @@ extern  cache const             chManageTime;
 extern  cache const             chKeysLevelB;
 extern  cache const             chShortProfileC;
 extern  cache const             chControlQ;
+extern  cache const             chControlK;
 extern  cache const             chPlcUFlag;
 extern  cache const             chPlcUSize;
 extern  cache const             chPlcUShutdown;
