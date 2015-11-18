46eba1f912782ffea61b0bfd12ed135361d8f89d
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-11-18 10:22:29 +0300

    fix

--- ek-tm4c1294xl/project1/self/devices/devices_init.c
+++ ek-tm4c1294xl/project1/self/devices/devices_init.c
@@ -57,6 +57,7 @@ cache const             chManageTime = {MANAGE_TIME, &boManageTime, sizeof(bool)
 
 cache const             chKeysLevelB = {KEYS_LEVEL_B, &bKeysLevelB, sizeof(uchar)};
 cache const             chShortProfileC = {SHORT_PROFILE_C, &boShortProfileC, sizeof(bool)};
+cache const             chControlK = {CONTROL_K, &boControlK, sizeof(bool)};
 cache const             chControlQ = {CONTROL_Q, &boControlQ, sizeof(bool)};
 cache const             chPlcUFlag = {PLC_U_FLAG, &boPlcUFlag, sizeof(bool)};
 cache const             chPlcUSize = {PLC_U_SIZE, &bPlcUSize, sizeof(uchar)};
@@ -74,7 +75,8 @@ void    InitDevices1(void)
 
   LoadCacheChar(&chKeysLevelB, 1, 2, 2);
   LoadCache(&chShortProfileC);
-  LoadCache(&chControlQ);
+  LoadCacheBool(&chControlK, true);
+  LoadCacheBool(&chControlQ, false);
   LoadCacheBool(&chPlcUFlag, false);
   LoadCacheChar(&chPlcUSize, 1, 8, 6);
   LoadCacheInt(&chPlcUShutdown, 10, 500, 100);
@@ -151,8 +153,8 @@ void    ResetDevices(bool  fFull)
   boShortProfileC = false;
   SaveCache(&chShortProfileC);
 
-  boControlQ = false;
-  SaveCache(&chControlQ);
+  SaveCacheBool(&chControlK, true);
+  SaveCacheBool(&chControlQ, false);
 
   boPlcUFlag = false;
   SaveCache(&chPlcUFlag);
