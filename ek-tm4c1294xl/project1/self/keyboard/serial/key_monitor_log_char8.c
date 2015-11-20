63855beb2a0ccb483bbe04bd8655d3fe7d40a1fa
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-11-20 16:07:52 +0300

    update monitor

---
+++ ek-tm4c1294xl/project1/self/keyboard/serial/key_monitor_log_char8.c
@@ -0,0 +1,25 @@
+/*------------------------------------------------------------------------------
+KEY_MONITOR_LOG_CHAR8.C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../serial/monitor_settings.h"
+#include "../common/key_bool.h"
+#include "key_monitor_log_char7.h"
+
+
+
+//                                          0123456789ABCDEF
+static char const       szMessage1[]     = " Монитор: лог в ",
+                        szMessage2[]     = " формате CHAR8 ?";
+
+static char const       *pszMessages[]   = { szMessage1, szMessage2, "" };
+
+
+
+void    key_MonitorLogChar8(void)
+{
+  key_SetBool(&chMonitorLogChar8, pszMessages);
+}
---
+++ ek-tm4c1294xl/project1/self/keyboard/serial/key_monitor_log_hex.c
@@ -0,0 +1,25 @@
+/*------------------------------------------------------------------------------
+KEY_MONITOR_LOG_HEX.C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../serial/monitor_settings.h"
+#include "../common/key_bool.h"
+#include "key_monitor_log_hex.h"
+
+
+
+//                                          0123456789ABCDEF
+static char const       szMessage1[]     = " Монитор: лог в ",
+                        szMessage2[]     = " формате HEX ?  ";
+
+static char const       *pszMessages[]   = { szMessage1, szMessage2, "" };
+
+
+
+void    key_MonitorLogHex(void)
+{
+  key_SetBool(&chMonitorLogHex, pszMessages);
+}
