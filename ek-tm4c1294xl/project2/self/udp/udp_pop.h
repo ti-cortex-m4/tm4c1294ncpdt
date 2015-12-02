fb9ab5124adb650223f06aed4f2806177dfab745
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-12-02 15:18:39 +0300

    rename

--- ek-tm4c1294xl/project2/self/udp/udp_pop.h
+++ ek-tm4c1294xl/project2/self/udp/udp_pop.h
@@ -10,4 +10,4 @@ UDP_POP.H
 
 err_t PopIntArgument(struct pbuf *p, uint *pw);
 err_t PopStringArgument(struct pbuf *p, char *sz, uchar  bSize);
-err_t PopSuffix(struct pbuf *p, uint *pw);
+err_t PopSfx(struct pbuf *p, uint *pw);
