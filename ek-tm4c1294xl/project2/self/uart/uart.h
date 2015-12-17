180face8d2ada9b0e2d0bc5a31a7ff7c5e975cfb
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-12-17 10:19:31 +0300

    fix

--- ek-tm4c1294xl/project2/self/uart/uart.h
+++ ek-tm4c1294xl/project2/self/uart/uart.h
@@ -9,6 +9,8 @@ UART.H
 
 
 void    InitUART(ulong dwSysClockFreq);
-void    UART_Out(struct tcp_pcb *tpcb, void *arg, uint len);
+
+void    UART_Out1(uchar* ptr, uint len);
+void    UART_Out2(struct tcp_pcb *tpcb);
 
 void    UART_1000Hz(void);
