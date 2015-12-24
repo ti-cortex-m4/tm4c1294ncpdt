565283cbd072931462f6c859981ca072b48b1683
TI Cortex M4 <ti.cortex.m4@gmail.com>
2015-12-24 11:59:27 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/automatic_w.h
+++ ek-tm4c1294xl/project1/self/sensors/automatic_w.h
@@ -5,12 +5,12 @@ AUTOMATIC_W.H
 ------------------------------------------------------------------------------*/
 
 void    QueryW(uint  cwIn, uchar  cbOut, uchar  cbMaxHeaderBcc);
-//serial  InputW(void);
+serial  InputW(void);
 
 //bool    QueryEngAbsW_Full(uchar  bPercent);
 //bool    QueryEngMonW_Full(uchar  bMonth, uchar  bYear, uchar  bPercent);
 //bool    QueryEngDayW_Full(uchar  bDay, uchar  bMonth, uchar  bYear, uchar  bPercent);
 
 time2   ReadTimeCanW(void);
-//double2 ReadCntCurrW(void);
+double2 ReadCntCurrW(void);
 //double2 ReadCntMonCanW(uchar  ibMonth);
