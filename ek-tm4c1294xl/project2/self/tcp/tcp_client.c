8b95de815ef96ac292c18004574c808c8eb32e4e
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-01-18 15:31:11 +0300

    add methods

--- ek-tm4c1294xl/project2/self/tcp/tcp_client.c
+++ ek-tm4c1294xl/project2/self/tcp/tcp_client.c
@@ -138,6 +138,134 @@ static err_t TCPClientPoll(void *arg, struct tcp_pcb *pcb)
 }
 
 
+static err_t TCPClientReceive(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err)
+{
+//    tTelnetSessionData *pState = arg;
+//    int iNextWrite;
+//    SYS_ARCH_DECL_PROTECT(lev);
+//
+//    DEBUG_MSG("TelnetReceive 0x%08x, 0x%08x, 0x%08x, %d\n", arg, pcb, p, err);
+//
+//    //
+//    // Place the incoming packet onto the queue if there is space.
+//    //
+//    if((err == ERR_OK) && (p != NULL))
+//    {
+//        //
+//        // This should be done in a protected/critical section.
+//        //
+//        SYS_ARCH_PROTECT(lev);
+//
+//        //
+//        // Do we have space in the queue?
+//        //
+//        iNextWrite = ((pState->iBufQWrite + 1) % PBUF_POOL_SIZE);
+//        if(iNextWrite == pState->iBufQRead)
+//        {
+//            //
+//            // The queue is full - discard the pbuf and return since we can't
+//            // handle it just now.
+//            //
+//
+//            //
+//            // Restore previous level of protection.
+//            //
+//            SYS_ARCH_UNPROTECT(lev);
+//
+//            //
+//            // Free up the pbuf.  Note that we don't acknowledge receipt of
+//            // the data since we want it to be retransmitted later.
+//            //
+//            pbuf_free(p);
+//        }
+//        else
+//        {
+//            //
+//            // Place the pbuf in the circular queue.
+//            //
+//            pState->pBufQ[pState->iBufQWrite] = p;
+//
+//            //
+//            // Increment the queue write index.
+//            //
+//            pState->iBufQWrite = iNextWrite;
+//
+//            //
+//            // Restore previous level of protection.
+//            //
+//            SYS_ARCH_UNPROTECT(lev);
+//        }
+//    }
+//
+//    //
+//    // If a null packet is passed in, close the connection.
+//    //
+//    else if((err == ERR_OK) && (p == NULL))
+//    {
+//        //
+//        // Clear out all of the TCP callbacks.
+//        //
+//        tcp_arg(pcb, NULL);
+//        tcp_sent(pcb, NULL);
+//        tcp_recv(pcb, NULL);
+//        tcp_err(pcb, NULL);
+//        tcp_poll(pcb, NULL, 1);
+//
+//        //
+//        // Close the TCP connection.
+//        //
+//        tcp_close(pcb);
+//
+//        //
+//        // Clear out any pbufs associated with this session.
+//        //
+//        TelnetFreePbufs(pState);
+//
+//        //
+//        // Clear out the telnet session PCB.
+//        //
+//        pState->pConnectPCB = NULL;
+//
+//        //
+//        // If we don't have a listen PCB, then we are in client mode, and
+//        // should try to reconnect.
+//        //
+//        if(pState->pListenPCB == NULL)
+//        {
+//            //
+//            // Re-open the connection.
+//            //
+//            TelnetOpen(pState->ulTelnetRemoteIP, pState->usTelnetRemotePort,
+//                       pState->usTelnetLocalPort, pState->ulSerialPort);
+//        }
+//        else
+//        {
+//            //
+//            // Revert to listening state.
+//            //
+//            pState->eTCPState = STATE_TCP_LISTEN;
+//        }
+//    }
+
+  return ERR_OK;
+}
+
+
+static err_t TCPClientSent(void *arg, struct tcp_pcb *pcb, u16_t len)
+{
+//    tTelnetSessionData *pState = arg;
+//
+//    DEBUG_MSG("TelnetSent 0x%08x, 0x%08x, %d\n", arg, pcb, len);
+//
+//    //
+//    // Reset the connection timeout.
+//    //
+//    pState->ulConnectionTimeout = 0;
+
+  return ERR_OK;
+}
+
+
 static err_t TCPClientConnected(void *arg, struct tcp_pcb *pcb, err_t err)
 {
   tState *pState = arg;
@@ -215,11 +343,9 @@ static err_t TCPClientConnected(void *arg, struct tcp_pcb *pcb, err_t err)
 //    // Setup the TCP connection priority.
 //    //
 //    tcp_setprio(pcb, TCP_PRIO_MIN);
-//
-//    //
-//    // Setup the TCP receive function.
-//    //
-//    tcp_recv(pcb, TelnetReceive);
+
+  // Setup the TCP receive function.
+  tcp_recv(pcb, TCPClientReceive);
 
   // Setup the TCP error function.
   tcp_err(pcb, TCPClientError);
@@ -227,10 +353,8 @@ static err_t TCPClientConnected(void *arg, struct tcp_pcb *pcb, err_t err)
   // Setup the TCP polling function/interval.
   tcp_poll(pcb, TCPClientPoll, (1000 / TCP_SLOW_INTERVAL));
 
-//    //
-//    // Setup the TCP sent callback function.
-//    //
-//    tcp_sent(pcb, TelnetSent);
+  // Setup the TCP sent callback function.
+  tcp_sent(pcb, TCPClientSent);
 
   return ERR_OK;
 }
