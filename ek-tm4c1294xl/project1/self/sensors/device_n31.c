c395b44faa7a1e75a74c1ad9433b75f5664bf672
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-01-19 15:00:44 +0300

    add template

---
+++ ek-tm4c1294xl/project1/self/sensors/device_n31.c
@@ -0,0 +1,36 @@
+/*------------------------------------------------------------------------------
+DEVICE_N31.C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+#include "../memory/mem_digitals.h"
+#include "../memory/mem_current.h"
+#include "../memory/mem_factors.h"
+#include "../memory/mem_realtime.h"
+#include "../memory/mem_energy_spec.h"
+#include "../memory/mem_profile.h"
+#include "../memory/mem_limits.h"
+#include "../serial/ports.h"
+#include "../serial/ports_devices.h"
+#include "../serial/monitor.h"
+#include "../display/display.h"
+#include "../keyboard/time/key_timedate.h"
+#include "../time/timedate.h"
+#include "../time/delay.h"
+#include "../devices/devices.h"
+#include "../devices/devices_time.h"
+#include "../digitals/current/current_run.h"
+#include "../digitals/limits.h"
+#include "../special/special.h"
+#include "automatic_w.h"
+#include "device_k.h"
+#include "device_w.h"
+
+
+
+#ifndef SKIP_N31
+
+
+#endif
