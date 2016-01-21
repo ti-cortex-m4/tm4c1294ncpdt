e258b888e98a9efce2c6db8f55f1d2f23a1dfe45
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-01-21 11:42:58 +0300

    add factors tuple

--- ek-tm4c1294xl/project1/self/kernel/tuples.c
+++ ek-tm4c1294xl/project1/self/kernel/tuples.c
@@ -9,7 +9,7 @@ TUPLES,C
 
 
 
-t2uint  GetTuple2Int(uint  w1,  uint  w2)
+t2uint  GetTuple2Int(uint  w1, uint  w2)
 {
   t2uint tp;
 
@@ -20,7 +20,7 @@ t2uint  GetTuple2Int(uint  w1,  uint  w2)
 }
 
 
-t2time  GetTuple2Time(time  ti1,  time  ti2)
+t2time  GetTuple2Time(time  ti1, time  ti2)
 {
   t2time tp;
 
@@ -28,4 +28,15 @@ t2time  GetTuple2Time(time  ti1,  time  ti2)
   tp.ti2 = ti2;
 
   return tp;
+}
+
+
+factors GetFactors(double  dbKtrans, double  dbKpulse)
+{
+  factors fc;
+
+  fc.dbKtrans = dbKtrans;
+  fc.dbKpulse = dbKpulse;
+
+  return fc;
 }
