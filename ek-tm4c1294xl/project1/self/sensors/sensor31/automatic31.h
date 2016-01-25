57ff2fd95019fe01d6061f1d41ceac83fa447b14
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-01-25 13:23:58 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor31/automatic31.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor31/automatic31.h
@@ -14,17 +14,12 @@ void    InitPopCod(void);
 uchar   PopCharCod(void);
 
 void    QueryN31(uint  cwIn, uchar  cbOut);
-//bool    ChecksumW(void);
 serial  InputN31(void);
 
 bool    AutomaticN31(void);
 
-//bool    QueryEngAbsW_Full(uchar  bPercent);
-//bool    QueryEngMonW_Full(uchar  bTime, uchar  bPercent);
-//bool    QueryEngDayW_Full(uchar  bTime, uchar  bPercent);
+time2   QueryTimeN31_Full(uchar  bPercent);
+double2 ReadTransN31_Full(void);
 
 time2   ReadTimeCanN31(void);
 double2 ReadCntCurrN31(void);
-//double2 ReadCntMonCanW(uchar  ibMon);
-//
-//status  ReadCntMonCanTariffW(uchar  ibMon, uchar  ibTrf);
