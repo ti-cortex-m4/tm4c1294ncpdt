ae10c9652082c0014621ece9f3000d463d4d224c
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-01-26 09:46:29 +0300

    add dev_32

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor32/automatic32.h
@@ -0,0 +1,25 @@
+/*------------------------------------------------------------------------------
+AUTOMATIC32.H
+
+
+------------------------------------------------------------------------------*/
+
+//extern  uchar               mpbCoder[4], ibCoder;
+//
+//
+//
+//void    InitPushCod(void);
+//void    PushCharCod(uchar  bT);
+//void    InitPopCod(void);
+//uchar   PopCharCod(void);
+//
+//void    Query31(uint  cwIn, uchar  cbOut);
+//serial  Input31(void);
+//
+//bool    Automatic31(void);
+//
+//time2   QueryTime31_Full(uchar  bPercent);
+//double2 ReadTrans31_Full(void);
+//
+//time2   ReadTimeCan31(void);
+//double2 ReadCntCurr31(void);
