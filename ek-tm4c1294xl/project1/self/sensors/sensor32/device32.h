25a0ef0af7638cc1b77dec8088e85fce58e44d2b
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-01-26 10:15:13 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor32/device32.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor32/device32.h
@@ -5,3 +5,11 @@ DEVICE32.H
 ------------------------------------------------------------------------------*/
 
 #define MAX_LINE_N32    1
+
+
+
+void    QueryOpen32(void);
+bool    ReadOpen32(void);
+
+void    QueryTime32(void);
+time    ReadTime32(void);
