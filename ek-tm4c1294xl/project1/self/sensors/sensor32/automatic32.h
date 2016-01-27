4781e08ee4015251232ef97859bf3c84fe77482a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-01-27 09:49:23 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor32/automatic32.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor32/automatic32.h
@@ -9,8 +9,8 @@ bool    Checksum32(uchar  bSize);
 void    Query32(uint  cwIn, uchar  cbOut);
 serial  Input32(void);
 
-//bool    Automatic31(void);
-//
+bool    Automatic32(void);
+
 //time2   QueryTime31_Full(uchar  bPercent);
 //double2 ReadTrans31_Full(void);
 
