17e8cc1fd3f1143237eadd709f5a233a771118de
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-01 10:11:57 +0300

    update monitor

--- ek-tm4c1294xl/project1/self/include/programs.h
+++ ek-tm4c1294xl/project1/self/include/programs.h
@@ -280,11 +280,12 @@ PROGRAMS!H
 #define bTEST_POWER_OFF             803
 #define wSET_MONITOR_BAUD           810
 #define wSET_MONITOR_TIMEOUT        811
-#define wSET_MONITOR_LOG_HEX        812
-#define wSET_MONITOR_LOG_CHAR7      813
-#define wSET_MONITOR_LOG_CHAR8      814
-#define wSET_MONITOR_OPEN           815
-#define wTEST_MONITOR               816
+#define wSET_MONITOR_LOG_BASIC      812
+#define wSET_MONITOR_LOG_HEX        813
+#define wSET_MONITOR_LOG_CHAR7      814
+#define wSET_MONITOR_LOG_CHAR8      815
+#define wSET_MONITOR_OPEN           816
+#define wTEST_MONITOR               817
 
 
 #define wSET_OUTPUT_DELAY           298
--- ek-tm4c1294xl/project1/self/serial/monitor_settings.h
+++ ek-tm4c1294xl/project1/self/serial/monitor_settings.h
@@ -4,10 +4,12 @@ MONITOR_SETTINGS.H
 
 -----------------------------------------------------------------------------*/
 
+extern bool                    fMonitorLogBasic;
 extern bool                    fMonitorLogHex;
 extern bool                    fMonitorLogChar7;
 extern bool                    fMonitorLogChar8;
 
+extern cache const             chMonitorLogBasic;
 extern cache const             chMonitorLogHex;
 extern cache const             chMonitorLogChar7;
 extern cache const             chMonitorLogChar8;
