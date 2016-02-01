02fe45fe429dabf4647703c69abfdffd34aadd78
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-01 14:27:16 +0300

    disable dev 32

--- ek-tm4c1294xl/project1/self/system.h
+++ ek-tm4c1294xl/project1/self/system.h
@@ -103,10 +103,10 @@ SYSTEM!H
 #define bGROUPS         32
 
 // количество типов цифровых счётчиков
-#define bDEVICES        13
+#define bDEVICES        12
 
 // максимальное количество типов цифровых счётчиков
-#define bMAXDEVICES     32
+#define bMAXDEVICES     31
 
 // максимальное количество линий учёта в цифровом счётчике
 #define bCHANNELS       128
