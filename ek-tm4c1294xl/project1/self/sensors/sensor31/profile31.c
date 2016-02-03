1ee648386bf6562464f4e313443014ab10a6653f
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-03 17:44:05 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor31/profile31.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor31/profile31.c
@@ -132,7 +132,7 @@ bool    ReadHeader31(void)
   MonitorString("\n time "); MonitorTime(ti1); if ((ti1.bMinute % 30) != 0) MonitorString(" ? ");
 
   ulong dw = DateToHouIndex(ti1);
-  dw--;
+  if (ti1.bMinute % 30 == 0) dw--;
   time ti2 = HouIndexToDate(dw); // время записи должно соответсвовать началу получасового блока
 
   MonitorTime(ti2);
