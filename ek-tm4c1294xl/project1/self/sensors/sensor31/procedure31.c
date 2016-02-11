dbff88e2d3d9417047fd9464109ffe154a741867
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-11 12:59:55 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor31/procedure31.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor31/procedure31.c
@@ -19,16 +19,11 @@ time                    mtiProcedure31Dig[bCANALS];
 
 void    ClearProcedure31(void)
 {
+  MonitorString("\n clear procedure");
   memset(&mpdbEngFracDigCan, 0, sizeof(mpdbEngFracDigCan));
   memset(&mtiProcedure31Dig, 0, sizeof(mtiProcedure31Dig));
 }
 
-
-
-bool    IsCleanProcedure31(uchar  ibDig)
-{
-  return mtiProcedure31Dig[ibDig].bYear == 0;
-}
 
 
 uchar   GetProcedure31Idx(time  ti)
@@ -38,24 +33,32 @@ uchar   GetProcedure31Idx(time  ti)
 
 
 
+static void MonitorBuff(uchar  ibDig, uchar  ibCan)
+{
+  MonitorLongDec(mpdbEngFracDigCan[ibDig][ibCan]*1000);
+}
+
+
 void    AddProcedure31(time  ti, uchar  ibDig, uchar  ibCan, double  db)
 {
   mtiProcedure31Dig[ibDig] = ti;
 
-  MonitorLongDec(mpdbEngFracDigCan[ibDig][ibCan]*1000); MonitorString("+"); MonitorLongDec(db*1000);
-  db /= 1000;
-  mpdbEngFracDigCan[ibDig][ibCan] += db;
-  MonitorString("="); MonitorLongDec(mpdbEngFracDigCan[ibDig][ibCan]*1000);
+  MonitorBuff(ibDig,ibCan); MonitorString("+"); MonitorLongDec(db);
+
+  mpdbEngFracDigCan[ibDig][ibCan] += (db/1000);
+
+  MonitorString("="); MonitorBuff(ibDig,ibCan);
 }
 
 
 void    SubProcedure31(time  ti, uchar  ibDig, uchar  ibCan, double  dbPulse)
 {
-  MonitorLongDec(mpdbEngFracDigCan[ibDig][ibCan]*1000);
+  MonitorBuff(ibDig,ibCan);
   uint w = (uint)(mpdbEngFracDigCan[ibDig][ibCan]*dbPulse);
+
   MonitorString("="); MonitorIntDec(w);
   mpwChannels[ibCan] = w;
 
   mpdbEngFracDigCan[ibDig][ibCan] -= (double)w/dbPulse;
-  MonitorString("+"); MonitorLongDec(mpdbEngFracDigCan[ibDig][ibCan]*1000);
+  MonitorString("+"); MonitorBuff(ibDig,ibCan);
 }
