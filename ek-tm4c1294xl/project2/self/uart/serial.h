8b748dd01331b692ca54f230384188d2a10f3247
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-13 10:01:02 +0300

    fix

--- ek-tm4c1294xl/project2/self/uart/serial.h
+++ ek-tm4c1294xl/project2/self/uart/serial.h
@@ -4,4 +4,5 @@ SERIAL.H
 
 ------------------------------------------------------------------------------*/
 
+extern long SerialReceive(uint32_t ulPort);
 extern uint32_t SerialReceiveAvailable(uint32_t ulPort);
