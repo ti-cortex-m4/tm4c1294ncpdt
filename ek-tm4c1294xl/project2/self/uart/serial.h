304878478edd07dd6aced4a08d8be66c43909f2a
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-13 10:09:15 +0300

    fix

--- ek-tm4c1294xl/project2/self/uart/serial.h
+++ ek-tm4c1294xl/project2/self/uart/serial.h
@@ -4,5 +4,7 @@ SERIAL.H
 
 ------------------------------------------------------------------------------*/
 
+extern void SerialSend(uint32_t ulPort, uint8_t ucChar);
+
 extern long SerialReceive(uint32_t ulPort);
 extern uint32_t SerialReceiveAvailable(uint32_t ulPort);
