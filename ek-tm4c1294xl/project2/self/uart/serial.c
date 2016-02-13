92ab5c3427b52fa28b6cbe99d3eb629ca2220a68
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-13 10:01:02 +0300

    fix

--- ek-tm4c1294xl/project2/self/uart/serial.c
+++ ek-tm4c1294xl/project2/self/uart/serial.c
@@ -229,7 +229,7 @@ SerialSend(uint32_t ulPort, uint8_t ucChar)
     //
     UARTIntEnable(g_ulUARTBase[ulPort], UART_INT_TX);
 }
-
+#endif
 //*****************************************************************************
 //
 //! Receives a character from the UART.
@@ -242,42 +242,29 @@ SerialSend(uint32_t ulPort, uint8_t ucChar)
 //! the receive ring buffer.
 //
 //*****************************************************************************
-long
-SerialReceive(uint32_t ulPort)
+long SerialReceive(uint32_t ulPort)
 {
     uint32_t ulData;
 
-    //
     // Check the arguments.
-    //
     ASSERT(ulPort < MAX_S2E_PORTS);
 
-    //
     // See if the receive buffer is empty and there is space in the FIFO.
-    //
     if(RingBufEmpty(&g_sRxBuf[ulPort]))
     {
-        //
         // Return -1 (EOF) to indicate no data available.
-        //
         return(-1);
     }
 
-    //
     // Read a single character.
-    //
     ulData = (long)RingBufReadOne(&g_sRxBuf[ulPort]);
 
-    //
     // If flow control is enabled, check the status of the RX buffer to
     // determine if flow control GPIO needs to be de-asserted.
-    //
     if(g_sParameters.sPort[ulPort].ucFlowControl == SERIAL_FLOW_CONTROL_HW)
     {
-        //
         // If the ring buffer is down to less than 25% used,
         // de-assert the outbound flow control pin.
-        //
         if((RingBufUsed(&g_sRxBuf[ulPort]) <
             (RingBufSize(&g_sRxBuf[ulPort]) / 4)) &&
            (g_ulSetFlowOutValue[ulPort] == 0))
@@ -286,12 +273,10 @@ SerialReceive(uint32_t ulPort)
         }
     }
 
-    //
     // Return the data that was read.
-    //
     return(ulData);
 }
-#endif
+
 //*****************************************************************************
 //! Returns number of characters available in the serial ring buffer.
 //!
