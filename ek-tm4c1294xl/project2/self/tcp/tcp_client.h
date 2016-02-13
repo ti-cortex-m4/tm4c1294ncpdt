f3fcbfc409f7235307fd40f5c445a92eb71741ea
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-13 14:41:38 +0300

    fix

--- ek-tm4c1294xl/project2/self/tcp/tcp_client.h
+++ ek-tm4c1294xl/project2/self/tcp/tcp_client.h
@@ -6,5 +6,10 @@ TCP_CLIENT.H
 
 extern void TelnetInit(void);
 
+extern void TelnetOpen(uint32_t ulIPAddr, uint16_t usTelnetRemotePort, uint16_t usTelnetLocalPort, uint32_t ulSerialPort);
+extern void TelnetClose(uint32_t ulSerialPort);
+
+#if false
 extern uint16_t TelnetGetLocalPort(uint32_t ulSerialPort);
 extern uint16_t TelnetGetRemotePort(uint32_t ulSerialPort);
+#endif
