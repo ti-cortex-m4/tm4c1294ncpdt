4ba08c021afd696e6a47dcab35552cf3111999fa
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-15 17:46:22 +0300

    fix

--- ek-tm4c1294xl/project2/self/entity.c
+++ ek-tm4c1294xl/project2/self/entity.c
@@ -11,4 +11,9 @@ ENTITY.C
 
 
 
-const entity            enConnectionTimeout = {EEPROM_CONNECTION_TIMEOUT, &bConnectionTimeout, 0, 255, 0, CHAR};
+const entity enConnectionTimeout = {
+        EEPROM_CONNECTION_TIMEOUT, &bConnectionTimeout, sizeof(uchar), CHAR,
+        0, 255, 0,
+        "CT",
+        "AI=CT;D=Connection timeout (min);T=INT;C=EDIT/SPIN/0/1/1/10;O=0-Disabled/0;V=CT>255?\"Maximum timeout is 255 minutes\":\"\"",
+};
