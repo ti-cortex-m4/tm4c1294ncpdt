fa6072cb2527e8cc9921fe67d24bd86d036c55ea
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-16 11:35:44 +0300

    add UART

--- ek-tm4c1294xl/project2/startup.c
+++ ek-tm4c1294xl/project2/startup.c
@@ -60,6 +60,7 @@ extern void lwIPEthernetIntHandler(void);
 extern void SysTickIntHandler(void);
 
 extern void UART0IntHandler(void);
+extern void UART3IntHandler(void);
 extern void UART4IntHandler(void);
 extern void Timer1IntHandler(void);
 
@@ -146,7 +147,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // GPIO Port L
     IntDefaultHandler,                      // SSI2 Rx and Tx
     IntDefaultHandler,                      // SSI3 Rx and Tx
-    IntDefaultHandler,                      // UART3 Rx and Tx
+	UART3IntHandler,                      // UART3 Rx and Tx
     UART4IntHandler,                      // UART4 Rx and Tx
     IntDefaultHandler,                      // UART5 Rx and Tx
     IntDefaultHandler,                      // UART6 Rx and Tx
