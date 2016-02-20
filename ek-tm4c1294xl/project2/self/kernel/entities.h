3342a847ae85895ac628fffb25965ec028a686fe
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-20 08:57:41 +0300

    extract

--- ek-tm4c1294xl/project2/self/kernel/entities.h
+++ ek-tm4c1294xl/project2/self/kernel/entities.h
@@ -52,33 +52,8 @@ typedef enum
 #endif
 
 
-extern const entity enConnectionTimeout0;
-extern const entity enConnectionTimeout1;
-extern const entity enConnectionTimeout2;
+#include "entities_channels.h"
 
-extern const entity enRoutingMode0;
-extern const entity enRoutingMode1;
-extern const entity enRoutingMode2;
-
-extern const entity enPort0;
-extern const entity enPort1;
-extern const entity enPort2;
-
-extern const entity enConnectionMode0;
-extern const entity enConnectionMode1;
-extern const entity enConnectionMode2;
-
-extern const entity enDestinationIP0;
-extern const entity enDestinationIP1;
-extern const entity enDestinationIP2;
-
-extern const entity enDestinationPort0;
-extern const entity enDestinationPort1;
-extern const entity enDestinationPort2;
-
-extern const entity enBaudRate0;
-extern const entity enBaudRate1;
-extern const entity enBaudRate2;
 
 extern const entity enSerialNumber;
 
