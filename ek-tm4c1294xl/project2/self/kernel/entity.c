4c020f5b913c9295fcc71078bae50bb136afe098
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-20 22:12:57 +0300

    fix

--- ek-tm4c1294xl/project2/self/kernel/entity.c
+++ ek-tm4c1294xl/project2/self/kernel/entity.c
@@ -10,19 +10,14 @@ entity,c
 
 
 
-static err_t Result(ulong dw)
-{
-  return (dw == 0) ? ERR_OK : ERR_VAL;
-}
-
 err_t SaveEntity(const entity * const pen)
 {
   switch(pen->eType)
   {
-    case CHAR: return Result(SaveChar(pen->pbRam, pen->dwEepRom));
-    case INT: return Result(SaveInt(pen->pbRam, pen->dwEepRom));
-    case IP: return Result(SaveLong(pen->pbRam, pen->dwEepRom));
-    case STRING: return Result(SaveString(pen->pbRam, pen->dwEepRom));
+    case CHAR: return LoadResult(SaveChar(pen->pbRam, pen->dwEepRom));
+    case INT: return LoadResult(SaveInt(pen->pbRam, pen->dwEepRom));
+    case IP: return LoadResult(SaveLong(pen->pbRam, pen->dwEepRom));
+    case STRING: return LoadResult(SaveString(pen->pbRam, pen->dwEepRom));
     default: ASSERT(false); return ERR_ARG;
   }
 }
@@ -37,6 +32,19 @@ void LoadEntity(const entity * const pen)
     case INT: LoadInt(pen->pbRam, pen->dwEepRom); break;
     case IP: LoadLong(pen->pbRam, pen->dwEepRom); break;
     case STRING: LoadString(pen->pbRam, pen->dwEepRom); break;
+    default: ASSERT(false); break;
+  }
+}
+
+
+void LoadEntityDef(const entity * const pen)
+{
+  switch(pen->eType)
+  {
+    case CHAR: *(uchar *)pen->pbRam = pen->dwDef; break;
+    case INT: *(uint *)pen->pbRam = pen->dwDef; break;
+    case IP: *(ulong *)pen->pbRam = pen->dwDef; break;
+    case STRING: memset(pen->pbRam, 0, NAME_SIZE); break;
     default: ASSERT(false); break;
   }
 }
--- ek-tm4c1294xl/project2/self/kernel/entity.h
+++ ek-tm4c1294xl/project2/self/kernel/entity.h
@@ -4,5 +4,33 @@ entity.h
 
 ------------------------------------------------------------------------------*/
 
+#ifndef __ENTITY_H__
+#define __ENTITY_H__
+
+
+typedef enum
+{
+  CHAR,
+  INT,
+  IP,
+  STRING,
+} entityType;
+
+
+typedef struct
+{
+  ulong         dwEepRom;
+  void         *pbRam;
+  uint          wSize;
+  entityType    eType;
+  ulong         dwMin, dwMax, dwDef;
+  const char    *szCode, *szName;
+} entity;
+
+
+#endif
+
+
 err_t SaveEntity(const entity * const pen);
 void LoadEntity(const entity * const pen);
+void LoadEntityDef(const entity * const pen);
