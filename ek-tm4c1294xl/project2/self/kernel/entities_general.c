f6dadeb34ff2b3d883556aa3b62a95e4b59919e5
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-20 09:27:37 +0300

    fix

--- ek-tm4c1294xl/project2/self/kernel/entities_general.c
+++ ek-tm4c1294xl/project2/self/kernel/entities_general.c
@@ -18,7 +18,6 @@ const entity enIP = {
   "AI=IP;D=IP-address;T=STRING;C=IPCTRL;F=R*",
 };
 
-
 const entity enGateway = {
   EEPROM_GATEWAY, &dwGateway, sizeof(ulong), IP,
   0, MAX_LONG, 0x00000001,
@@ -26,10 +25,24 @@ const entity enGateway = {
   "AI=GI;D=Gateway IP-address;T=STRING;C=IPCTRL;F=R*",
 };
 
-
 const entity enNetmask = {
   EEPROM_NETMASK, &dwNetmask, sizeof(ulong), IP,
   0, MAX_LONG, 0x00000000,
   "NM",
   "AI=NM;D=Subnet mask;T=STRING;C=IPCTRL;F=R*",
+};
+
+
+const entity enOwnerName = {
+  EEPROM_OWNER_NAME, &szOwnerName, NAME_SIZE, STRING,
+  0, 0, 0,
+  "ON",
+  "AI=ON;D=Owner name;T=STRING;C=EDIT;MAXLEN=8;F=R*",
+};
+
+const entity enDeviceName = {
+  EEPROM_DEVICE_NAME, &szDeviceName, NAME_SIZE, STRING,
+  0, 0, 0,
+  "DN",
+  "AI=DN;D=Device name;T=STRING;C=EDIT;MAXLEN=8;F=R*",
 };
