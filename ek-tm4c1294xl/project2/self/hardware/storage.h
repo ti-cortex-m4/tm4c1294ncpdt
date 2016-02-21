daae9db3e05d28fa7d328b512b5c4300b4f403e7
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-21 09:10:57 +0300

    fix

--- ek-tm4c1294xl/project2/self/hardware/storage.h
+++ ek-tm4c1294xl/project2/self/hardware/storage.h
@@ -4,6 +4,8 @@ storage.h
 
 ------------------------------------------------------------------------------*/
 
+ulong InitStorage(void);
+
 ulong SaveChar(uchar *pb, ulong dwEepRom);
 ulong SaveInt(uint *pw, ulong dwEepRom);
 ulong SaveLong(ulong *pdw, ulong dwEepRom);
@@ -13,5 +15,3 @@ void LoadChar(uchar *pb, ulong dwEepRom);
 void LoadInt(uint *pw, ulong dwEepRom);
 void LoadLong(ulong *pdw, ulong dwEepRom);
 void LoadString(char *sz, ulong dwEepRom);
-
-err_t LoadResult(ulong dw);
