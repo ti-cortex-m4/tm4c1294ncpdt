eb1738241149ce7157e7d082f2ca1423c5acdcf0
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-22 12:17:26 +0300

    add ports

--- ek-tm4c1294xl/project2/self/kernel/entities.c
+++ ek-tm4c1294xl/project2/self/kernel/entities.c
@@ -22,30 +22,44 @@ const entity * const mpenEntities[] = {
   &enConnectionTimeout0,
   &enConnectionTimeout1,
   &enConnectionTimeout2,
+  &enConnectionTimeout3,
+  &enConnectionTimeout4,
 
   &enRoutingMode0,
   &enRoutingMode1,
   &enRoutingMode2,
+  &enRoutingMode3,
+  &enRoutingMode4,
 
   &enPort0,
   &enPort1,
   &enPort2,
+  &enPort3,
+  &enPort4,
 
   &enConnectionMode0,
   &enConnectionMode1,
   &enConnectionMode2,
+  &enConnectionMode3,
+  &enConnectionMode4,
 
   &enDestinationIP0,
   &enDestinationIP1,
   &enDestinationIP2,
+  &enDestinationIP3,
+  &enDestinationIP4,
 
   &enDestinationPort0,
   &enDestinationPort1,
   &enDestinationPort2,
+  &enDestinationPort3,
+  &enDestinationPort4,
 
   &enBaudRate0,
   &enBaudRate1,
   &enBaudRate2,
+  &enBaudRate3,
+  &enBaudRate4,
 
   &enSerialNumber,
 
