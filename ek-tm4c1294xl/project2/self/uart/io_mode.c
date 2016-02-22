a777060adee4803bbc5b668f56b83941d1caf685
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-22 10:32:12 +0300

    fix

--- ek-tm4c1294xl/project2/self/uart/io_mode.c
+++ ek-tm4c1294xl/project2/self/uart/io_mode.c
@@ -5,36 +5,19 @@ io.c
 ------------------------------------------------------------------------------*/
 
 #include "../main.h"
-//#include "inc/hw_memmap.h"
+#include "inc/hw_memmap.h"
 //#include "inc/hw_uart.h"
 #include "inc/hw_types.h"
 //#include "inc/hw_ints.h"
 #include "inc/hw_sysctl.h"
-//#include "inc/hw_gpio.h"
+#include "inc/hw_gpio.h"
 #include "../hardware/delay.h"
 #include "io_mode.h"
 
 
 
-typedef void (*fn_io_mode)(void);
-
-
 ioMode mIoModes[UART_COUNT];
 
-
-
-#if 0
-void    InputMode2(void) {
-  HWREG(GPIO_PORTD_AHB_BASE + GPIO_O_DATA + 0x0080) = ~0x0020;
-  bPortsStatus &= 0xFE;
-}
-
-
-void    OutputMode2(void) {
-  HWREG(GPIO_PORTD_AHB_BASE + GPIO_O_DATA + 0x0080) = 0x0020;
-  bPortsStatus |= 0x01;
-}
-#endif
 
 
 static void InputMode0(void)
@@ -48,33 +31,68 @@ static void OutputMode0(void)
 
 static void InputMode1(void)
 {
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0020) = ~0x0008;
 }
 
 static void OutputMode1(void)
 {
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0020) = 0x0008;
 }
 
 
 static void InputMode2(void)
 {
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0010) = ~0x0004;
 }
 
 static void OutputMode2(void)
 {
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0010) = 0x0004;
+}
+
+/*
+static void InputMode3(void)
+{
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0008) = ~0x0002;
+}
+
+static void OutputMode3(void)
+{
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0008) = 0x0002;
 }
 
 
+static void InputMode4(void)
+{
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0004) = ~0x0001;
+}
 
-fn_io_mode FnInputModes[UART_COUNT] = {
+static void OutputMode4(void)
+{
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0004) = 0x0001;
+}
+*/
+
+
+typedef void (*fn_io_mode)(void);
+
+
+fn_io_mode mfnInputModes[UART_COUNT] =
+{
   &InputMode0,
   &InputMode1,
   &InputMode2,
+//  &InputMode3,
+//  &InputMode4,
 };
 
-fn_io_mode FnOutputModes[UART_COUNT] = {
+fn_io_mode mfnOutputModes[UART_COUNT] =
+{
   &OutputMode0,
   &OutputMode1,
   &OutputMode2,
+//  &OutputMode3,
+//  &OutputMode4,
 };
 
 
@@ -84,7 +102,7 @@ void InputMode(uchar u)
   ASSERT(u < UART_COUNT);
 
   mIoModes[u] = IO_MODE_INPUT;
-  (*FnInputModes[u])();
+  (*mfnInputModes[u])();
 }
 
 
@@ -93,22 +111,55 @@ void OutputMode(uchar u)
   ASSERT(u < UART_COUNT);
 
   mIoModes[u] = IO_MODE_OUTPUT;
-  (*FnOutputModes[u])();
+  (*mfnOutputModes[u])();
 }
 
 
 
-static void InitIoMode1(void)
+static void InitIoMode0(void)
+{
+}
+
+static void InitIoMode1(void) // PF3
 {
   HWREG(SYSCTL_RCGCGPIO) |= SYSCTL_RCGCGPIO_R5; // GPIO Port F Run Mode Clock Gating Control
   DelayGPIO();
-//  HWREG(GPIO_PORTD_AHB_BASE + GPIO_O_DIR) |= 0x0020; // GPIO Direction
-//  HWREG(GPIO_PORTD_AHB_BASE + GPIO_O_DEN) |= 0x0020; // GPIO Digital Enable
-//
-//  InputMode2();
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DIR) |= 0x0008; // GPIO Direction
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DEN) |= 0x0008; // GPIO Digital Enable
+
+  InputMode(1);
 }
 
+static void InitIoMode2(void) // PF2
+{
+  HWREG(SYSCTL_RCGCGPIO) |= SYSCTL_RCGCGPIO_R5; // GPIO Port F Run Mode Clock Gating Control
+  DelayGPIO();
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DIR) |= 0x0004; // GPIO Direction
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DEN) |= 0x0004; // GPIO Digital Enable
 
+  InputMode(2);
+}
+/*
+static void InitIoMode3(void) // PF1
+{
+  HWREG(SYSCTL_RCGCGPIO) |= SYSCTL_RCGCGPIO_R5; // GPIO Port F Run Mode Clock Gating Control
+  DelayGPIO();
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DIR) |= 0x0002; // GPIO Direction
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DEN) |= 0x0002; // GPIO Digital Enable
+
+  InputMode(3);
+}
+
+static void InitIoMode4(void) // PF0
+{
+  HWREG(SYSCTL_RCGCGPIO) |= SYSCTL_RCGCGPIO_R5; // GPIO Port F Run Mode Clock Gating Control
+  DelayGPIO();
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DIR) |= 0x0001; // GPIO Direction
+  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DEN) |= 0x0001; // GPIO Digital Enable
+
+  InputMode(4);
+}
+*/
 
 void InitIoModes(void)
 {
@@ -118,7 +169,9 @@ void InitIoModes(void)
     mIoModes[u] = IO_MODE_DEFAULT;
   }
 
-//  InitIoMode0();
+  InitIoMode0();
   InitIoMode1();
-//  InitIoMode2();
+  InitIoMode2();
+//  InitIoMode3();
+//  InitIoMode4();
 }
