5de8ff48cd3b55089d853ce48c31af92c4e4f8c1
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-22 12:17:26 +0300

    add ports

--- ek-tm4c1294xl/project2/self/kernel/entities_channels.c
+++ ek-tm4c1294xl/project2/self/kernel/entities_channels.c
@@ -21,15 +21,29 @@ const entity enConnectionTimeout0 = {
 const entity enConnectionTimeout1 = {
   EEPROM_CONNECTION_TIMEOUT_1, &mbConnectionTimeout[1], sizeof(uchar), CHAR, 0,
   0, 255, DEFAULT_CONNECTION_TIMEOUT,
-  "CT@2",
-  "AI=CT@2;D=Connection timeout (minutes);T=INT;C=EDIT/SPIN/0/1/1/10;O=0-Disabled/0;V=CT@2>255?\"Maximum timeout is 255 minutes\":\"\"",
+  "CT@1",
+  "AI=CT@1;D=Connection timeout (minutes);T=INT;C=EDIT/SPIN/0/1/1/10;O=0-Disabled/0;V=CT@1>255?\"Maximum timeout is 255 minutes\":\"\"",
 };
 
 const entity enConnectionTimeout2 = {
   EEPROM_CONNECTION_TIMEOUT_2, &mbConnectionTimeout[2], sizeof(uchar), CHAR, 0,
   0, 255, DEFAULT_CONNECTION_TIMEOUT,
+  "CT@2",
+  "AI=CT@2;D=Connection timeout (minutes);T=INT;C=EDIT/SPIN/0/1/1/10;O=0-Disabled/0;V=CT@2>255?\"Maximum timeout is 255 minutes\":\"\"",
+};
+
+const entity enConnectionTimeout3 = {
+  EEPROM_CONNECTION_TIMEOUT_3, &mbConnectionTimeout[3], sizeof(uchar), CHAR, 0,
+  0, 255, DEFAULT_CONNECTION_TIMEOUT,
   "CT@3",
   "AI=CT@3;D=Connection timeout (minutes);T=INT;C=EDIT/SPIN/0/1/1/10;O=0-Disabled/0;V=CT@3>255?\"Maximum timeout is 255 minutes\":\"\"",
+};
+
+const entity enConnectionTimeout4 = {
+  EEPROM_CONNECTION_TIMEOUT_4, &mbConnectionTimeout[4], sizeof(uchar), CHAR, 0,
+  0, 255, DEFAULT_CONNECTION_TIMEOUT,
+  "CT@4",
+  "AI=CT@4;D=Connection timeout (minutes);T=INT;C=EDIT/SPIN/0/1/1/10;O=0-Disabled/0;V=CT@4>255?\"Maximum timeout is 255 minutes\":\"\"",
 };
 
 
@@ -43,15 +57,29 @@ const entity enRoutingMode0 = {
 const entity enRoutingMode1 = {
   EEPROM_ROUTING_MODE_1, &mbRoutingMode[1], sizeof(uchar), CHAR, 0,
   0, 1, DEFAULT_ROUTING_MODE,
-  "RM@2",
-  "AI=RM@2;D=Routing mode;T=INT;C=STATIC;O=0-Server (Slave)/0/1-Client (Master)/1",
+  "RM@1",
+  "AI=RM@1;D=Routing mode;T=INT;C=STATIC;O=0-Server (Slave)/0/1-Client (Master)/1",
 };
 
 const entity enRoutingMode2 = {
   EEPROM_ROUTING_MODE_2, &mbRoutingMode[2], sizeof(uchar), CHAR, 0,
   0, 1, DEFAULT_ROUTING_MODE,
+  "RM@2",
+  "AI=RM@2;D=Routing mode;T=INT;C=STATIC;O=0-Server (Slave)/0/1-Client (Master)/1",
+};
+
+const entity enRoutingMode3 = {
+  EEPROM_ROUTING_MODE_3, &mbRoutingMode[3], sizeof(uchar), CHAR, 0,
+  0, 1, DEFAULT_ROUTING_MODE,
   "RM@3",
   "AI=RM@3;D=Routing mode;T=INT;C=STATIC;O=0-Server (Slave)/0/1-Client (Master)/1",
+};
+
+const entity enRoutingMode4 = {
+  EEPROM_ROUTING_MODE_4, &mbRoutingMode[4], sizeof(uchar), CHAR, 0,
+  0, 1, DEFAULT_ROUTING_MODE,
+  "RM@4",
+  "AI=RM@4;D=Routing mode;T=INT;C=STATIC;O=0-Server (Slave)/0/1-Client (Master)/1",
 };
 
 
@@ -65,19 +93,31 @@ const entity enPort0 = {
 const entity enPort1 = {
   EEPROM_PORT_1, &mwPort[1], sizeof(uint), INT, 0,
   0, 65534, 1002,
-  "PN@2",
-  "AI=PN@2;E=1;D=Port;T=INT;C=EDIT;V=PN@2>65534?\"Port number must be between 0 and 65534\":\"\";S=RM@2!=1?\"e\":\"i\"",
+  "PN@1",
+  "AI=PN@1;E=1;D=Port;T=INT;C=EDIT;V=PN@1>65534?\"Port number must be between 0 and 65534\":\"\";S=RM@1!=1?\"e\":\"i\"",
 };
 
 const entity enPort2 = {
   EEPROM_PORT_2, &mwPort[2], sizeof(uint), INT, 0,
   0, 65534, 1003,
+  "PN@2",
+  "AI=PN@2;E=1;D=Port;T=INT;C=EDIT;V=PN@2>65534?\"Port number must be between 0 and 65534\":\"\";S=RM@2!=1?\"e\":\"i\"",
+};
+
+const entity enPort3 = {
+  EEPROM_PORT_3, &mwPort[3], sizeof(uint), INT, 0,
+  0, 65534, 1004,
   "PN@3",
   "AI=PN@3;E=1;D=Port;T=INT;C=EDIT;V=PN@3>65534?\"Port number must be between 0 and 65534\":\"\";S=RM@3!=1?\"e\":\"i\"",
 };
 
+const entity enPort4 = {
+  EEPROM_PORT_4, &mwPort[4], sizeof(uint), INT, 0,
+  0, 65534, 1005,
+  "PN@4",
+  "AI=PN@4;E=1;D=Port;T=INT;C=EDIT;V=PN@4>65534?\"Port number must be between 0 and 65534\":\"\";S=RM@4!=1?\"e\":\"i\"",
+};
 
-// http://docs.tibbo,Com/soism/index.html?setting_cm.htm
 
 const entity enConnectionMode0 = {
   EEPROM_CONNECTION_MODE_0, &mbConnectionMode[0], sizeof(uchar), CHAR, 0,
@@ -89,19 +129,31 @@ const entity enConnectionMode0 = {
 const entity enConnectionMode1 = {
   EEPROM_CONNECTION_MODE_1, &mbConnectionMode[1], sizeof(uchar), CHAR, 0,
   0, 1, DEFAULT_CONNECTION_MODE,
-  "CM@2",
-  "AI=CM@2;E=1;D=Connection mode;T=INT;C=STATIC;O=0-Immediately/0/1-On data/1;S=RM@2!=0?\"e\":\"i\"",
+  "CM@1",
+  "AI=CM@1;E=1;D=Connection mode;T=INT;C=STATIC;O=0-Immediately/0/1-On data/1;S=RM@1!=0?\"e\":\"i\"",
 };
 
 const entity enConnectionMode2 = {
   EEPROM_CONNECTION_MODE_2, &mbConnectionMode[2], sizeof(uchar), CHAR, 0,
   0, 1, DEFAULT_CONNECTION_MODE,
+  "CM@2",
+  "AI=CM@2;E=1;D=Connection mode;T=INT;C=STATIC;O=0-Immediately/0/1-On data/1;S=RM@2!=0?\"e\":\"i\"",
+};
+
+const entity enConnectionMode3 = {
+  EEPROM_CONNECTION_MODE_3, &mbConnectionMode[3], sizeof(uchar), CHAR, 0,
+  0, 1, DEFAULT_CONNECTION_MODE,
   "CM@3",
   "AI=CM@3;E=1;D=Connection mode;T=INT;C=STATIC;O=0-Immediately/0/1-On data/1;S=RM@3!=0?\"e\":\"i\"",
 };
 
+const entity enConnectionMode4 = {
+  EEPROM_CONNECTION_MODE_4, &mbConnectionMode[4], sizeof(uchar), CHAR, 0,
+  0, 1, DEFAULT_CONNECTION_MODE,
+  "CM@4",
+  "AI=CM@4;E=1;D=Connection mode;T=INT;C=STATIC;O=0-Immediately/0/1-On data/1;S=RM@4!=0?\"e\":\"i\"",
+};
 
-// http://docs.tibbo,Com/soism/index.html?setting_di.htm
 
 const entity enDestinationIP0 = {
   EEPROM_DESTINATION_IP_0, &mdwDestinationIP[0], sizeof(ulong), IP, 0,
@@ -113,19 +165,31 @@ const entity enDestinationIP0 = {
 const entity enDestinationIP1 = {
   EEPROM_DESTINATION_IP_1, &mdwDestinationIP[1], sizeof(ulong), IP, 0,
   0, MAX_LONG, DEFAULT_DESTINATION_IP,
-  "DI@2",
-  "AI=DI@2;E=1;D=Destination IP-address;T=STRING;C=IPCTRL;S=RM@2==1?\"e\":\"i\"",
+  "DI@1",
+  "AI=DI@1;E=1;D=Destination IP-address;T=STRING;C=IPCTRL;S=RM@1==1?\"e\":\"i\"",
 };
 
 const entity enDestinationIP2 = {
   EEPROM_DESTINATION_IP_2, &mdwDestinationIP[2], sizeof(ulong), IP, 0,
   0, MAX_LONG, DEFAULT_DESTINATION_IP,
+  "DI@2",
+  "AI=DI@2;E=1;D=Destination IP-address;T=STRING;C=IPCTRL;S=RM@2==1?\"e\":\"i\"",
+};
+
+const entity enDestinationIP3 = {
+  EEPROM_DESTINATION_IP_3, &mdwDestinationIP[3], sizeof(ulong), IP, 0,
+  0, MAX_LONG, DEFAULT_DESTINATION_IP,
   "DI@3",
   "AI=DI@3;E=1;D=Destination IP-address;T=STRING;C=IPCTRL;S=RM@3==1?\"e\":\"i\"",
 };
 
+const entity enDestinationIP4 = {
+  EEPROM_DESTINATION_IP_4, &mdwDestinationIP[4], sizeof(ulong), IP, 0,
+  0, MAX_LONG, DEFAULT_DESTINATION_IP,
+  "DI@4",
+  "AI=DI@4;E=1;D=Destination IP-address;T=STRING;C=IPCTRL;S=RM@4==1?\"e\":\"i\"",
+};
 
-// http://docs.tibbo,Com/soism/index.html?setting_dp.htm
 
 const entity enDestinationPort0 = {
   EEPROM_DESTINATION_PORT_0, &mwDestinationPort[0], sizeof(uint), INT, 0,
@@ -137,15 +201,29 @@ const entity enDestinationPort0 = {
 const entity enDestinationPort1 = {
   EEPROM_DESTINATION_PORT_1, &mwDestinationPort[1], sizeof(uint), INT, 0,
   0, MAX_INT, DEFAULT_DESTINATION_PORT+1,
-  "DP@2",
-  "AI=DP@2;E=1;D=Destination port;T=INT;C=EDIT;S=RM@2!=0?\"e\":\"i\"",
+  "DP@1",
+  "AI=DP@1;E=1;D=Destination port;T=INT;C=EDIT;S=RM@1!=0?\"e\":\"i\"",
 };
 
 const entity enDestinationPort2 = {
   EEPROM_DESTINATION_PORT_2, &mwDestinationPort[2], sizeof(uint), INT, 0,
   0, MAX_INT, DEFAULT_DESTINATION_PORT+2,
+  "DP@2",
+  "AI=DP@2;E=1;D=Destination port;T=INT;C=EDIT;S=RM@2!=0?\"e\":\"i\"",
+};
+
+const entity enDestinationPort3 = {
+  EEPROM_DESTINATION_PORT_3, &mwDestinationPort[3], sizeof(uint), INT, 0,
+  0, MAX_INT, DEFAULT_DESTINATION_PORT+3,
   "DP@3",
   "AI=DP@3;E=1;D=Destination port;T=INT;C=EDIT;S=RM@3!=0?\"e\":\"i\"",
+};
+
+const entity enDestinationPort4 = {
+  EEPROM_DESTINATION_PORT_4, &mwDestinationPort[4], sizeof(uint), INT, 0,
+  0, MAX_INT, DEFAULT_DESTINATION_PORT+4,
+  "DP@4",
+  "AI=DP@4;E=1;D=Destination port;T=INT;C=EDIT;S=RM@4!=0?\"e\":\"i\"",
 };
 
 
@@ -159,13 +237,27 @@ const entity enBaudRate0 = {
 const entity enBaudRate1 = {
   EEPROM_BAUD_RATE_1, &mibBaudRate[1], sizeof(uchar), CHAR, 0,
   0, BAUD_RATE_COUNT-1, DEFAULT_BAUD_RATE,
-  "BR@2",
-  "AI=BR@2;D=Baud rate;T=INT;C=STATIC;O=0-150bps/0/1-300bps/1/2-600bps/2/3-1200bps/3/4-2400bps/4/5-4800bps/5/6-9600bps/6/7-19200bps/7/8-28800bps/8/9-38400bps/9/10-57600bps/10/11-115200bps/11/12-230400bps/12/13-460800bps/13",
+  "BR@1",
+  "AI=BR@1;D=Baud rate;T=INT;C=STATIC;O=0-150bps/0/1-300bps/1/2-600bps/2/3-1200bps/3/4-2400bps/4/5-4800bps/5/6-9600bps/6/7-19200bps/7/8-28800bps/8/9-38400bps/9/10-57600bps/10/11-115200bps/11/12-230400bps/12/13-460800bps/13",
 };
 
 const entity enBaudRate2 = {
   EEPROM_BAUD_RATE_2, &mibBaudRate[2], sizeof(uchar), CHAR, 0,
   0, BAUD_RATE_COUNT-1, DEFAULT_BAUD_RATE,
+  "BR@2",
+  "AI=BR@2;D=Baud rate;T=INT;C=STATIC;O=0-150bps/0/1-300bps/1/2-600bps/2/3-1200bps/3/4-2400bps/4/5-4800bps/5/6-9600bps/6/7-19200bps/7/8-28800bps/8/9-38400bps/9/10-57600bps/10/11-115200bps/11/12-230400bps/12/13-460800bps/13",
+};
+
+const entity enBaudRate3 = {
+  EEPROM_BAUD_RATE_3, &mibBaudRate[3], sizeof(uchar), CHAR, 0,
+  0, BAUD_RATE_COUNT-1, DEFAULT_BAUD_RATE,
   "BR@3",
   "AI=BR@3;D=Baud rate;T=INT;C=STATIC;O=0-150bps/0/1-300bps/1/2-600bps/2/3-1200bps/3/4-2400bps/4/5-4800bps/5/6-9600bps/6/7-19200bps/7/8-28800bps/8/9-38400bps/9/10-57600bps/10/11-115200bps/11/12-230400bps/12/13-460800bps/13",
+};
+
+const entity enBaudRate4 = {
+  EEPROM_BAUD_RATE_4, &mibBaudRate[4], sizeof(uchar), CHAR, 0,
+  0, BAUD_RATE_COUNT-1, DEFAULT_BAUD_RATE,
+  "BR@4",
+  "AI=BR@4;D=Baud rate;T=INT;C=STATIC;O=0-150bps/0/1-300bps/1/2-600bps/2/3-1200bps/3/4-2400bps/4/5-4800bps/5/6-9600bps/6/7-19200bps/7/8-28800bps/8/9-38400bps/9/10-57600bps/10/11-115200bps/11/12-230400bps/12/13-460800bps/13",
 };
--- ek-tm4c1294xl/project2/self/kernel/entities_channels.h
+++ ek-tm4c1294xl/project2/self/kernel/entities_channels.h
@@ -7,27 +7,41 @@ entities_channels.h
 extern const entity enConnectionTimeout0;
 extern const entity enConnectionTimeout1;
 extern const entity enConnectionTimeout2;
+extern const entity enConnectionTimeout3;
+extern const entity enConnectionTimeout4;
 
 extern const entity enRoutingMode0;
 extern const entity enRoutingMode1;
 extern const entity enRoutingMode2;
+extern const entity enRoutingMode3;
+extern const entity enRoutingMode4;
 
 extern const entity enPort0;
 extern const entity enPort1;
 extern const entity enPort2;
+extern const entity enPort3;
+extern const entity enPort4;
 
 extern const entity enConnectionMode0;
 extern const entity enConnectionMode1;
 extern const entity enConnectionMode2;
+extern const entity enConnectionMode3;
+extern const entity enConnectionMode4;
 
 extern const entity enDestinationIP0;
 extern const entity enDestinationIP1;
 extern const entity enDestinationIP2;
+extern const entity enDestinationIP3;
+extern const entity enDestinationIP4;
 
 extern const entity enDestinationPort0;
 extern const entity enDestinationPort1;
 extern const entity enDestinationPort2;
+extern const entity enDestinationPort3;
+extern const entity enDestinationPort4;
 
 extern const entity enBaudRate0;
 extern const entity enBaudRate1;
 extern const entity enBaudRate2;
+extern const entity enBaudRate3;
+extern const entity enBaudRate4;
