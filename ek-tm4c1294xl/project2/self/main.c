776aee46a8b44501801625eba2fc13a178a469c1
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-23 21:44:30 +0300

    doc

--- ek-tm4c1294xl/project2/self/main.c
+++ ek-tm4c1294xl/project2/self/main.c
@@ -123,7 +123,7 @@ int     main(void)
 
   while (true)
   {
-    DelayMillySecond(100);
+    DelayMillySecond(100); // SysCtlDelay((SysCtlClockGet() / 3) * 2); //
 
     TaskConnections();
     TaskLwIpDebug();
