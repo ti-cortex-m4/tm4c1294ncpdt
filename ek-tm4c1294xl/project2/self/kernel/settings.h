62ef092e6a5796dd7ef8fe1f1e28c4e76a0fb34d
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-23 15:03:38 +0300

    extract

--- ek-tm4c1294xl/project2/self/kernel/settings.h
+++ ek-tm4c1294xl/project2/self/kernel/settings.h
@@ -5,27 +5,7 @@ SETTINGS,H
 ------------------------------------------------------------------------------*/
 
 #include "entities.h"
-
-
-
-#define DEFAULT_CONNECTION_TIMEOUT 1 // 5
-#define DEFAULT_ROUTING_MODE 0
-#define DEFAULT_PORT 1001
-#define DEFAULT_CONNECTION_MODE 1
-#define DEFAULT_DESTINATION_IP 0x00000001
-#define DEFAULT_DESTINATION_PORT 1001
-
-#define BAUD_RATE_COUNT 14
-#define DEFAULT_BAUD_RATE 6
-
-#define PARITY_COUNT 5
-#define DEFAULT_PARITY 0
-
-#define DATA_BITS_COUNT 2
-#define DEFAULT_DATA_BITS 1
-
-#define STOP_BITS_COUNT 2
-#define DEFAULT_STOP_BITS 0
+#include "entities_default.h"
 
 
 
