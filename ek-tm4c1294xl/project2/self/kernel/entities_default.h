1e3d3caad9878f08faeb8e374e8f6b6424f25f79
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-23 15:03:38 +0300

    extract

---
+++ ek-tm4c1294xl/project2/self/kernel/entities_default.h
@@ -0,0 +1,31 @@
+/*------------------------------------------------------------------------------
+entities_default.h
+
+
+------------------------------------------------------------------------------*/
+
+#ifndef __ENTITIES_DEFAULT_H__
+#define __ENTITIES_DEFAULT_H__
+
+
+#define DEFAULT_CONNECTION_TIMEOUT     1 // TODO 5
+#define DEFAULT_ROUTING_MODE           0
+#define DEFAULT_PORT                   1001
+#define DEFAULT_CONNECTION_MODE        1
+#define DEFAULT_DESTINATION_IP         0x00000001
+#define DEFAULT_DESTINATION_PORT       1001
+
+#define BAUD_RATE_COUNT                14
+#define DEFAULT_BAUD_RATE              6
+
+#define PARITY_COUNT                   5
+#define DEFAULT_PARITY                 0
+
+#define DATA_BITS_COUNT                2
+#define DEFAULT_DATA_BITS              1
+
+#define STOP_BITS_COUNT                2
+#define DEFAULT_STOP_BITS              0
+
+
+#endif
