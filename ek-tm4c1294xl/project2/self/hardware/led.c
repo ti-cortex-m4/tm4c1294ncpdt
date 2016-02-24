bfa21fe20487d1e431efd836875aa91dd573a9e6
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-24 11:48:13 +0300

    fix

--- ek-tm4c1294xl/project2/self/hardware/led.c
+++ ek-tm4c1294xl/project2/self/hardware/led.c
@@ -15,7 +15,40 @@ led.c
 
 
 
-void InitLED(void)
+void InitLEDs(void)
+{
+  GPIOPinTypeGPIOOutput(GPIO_PORTK_BASE, GPIO_PIN_4 | GPIO_PIN_6);
+
+  GPIOPinWrite(GPIO_PORTK_BASE, GPIO_PIN_4 | GPIO_PIN_4, 0);
+  GPIOPadConfigSet(GPIO_PORTK_BASE, GPIO_PIN_4 | GPIO_PIN_6, GPIO_STRENGTH_12MA, GPIO_PIN_TYPE_STD);
+}
+
+
+
+void OffLED1(void)
+{
+  GPIOPinWrite(GPIO_PORTK_BASE, GPIO_PIN_4, GPIO_PIN_4);
+}
+
+void OnLED1(void)
+{
+  GPIOPinWrite(GPIO_PORTK_BASE, GPIO_PIN_4, 0);
+}
+
+
+void OffLED2(void)
+{
+  GPIOPinWrite(GPIO_PORTK_BASE, GPIO_PIN_6, GPIO_PIN_6);
+}
+
+void OnLED2(void)
+{
+  GPIOPinWrite(GPIO_PORTK_BASE, GPIO_PIN_6, 0);
+}
+
+
+
+void InitEthernetLEDs(void)
 {
   GPIOPinConfigure(GPIO_PK4_EN0LED0);
   GPIOPinConfigure(GPIO_PK6_EN0LED1);
