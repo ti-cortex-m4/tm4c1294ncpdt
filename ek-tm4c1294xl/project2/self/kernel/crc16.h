c033f48e7875325d85bd62be5f610a3b736fa667
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-24 10:39:10 +0300

    final check

--- ek-tm4c1294xl/project2/self/kernel/crc16.h
+++ ek-tm4c1294xl/project2/self/kernel/crc16.h
@@ -1,12 +1,7 @@
 /*------------------------------------------------------------------------------
-crc16,h
+crc16.h
 
 
 ------------------------------------------------------------------------------*/
 
-extern  const uchar             mpbCRCHi[0x100], mpbCRCLo[0x100];
-extern  const uchar             bCRCHi, bCRCLo;
-
-void    InitCRC(void);
-void    CalcCRC(uchar  bT);
-void    MakeCRC16(uchar  *pbData, uint  wSize);
+void MakeCRC16(uchar *pbData, uint wSize);
