7028ef9045789c9354f58f64b0dfa93112d784d8
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-24 12:16:51 +0300

    update

--- ek-tm4c1294xl/project2/self/kernel/error.c
+++ ek-tm4c1294xl/project2/self/kernel/error.c
@@ -7,7 +7,10 @@ error.c
 #include "../main.h"
 #include "driverlib/interrupt.h"
 #include "../kernel/log.h"
+#include "../hardware/led.h"
+#include "../hardware/delay.h"
 #include "../uart/uart_log.h"
+#include "error.h"
 
 
 
@@ -18,9 +21,15 @@ void __error__(char *pszFileName, ulong dwLine)
   CONSOLE("assert error: file %s, line %d\n", pszFileName, dwLine);
 
   IntMasterDisable();
-
   while (true)
   {
+    OffLED1();
+    OffLED2();
+    DelayMilliSecond(500);
+
+    OnLED1();
+    OnLED2();
+    DelayMilliSecond(500);
   }
 }
 
