fa04edc3b2a44ee18c48e815edf4add8c9c813e9
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-24 08:56:34 +0300

    fix

---
+++ ek-tm4c1294xl/project2/self/hardware/watchdog.h
@@ -0,0 +1,14 @@
+/*------------------------------------------------------------------------------
+WATCHDOG!H
+
+
+------------------------------------------------------------------------------*/
+#if 0
+#define ENABLE_WATCHDOG
+
+
+void    EnableWatchdog(void);
+void    DisableWatchdog(void);
+void    ResetWatchdog(void);
+bool    IsResetWatchdog(void);
+#endif
