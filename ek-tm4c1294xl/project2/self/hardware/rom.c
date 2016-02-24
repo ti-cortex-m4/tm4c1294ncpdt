372df5eeb3c2ba527679c6d49bd14847d4a04a00
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-24 11:28:23 +0300

    fix

--- ek-tm4c1294xl/project2/self/hardware/rom.c
+++ ek-tm4c1294xl/project2/self/hardware/rom.c
@@ -1,81 +1,86 @@
 /*------------------------------------------------------------------------------
-TODO MEMORY!C
+rom,c
 
 
 ------------------------------------------------------------------------------*/
-#if 0
+
 #include "../main.h"
 #include "inc/hw_types.h"
-#include "driverlib/sysctl.h"
-#include "../display/lcd.h"
-#include "../display/messages.h"
-#include "../kernel/crc-16.h"
+#include "../kernel/crc16.h"
+#include "../kernel/log.h"
+#include "rom.h"
 
 
 
-char const              szImageSize[] = "ImageSize    ImageSize";
+static char const       szBinFileSize[] = "BinFileSize    BinFileSize";
 
 
 
-ulong   GetFileSize(void)
+ulong   GetBinFileSize(void)
 {
   combo32 co;
-  co.mpbBuff[0] = szImageSize[9];
-  co.mpbBuff[1] = szImageSize[10];
-  co.mpbBuff[2] = szImageSize[11];
-  co.mpbBuff[3] = szImageSize[12];
+  co.mpbBuff[0] = szBinFileSize[11];
+  co.mpbBuff[1] = szBinFileSize[12];
+  co.mpbBuff[2] = szBinFileSize[13];
+  co.mpbBuff[3] = szBinFileSize[14];
 
   return co.dwBuff;
 }
 
 
-uchar   GetCODE(uchar  i)
+uchar   GetRomChar(uchar  i)
 {
-  ulong dwFileSize = GetFileSize();
+  ulong dwFileSize = GetBinFileSize();
   return dwFileSize == 0x20202020 ? 0 : HWREGB(dwFileSize + i);
 }
 
 
 uint    GetRomChecksum(void)
 {
-  return GetCODE(0)*0x100 + GetCODE(1);
+  return GetRomChar(0)*0x100 + GetRomChar(1);
 }
 
 
 uint    GetBuildNumber(void)
 {
-  return GetCODE(2)*0x100 + GetCODE(3);
+  return GetRomChar(2)*0x100 + GetRomChar(3);
 }
 
 
-uchar   GetBuildDate_Second(void) { return GetCODE(4); }
-uchar   GetBuildDate_Minute(void) { return GetCODE(5); }
-uchar   GetBuildDate_Hour(void)   { return GetCODE(6); }
-uchar   GetBuildDate_Day(void)    { return GetCODE(7); }
-uchar   GetBuildDate_Month(void)  { return GetCODE(8); }
-uchar   GetBuildDate_Year(void)   { return GetCODE(9); }
+uchar   GetBuildDate_Second(void) { return GetRomChar(4); }
+uchar   GetBuildDate_Minute(void) { return GetRomChar(5); }
+uchar   GetBuildDate_Hour(void)   { return GetRomChar(6); }
+uchar   GetBuildDate_Day(void)    { return GetRomChar(7); }
+uchar   GetBuildDate_Month(void)  { return GetRomChar(8); }
+uchar   GetBuildDate_Year(void)   { return GetRomChar(9); }
 
 
-
+/*
 void    Restart(void)
 {
   SysCtlReset();
 }
+*/
 
 
-
-void    InitCODE(void)
+void    InitROM(void)
 {
-  ulong dwFileSize = GetFileSize();
-  if (dwFileSize != 0x20202020)
+  ulong dwSize = GetBinFileSize();
+  if (dwSize != 0x20202020)
   {
+    dwSize += 2;
+
     ulong i = 0;
-    ulong dwSize = dwFileSize + 2;
 
-    InitCRC();
-    while (dwSize-- > 0) CalcCRC(HWREGB(i++));
+    InitCRC16();
+    while (dwSize-- > 0)
+    {
+      CalcCRC16(HWREGB(i++));
+    }
 
-    if ((bCRCHi != 0) || (bCRCLo != 0)) TestError(szBadCODE);
+    if ((bCRCHi != 0) || (bCRCLo != 0))
+    {
+    }
   }
 }
-#endif
+
