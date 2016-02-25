304d3a0c4a02da2fb4bf6e7b4d887860803897a5
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-25 11:57:32 +0300

    extract

--- ek-tm4c1294xl/project2/self/tcp/telnet.h
+++ ek-tm4c1294xl/project2/self/tcp/telnet.h
@@ -15,9 +15,6 @@ void TelnetFreePbufs(tTelnetSession *pState);
 extern void TelnetInit(void);
 err_t TelnetConnected(void *arg, struct tcp_pcb *pcb, err_t err);
 
-extern void TelnetOpen(uint32_t ulIPAddr, uint16_t usTelnetRemotePort,/* uint16_t usTelnetLocalPort,*/ uint32_t ulSerialPort);
-extern void TelnetListen(uint16_t usTelnetPort, uint32_t ulSerialPort);
-
 extern err_t TelnetCloseClient(uint32_t ulSerialPort);
 extern err_t TelnetCloseServer(uint32_t ulSerialPort);
 extern void TelnetClose(uint32_t ulSerialPort);
--- ek-tm4c1294xl/project2/self/tcp/telnet_open.h
+++ ek-tm4c1294xl/project2/self/tcp/telnet_open.h
@@ -1,6 +1,7 @@
 /*------------------------------------------------------------------------------
-telnet_handler.h
+telnet_open,h
 
 
 ------------------------------------------------------------------------------*/
 
+void TelnetOpen(uint32_t ulIPAddr, uint16_t usTelnetRemotePort,/* uint16_t usTelnetLocalPort,*/ uint32_t ulSerialPort);
