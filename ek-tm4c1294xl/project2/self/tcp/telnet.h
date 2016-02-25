62d1b3295e8d46e2d82dbfe9ef5a137e46764907
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-25 17:10:08 +0300

    fix

--- ek-tm4c1294xl/project2/self/tcp/telnet.h
+++ ek-tm4c1294xl/project2/self/tcp/telnet.h
@@ -21,10 +21,6 @@ err_t TelnetAccept(void *arg, struct tcp_pcb *pcb, err_t err);
 err_t TelnetConnected(void *arg, struct tcp_pcb *pcb, err_t err);
 void TelnetError(void *arg, err_t err);
 
-extern err_t TelnetCloseClient(uint8_t ucSerialPort);
-extern err_t TelnetCloseServer(uint8_t ucSerialPort);
-extern void TelnetClose(uint8_t ucSerialPort);
-
 extern void TelnetHandler(void);
 
 void TelnetNotifyLinkStatus(bool bLinkStatusUp);
