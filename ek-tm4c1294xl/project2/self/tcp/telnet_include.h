2630107faa3731bf97791ee8bbfdea55a9814fa8
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-25 15:16:06 +0300

    disable

--- ek-tm4c1294xl/project2/self/tcp/telnet_include.h
+++ ek-tm4c1294xl/project2/self/tcp/telnet_include.h
@@ -9,33 +9,11 @@ telnet_include.h
 
 
 //*****************************************************************************
-//! The bit in the flag that is set when the remote client has sent a WILL
-//! request for SUPPRESS_GA and the server has accepted it.
-//*****************************************************************************
-#define OPT_FLAG_WILL_SUPPRESS_GA 0
-
-
-//*****************************************************************************
-//! The bit in the flag that is set when the remote client has sent a DO
-//! request for SUPPRESS_GA and the server has accepted it.
-//*****************************************************************************
-#define OPT_FLAG_DO_SUPPRESS_GA 1
-
-
-//*****************************************************************************
-//! The bit in the flag that is set when a connection is operating as a telnet
-//! server.  If clear, this implies that this connection is a telnet client.
-//*****************************************************************************
-#define OPT_FLAG_SERVER         4
-
-
-//*****************************************************************************
 //! The possible states of the TCP session.
 //*****************************************************************************
 typedef enum
 {
-    //! The TCP session is idle.  No connection has been attempted, nor has it
-    //! been configured to listen on any port.
+    //! The TCP session is idle.  No connection has been attempted, nor has it been configured to listen on any port.
     STATE_TCP_IDLE = 0,
 
     //! The TCP session is listening (server mode).
@@ -55,22 +33,19 @@ tTCPState;
 //*****************************************************************************
 typedef struct
 {
-    //! This value holds the pointer to the TCP PCB associated with this
-    //! connected telnet session.
+    //! This value holds the pointer to the TCP PCB associated with this connected telnet session.
     struct tcp_pcb *pConnectPCB;
 
-    //! This value holds the pointer to the TCP PCB associated with this
-    //! listening telnet session.
+    //! This value holds the pointer to the TCP PCB associated with this listening telnet session.
     struct tcp_pcb *pListenPCB;
 
     //! The current state of the TCP session.
     tTCPState eTCPState;
 
-//    //! The current state of the telnet option parser.
+    //! The current state of the telnet option parser.
 //    tTelnetState eTelnetState;
 
-    //! The listen port for the telnet server or the local port for the telnet
-    //! client.
+    //! The listen port for the telnet server or the local port for the telnet client.
     uint16_t usTelnetLocalPort;
 
     //! The remote port that the telnet client connects to.
@@ -80,7 +55,7 @@ typedef struct
     uint32_t ulTelnetRemoteIP;
 
     //! Flags for various options associated with the telnet session.
-    uint8_t ucFlags;
+//    uint8_t ucFlags;
 
     //! A counter for the TCP connection timeout.
     uint32_t ulConnectionTimeout;
@@ -100,16 +75,13 @@ typedef struct
     //! This value holds the write index for the pbuf queue.
     int iBufQWrite;
 
-    //! This value holds the head of the pbuf that is currently being
-    //! processed (that has been popped from the queue).
+    //! This value holds the head of the pbuf that is currently being processed (that has been popped from the queue).
     struct pbuf *pBufHead;
 
-    //! This value holds the actual pbuf that is being processed within the
-    //! pbuf chain pointed to by the pbuf head.
+    //! This value holds the actual pbuf that is being processed within the pbuf chain pointed to by the pbuf head.
     struct pbuf *pBufCurrent;
 
-    //! This value holds the offset into the payload section of the current
-    //! pbuf.
+    //! This value holds the offset into the payload section of the current pbuf.
     uint32_t ulBufIndex;
 
     //! The amount of time passed since rx byte count has changed.
