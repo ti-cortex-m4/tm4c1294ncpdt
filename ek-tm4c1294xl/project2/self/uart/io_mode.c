6b268eeed8bd2d4bbb84c2c248ebe3f0f31a9466
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-02-26 16:22:01 +0300

    fix

--- ek-tm4c1294xl/project2/self/uart/io_mode.c
+++ ek-tm4c1294xl/project2/self/uart/io_mode.c
@@ -10,12 +10,12 @@ io_mode.c
 #include "inc/hw_sysctl.h"
 #include "inc/hw_gpio.h"
 #include "../hardware/delay.h"
-#include "../kernel/printf.h"
+#include "../kernel/log.h"
 #include "io_mode.h"
 
 
 
-/*static*/ ioMode mIoModes[UART_COUNT];
+ioMode                  mbIoModes[UART_COUNT];
 
 
 
@@ -30,52 +30,52 @@ static void OutputMode0(void)
 
 static void InputMode1(void)
 {
-  mIoModes[1] = IO_MODE_INPUT;
+  mbIoModes[1] = IO_MODE_INPUT;
   HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0020) = ~0x0008;
 }
 
 static void OutputMode1(void)
 {
-  mIoModes[1] = IO_MODE_OUTPUT;
+  mbIoModes[1] = IO_MODE_OUTPUT;
   HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0020) = 0x0008;
 }
 
 
 static void InputMode2(void)
 {
-  mIoModes[2] = IO_MODE_INPUT;
+  mbIoModes[2] = IO_MODE_INPUT;
   HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0010) = ~0x0004;
 }
 
 static void OutputMode2(void)
 {
-  mIoModes[2] = IO_MODE_OUTPUT;
+  mbIoModes[2] = IO_MODE_OUTPUT;
   HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0010) = 0x0004;
 }
 
 
 static void InputMode3(void)
 {
-  mIoModes[3] = IO_MODE_INPUT;
+  mbIoModes[3] = IO_MODE_INPUT;
   HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0008) = ~0x0002;
 }
 
 static void OutputMode3(void)
 {
-  mIoModes[3] = IO_MODE_OUTPUT;
+  mbIoModes[3] = IO_MODE_OUTPUT;
   HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0008) = 0x0002;
 }
 
 
 static void InputMode4(void)
 {
-  mIoModes[4] = IO_MODE_INPUT;
+  mbIoModes[4] = IO_MODE_INPUT;
   HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0004) = ~0x0001;
 }
 
 static void OutputMode4(void)
 {
-  mIoModes[4] = IO_MODE_OUTPUT;
+  mbIoModes[4] = IO_MODE_OUTPUT;
   HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DATA + 0x0004) = 0x0001;
 }
 
@@ -107,7 +107,7 @@ static const fn_io_mode mfnOutputModes[UART_COUNT] =
 void InputMode(uchar u)
 {
   ASSERT(u < UART_COUNT);
-  UdpPrintF("%u: input mode\n",u);
+  CONSOLE("%u: input mode\n",u);
   (*mfnInputModes[u])();
 }
 
@@ -115,7 +115,7 @@ void InputMode(uchar u)
 void OutputMode(uchar u)
 {
   ASSERT(u < UART_COUNT);
-  UdpPrintF("%u: output mode\n",u);
+  CONSOLE("%u: output mode\n",u);
   (*mfnOutputModes[u])();
 }
 
@@ -171,7 +171,7 @@ void InitIoModes(void)
   uchar u;
   for(u = 0; u < UART_COUNT; u++)
   {
-    mIoModes[u] = IO_MODE_DEFAULT;
+    mbIoModes[u] = IO_MODE_DEFAULT;
   }
 
   InitIoMode0();
