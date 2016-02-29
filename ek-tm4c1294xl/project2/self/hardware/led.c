3fbcc6e2ccdcf6d4ec869c6355c7f1cb83fc2480
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-02-29 12:52:37 +0300

    fix

--- ek-tm4c1294xl/project2/self/hardware/led.c
+++ ek-tm4c1294xl/project2/self/hardware/led.c
@@ -7,6 +7,7 @@ led.c
 #include "../main.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_emac.h"
+#include "inc/hw_types.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 #include "driverlib/pin_map.h"
@@ -63,8 +64,27 @@ void OnLED1(void)
 
 void InitEthernetLEDs(void)
 {
+  // Set Ethernet LED polarity to be active low
+  HWREG(EMAC0_BASE + EMAC_O_CC) |= EMAC_CC_POL;
+
+
   GPIOPinConfigure(GPIO_PK4_EN0LED0);
   GPIOPinConfigure(GPIO_PK6_EN0LED1);
 
   GPIOPinTypeEthernetLED(GPIO_PORTK_BASE, GPIO_PIN_4 | GPIO_PIN_6);
+
+
+  EMACPHYExtendedWrite(EMAC0_BASE, 0 , EPHY_LEDCR,
+  (EMACPHYExtendedRead(EMAC0_BASE, 0 , EPHY_LEDCR) & ~ EPHY_LEDCR_BLINKRATE_M) |
+  EPHY_LEDCR_BLINKRATE_20HZ);
+
+  EMACPHYExtendedWrite(EMAC0_BASE, 0 , EPHY_LEDCFG,
+  (EMACPHYExtendedRead(EMAC0_BASE, 0 , EPHY_LEDCFG) & ~ (EPHY_LEDCFG_LED0_M | EPHY_LEDCFG_LED1_M | EPHY_LEDCFG_LED2_M)) |
+  EPHY_LEDCFG_LED0_LINK |
+  EPHY_LEDCFG_LED1_RXTX);
 }
+
+
+
+// http://e2e.ti.com/support/microcontrollers/tiva_arm/f/908/t/378413
+// http://e2e.ti.com/support/microcontrollers/tiva_arm/f/908/p/377773/1333509
