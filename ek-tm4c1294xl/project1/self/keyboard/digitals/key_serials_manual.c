f46fee39badeb61000117034d7123e841985c302
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-03-04 16:30:22 +0300

    fix

--- ek-tm4c1294xl/project1/self/keyboard/digitals/key_serials_manual.c
+++ ek-tm4c1294xl/project1/self/keyboard/digitals/key_serials_manual.c
@@ -4,16 +4,18 @@ KEY_SERIALS_MANUAL.C
 
 ------------------------------------------------------------------------------*/
 
-#include "../../main.h"
-#include "../../digital/digitals.h"
-#include "../common/key_bool.h"
+#include "../../console.h"
+#include "../../digitals/digitals.h"
+#include "../common/key_flag.h"
 #include "key_serials_manual.h"
 
 
 
 //                                         0123456789ABCDEF
-static char const       szMessage1[]    = "Ручная установка",
-                        szMessage2[]    = "заводск. номеров";
+static char const       szMessage1[]    = "   Установка    ",
+                        szMessage2[]    = "заводск. номеров",
+                        szTrue[]        = " ручная         ",
+                        szFalse[]       = " автоматическая ";
 
 static char const       *pszMessages[]  = { szMessage1, szMessage2, "" };
 
@@ -21,5 +23,5 @@ static char const       *pszMessages[]  = { szMessage1, szMessage2, "" };
 
 void    key_SetSerialsManual(void)
 {
-  key_SetBool(&chSerialsManual, pszMessages);
+  key_SetFlag(&chSerialsManual, pszMessages, szTrue, szFalse);
 }
