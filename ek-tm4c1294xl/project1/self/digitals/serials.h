50b137aa0c25134ff2d088198304f2b5a2f3caa0
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-03-09 14:51:54 +0300

    fix

--- ek-tm4c1294xl/project1/self/digitals/serials.h
+++ ek-tm4c1294xl/project1/self/digitals/serials.h
@@ -6,11 +6,13 @@ serials.h
 
 extern  bool                    fSerialsManual;
 extern  ulong                   mdwSerialValues[bCANALS];
+extern  time                    mtiSerialTimes[bCANALS];
 extern  bool                    mfSerialFlags[bCANALS];
 
 
 extern  cache const             chSerialsManual;
 extern  cache const             chSerialValues;
+extern  cache const             chSerialTimes;
 extern  cache const             chSerialFlags;
 
 
