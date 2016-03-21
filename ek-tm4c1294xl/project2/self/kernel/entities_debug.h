ae9c4abb842f5defd1557ac55c08541a96b5d713
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-03-21 14:46:57 +0300

    fix

--- ek-tm4c1294xl/project2/self/kernel/entities_debug.h
+++ ek-tm4c1294xl/project2/self/kernel/entities_debug.h
@@ -13,6 +13,7 @@ extern const entity enLwipDebugFlag;
 extern const entity enLwipDebugTimeout;
 
 extern const entity enDataDebugFlag;
+extern const entity enIOModeDebugFlag;
 
 extern const entity enLED0Mode;
 extern const entity enLED1Mode;
