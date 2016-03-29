65e1c60765967e21f24ec986f6890e80dab1ff23
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-03-29 15:10:34 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor33/automatic33.h
@@ -0,0 +1,18 @@
+/*------------------------------------------------------------------------------
+AUTOMATIC33.H
+
+
+------------------------------------------------------------------------------*/
+
+//bool    Checksum33(uchar  bSize);
+
+//void    Query33(uint  cwIn, uchar  cbOut);
+//serial  Input33(void);
+
+//bool    Automatic33(void);
+
+//bool    QueryOpen33_Full(uchar  bPercent);
+//time2   QueryTime33_Full(uchar  bPercent);
+
+//time2   ReadTimeCan33(void);
+//double2 ReadCntCurr33(void);
