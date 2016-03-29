4e7c394fad51274c5e9a8e59d3e9c1cec294bd38
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-03-29 15:11:27 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor33/device33.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor33/device33.h
@@ -1,12 +1,25 @@
 /*------------------------------------------------------------------------------
+DEVICE33.H
 
 
 ------------------------------------------------------------------------------*/
 
+#define MAX_LINE_N33    1
 
 
 
+//ulong   PopChar3Big33(void);
 
+//void    QueryOpen33(void);
+//bool    ReadOpen33(void);
+//uchar   GetVersion33(void);
+//bool    OldVersion33(void);
 
+//void    QueryTime33(void);
+//time    ReadTime33(void);
+//time    ReadPackTime33(void);
+//void    QueryControl33(time  ti);
 
+//void    QueryEngAbs33(uchar  ibTrf);
 
+//bool    Checksum33(uchar  bSize);
