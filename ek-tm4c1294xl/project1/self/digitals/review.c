19ff5e67b86538d32f09fba2e57b23be068b7735
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-05-16 09:53:02 +0300

    rename

---
+++ ek-tm4c1294xl/project1/self/digitals/review.c
@@ -0,0 +1,33 @@
+/*------------------------------------------------------------------------------
+review.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+#include "../nvram/cache.h"
+#include "../nvram/cache2.h"
+#include "review.h"
+
+
+
+bool                    fReviewReadId;
+uchar                   bReviewRepeats;
+
+cache const             chReviewReadId = {REVIEW_READ_ID, &fReviewReadId, sizeof(bool)};
+cache const             chbReviewRepeats = {REVIEW_REPEATS, &bReviewRepeats, sizeof(uchar)};
+
+
+
+void    InitReview(void)
+{
+  LoadCacheBool(&chReviewReadId, false);
+  LoadCacheChar(&chbReviewRepeats, 0, 4, 0);
+}
+
+
+void    ResetReview(void)
+{
+  SaveCacheBool(&chReviewReadId, false);
+  SaveCacheChar(&chbReviewRepeats, 0);
+}
