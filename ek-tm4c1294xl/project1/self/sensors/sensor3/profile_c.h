42db50ffae948edd1332d85e43015eed66224f56
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-05-19 14:57:29 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor3/profile_c.h
@@ -0,0 +1,41 @@
+/*------------------------------------------------------------------------------
+DEVICE_C!H
+
+
+------------------------------------------------------------------------------*/
+
+extern  uint                    cwShutdownC;
+
+
+
+bool    ReadResultC(void);
+void    QueryOpenC(void);
+
+void    QuerySerialC(void);
+ulong   ReadSerialC(void);
+
+void    QueryIdC(void);
+bool    ReadIdC(void);
+
+void    QueryTimeC(void);
+time    ReadTimeC(void);
+
+void    QueryControlC(void);
+
+void    QueryVersionC(void);
+void    ReadVersionC(void);
+
+void    QueryEnergyAbsC(void);
+void    QueryEnergyMonC(uchar  ibMonth);
+void    QueryEnergyDayC(uchar  ibDay);
+void    ReadEnergyC(void);
+
+void    InitHeaderC1(void);
+void    InitHeaderC6(void);
+void    QueryHeaderC1(void);
+void    QueryHeaderC6(void);
+bool    ReadHeaderC1(void);
+bool    ReadHeaderC6(void);
+
+void    ReadCurrentC(void);
+
