24fc8a1d1bb33bacbf0998ee70c7d8f75568168e
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-05-24 08:52:57 +0300

    Revert "temporary"
    This reverts commit 7991bdfe9df87c12f86bae83f6ccd7c0b6cf9f47.

--- ek-tm4c1294xl/project1/self/display/lcd.c
+++ ek-tm4c1294xl/project1/self/display/lcd.c
@@ -25,7 +25,7 @@ LCD!C
 #define LCD_FLAG_DATA    0x01 // передать данные
 
 
-static uchar const      szName[]        = "*   СЭМ-2.01   *",
+static uchar const      szName[]        = "    СЭМ-2.01    ",
                         szTest[]        = "                ";
 
 
