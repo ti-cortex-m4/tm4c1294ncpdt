1b5467f002195d05d448c2fa82c88c2c33a84f43
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-05-26 11:38:23 +0300

    fix

--- ek-tm4c1294xl/project1/self/keyboard/digitals/review/key_review_flag.c
+++ ek-tm4c1294xl/project1/self/keyboard/digitals/review/key_review_flag.c
@@ -12,11 +12,10 @@ key_review_flag.c
 
 
 //                                         0123456789ABCDEF
-static char const       szMessage1[]    = " Чтение логич.  ",
-                        szMessage2[]    = "  номеров при   ",
-                        szMessage3[]    = "чтении профилей ";
+static char const       szMessage1[]    = "  Проверка при  ",
+                        szMessage2[]    = "чтении профилей ";
 
-static char const       *pszMessages[]  = { szMessage1, szMessage2, szMessage3, "" };
+static char const       *pszMessages[]  = { szMessage1, szMessage2, "" };
 
 
 
