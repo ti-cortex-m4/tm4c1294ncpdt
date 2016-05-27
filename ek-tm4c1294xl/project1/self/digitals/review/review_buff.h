d07c5a6d5b717479077749c9fbf3b80bb87f613d
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-05-27 14:47:55 +0300

    fix

--- ek-tm4c1294xl/project1/self/digitals/review/review_buff.h
+++ ek-tm4c1294xl/project1/self/digitals/review/review_buff.h
@@ -8,7 +8,6 @@ void StartReviewBuff(void);
 void RestartReviewBuff(void);
 
 void NextReviewBuff(void);
-bool WarnReviewBuff(uchar  bSize);
 void SaveReviewBuff(uchar  ibMin, uchar  ibMax);
 bool TestReviewBuff(uchar  ibMin, uchar  ibMax);
 
