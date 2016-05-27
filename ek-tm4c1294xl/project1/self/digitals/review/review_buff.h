46a21bae152e2f5166cb92244ea476057a7f2d64
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-05-27 11:24:23 +0300

    fix

--- ek-tm4c1294xl/project1/self/digitals/review/review_buff.h
+++ ek-tm4c1294xl/project1/self/digitals/review/review_buff.h
@@ -8,7 +8,7 @@ void StartReviewBuff(void);
 void RestartReviewBuff(void);
 
 void SwitchReviewBuff(void);
-bool WarnReviewBuff(uchar  bCount);
+bool WarnReviewBuff(uchar  bSize);
 void SaveReviewBuff(uchar  ibMin, uchar  ibMax);
 bool TestReviewBuff(uchar  ibMin, uchar  ibMax);
 
