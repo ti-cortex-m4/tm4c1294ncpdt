6aaa78be446e6736a21c2454b8d9b4066fa7a937
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-05-27 14:58:57 +0300

    rename

--- ek-tm4c1294xl/project1/self/digitals/review/review_warning.c
+++ ek-tm4c1294xl/project1/self/digitals/review/review_warning.c
@@ -17,7 +17,7 @@ review_warning.c
 
 
 
-static bool Warn(uint  wPrev, uint  wCurr)
+static bool Warning(uint  wPrev, uint  wCurr)
 {
   MonitorString("\n "); MonitorIntDec(wPrev); MonitorString(" -> "); MonitorIntDec(wCurr);
 
@@ -42,11 +42,11 @@ static bool Warn(uint  wPrev, uint  wCurr)
   return false;
 }
 
-static bool WarnReviewBuffInner6(void)
+static bool WarningReviewBuffInner6(void)
 {
   uchar i;
   for (i=0; i<4; i++) {
-    if (Warn(Prev(5*4+i),Curr(0*4+i))) return true;
+    if (Warning(Prev(5*4+i),Curr(0*4+i))) return true;
   }
 
   uchar j;
@@ -55,29 +55,29 @@ static bool WarnReviewBuffInner6(void)
 
     uchar i;
     for (i=0; i<4; i++) {
-      if (Warn(Curr(j*4+i),Curr((j+1)*4+i))) return true;
+      if (Warning(Curr(j*4+i),Curr((j+1)*4+i))) return true;
     }
   }
 
   return false;
 }
 
-static bool WarnReviewBuffInner1(void)
+static bool WarningReviewBuffInner1(void)
 {
   uchar i;
   for (i=0; i<4; i++) {
-    if (Warn(Prev(i),Curr(i))) return true;
+    if (Warning(Prev(i),Curr(i))) return true;
   }
 
   return false;
 }
 
-bool WarnReviewBuff(uchar  bSize)
+bool WarningReviewBuff(uchar  bSize)
 {
   switch(bSize) {
     case 0: return false;
-    case 6: return WarnReviewBuffInner6();
-    case 1: return WarnReviewBuffInner1();
+    case 6: return WarningReviewBuffInner6();
+    case 1: return WarningReviewBuffInner1();
     default: ASSERT(false); return false;
   }
 }
