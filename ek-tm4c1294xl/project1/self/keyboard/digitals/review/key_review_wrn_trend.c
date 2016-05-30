1938d7555fd2a55423ff7a2afdd759e8f9975cce
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-05-30 13:53:03 +0300

    fix

--- ek-tm4c1294xl/project1/self/keyboard/digitals/review/key_review_wrn_trend.c
+++ ek-tm4c1294xl/project1/self/keyboard/digitals/review/key_review_wrn_trend.c
@@ -4,6 +4,24 @@ key_review_wrn_trend.c
 
 ------------------------------------------------------------------------------*/
 
+#include "../../../console.h"
+#include "../../../digitals/review/review.h"
+#include "../../common/key_char_blocked.h"
+#include "key_review_wrn_trend.h"
+
+
+
+//                                         0123456789ABCDEF
+static char const       szMessage1[]    = "  ������������  ",
+                        szMessage2[]    = "  % ����������  ",
+                        szMessage3[]    = " ��������� ���  ",
+                        szMessage4[]    = "������ �������� ";
+
+static char const       *pszMessages[]  = { szMessage1, szMessage2, szMessage3, szMessage4, szNumberLimits, "" };
+
+
+
 void    key_SetReviewWrnTrend(void)
 {
+  key_SetCharBlocked(&chReviewWrnTrend, pszMessages, REVIEW_WRN_TREND_MIN, REVIEW_WRN_TREND_MAX, fReviewFlag, wSET_REVIEW_FLAG);
 }
