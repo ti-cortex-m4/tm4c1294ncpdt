8dbc20babacc4a575f769976395e38841c866fa1
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-06-01 09:43:55 +0300

    fix

--- ek-tm4c1294xl/project1/self/digitals/review/review.c
+++ ek-tm4c1294xl/project1/self/digitals/review/review.c
@@ -14,6 +14,7 @@ review.c
 bool                    fReviewFlag;
 bool                    fReviewReadId;
 uchar                   bReviewRepeats;
+uchar                   bReviewBorders;
 uchar                   bReviewMargins;
 uchar                   bReviewWrnTrend;
 uint                    wReviewWrnTop;
@@ -21,6 +22,7 @@ uint                    wReviewWrnTop;
 cache const             chReviewFlag = {REVIEW_FLAG, &fReviewFlag, sizeof(bool)};
 cache const             chReviewReadId = {REVIEW_READ_ID, &fReviewReadId, sizeof(bool)};
 cache const             chReviewRepeats = {REVIEW_REPEATS, &bReviewRepeats, sizeof(uchar)};
+cache const             chReviewBorders = {REVIEW_BORDERS, &bReviewBorders, sizeof(uchar)};
 cache const             chReviewMargins = {REVIEW_MARGINS, &bReviewMargins, sizeof(uchar)};
 cache const             chReviewWrnTrend = {REVIEW_WRN_TREND, &bReviewWrnTrend, sizeof(uchar)};
 cache const             chReviewWrnTop = {REVIEW_WRN_TOP, &wReviewWrnTop, sizeof(uint)};
@@ -32,6 +34,7 @@ void InitReview(void)
   LoadCacheBool(&chReviewFlag, false);
   LoadCacheBool(&chReviewReadId, false);
   LoadCacheChar(&chReviewRepeats, REVIEW_REPEATS_MIN, REVIEW_REPEATS_MAX, REVIEW_REPEATS_DEF);
+  LoadCacheChar(&chReviewBorders, REVIEW_BORDERS_MIN, REVIEW_BORDERS_MAX, REVIEW_BORDERS_DEF);
   LoadCacheChar(&chReviewMargins, REVIEW_MARGINS_MIN, REVIEW_MARGINS_MAX, REVIEW_MARGINS_DEF);
   LoadCacheChar(&chReviewWrnTrend, REVIEW_WRN_TREND_MIN, REVIEW_WRN_TREND_MAX, REVIEW_WRN_TREND_DEF);
   LoadCacheInt(&chReviewWrnTop, REVIEW_WRN_TOP_MIN, REVIEW_WRN_TOP_MAX, REVIEW_WRN_TOP_DEF);
@@ -45,6 +48,7 @@ void ResetReview(bool  fFull)
     SaveCacheBool(&chReviewFlag, false);
     SaveCacheBool(&chReviewReadId, false);
     SaveCacheChar(&chReviewRepeats, REVIEW_REPEATS_DEF);
+    SaveCacheChar(&chReviewBorders, REVIEW_BORDERS_DEF);
     SaveCacheChar(&chReviewMargins, REVIEW_MARGINS_DEF);
     SaveCacheChar(&chReviewWrnTrend, REVIEW_WRN_TREND_DEF);
     SaveCacheInt(&chReviewWrnTop, REVIEW_WRN_TOP_DEF);
--- ek-tm4c1294xl/project1/self/digitals/review/review.h
+++ ek-tm4c1294xl/project1/self/digitals/review/review.h
@@ -11,23 +11,28 @@ review.h
 #define REVIEW_REPEATS_MAX    5
 #define REVIEW_REPEATS_DEF    1
 
+#define REVIEW_BORDERS_MIN    1
+#define REVIEW_BORDERS_MAX    5
+#define REVIEW_BORDERS_DEF    5
+
 #define REVIEW_MARGINS_MIN    10
 #define REVIEW_MARGINS_MAX    50
 #define REVIEW_MARGINS_DEF    20
 
-#define REVIEW_WRN_TREND_MIN  10
-#define REVIEW_WRN_TREND_MAX  100
+#define REVIEW_WRN_TREND_MIN  5
+#define REVIEW_WRN_TREND_MAX  95
 #define REVIEW_WRN_TREND_DEF  50
 
 #define REVIEW_WRN_TOP_MIN    100
 #define REVIEW_WRN_TOP_MAX    0xFFFF
-#define REVIEW_WRN_TOP_DEF    30000
+#define REVIEW_WRN_TOP_DEF    0x8000
 
 
 
 extern bool                    fReviewFlag;
 extern bool                    fReviewReadId;
 extern uchar                   bReviewRepeats;
+extern uchar                   bReviewBorders;
 extern uchar                   bReviewMargins;
 extern uchar                   bReviewWrnTrend;
 extern uint                    wReviewWrnTop;
@@ -35,6 +40,7 @@ extern uint                    wReviewWrnTop;
 extern cache const             chReviewFlag;
 extern cache const             chReviewReadId;
 extern cache const             chReviewRepeats;
+extern cache const             chReviewBorders;
 extern cache const             chReviewMargins;
 extern cache const             chReviewWrnTrend;
 extern cache const             chReviewWrnTop;
