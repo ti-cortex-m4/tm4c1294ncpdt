7f6636838cdd8e13fbbd9c0443e4e967a87352ce
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-06-09 11:37:30 +0300

    fix

--- ek-tm4c1294xl/project2/self/udp/routing_status.c
+++ ek-tm4c1294xl/project2/self/udp/routing_status.c
@@ -6,3 +6,25 @@ routing_status.c
 
 #include "../main.h"
 #include "routing_status.h"
+
+
+
+bool IsRoutingStatusSize(struct pbuf *p) {
+  return false;
+}
+
+
+err_t GetRouingStatusSize(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast) {
+  return ERR_OK;
+}
+
+
+
+bool IsRoutingStatusContent(struct pbuf *p) {
+  return false;
+}
+
+
+err_t GetRouingStatusContent(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast) {
+  return ERR_OK;
+}
--- ek-tm4c1294xl/project2/self/udp/routing_status.h
+++ ek-tm4c1294xl/project2/self/udp/routing_status.h
@@ -4,3 +4,12 @@ routing_status.h
 
 ------------------------------------------------------------------------------*/
 
+#include "lwip/udp.h"
+
+
+
+bool IsRoutingStatusSize(struct pbuf *p);
+err_t GetRouingStatusSize(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast);
+
+bool IsRoutingStatusContent(struct pbuf *p);
+err_t GetRouingStatusContent(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast);
