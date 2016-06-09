47e84d0c1e1b7f69a94a80ab948ddade8cfecf90
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-06-09 14:13:49 +0300

    fix

--- ek-tm4c1294xl/project2/self/kernel/wrappers.c
+++ ek-tm4c1294xl/project2/self/kernel/wrappers.c
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------------
-WRAPPERS,C
+wrappers.c
 
- Значения с флагом достоверности
+
 ------------------------------------------------------------------------------*/
 
 #include "../main.h"
@@ -9,12 +9,46 @@ WRAPPERS,C
 
 
 
-ulong2  GetLong2(ulong  dwValue, bool  fValid)
+uchar GetChar2(uchar  w, err_t  err);
+{
+  uchar dw;
+
+  dw.dw = dw;
+  dw.err = err;
+
+  return dw;
+}
+
+
+uchar  GetChar2Error(void)
+{
+  return GetChar2(MAX_CHAR, ERR_VAL);
+}
+
+uint GetInt2(uint  w, err_t  err);
+{
+  uint dw;
+
+  dw.dw = dw;
+  dw.err = err;
+
+  return dw;
+}
+
+
+uint  GetInt2Error(void)
+{
+  return GetInt2(MAX_INT, ERR_VAL);
+}
+
+
+
+ulong2 GetLong2(ulong  dw, err_t  err);
 {
   ulong2 dw2;
 
-  dw2.dwValue = dwValue;
-  dw2.fValid = fValid;
+  dw2.dw = dw;
+  dw2.err = err;
 
   return dw2;
 }
@@ -22,78 +56,5 @@ ulong2  GetLong2(ulong  dwValue, bool  fValid)
 
 ulong2  GetLong2Error(void)
 {
-  return GetLong2(MAX_LONG, false);
-}
-
-
-
-float2  GetFloat2(float  flValue, bool  fValid)
-{
-  float2 fl2;
-
-  fl2.flValue = flValue;
-  fl2.fValid = fValid;
-
-  return fl2;
-}
-
-
-float2  GetFloat2Error(void)
-{
-  return GetFloat2(-1, false);
-}
-
-
-
-double2 GetDouble2(double  dbValue, bool  fValid)
-{
-  double2 db2;
-
-  db2.dbValue = dbValue;
-  db2.fValid = fValid;
-
-  return db2;
-}
-
-
-double2 GetDouble2Error(void)
-{
-  return GetDouble2(-1, false);
-}
-
-
-
-time2   GetTime2(time  tiValue, bool  fValid)
-{
-  time2 ti2;
-
-  ti2.tiValue = tiValue;
-  ti2.fValid = fValid;
-
-  return ti2;
-}
-
-
-time2   GetTime2Error(void)
-{
-  static const time tiZero = { 0, 0, 0, 0, 0, 0 };
-  return GetTime2(tiZero, false);
-}
-
-
-
-factors2 GetFactors2(factors  fcValue, bool  fValid)
-{
-  factors2 fc2;
-
-  fc2.fcValue = fcValue;
-  fc2.fValid = fValid;
-
-  return fc2;
-}
-
-
-factors2 GetFactors2Error(void)
-{
-  return GetFactors2(GetFactors(-1,-1), false);
+  return GetLong2(MAX_LONG, ERR_VAL);
 }
