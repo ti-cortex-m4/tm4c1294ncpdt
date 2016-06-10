c2875ee14aade16be8030d95ad66974eca8f5f35
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-06-10 10:52:41 +0300

    fix

--- ek-tm4c1294xl/project2/self/tcp/telnet_handler.c
+++ ek-tm4c1294xl/project2/self/tcp/telnet_handler.c
@@ -211,7 +211,7 @@ void TelnetHandler(void)
                     uint8_t ucChar = SerialReceive(pState->ucSerialPort);
 
                     if (fDataDebugFlag)
-                      CONSOLE("%u: %02X}\n", u, ucChar);
+                      CONSOLE("%u: to TCP %02X\n", u, ucChar);
 
                     pucTemp[lIndex] = ucChar;
                     lIndex++;
