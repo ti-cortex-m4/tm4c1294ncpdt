6fcc5534d6cb1fa02cdc406aea9c6db878266ee9
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-06-10 15:11:45 +0300

    fix

--- ek-tm4c1294xl/project2/self/kernel/log.h
+++ ek-tm4c1294xl/project2/self/kernel/log.h
@@ -10,10 +10,12 @@ log.h
 
 #ifdef DEBUG
 #define CONSOLE DebugPrintF
-#define WARNING DebugPrintF
+#define WARNING DebugPrintF("WARNING "); DebugPrintF
+#define ERROR DebugPrintF("ERROR "); DebugPrintF
 #else
 #define CONSOLE
 #define WARNING
+#define ERROR
 #endif
 
 
