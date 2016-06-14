7f8f8052b71eabc8480e7650fea2561cb02fd469
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-06-14 10:38:15 +0300

    fix

--- ek-tm4c1294xl/project2/self/udp/routing_status.c
+++ ek-tm4c1294xl/project2/self/udp/routing_status.c
@@ -11,6 +11,7 @@ routing_status.c
 #include "../kernel/settings.h"
 #include "../uart/io_mode.h"
 #include "../uart/serial.h"
+#include "udp_log.h"
 #include "udp_pop.h"
 #include "udp_out.h"
 #include "udp_in_common.h"
