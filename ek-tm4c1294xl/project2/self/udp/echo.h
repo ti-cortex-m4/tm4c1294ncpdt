6ec4a2841ac74b56cafc4f0297baa1b81d7756ba
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-06-14 14:42:54 +0300

    fix

--- ek-tm4c1294xl/project2/self/udp/echo.h
+++ ek-tm4c1294xl/project2/self/udp/echo.h
@@ -8,4 +8,4 @@ echo.h
 
 
 
-err_t CmdX(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast);
+err_t OutEcho(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast);
