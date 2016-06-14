47ebacf2e50d0caab2f8053da824350f271b9bf8
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-06-14 09:18:52 +0300

    extract

--- ek-tm4c1294xl/project2/self/udp/udp_entities.h
+++ ek-tm4c1294xl/project2/self/udp/udp_entities.h
@@ -3,3 +3,9 @@ udp_entities,h
 
 
 ------------------------------------------------------------------------------*/
+
+#include "lwip/udp.h"
+
+
+
+bool IsEnity(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, u16_t port, u8_t broadcast, entity const *pen);
