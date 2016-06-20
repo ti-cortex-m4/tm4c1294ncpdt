f4e6d0f306c52f8f540db1bf120a4f014357499d
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-06-20 11:34:17 +0300

    fix

--- ek-tm4c1294xl/project2/third_party/lwip-1.4.1.patched/src/core/udp.c
+++ ek-tm4c1294xl/project2/third_party/lwip-1.4.1.patched/src/core/udp.c
@@ -400,7 +400,7 @@ udp_input(struct pbuf *p, struct netif *inp)
       /* callback */
       if (pcb->recv != NULL) {
         /* now the recv function is responsible for freeing p */
-        pcb->recv(pcb->recv_arg, pcb, p, ip_current_src_addr(), src, broadcast); // project2 patch
+        pcb->recv(pcb->recv_arg, pcb, p, ip_current_src_addr(), src, broadcast); // project2
       } else {
         /* no recv function registered? then we have to free the pbuf! */
         pbuf_free(p);
