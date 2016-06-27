e939b8986f561f973e72b8e620e59d3dbe20789e
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-06-27 15:50:04 +0300

    fix

--- ek-tm4c1294xl/project2/self/tcp/telnet.h
+++ ek-tm4c1294xl/project2/self/tcp/telnet.h
@@ -15,12 +15,12 @@ extern tState g_sState[UART_COUNT];
 ulong getTelnetTimeout(uchar u);
 
 void TelnetFreePbufs(tState *pState);
-extern void TelnetInit(void);
+void TelnetInit(void);
 
 err_t TelnetConnected(void *arg, struct tcp_pcb *pcb, err_t err);
 void TelnetError(void *arg, err_t err);
 err_t TelnetSent(void *arg, struct tcp_pcb *pcb, u16_t len);
 
-extern void TelnetHandler(void);
+void TelnetHandler(void);
 
 void TelnetNotifyLinkStatus(bool bLinkStatusUp);
