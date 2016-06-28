4d34b6dd819c1928a8023fe5cdec24cafb4c7e97
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-06-28 11:11:00 +0300

    fix

--- ek-tm4c1294xl/project2/self/tcp/tcp_errors.c
+++ ek-tm4c1294xl/project2/self/tcp/tcp_errors.c
@@ -5,19 +5,26 @@ tcp_errors.c
 ------------------------------------------------------------------------------*/
 
 #include "../main.h"
+#include "../kernel/clock.h"
 #include "tcp_errors.h"
 
 
 
-err_t                   mwErrors[UART_COUNT]
-uint                    mcwErrorTCPFunctions[UART_COUNT][10];
-ulong                   mdwErrorClock[UART_COUNT][10];
+#define TCP_OPERATIONS  10
 
 
 
-void ErrorTCPFunction(uchar u, err_t err, tcp_function_t func)
+err_t                   mwErrTCPCodes[UART_COUNT][TCP_OPERATIONS];
+uint                    mcwErrTCPCounters[UART_COUNT][TCP_OPERATIONS];
+ulong                   mdwErrTCPClockSeconds[UART_COUNT][TCP_OPERATIONS];
+
+
+
+void ErrorTCPOperation(uchar u, err_t err, tcp_operation_t op)
 {
-  mwErrors[u] = err;
-  mcwErrorTCPFunctions[u][tcp_function_t]++;
-  mdwErrorClock[u][tcp_function_t] = GetClockSeconds();
+  ASSERT(op < TCP_OPERATIONS);
+
+  mwErrTCPCodes[u][op] = err;
+  mcwErrTCPCounters[u][op]++;
+  mdwErrTCPClockSeconds[u][op] = GetClockSeconds();
 }
