31a262af7a2af0b885ea24a3a971209484076c82
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-06-28 10:44:12 +0300

    fix

--- ek-tm4c1294xl/project2/self/tcp/tcp_errors.h
+++ ek-tm4c1294xl/project2/self/tcp/tcp_errors.h
@@ -1,7 +1,18 @@
 /*------------------------------------------------------------------------------
-tcp_common.h
+tcp_errors.h
 
 
 ------------------------------------------------------------------------------*/
 
+#ifndef __TCP_ERRORS_H__
+#define __TCP_ERRORS_H__
 
+
+typedef enum
+{
+  TCP_NEW,
+  TCP_BIND,
+} tcp_function_t;
+
+
+#endif
