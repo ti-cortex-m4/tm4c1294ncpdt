7bc60eb1ebcb3f899f2d9f7f2067ea5a30f31375
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-06-29 09:14:22 +0300

    fix

--- ek-tm4c1294xl/project2/self/tcp/telnet_error.c
+++ ek-tm4c1294xl/project2/self/tcp/telnet_error.c
@@ -9,9 +9,7 @@ telnet_error.c
 #include "../kernel/log.h"
 #include "tcp_errors.h"
 #include "telnet.h"
-//#include "telnet_poll.h"
 #include "telnet_open.h"
-//#include "telnet_receive.h"
 #include "telnet_error.h"
 
 
