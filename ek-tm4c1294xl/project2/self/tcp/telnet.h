6236699a5c78b09452bc3cef6ed75297f826880f
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-06-29 08:34:39 +0300

    extract

--- ek-tm4c1294xl/project2/self/tcp/telnet.h
+++ ek-tm4c1294xl/project2/self/tcp/telnet.h
@@ -17,7 +17,6 @@ ulong getTelnetTimeout(uchar u);
 void TelnetFreePbufs(tState *pState);
 void TelnetInit(void);
 
-err_t TelnetConnected(void *arg, struct tcp_pcb *pcb, err_t err);
 void TelnetError(void *arg, err_t err);
 err_t TelnetSent(void *arg, struct tcp_pcb *pcb, u16_t len);
 
