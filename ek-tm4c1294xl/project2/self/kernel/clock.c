d032de08312b891029085b2ea3ca83a3078e2bd5
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-06-29 11:01:42 +0300

    fix

--- ek-tm4c1294xl/project2/self/kernel/clock.c
+++ ek-tm4c1294xl/project2/self/kernel/clock.c
@@ -9,6 +9,12 @@ clock.c
 
 
 
+#define SECONDS_IN_DAY      (ulong)(24*60*60)
+#define SECONDS_IN_HOUR     (uint)(60*60)
+#define SECONDS_IN_MINUTE   (uchar)60
+
+
+
 static volatile ulong dwClockSeconds = 0;
 
 
@@ -22,4 +28,23 @@ void Clock_1Hz(void)
 ulong GetClockSeconds(void)
 {
   return dwClockSeconds;
+}
+
+
+
+days_t SecondsToDays(ulong dwSeconds)
+{
+  days_t days;
+
+  days.wDays = dwSeconds / SECONDS_IN_DAY;
+  dwSeconds %= SECONDS_IN_DAY;
+
+  days.bHours = dwSeconds / SECONDS_IN_HOUR;
+  dwSeconds %= SECONDS_IN_HOUR;
+
+  days.bMinutes = dwSeconds / SECONDS_IN_MINUTE;
+
+  days.bSeconds = dwSeconds % SECONDS_IN_MINUTE;
+
+  return days;
 }
