b1e6b1c77d569821d319869ec77a1ff0ab5b0826
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-07-06 16:59:24 +0300

    fix

--- ek-tm4c1294xl/project2/self/uart/modem.c
+++ ek-tm4c1294xl/project2/self/uart/modem.c
@@ -7,6 +7,9 @@ modem.c
 #include "../main.h"
 #include "../kernel/settings.h"
 #include "../kernel/log.h"
+#include "../kernel/log.h"
+#include "../tcp/telnet_open.h"
+#include "serial_send.h"
 #include "modem.h"
 
 
@@ -80,16 +83,36 @@ bool IsModemCmdPrefix(const char u, const char *pcszCmd)
 }
 
 
+void ModemOut(const char u, const char b)
+{
+  SerialSend(u, b);
+}
+
+
+void ModemConnect(const char u, const char b)
+{
+  ulong dwIP = 0;
+  uint wPort = 0;
+
+  CONSOLE("%u: connect as modem to %u.%u.%u.%u port %u\n",
+    u,
+    (dwIP >> 24), (dwIP >> 16) & 0xFF, (dwIP >> 8) & 0xFF, dwIP & 0xFF,
+	wPort);
+
+  TelnetOpen(dwIP, wPort, u);
+}
+
+
 void RunModem(const char u)
 {
   if (mfModemDataMode[u] == false)
   {
     if (IsModemCmd(u, "AT"))
-      ModemOut(0);
+      ModemOut(u, 0);
     else if (IsModemCmdPrefix(u, "ATDP"))
-      ModemConnect();
+      ModemConnect(u);
     else if (IsModemCmd(u, "ATH0"))
-      ModemDisconnect();
+      ModemDisconnect(u);
     else
       WARNING("RunModem: unknown command\n");
   }
