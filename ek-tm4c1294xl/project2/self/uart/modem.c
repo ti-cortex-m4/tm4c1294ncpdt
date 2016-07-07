a8626ed5c338c72c74bd12c9cb2b0c0c890d7709
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-07-07 15:58:56 +0300

    fix

--- ek-tm4c1294xl/project2/self/uart/modem.c
+++ ek-tm4c1294xl/project2/self/uart/modem.c
@@ -18,34 +18,6 @@ modem.c
 
 
 
-typedef enum
-{
-  MODEM_MODE_COMMAND = 0,
-  MODEM_MODE_DATA = 1,
-} modem_mode_t;
-
-
-typedef enum
-{
-  INPUT_MODE_BEGIN = 0,
-  INPUT_MODE_DATA = 1,
-  INPUT_MODE_READY = 2,
-} input_mode_t;
-
-
-typedef enum
-{
-  ESCAPE_MODE_BEGIN = 0,
-  ESCAPE_MODE_DATA = 1,
-  ESCAPE_MODE_PAUSE_BEFORE = 2,
-  ESCAPE_MODE_PLUS_1 = 3,
-  ESCAPE_MODE_PLUS_2 = 4,
-  ESCAPE_MODE_PLUS_3 = 5,
-  ESCAPE_MODE_PAUSE_AFTER = 6,
-} escape_mode_t;
-
-
-
 volatile modem_mode_t   mbModemMode[UART_COUNT];
 volatile input_mode_t   mbInputMode[UART_COUNT];
 
