997a51f1faeab137db5e49c84f4eba810a161a57
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-07-07 15:58:56 +0300

    fix

--- ek-tm4c1294xl/project2/self/uart/modem.h
+++ ek-tm4c1294xl/project2/self/uart/modem.h
@@ -4,6 +4,38 @@ modem.h
 
 ------------------------------------------------------------------------------*/
 
+#ifndef __MODEM_H__
+#define __MODEM_H__
+
+
+typedef enum
+{
+  MODEM_MODE_COMMAND = 0,
+  MODEM_MODE_DATA = 1,
+} modem_mode_t;
+
+
+typedef enum
+{
+  INPUT_MODE_BEGIN = 0,
+  INPUT_MODE_DATA = 1,
+  INPUT_MODE_READY = 2,
+} input_mode_t;
+
+
+typedef enum
+{
+  ESCAPE_MODE_BEGIN = 0,
+  ESCAPE_MODE_DATA = 1,
+  ESCAPE_MODE_PAUSE_BEFORE = 2,
+  ESCAPE_MODE_PLUS_1 = 3,
+  ESCAPE_MODE_PLUS_2 = 4,
+  ESCAPE_MODE_PLUS_3 = 5,
+  ESCAPE_MODE_PAUSE_AFTER = 6,
+} escape_mode_t;
+
+
+
 extern volatile modem_mode_t   mbModemMode[UART_COUNT];
 extern volatile input_mode_t   mbInputMode[UART_COUNT];
 
@@ -23,3 +55,6 @@ void ModemConnected(const uchar u);
 
 void Modem_10Hz(void);
 void ProcessModemModeData(const uchar u, const uchar b);
+
+
+#endif
