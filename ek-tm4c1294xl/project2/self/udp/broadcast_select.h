812df408bb0bd98981a8992c675bb6d5149051f2
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2016-07-11 12:08:42 +0300

    fix

--- ek-tm4c1294xl/project2/self/udp/broadcast_select.h
+++ ek-tm4c1294xl/project2/self/udp/broadcast_select.h
@@ -4,4 +4,8 @@ broadcast_select.h
 
 ------------------------------------------------------------------------------*/
 
-err_t OutBroadcastSelect(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast);
+extern bool                    fBroadcastSelect;
+
+
+
+void OutBroadcastSelect(struct udp_pcb *pcb, struct pbuf *p, struct ip_addr *addr, uint port, uchar broadcast);
