250243498f7348c473f69a9d48af688401c25b89
TI Cortex M4 <ti.cortex.m4@gmail.com>
2016-07-26 14:30:21 +0300

    fix

--- ek-tm4c1294xl/project2/self/uart/server_to_modem.h
+++ ek-tm4c1294xl/project2/self/uart/server_to_modem.h
@@ -4,3 +4,19 @@ server_to_modem.h
 
 ------------------------------------------------------------------------------*/
 
+#ifndef __SERVER_TO_MODEM_H__
+#define __SERVER_TO_MODEM_H__
+
+
+typedef enum
+{
+  EM_BEGIN = 0,
+  EM_PAUSE_BEFORE = 1,
+  EM_PLUS_1 = 2,
+  EM_PLUS_2 = 3,
+  EM_PLUS_3 = 4,
+  EM_PAUSE_AFTER = 5,
+} escape_mode_t;
+
+
+#endif
