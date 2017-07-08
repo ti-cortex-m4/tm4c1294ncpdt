6415fa636aefc7699d45e08b7132d3eaba9530ad
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2017-07-08 13:24:24 +0300

    update code

---
+++ ek-tm4c1294xl/project1/self/sensors/automatic_u2.c
@@ -0,0 +1,147 @@
+/*------------------------------------------------------------------------------
+AUTOMATIC_U2!C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+#include "../memory/mem_digitals.h"
+#include "../memory/mem_factors.h"
+#include "../display/display.h"
+#include "../keyboard/keyboard.h"
+#include "../time/delay.h"
+#include "../time/timedate.h"
+#include "../serial/ports.h"
+#include "../serial/ports_devices.h"
+#include "../devices/devices.h"
+#include "../digitals/digitals.h"
+#include "automatic1.h"
+#include "device_u.h"
+#include "device_u2.h"
+#include "automatic_u.h"
+#include "automatic_u2.h"
+
+
+
+#ifndef SKIP_U
+
+bool    ReadEnergyDayDatesU_Full(void)
+{
+  uchar r;
+  for (r=0; r<bMINORREPEATS; r++)
+  {
+    QueryCloseU();
+    QueryEnergyDayDatesU();
+
+    if (BccInput() == SER_GOODCHECK) break;
+    if (fKey == true) return(0);
+  }
+
+  if (r == bMINORREPEATS) return(0);
+  ShowPercent(60);
+
+  ReadEnergyDayDatesU();
+
+  return(1);
+}
+
+
+bool    ReadEnergyMonDatesU_Full(void)
+{
+  uchar r;
+  for (r=0; r<bMINORREPEATS; r++)
+  {
+    QueryCloseU();
+    QueryEnergyMonDatesU();
+
+    if (BccInput() == SER_GOODCHECK) break;
+    if (fKey == true) return(0);
+  }
+
+  if (r == bMINORREPEATS) return(0);
+  ShowPercent(60);
+
+  ReadEnergyMonDatesU();
+
+  return(1);
+}
+
+
+
+bool    ReadEnergyMonTariffU_Full(uchar  bMon, uchar  ibTariff, uchar  bMaxLines)
+{
+  uchar i;
+  for (i=0; i<bMaxLines; i++)
+  {
+    uchar r;
+    for (r=0; r<bMINORREPEATS; r++)
+    {
+      QueryCloseU();
+      QueryEnergyMonU(i,bMon);
+
+      if (BccInput() == SER_GOODCHECK) break;
+      if (fKey == true) return(0);
+    }
+
+    if (r == bMINORREPEATS) return(0);
+    ShowPercent(70+i);
+
+    ReadEnergyTariffU(i,ibTariff);
+  }
+
+  return(1);
+}
+
+
+
+status  ReadCntMonCanTariffU(uchar  ibMonth, uchar  ibTariff, uchar  bMaxLines) // на начало мес€ца
+{
+  time2 ti2 = ReadTimeCanU();
+  if (ti2.fValid == 0) return ST_BADDIGITAL;
+
+  time ti = ti2.tiValue;
+  if (ibMonth == 0)
+  {
+    ti.bMonth = 12;
+    ti.bYear--;
+  }
+  else
+  {
+    if (ibMonth+1 > ti.bMonth) ti.bYear--;
+    ti.bMonth = ibMonth;
+  }
+
+  if (ReadEnergyMonDatesU_Full() == 0) return ST_BADDIGITAL;
+
+  date dt;
+  dt.bDay   = ti.bDay;
+  dt.bMonth = ti.bMonth;
+  dt.bYear  = ti.bYear;
+  uchar bMon = IsMonAddedU(dt);
+
+  if (bMon == 0)
+  {
+    Clear();
+    sprintf(szLo+1, "мес€ц %02u.%02u ?",ti.bMonth,ti.bYear);
+    Delay(1000);
+    return(ST_NOTPRESENTED);
+  }
+  if (ReadEnergyMonTariffU_Full(bMon,ibTariff,bMaxLines) == 0) return ST_BADDIGITAL;
+
+  QueryCloseU();
+
+
+  double dbTrans = mpdbTransCnt[ibDig];
+
+  uchar i;
+  for (i=0; i<bMaxLines; i++)
+  {
+    mpdbChannelsC[i] *= dbTrans;
+    mpboChannelsA[i] = true;
+  }
+
+  return ST_OK;
+}
+
+#endif
+
---
+++ ek-tm4c1294xl/project1/self/sensors/device_u2.c
@@ -0,0 +1,262 @@
+/*------------------------------------------------------------------------------
+DEVICE_U2.C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+#include "../memory/mem_digitals.h"
+#include "../display/display.h"
+#include "../time/delay.h"
+#include "../serial/ports_stack.h"
+#include "../serial/ports_devices.h"
+#include "device_k.h"
+#include "device_q.h"
+#include "device_u2.h"
+
+
+
+static uchar            cbDatesU;
+
+static date             mpdtDatesU[128];
+
+
+
+void    QueryEnergyDayDatesU(void)
+{
+  uchar n = PushAddress2Bcc();
+
+  PushChar1Bcc('D');
+  PushChar1Bcc('A');
+  PushChar1Bcc('T');
+  PushChar1Bcc('E');
+  PushChar1Bcc('D');
+  PushChar1Bcc('(');
+  PushChar1Bcc(')');
+  PushChar1Bcc(0x03);
+
+  BccQueryIO(1+128*17+2, n+8+1, 45);
+}
+
+
+void    QueryEnergyMonDatesU(void)
+{
+  uchar n = PushAddress2Bcc();
+
+  PushChar1Bcc('D');
+  PushChar1Bcc('A');
+  PushChar1Bcc('T');
+  PushChar1Bcc('E');
+  PushChar1Bcc('M');
+  PushChar1Bcc('(');
+  PushChar1Bcc(')');
+  PushChar1Bcc(0x03);
+
+  BccQueryIO(1+36*14+2, n+8+1, 24);
+}
+
+
+
+void    QueryEnergyDayU(uchar  ibLine, uchar  bDay)
+{
+  uchar n = PushAddress2Bcc();
+
+  PushChar1Bcc('E');
+  PushChar1Bcc('D');
+  PushChar1Bcc('0');
+
+  PushLineBcc(ibLine);
+
+  PushChar1Bcc('(');
+  PushChar2Bcc(bDay);
+  PushChar1Bcc(')');
+  PushChar1Bcc(0x03);
+
+  BccQueryIO(1+6*28+2, n+10+1, 6);
+}
+
+
+void    QueryEnergyMonU(uchar  ibLine, uchar  bMon)
+{
+  uchar n = PushAddress2Bcc();
+
+  PushChar1Bcc('E');
+  PushChar1Bcc('M');
+  PushChar1Bcc('0');
+
+  PushLineBcc(ibLine);
+
+  PushChar1Bcc('(');
+  PushChar2Bcc(bMon);
+  PushChar1Bcc(')');
+  PushChar1Bcc(0x03);
+
+  BccQueryIO(1+6*28+2, n+10+1, 6);
+}
+
+
+
+void    ClearDatesU(void)
+{
+  cbDatesU = 0;
+  memset(&mpdtDatesU, 0, sizeof(mpdtDatesU));
+}
+
+
+
+uchar   PopValueU(uchar  bCode)
+{
+  uchar i = 0;
+  while (true)
+  {
+    uchar b = PopChar() & 0x7F;
+    if (b == bCode) break;
+
+    if ((b >= '0') && (b <= '9'))
+      i = i*10 + (b-'0');
+  }
+
+  return i;
+}
+
+
+date    PopDayDateU(void)
+{
+  date dt;
+  dt.bDay   = 0;
+  dt.bMonth = 0;
+  dt.bYear  = 0;
+
+  while (true)
+  {
+    uchar b = PopChar() & 0x7F;
+
+    if (b == 0x0D) return dt;
+    if (b != '(') continue;
+
+    dt.bDay   = PopValueU('.');
+    dt.bMonth = PopValueU('.');
+    dt.bYear  = PopValueU(')');
+  }
+}
+
+
+date    PopMonDateU(void)
+{
+  date dt;
+  dt.bDay   = 0;
+  dt.bMonth = 0;
+  dt.bYear  = 0;
+
+  while (true)
+  {
+    uchar b = PopChar() & 0x7F;
+
+    if (b == 0x0D) return dt;
+    if (b != '(') continue;
+
+    dt.bMonth = PopValueU('.');
+    dt.bYear  = PopValueU(')');
+  }
+}
+
+
+
+void    ReadEnergyDayDatesU(void)
+{
+  InitPop(1);
+
+  ClearDatesU();
+
+  uchar i;
+  for (i=0; i<45; i++)
+  {
+    mpdtDatesU[cbDatesU++] = PopDayDateU();
+  }
+}
+
+
+void    ReadEnergyMonDatesU(void)
+{
+  InitPop(1);
+
+  ClearDatesU();
+
+  uchar	i;
+  for (i=0; i<24; i++)
+  {
+    mpdtDatesU[cbDatesU++] = PopMonDateU();
+  }
+}
+
+
+
+uchar    IsDayAddedU(date  dt)
+{
+  if (boShowMessages == true) { Clear(); sprintf(szLo+0,"сутки %02u.%02u.%02u",dt.bDay,dt.bMonth,dt.bYear); DelayInf(); Clear(); }
+
+  if (cbDatesU == 0)
+  {
+    return 0;
+  }
+  else
+  {
+    uchar i;
+    for (i=0; i<cbDatesU; i++)
+    {
+      date mp = mpdtDatesU[i];
+      if ((mp.bDay   == dt.bDay)   &&
+          (mp.bMonth == dt.bMonth) &&
+          (mp.bYear  == dt.bYear))
+      {
+        return i+1;
+      }
+    }
+
+    return 0;
+  }
+}
+
+
+uchar    IsMonAddedU(date  dt)
+{
+  if (boShowMessages == true) { Clear(); sprintf(szLo+1,"мес€ц %02u.%02u",dt.bMonth,dt.bYear); DelayInf(); Clear(); }
+
+  if (cbDatesU == 0)
+  {
+    return 0;
+  }
+  else
+  {
+    uchar i;
+    for (i=0; i<cbDatesU; i++)
+    {
+      date mp = mpdtDatesU[i];
+      if ((mp.bMonth == dt.bMonth) &&
+          (mp.bYear  == dt.bYear))
+      {
+        return i+1;
+      }
+    }
+
+    return 0;
+  }
+}
+
+
+
+void    ReadEnergyTariffU(uchar  ibLine, uchar  ibTrf)
+{
+double db;
+
+  InitPop(1);
+
+  uchar i;
+  for (i=0; i<ibTrf+2; i++)
+  {
+    db = PopDoubleQ();
+  }
+
+  mpdbChannelsC[ibLine] = db;
+  mpboChannelsA[ibLine] = true;
+}
