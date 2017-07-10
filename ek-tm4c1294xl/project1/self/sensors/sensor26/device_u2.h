e0fd9837e25a741047a7a8faaf223c2a6686b943
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2017-07-10 19:42:08 +0300

    update

--- ek-tm4c1294xl/project1/self/sensors/sensor26/device_u2.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor26/device_u2.h
@@ -4,13 +4,4 @@ DEVICE_U2.H
 
 ------------------------------------------------------------------------------*/
 
-void    QueryEnergyDayDatesU(void);
-void    QueryEnergyMonDatesU(void);
-
-void    ReadEnergyDayDatesU(void);
-void    ReadEnergyMonDatesU(void);
-
-uchar   IsDayAddedU(date  dt);
-uchar   IsMonAddedU(date  dt);
-
 void    ReadEnergyTariffU(uchar  ibLine, uchar  ibTrf);
