ba2bead9050bf5dd6ba52f74c8f530ddf96c3719
TI Cortex M4 <ti.cortex.m4@gmail.com>
2017-07-15 09:56:49 +0300

    update memory

--- ek-tm4c1294xl/project1/self/memory/mem_diagram.h
+++ ek-tm4c1294xl/project1/self/memory/mem_diagram.h
@@ -5,3 +5,9 @@ MEM_DIAGRAM,H
 ------------------------------------------------------------------------------*/
 
 extern  diagram                 mpDiagram[bCANALS];
+
+extern  uchar                   ibSoftDgr;
+
+extern  uint                    iwHardDgr;
+
+extern  bool                    fEnblDiagram;
