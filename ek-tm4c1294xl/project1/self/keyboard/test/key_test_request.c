fc80bea420aad5484b615e3a5fd5d430c6b01eef
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2017-08-09 12:28:51 +0300

    fix

--- ek-tm4c1294xl/project1/self/keyboard/test/key_test_request.c
+++ ek-tm4c1294xl/project1/self/keyboard/test/key_test_request.c
@@ -52,7 +52,7 @@ void    key_TestRequest(void)
 static void LoHex(uchar  i, uint iwInBuff, uchar  bT) {
   uchar j = i*2;
 
-  if (i + wOffset <= iwInBuff)
+  if (i + wOffset < iwInBuff)
     Lo(j, bT);
   else
   {
@@ -69,11 +69,14 @@ static void HiDec(uchar  i, uchar  bT) {
 
 
 static void TestRequest(uint  iwInBuff, uchar  *pbData) {
-  HiDec(0, iwInBuff);
+  szHi[0+0] = 'Ï';
+  szHi[0+1] = '1'+ibPortActive;
 
-  HiDec(4, wOffset);
-  szHi[7] = '-';
-  HiDec(8, wOffset+7);
+  HiDec(5, iwInBuff);
+
+  HiDec(9, wOffset);
+  szHi[12] = '-';
+  HiDec(13, wOffset+7);
 
   LoHex(0, iwInBuff, *(pbData++));
   LoHex(1, iwInBuff, *(pbData++));
@@ -93,27 +96,15 @@ void    auto_TestRequest(void)
     switch (ibPortActive)
     {
       case 0:
-        Lo(0,  mpSerial[0]);
-        Lo(3,  cwIn0 % 0x100);
-        Lo(5,  bIn0);
-        Lo(8,  iwInBuff0 % 0x100);
-        Lo(11, ibPacket0);
+        TestRequest(iwInBuff0, mpbInBuff0 + wOffset);
         break;
 
       case 1:
-        Lo(0,  mpSerial[1]);
-        Lo(3,  cwIn1 % 0x100);
-        Lo(5,  bIn1);
-        Lo(8,  iwInBuff1 % 0x100);
-        Lo(11, ibPacket1);
+        TestRequest(iwInBuff1, mpbInBuff1 + wOffset);
         break;
 
       case 2:
-        Lo(0,  mpSerial[2]);
-        Lo(3,  cwIn2 % 0x100);
-        Lo(5,  bIn2);
-        Lo(8,  iwInBuff2 % 0x100);
-        Lo(11, ibPacket2);
+        TestRequest(iwInBuff2, mpbInBuff2 + wOffset);
         break;
 
       case 3:
