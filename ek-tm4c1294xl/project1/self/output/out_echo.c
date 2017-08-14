420b78c48ca281a9ef400e04f7fcd8a2250dcc71
TI Cortex M4 <ti.cortex.m4@gmail.com>
2017-08-14 17:26:25 +0300

    add

---
+++ ek-tm4c1294xl/project1/self/output/out_echo.c
@@ -0,0 +1,27 @@
+/*------------------------------------------------------------------------------
+OUT_ECHO!C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+#include "../memory/mem_settings.h"
+#include "../memory/mem_realtime.h"
+#include "../memory/mem_ports.h"
+#include "../serial/ports.h"
+
+
+
+void    OutEcho(void)
+{
+uint    i;
+
+  InitPushCRC();
+  for (i=0; i<100; i++) PushChar(0x55);
+
+  InitPushCRC();
+  PushBool(fHealthLabelRTC);
+  PushBool(fHealthTimeRTC);
+
+  Output(1000);
+}
