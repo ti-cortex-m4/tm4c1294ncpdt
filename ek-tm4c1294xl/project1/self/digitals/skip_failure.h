4d8bad0be88b8859a323c9bb5a3eac2d80218b99
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2017-08-18 15:09:26 +0300

    fix

--- ek-tm4c1294xl/project1/self/digitals/skip_failure.h
+++ ek-tm4c1294xl/project1/self/digitals/skip_failure.h
@@ -11,6 +11,8 @@ extern  cache const             chSkipFailure;
 void    InitSkipFailure(void);
 void    ResetSkipFailure(void);
 
+bool    SkipFailureEnbl(void);
+
 void    SkipFailure_Start(void);
 void    SkipFailure_Success(void);
 void    SkipFailure_Failure(void);
