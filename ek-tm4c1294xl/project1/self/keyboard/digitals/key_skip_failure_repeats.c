822040cdb056917d732648dde716b95fcc1d11c2
TI Cortex M4 <ti.cortex.m4@gmail.com>
2017-08-23 15:14:10 +0300

    update

--- ek-tm4c1294xl/project1/self/keyboard/digitals/key_skip_failure_repeats.c
+++ ek-tm4c1294xl/project1/self/keyboard/digitals/key_skip_failure_repeats.c
@@ -6,7 +6,7 @@ KEY_SKIP_FAILURE_REPEATS!C
 
 #include "../../main.h"
 #include "../../digitals/skip_failure.h"
-#include "../common/key_char.h"
+#include "../common/key_char_blocked.h"
 
 
 
@@ -22,5 +22,5 @@ static char const       *pszMessages[]     = { szMessage1, szMessage2, szMessage
 
 void    key_SetSkipFailureRepeats(void)
 {
-  key_SetChar(&chSkipFailureRepeats, pszMessages, 1, 5);
+  key_SetCharBlocked(&chSkipFailureRepeats, pszMessages, 1, 5, fSkipFailureEnbl, wSET_SKIP_FAILURE_ENBL);
 }
