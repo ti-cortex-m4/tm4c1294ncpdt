705b6d4f52c34b513703336a1ac188a2fed2b55c
TI Cortex M4 <ti.cortex.m4@gmail.com>
2017-08-24 12:48:27 +0300

    update

--- ek-tm4c1294xl/project1/self/digitals/skip_failure.h
+++ ek-tm4c1294xl/project1/self/digitals/skip_failure.h
@@ -13,6 +13,7 @@ void    InitSkipFailure(void);
 void    ResetSkipFailure(void);
 
 bool    SkipFailureEnbl(void);
+uchar   SkipFailureRepeats(void);
 
 void    SkipFailure_BeforeExtended(void);
 void    SkipFailure_AfterExtended(void);
@@ -21,6 +22,6 @@ void    SkipFailure_InputStart(void);
 void    SkipFailure_InputGoodCheck(void);
 
 bool    SkipFailure_IsFailure(void);
+bool    SkipFailure_IsFailureMsg(void);
 
 bool    SkipFailure_IsReducedRepeats(void);
-uchar   SkipFailureRepeats(void);
