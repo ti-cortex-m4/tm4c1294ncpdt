0a5c7e3af20d6fe4ca802a1d795a40ea5c60d72d
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2017-08-24 10:33:24 +0300

    rename

--- ek-tm4c1294xl/project1/self/digitals/max_repeats.c
+++ ek-tm4c1294xl/project1/self/digitals/max_repeats.c
@@ -34,7 +34,7 @@ void    ResetMaxRepeats(void)
 
 uchar   MaxRepeats(void)
 {
-  return SkipFailure_IsReducedRepeats() ? MaxRepeatsSkipFailure() : MaxRepeatsFixed();
+  return SkipFailure_IsReducedRepeats() ? SkipFailureRepeats() : bMaxRepeats;
 }
 
 
