c4e951324c50363075cc45786f6c315492c4d204
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2017-08-28 11:48:04 +0300

    fix

--- ek-tm4c1294xl/project2/self/kernel/console_pins.c
+++ ek-tm4c1294xl/project2/self/kernel/console_pins.c
@@ -13,11 +13,29 @@ console_pins.c
 
 void ConsolePins(void)
 {
-  timedate_t td = GetROMBuildTimeDate();
-  CONSOLE("version %u.%u.%u.%04X %02u.%02u.%02u %02u:%02u:%02u\n",
-    MAJOR_VERSION, MINOR_VERSION,
-    GetROMBuildNumber(),
-    GetROMChecksum(),
-    td.bDay, td.bMonth, td.bYear, td.bHour, td.bMinute, td.bSecond
-  );
+#if 1
+  uint32_t dwStrength = 0;
+  uint32_t dwPadType = 0;
+
+  CONSOLE("UART 0 \n");
+  GPIOPadConfigGet(GPIO_PORTA_BASE, GPIO_PIN_0, &dwStrength, &dwPadType); CONSOLE("A.0 %X %X\n", dwStrength, dwPadType);
+  GPIOPadConfigGet(GPIO_PORTA_BASE, GPIO_PIN_1, &dwStrength, &dwPadType); CONSOLE("A.1 %X %X\n", dwStrength, dwPadType);
+  CONSOLE("UART 4 \n");
+  GPIOPadConfigGet(GPIO_PORTA_BASE, GPIO_PIN_2, &dwStrength, &dwPadType); CONSOLE("A.2 %X %X\n", dwStrength, dwPadType);
+  GPIOPadConfigGet(GPIO_PORTA_BASE, GPIO_PIN_3, &dwStrength, &dwPadType); CONSOLE("A.3 %X %X\n", dwStrength, dwPadType);
+  CONSOLE("UART 3 \n");
+  GPIOPadConfigGet(GPIO_PORTA_BASE, GPIO_PIN_4, &dwStrength, &dwPadType); CONSOLE("A.4 %X %X\n", dwStrength, dwPadType);
+  GPIOPadConfigGet(GPIO_PORTA_BASE, GPIO_PIN_5, &dwStrength, &dwPadType); CONSOLE("A.5 %X %X\n", dwStrength, dwPadType);
+  CONSOLE("UART 2 \n");
+  GPIOPadConfigGet(GPIO_PORTA_BASE, GPIO_PIN_6, &dwStrength, &dwPadType); CONSOLE("A.6 %X %X\n", dwStrength, dwPadType);
+  GPIOPadConfigGet(GPIO_PORTA_BASE, GPIO_PIN_7, &dwStrength, &dwPadType); CONSOLE("A.7 %X %X\n", dwStrength, dwPadType);
+  CONSOLE("UART 1 \n");
+  GPIOPadConfigGet(GPIO_PORTB_BASE, GPIO_PIN_0, &dwStrength, &dwPadType); CONSOLE("B.0 %X %X\n", dwStrength, dwPadType);
+  GPIOPadConfigGet(GPIO_PORTB_BASE, GPIO_PIN_1, &dwStrength, &dwPadType); CONSOLE("B.1 %X %X\n", dwStrength, dwPadType);
+
+  GPIOPadConfigGet(GPIO_PORTF_BASE, GPIO_PIN_3, &dwStrength, &dwPadType); CONSOLE("F.3 %X %X\n", dwStrength, dwPadType);
+  GPIOPadConfigGet(GPIO_PORTF_BASE, GPIO_PIN_2, &dwStrength, &dwPadType); CONSOLE("F.2 %X %X\n", dwStrength, dwPadType);
+  GPIOPadConfigGet(GPIO_PORTF_BASE, GPIO_PIN_1, &dwStrength, &dwPadType); CONSOLE("F.1 %X %X\n", dwStrength, dwPadType);
+  GPIOPadConfigGet(GPIO_PORTF_BASE, GPIO_PIN_0, &dwStrength, &dwPadType); CONSOLE("F.0 %X %X\n", dwStrength, dwPadType);
+#endif
 }
