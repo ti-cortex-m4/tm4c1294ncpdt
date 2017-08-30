999bfd7781b4a225c58323df7625ffdf50668a7f
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2017-08-30 16:28:04 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/digitals/phones/phones2_include.h
@@ -0,0 +1,29 @@
+/*------------------------------------------------------------------------------
+phones2_include.h
+
+
+------------------------------------------------------------------------------*/
+
+#ifndef __PHONES2_INCLUDE
+#define __PHONES2_INCLUDE
+
+
+// размер буфера результата СМС-контроля
+#define PHONE2_ANSWER     50
+
+// размер буфера журнала СМС-контроля
+#define PHONE2_RECORD     8
+
+
+typedef enum
+{
+  PH2_OK                = 1,
+  PH2_KEYBREAK          = 2,
+  PH2_MODEMCOMMON       = 3,
+  PH2_MESSAGEMODE       = 4,
+  PH2_MESSAGE1          = 5,
+  PH2_MESSAGE2          = 6
+} phones2;
+
+
+#endif
