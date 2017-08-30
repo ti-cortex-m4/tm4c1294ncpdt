a90f6a6afb0b19dd194b2758d917684d7c9a6fa4
TI Cortex M4 <ti.cortex.m4@gmail.com>
2017-08-30 13:21:02 +0300

    fix

--- ek-tm4c1294xl/project1/self/digitals/phones/phones2.h
+++ ek-tm4c1294xl/project1/self/digitals/phones/phones2.h
@@ -1,8 +1,22 @@
 /*------------------------------------------------------------------------------
-PHONES2.H
+PHONES2!H
 
 
 ------------------------------------------------------------------------------*/
+
+
+
+
+
+typedef enum
+{
+  PH2_OK                = 1,
+  PH2_KEYBREAK          = 2,
+  PH2_MODEMCOMMON       = 3,
+  PH2_MESSAGEMODE       = 4,
+  PH2_MESSAGE1          = 5,
+  PH2_MESSAGE2          = 6
+} phones2;
 
 //void    ResetPhones2(void);
 //bit     UsePhones2(void);
---
+++ ek-tm4c1294xl/project1/self/digitals/phones/phones22.c
@@ -0,0 +1,126 @@
+/*------------------------------------------------------------------------------
+PHONES22!C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+//#include "../../memory/mem_settings.h"
+//#include "../../memory/mem_digitals.h"
+//#include "../../display/display.h"
+//#include "../../keyboard/keyboard.h"
+//#include "../../serial/ports.h"
+//#include "../../serial/ports_push.h"
+//#include "../../serial/ports_devices.h"
+//#include "../../serial/ports_modems.h"
+//#include "../../serial/modems.h"
+#include "phones22.h"
+
+
+/*
+bit     MakePhones2Input0(void) {
+uchar i;
+
+  memset(&mpbAnswer1Phones2, 0, sizeof(mpbAnswer1Phones2));
+  InitPop(0);
+  for (i=0; i<IndexInBuff(); i++) {
+    if (i >= PHONE2_ANSWER) break;
+    mpbAnswer1Phones2[i] = PopChar();
+  }
+
+  if (IndexInBuff() != 4) return 0;
+
+  InitPop(0);
+  if (PopChar() != 0x0D) return 0;
+  if (PopChar() != 0x0A) return 0;
+  if (PopChar() != 0x3E) return 0;
+  if (PopChar() != 0x20) return 0;
+
+  return 1;
+}
+
+
+bit     MakePhones2Input1(void) {
+uchar i;
+
+  memset(&mpbAnswer2Phones2, 0, sizeof(mpbAnswer2Phones2));
+  InitPop(0);
+  for (i=0; i<IndexInBuff(); i++) {
+    if (i >= PHONE2_ANSWER) break;
+    mpbAnswer2Phones2[i] = PopChar();
+  }
+
+  if (IndexInBuff() < 2) return 0;
+
+  InitPop(IndexInBuff() - 2);
+  if (PopChar() != 0x30) return 0;
+  if (PopChar() != 0x0D) return 0;
+
+  return 1;
+}
+
+
+bit     Phones2Answer(void)  {
+  return  (IndexInBuff() >= 2);
+}
+
+
+uchar   Phones2Result(void)  {
+  InitPop(IndexInBuff() - 2);
+  return  PopChar();
+}
+
+
+serial  Phones2Input0(void)
+{
+  InitWaitAnswer();
+
+  while (1)
+  {
+    if (fKey == 1) { mpSerial[ibPort] = SER_BADLINK; break; }
+
+    ResetWDT();
+    ShowWaitAnswer(1);
+    if (GetWaitAnswer()) { mpSerial[ibPort] = SER_BADLINK; break; }
+
+    if (mpSerial[ibPort] == SER_POSTINPUT_MASTER)
+    {
+      if (MakePhones2Input0())
+        mpSerial[ibPort] = SER_GOODCHECK;
+      else
+        mpSerial[ibPort] = SER_BADCHECK;
+
+      break;
+    }
+    else if ((mpSerial[ibPort] == SER_OVERFLOW) ||
+             (mpSerial[ibPort] == SER_BADLINK)) break;
+  }
+
+  return( mpSerial[ibPort] );
+}
+
+
+serial  Phones2Input1(void)
+{
+  InitWaitAnswer();
+
+  while (1)
+  {
+    if (fKey == 1) { mpSerial[ibPort] = SER_BADLINK; break; }
+
+    ResetWDT();
+    ShowWaitAnswer(1);
+    if (GetWaitAnswer()) { if (MakePhones2Input1()) mpSerial[ibPort] = SER_GOODCHECK; else mpSerial[ibPort] = SER_BADCHECK; break; }
+
+    if (mpSerial[ibPort] == SER_POSTINPUT_MASTER)
+    {
+      mpSerial[ibPort] = SER_BADCHECK;
+      break;
+    }
+    else if ((mpSerial[ibPort] == SER_OVERFLOW) ||
+             (mpSerial[ibPort] == SER_BADLINK)) break;
+  }
+
+  return( mpSerial[ibPort] );
+}
+*/
