7d26fce94c79e38879ee52f6a3d8ac8acc2f1d1b
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2017-08-31 10:35:57 +0300

    move

--- ek-tm4c1294xl/project1/self/keyboard/phones2/key_phones22.c
+++ ek-tm4c1294xl/project1/self/keyboard/phones2/key_phones22.c
@@ -3,43 +3,42 @@ KEY_PHONES22.C
 
 
 ------------------------------------------------------------------------------*/
-/*
-#include        "main.h"
-#include        "xdata.h"
-#include        "beep.h"
-#include        "timer0.h"
-#include        "display.h"
-#include        "keyboard.h"
-#include        "programs.h"
-#include        "phones2.h"
+
+#include "../../main.h"
+#include "../../console.h"
+#include "../../memory/mem_phones.h"
+#include "../../memory/mem_ports.h"
+#include "../../digitals/phones.h"
 
 
 
 //                                         0123456789ABCDEF
-message         code    szUse22         = "    Телефоны    ",
-                code    szUsePhones22   = "для СМС-контроля",
-                code    szMaskPhones22  = "_____________";
+static char const       szMessage1[]    = "    Телефоны    ",
+                        szMessage2[]    = "для СМС-контроля",
+                        szMask[]        = "_____________";
+
+static char const       *pszPhones22[]   = { szMessage1, szMessage2, "" };
+
+static uchar            ibCan, ibPos;
 
 
-uchar           *code   pszPhones22[]   = { szUse22, szUsePhones22, "" };
 
-
-void    ShowPhones2(void)
+static void Show(void)
 {
   Clear();
-  strcpy(szLo, mpphPhones2[ibX].szNumber);
+  strcpy(szLo, mpphPhones2[ibCan].szNumber);
 
-  sprintf(szLo+14,"%2bu",ibX+1);
+  sprintf(szLo+14,"%2bu",ibCan+1);
 }
 
 
 
-void    ShowMaskPhones2(void)
+static void Mask(void)
 {
   enKeyboard = KBD_INPUT2;
-  ShowLo(szMaskPhones22);
+  ShowLo(szMask);
 
-  ibY = 0;
+  ibPos = 0;
 }
 
 
@@ -63,41 +62,41 @@ void    key_SetPhones22(void)
     {
       enKeyboard = KBD_POSTENTER;
 
-      ibX = 0;
-      ShowPhones2();
+      ibCan = 0;
+      Show();
     }
     else if (enKeyboard == KBD_POSTINPUT1)
     {
-      if ((ibX = GetChar(10,11) - 1) < bPHONES2)
+      if ((ibCan = GetChar(10,11) - 1) < bPHONES2)
       {
         enKeyboard = KBD_POSTENTER;
-        ShowPhones2();
+        Show();
       }
       else Beep();
     }
     else if (enKeyboard == KBD_POSTENTER)
     {
-      if (++ibX >= bPHONES2)
-        ibX = 0;
+      if (++ibCan >= bPHONES2)
+        ibCan = 0;
 
-      ShowPhones2();
+      Show();
     }
     else if (enKeyboard == KBD_POSTINPUT2)
     {
-      if (ibY > 0)
+      if (ibPos > 0)
       {
         enKeyboard = KBD_POSTENTER;
 
-        szLo[ibY] = 0;
-        strcpy(&mpphPhones2[ibX].szNumber,szLo);
+        szLo[ibPos] = 0;
+        strcpy(&mpphPhones2[ibCan].szNumber,szLo);
       }
 
       if (enKeyboard == KBD_POSTENTER)
       {
-        if (++ibX >= bPHONES2)
-          ibX = 0;
+        if (++ibCan >= bPHONES2)
+          ibCan = 0;
 
-        ShowPhones2();
+        Show();
       }
       else Beep();
     }
@@ -110,7 +109,7 @@ void    key_SetPhones22(void)
     if ((enKeyboard == KBD_POSTENTER) || (enKeyboard == KBD_POSTINPUT2))
     {
       if (enGlobal != GLB_WORK)
-        ShowMaskPhones2();
+        Mask();
       else Beep();
     }
     else Beep();
@@ -121,8 +120,8 @@ void    key_SetPhones22(void)
   {
     if (enKeyboard == KBD_POSTINPUT2)
     {
-      if (ibY == 1)
-        szLo[ibY++] = 'W';
+      if (ibPos == 1)
+        szLo[ibPos++] = 'W';
       else Beep();
     }
     else Beep();
@@ -134,7 +133,7 @@ void    key_SetPhones22(void)
     if (enKeyboard == KBD_POSTENTER)
     {
       if (enGlobal != GLB_WORK)
-        ShowMaskPhones2();
+        Mask();
       else Beep();
     }
 
@@ -148,12 +147,12 @@ void    key_SetPhones22(void)
     {
       enKeyboard = KBD_POSTINPUT2;
 
-      if (ibY < bPHONENUMBER)
-        szLo[ibY++] = szDigits[bKey];
+      if (ibPos < bPHONENUMBER)
+        szLo[ibPos++] = szDigits[bKey];
       else Beep();
     }
     else Beep();
   }
   else Beep();
 }
-*/
+
