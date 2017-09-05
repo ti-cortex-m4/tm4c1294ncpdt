2ac6a6bb09cabb8758e4fdce1d973b906f0d8628
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2017-09-05 16:19:24 +0300

    extract

---
+++ ek-tm4c1294xl/project1/self/digitals/phones2/phones2_x.c
@@ -0,0 +1,116 @@
+/*------------------------------------------------------------------------------
+PHONES2!C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../memory/mem_settings.h"
+#include "../../memory/mem_digitals.h"
+#include "../../memory/mem_tariffs.h"
+#include "../../memory/mem_realtime.h"
+#include "../../memory/mem_phones2.h"
+#include "../../display/display.h"
+#include "../../keyboard/keyboard.h"
+#include "../../time/rtc.h"
+#include "../../time/timedate.h"
+#include "../../realtime/realtime.h"
+#include "../../energy4.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports_push.h"
+#include "../../serial/ports_devices.h"
+#include "../../serial/ports_modems.h"
+#include "../../serial/modems.h"
+#include "../../flash/records.h"
+#include "../../nvram/cache.h"
+#include "../../nvram/cache2.h"
+#include "phones2_serial.h"
+#include "phones2_modem.h"
+#include "phones2.h"
+
+
+
+phones2 WritePhones2(bool  fDebug) {
+uchar i;
+
+  ShowHi(szPhonesRun2);
+  QueryMessage0();
+
+  ShowLo(szPhonesMode21);
+  for (i=0; i<MaxRepeatsFixed(); i++) {
+    DelayOff();
+    QueryModemBaud(0);
+
+    if (ModInputOK(1) == 1) break;
+    if (fKey == 1) return PH2_KEYBREAK;
+  }
+
+  if (i == MaxRepeatsFixed()) ;
+
+
+  ShowLo(szPhonesMode22);
+  for (i=0; i<MaxRepeatsFixed(); i++) {
+    DelayOff();
+    QueryModemCommon(0);
+
+    if (ModInputOK(1) == 1) break;
+    if (fKey == 1) return PH2_KEYBREAK;
+    sprintf(szLo+15, "%1u", i+1);
+  }
+
+  if (i == MaxRepeatsFixed())
+    return PH2_MODEMCOMMON;
+
+
+  ShowLo(szPhonesMode23);
+  for (i=0; i<MaxRepeatsFixed(); i++) {
+    DelayOff();
+    QueryMessageMode();
+
+    if (ModInputOK(1) == 1) break;
+    if (fKey == 1) return PH2_KEYBREAK;
+    sprintf(szLo+15, "%1u", i+1);
+  }
+
+  if (i == MaxRepeatsFixed())
+    return PH2_MESSAGEMODE;
+
+
+  uchar ibCan;
+  for (ibCan=0; ibCan<bPHONES2; ibCan++) {
+
+    line phT = mpphPhones2[ibCan];
+    if ((phT.szLine[0] == '0') && (phT.szLine[1] == 0)) continue;
+
+    mpbBuffPhones2[ibCan] = ibCan+1;
+
+    for (i=0; i<MaxRepeatsFixed(); i++) {
+      DelayOff();
+      QueryMessage1(ibCan);
+
+      if (Phones2Input0() == SER_GOODCHECK) break;
+      if (fKey == 1) return PH2_KEYBREAK;
+      sprintf(szLo+15, "%1u", i+1);
+    }
+
+    if (i == MaxRepeatsFixed())
+      return PH2_MESSAGE1;
+
+    DelayOff();
+    QueryMessage2(fDebug);
+
+    if (Phones2Input1() == SER_GOODCHECK) ;
+
+    Clear();
+    if (Phones2Answer()) {
+      (Phones2Result() == 0x30) ? OK() : sprintf(szLo+3, "ошибка: %c", Phones2Result()); // нужно обрабатывать ошибку
+    }
+    else Error(); // нужно обрабатывать ошибку
+    DelayMsg();
+  }
+
+
+  return PH2_OK;
+}
+
+
--- ek-tm4c1294xl/project1/self/digitals/phones2/phones2_y.c
+++ ek-tm4c1294xl/project1/self/digitals/phones2/phones2_y.c
@@ -24,83 +24,8 @@ PHONES2!C
 #include "../../flash/records.h"
 #include "../../nvram/cache.h"
 #include "../../nvram/cache2.h"
-#include "phones2_modem.h"
+#include "phones2_x.h"
 #include "phones2.h"
-
-
-
-// прогнозируемая мощность
-float                  reCurrPhones2;
-
-// буфера
-uchar                  mpbAnswer1Phones2[PHONE2_ANSWER], mpbAnswer2Phones2[PHONE2_ANSWER];
-
-// буфер
-stamp2                 mpstPhones2[PHONE2_CODES];
-
-// буфер
-uchar                  mpbBuffPhones2[PHONE2_RECORD];
-
-// счётчики
-ulong                  cdwPhones20, cdwPhones21, cdwPhones22, cdwPhones23, cdwPhones24, cdwPhones25;
-
-// промежуточный буфер
-char                   mpbInBuffSave2[100];
-
-
-
-cache const             chPortPhones2 = {PORT_PHONES2, &bPortPhones2, sizeof(uchar)};
-cache const             chPhones2 = {PHONES2, &mpphPhones2, sizeof(mpphPhones2)};
-cache const             chMaxxPhones2 = {PORT_PHONES2, &reMaxxPhones2, sizeof(float)};
-
-
-//                                         0123456789ABCDEF
-static char const       szPhonesRun2[]    = "СМС-контроль    ",
-                        szPhonesMode21[]  = " настройки 1... ",
-                        szPhonesMode22[]  = " настройки 2... ",
-                        szPhonesMode23[]  = " настройки 3... ";
-
-
-
-void    ResetPhones2(void) {
-uchar   i;
-
-  bPortPhones2 = 0;
-  reCurrPhones2 = 0;
-  reMaxxPhones2 = 1000;
-
-  uchar c;
-  for (c=0; c<bPHONES2; c++)
-  {
-    mpphPhones2[c].szLine[0] = '0';
-  }
-
-  memset(&mpbAnswer1Phones2, 0, sizeof(mpbAnswer1Phones2));
-  memset(&mpbAnswer2Phones2, 0, sizeof(mpbAnswer2Phones2));
-
-  for (i=0; i<PHONE2_CODES; i++) {
-    mpstPhones2[i].cwSelf = 0;
-    mpstPhones2[i].tiSelf = tiZero;
-  }
-
-  memset(&mpbBuffPhones2, 0, sizeof(mpbBuffPhones2));
-
-  cdwPhones20 = 0;
-  cdwPhones21 = 0;
-  cdwPhones22 = 0;
-  cdwPhones23 = 0;
-  cdwPhones24 = 0;
-  cdwPhones25 = 0;
-}
-
-
-
-bool    UsePhones2(void) {
-  return ((bPortPhones2 > 0) && (bPortPhones2 <= bPORTS));
-}
-
-
-
 
 
 
