da22493f0e51678836313872b5fc7440541d1723
TI Cortex M4 <ti.cortex.m4@gmail.com>
2017-09-05 12:11:15 +0300

    fix

--- ek-tm4c1294xl/project1/self/digitals/phones2/phones2.h
+++ ek-tm4c1294xl/project1/self/digitals/phones2/phones2.h
@@ -4,15 +4,19 @@ PHONES2!H
 
 ------------------------------------------------------------------------------*/
 
+#include "../../memory/mem_phones2.h"
 #include "phones2_include.h"
+
 
 
 extern uchar                  bPortPhones2;
 extern float                  reCurrPhones2;
 extern float                  reMaxxPhones2;
-extern line                   mpphPhones2[bPHONES2];
 extern bool                   boDebugPhones2;
 extern uchar                  mpbBuffPhones2[PHONE2_RECORD];
+
+
+extern cache const             chPhones2;
 
 
 //void    ResetPhones2(void);
