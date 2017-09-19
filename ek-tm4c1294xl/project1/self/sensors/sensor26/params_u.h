84e3d5ef0a64b6aef1e84afda1aa4a16f9b89ce9
TI Cortex M4 <ti.cortex.m4@gmail.com>
2017-09-19 10:55:39 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor26/params_u.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor26/params_u.h
@@ -22,5 +22,8 @@ void    ReadParamU_Qt(void);
 void    QueryParamU_Q123(void);
 void    ReadParamU_Q123(void);
 
+void    QueryParamU_Ct123(void);
+void    ReadParamU_Ct123(void);
+
 void    QueryParamU_Ft(void);
 void    ReadParamU_Ft(void);
