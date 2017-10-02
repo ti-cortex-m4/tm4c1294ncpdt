287748e6bd90d8c7ff7965cb5099cfda6be6975e
TI Cortex M4 <ti.cortex.m4@gmail.com>
2017-10-02 16:11:48 +0300

    fix

--- ek-tm4c1294xl/project2/self/kernel/entities.c
+++ ek-tm4c1294xl/project2/self/kernel/entities.c
@@ -21,82 +21,108 @@ const entity * const mpenEntities[] =
   &enOwnerName,
 
   &enConnectionTimeout0,
+#ifndef SINGLE_UART
   &enConnectionTimeout1,
   &enConnectionTimeout2,
   &enConnectionTimeout3,
   &enConnectionTimeout4,
+#endif
 
   &enRoutingMode0,
+#ifndef SINGLE_UART
   &enRoutingMode1,
   &enRoutingMode2,
   &enRoutingMode3,
   &enRoutingMode4,
+#endif
 
   &enPort0,
+#ifndef SINGLE_UART
   &enPort1,
   &enPort2,
   &enPort3,
   &enPort4,
+#endif
 
   &enConnectionMode0,
+#ifndef SINGLE_UART
   &enConnectionMode1,
   &enConnectionMode2,
   &enConnectionMode3,
   &enConnectionMode4,
+#endif
 
   &enDestinationIP0,
+#ifndef SINGLE_UART
   &enDestinationIP1,
   &enDestinationIP2,
   &enDestinationIP3,
   &enDestinationIP4,
+#endif
 
   &enDestinationPort0,
+#ifndef SINGLE_UART
   &enDestinationPort1,
   &enDestinationPort2,
   &enDestinationPort3,
   &enDestinationPort4,
+#endif
 
   &enServerToModem0,
+#ifndef SINGLE_UART
   &enServerToModem1,
   &enServerToModem2,
   &enServerToModem3,
   &enServerToModem4,
+#endif
 
   &enModemToServerTimeout0,
+#ifndef SINGLE_UART
   &enModemToServerTimeout1,
   &enModemToServerTimeout2,
   &enModemToServerTimeout3,
   &enModemToServerTimeout4,
+#endif
 
   &enBaudRate0,
+#ifndef SINGLE_UART
   &enBaudRate1,
   &enBaudRate2,
   &enBaudRate3,
   &enBaudRate4,
+#endif
 
   &enParity0,
+#ifndef SINGLE_UART
   &enParity1,
   &enParity2,
   &enParity3,
   &enParity4,
+#endif
 
   &enDataBits0,
+#ifndef SINGLE_UART
   &enDataBits1,
   &enDataBits2,
   &enDataBits3,
   &enDataBits4,
+#endif
 
   &enStopBits0,
+#ifndef SINGLE_UART
   &enStopBits1,
   &enStopBits2,
   &enStopBits3,
   &enStopBits4,
+#endif
 
   &enLoopback0,
+#ifndef SINGLE_UART
   &enLoopback1,
   &enLoopback2,
   &enLoopback3,
   &enLoopback4,
+#endif
 
   &enWatchdogFlag,
   &enDebugMode,
@@ -125,8 +151,10 @@ const uchar bEntitiesSize = sizeof(mpenEntities) / sizeof(mpenEntities[0]);
 const entity * const mpenRoutingModes[UART_COUNT] =
 {
   &enRoutingMode0,
+#ifndef SINGLE_UART
   &enRoutingMode1,
   &enRoutingMode2,
   &enRoutingMode3,
   &enRoutingMode4,
+#endif
 };
