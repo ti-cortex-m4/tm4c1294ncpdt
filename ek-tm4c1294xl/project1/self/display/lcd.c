09ba0067e1f80bb635e7d7920ea951fccd7b7af1
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2018-02-24 12:29:15 +0300

    fix

--- ek-tm4c1294xl/project1/self/display/lcd.c
+++ ek-tm4c1294xl/project1/self/display/lcd.c
@@ -61,52 +61,6 @@ static void _Delay(unsigned long ulgTime)
 }
 
 
-/*
-//Включает мерцание курсора
-void CursorOnLCD(void)
-{
-  ReadyLCD();  SetCommLCD(0x0D);
-}
-
-//Отключает мерцание курсора
-void CursorOffLCD(void)
-{
-  ReadyLCD();  SetCommLCD(0x0C);
-}
-
-//Возвращает курсор в начальную позицию
-void CursorClearPosLCD(void)
-{
-  ReadyLCD();  SetCommLCD(0x02);
-  bCursorPos = 0;
-}
-
-//Смещает курсор вправо на одну позицию (если конец строки, то переходит на следующую)
-void CursorRShiftLCD(void)
-{
-  ReadyLCD();  SetCommLCD(0x14);
-  bCursorPos++;
-}
-
-//Смещает курсор влево на одну позицию (если конец строки, то переходит на следующую)
-void CursorLShiftLCD(void)
-{
-  ReadyLCD(); SetCommLCD(0x10);
-  bCursorPos--;
-}
-
-//установка курсора в заданную позицию (0..15 - верхняя строка, 16..31 - нижняя строка)
-void CursorSetPosLCD(unsigned char bPos)
-{
- if(bPos != bCursorPos)
- {
-  CursorOffLCD();
-  if(bPos > bCursorPos) while(bCursorPos != bPos) CursorRShiftLCD();
-  else  while(bCursorPos != bPos) CursorLShiftLCD();
-  CursorOnLCD();
- }
-}
-*/
 // Запись команды в контроллер ЖКИ
 void SetCommLCD(unsigned char  bT)
 {
@@ -130,28 +84,6 @@ void    ReadyLCD(void)
 {
  // при готовности контроллера ЖКИ, бит готовности = 0
   while ( GetCommLCD() );
-}
-
-//Вывод байта в заданную позицию ЖКИ в HEX(два символа)
-void  ShowCharLCD_HEX(unsigned char  bPos, unsigned char  bHEX)
-{
- ReadyLCD(); SetCommLCD(bPos);
- ReadyLCD(); if(((bHEX >> 4)& 0x0F) > 9) SetDataLCD( (((bHEX >> 4) & 0x0F) - 10) + 0x41);
-             else SetDataLCD( ((bHEX >> 4) & 0x0F) + 0x30 );
-
- ReadyLCD(); SetCommLCD(bPos+1);
- ReadyLCD(); if((bHEX & 0x0F) > 9) SetDataLCD( ((bHEX & 0x0F) - 10) + 0x41);
-             else SetDataLCD( (bHEX & 0x0F) + 0x30 );
-}
-
-//Вывод байта в заданную позицию ЖКИ в BCD(два символа)
-void  ShowCharLCD_BCD(unsigned char  bPos, unsigned char  bBCD)
-{
- ReadyLCD(); SetCommLCD(bPos);
- ReadyLCD(); SetDataLCD( ((bBCD >> 4) & 0x0F) + 0x30 );
-
- ReadyLCD(); SetCommLCD(bPos+1);
- ReadyLCD(); SetDataLCD( (bBCD & 0x0F) + 0x30 );
 }
 
 //Вывод символа в заданную позицию ЖКИ
@@ -225,7 +157,7 @@ void    TestError(const char  *szT)
 #ifdef NO_DISPLAY
   StartError(szT);
 #else
-	IntMasterDisable();
+  IntMasterDisable();
 
   ShowMsgLCD(0x80, (const uchar *)szAlarm);
   ShowMsgLCD(0xC0, (const uchar *)szT);
