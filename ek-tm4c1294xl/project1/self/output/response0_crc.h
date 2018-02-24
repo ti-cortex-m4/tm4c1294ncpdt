452f0d8b0bf1c38dec831de3e49d67535a21184d
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2018-02-24 11:50:42 +0300

    fix

--- ek-tm4c1294xl/project1/self/output/response0_crc.h
+++ ek-tm4c1294xl/project1/self/output/response0_crc.h
@@ -5,3 +5,6 @@ RESPONSE0_CRC,H
 ------------------------------------------------------------------------------*/
 
 void    Response0_CRC(void);
+#ifdef NO_DISPLAY
+void    Response0_CRC_StartError(void);
+#endif
