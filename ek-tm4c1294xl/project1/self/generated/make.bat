2ce327b48b98fe063f360b40cc64f5c57c761fae
TI Cortex M4 <ti.cortex.m4@gmail.com>
2018-02-25 10:27:00 +0300

    fix

--- ek-tm4c1294xl/project1/self/generated/make.bat
+++ ek-tm4c1294xl/project1/self/generated/make.bat
@@ -1,2 +1,3 @@
 ..\..\exe\stack.exe flash_stack.txt flash_enum.txt out_flash.txt
+..\..\exe\stack.exe flash_stack.no_display.txt flash_enum.no_display.txt out_flash.no_display.txt
 ..\..\exe\stack.exe nvram_stack.txt nvram_enum.txt out_nvram.txt
