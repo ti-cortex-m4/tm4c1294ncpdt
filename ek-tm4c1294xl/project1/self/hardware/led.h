2a26ef86e6e10f3233e7b4e8ee7b74cd3de8a485
TI Cortex M4 <ti.cortex.m4@gmail.com>
2018-02-27 19:32:03 +0300

    led

---
+++ ek-tm4c1294xl/project1/self/hardware/led.h
@@ -0,0 +1,16 @@
+/*------------------------------------------------------------------------------
+led.h
+
+
+------------------------------------------------------------------------------*/
+
+void InitLEDs(void);
+
+void OffLED0(void);
+void OnLED0(void);
+void OffLED1(void);
+void OnLED1(void);
+
+void InitEthernetLEDs(void);
+
+void InitLEDs_After(void);
