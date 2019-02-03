624bbb2df4b3fd0d8572ff536d9c2d86922a1cec
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-02-03 16:15:40 +0300

    time

--- ek-tm4c1294xl/project1/self/sensors/sensor34/device34.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/device34.c
@@ -121,6 +121,61 @@ void    QueryTime34(void)
 }
 
 
+void convertUnixTimeToDate(time_t t, DateTime *date)
+{
+   uint32_t a;
+   uint32_t b;
+   uint32_t c;
+   uint32_t d;
+   uint32_t e;
+   uint32_t f;
+
+   //Negative Unix time values are not supported
+   if(t < 1)
+      t = 0;
+
+   //Clear milliseconds
+   date->milliseconds = 0;
+
+   //Retrieve hours, minutes and seconds
+   date->seconds = t % 60;
+   t /= 60;
+   date->minutes = t % 60;
+   t /= 60;
+   date->hours = t % 24;
+   t /= 24;
+
+   //Convert Unix time to date
+   a = (uint32_t) ((4 * t + 102032) / 146097 + 15);
+   b = (uint32_t) (t + 2442113 + a - (a / 4));
+   c = (20 * b - 2442) / 7305;
+   d = b - 365 * c - (c / 4);
+   e = d * 1000 / 30601;
+   f = d - e * 30 - e * 601 / 1000;
+
+   //January and February are counted as months 13 and 14 of the previous year
+   if(e <= 13)
+   {
+      c -= 4716;
+      e -= 1;
+   }
+   else
+   {
+      c -= 4715;
+      e -= 13;
+   }
+
+   //Retrieve year, month and day
+   date->year = c;
+   date->month = e;
+   date->day = f;
+
+   //Calculate day of week
+   date->dayOfWeek = computeDayOfWeek(c, e, f);
+}
+
+
+
 time    ReadTime34(void)
 {
   InitPop(3);
