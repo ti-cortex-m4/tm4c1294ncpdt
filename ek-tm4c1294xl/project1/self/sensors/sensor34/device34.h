c1f9b699ee0188c5968f9a8639d80efc7a78390b
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-02-03 12:21:24 +0300

    new

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/device34.h
@@ -0,0 +1,26 @@
+/*------------------------------------------------------------------------------
+DEVICE34.H
+
+
+------------------------------------------------------------------------------*/
+
+#define MAX_LINE_N34    8
+
+
+/*
+uint    PopInt33(void);
+ulong   PopLong33(void);
+
+void    QueryOpen33(void);
+bool    ReadOpen33(void);
+uchar   GetVersion33(void);
+
+void    QueryTime33(void);
+time    ReadTime33(void);
+time    ReadPackTime33(void);
+void    QueryControl33(time  ti);
+
+void    QueryEngAbs33(uchar  ibTrf);
+
+bool    Checksum33(uchar  bSize);
+*/
