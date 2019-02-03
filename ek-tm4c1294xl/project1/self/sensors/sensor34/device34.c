fd445d42a77d8f23ebc2e6174e34c547da96f058
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-02-03 16:31:17 +0300

    date

--- ek-tm4c1294xl/project1/self/sensors/sensor34/device34.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/device34.c
@@ -121,7 +121,7 @@ void    QueryTime34(void)
 }
 
 
-void convertUnixTimeToDate(time_t t, DateTime *date)
+time convertUnixTimeToDate(uint32_t  t)
 {
    uint32_t a;
    uint32_t b;
@@ -130,22 +130,21 @@ void convertUnixTimeToDate(time_t t, DateTime *date)
    uint32_t e;
    uint32_t f;
 
-   //Negative Unix time values are not supported
-   if(t < 1)
+   time ti;
+
+   // negative Unix time values are not supported
+   if (t < 1)
       t = 0;
 
-   //Clear milliseconds
-   date->milliseconds = 0;
-
-   //Retrieve hours, minutes and seconds
-   date->seconds = t % 60;
+   // retrieve hours, minutes and seconds
+   ti.bSecond = t % 60;
    t /= 60;
-   date->minutes = t % 60;
+   ti.bMinute = t % 60;
    t /= 60;
-   date->hours = t % 24;
+   ti.bHour = t % 24;
    t /= 24;
 
-   //Convert Unix time to date
+   // convert Unix time to date
    a = (uint32_t) ((4 * t + 102032) / 146097 + 15);
    b = (uint32_t) (t + 2442113 + a - (a / 4));
    c = (20 * b - 2442) / 7305;
@@ -153,7 +152,7 @@ void convertUnixTimeToDate(time_t t, DateTime *date)
    e = d * 1000 / 30601;
    f = d - e * 30 - e * 601 / 1000;
 
-   //January and February are counted as months 13 and 14 of the previous year
+   // January and February are counted as months 13 and 14 of the previous year
    if(e <= 13)
    {
       c -= 4716;
@@ -165,31 +164,19 @@ void convertUnixTimeToDate(time_t t, DateTime *date)
       e -= 13;
    }
 
-   //Retrieve year, month and day
-   date->year = c;
-   date->month = e;
-   date->day = f;
+   // retrieve year, month and day
+   ti.bYear = c % 2000;
+   ti.bMonth = e;
+   ti.bDay = f;
 
-   //Calculate day of week
-   date->dayOfWeek = computeDayOfWeek(c, e, f);
+   return ti;
 }
-
 
 
 time    ReadTime34(void)
 {
   InitPop(3);
-
-  time ti;
-  ti.bSecond = PopChar();
-  ti.bMinute = PopChar();
-  ti.bHour   = PopChar();
-               PopChar();
-  ti.bDay    = PopChar();
-  ti.bMonth  = PopChar();
-  ti.bYear   = PopChar();
-
-  return ti;
+  return convertUnixTimeToDate(PopLongLtl());
 }
 
 /*
