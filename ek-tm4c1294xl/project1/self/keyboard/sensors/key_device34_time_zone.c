e25172a2c619b88a8a63ea67d16f43f45eaaa956
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-02-03 16:37:50 +0300

    timezone

---
+++ ek-tm4c1294xl/project1/self/keyboard/sensors/key_device34_time_zone.c
@@ -0,0 +1,83 @@
+/*------------------------------------------------------------------------------
+KEY_GPS_GMT!C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../console.h"
+#include "../../time/gps.h"
+
+
+
+//                                         0123456789ABCDEF
+static char const       szMessage[]     = "Часовой пояс    ",
+                        szMask[]        = "      ___       ";
+
+
+
+static void Show(void)
+{
+  ShowChar(bGmtGps);
+}
+
+
+
+void    key_SetGpsGmt(void)
+{
+  if (bKey == bKEY_ENTER)
+  {
+    if (enKeyboard == KBD_ENTER)
+    {
+      if (UseGps())
+      {
+        enKeyboard = KBD_POSTENTER;
+        Clear();
+
+        ShowHi(szMessage);
+        Show();
+      }
+      else BlockProgram(bSET_GPS_CONFIG);
+    } 
+    else if (enKeyboard == KBD_POSTINPUT1)
+    {
+      enKeyboard = KBD_POSTENTER;
+
+      uchar b = GetCharLo(6,8);
+      if (b <= 13)
+      {
+        bGmtGps = b;
+        SaveCache(&chGmtGps);
+
+        Show();
+      }
+      else Beep();
+    }
+    else Beep();
+  }
+
+
+  else if (bKey == bKEY_MINUS)
+    ShowTimeDateGps(0);
+
+
+  else if (bKey == bKEY_POINT)
+    ShowTimeDateGps(1);
+
+
+  else if (bKey < 10)
+  {
+    if ((enGlobal != GLB_WORK) && (enKeyboard == KBD_POSTENTER))
+    {
+      enKeyboard = KBD_INPUT1;
+      ShowLo(szMask);
+    }
+
+    if ((enKeyboard == KBD_INPUT1) || (enKeyboard == KBD_POSTINPUT1))
+    {
+      enKeyboard = KBD_POSTINPUT1;
+      ShiftLo(6,8);
+    }
+  }
+  else Beep();
+}
