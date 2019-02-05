86fb56429f750bd89562dcf4cd8c9c7079e60266
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-02-05 07:51:49 +0300

    upd

--- ek-tm4c1294xl/project1/self/sensors/sensor34/automatic34.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/automatic34.h
@@ -5,3 +5,4 @@ AUTOMATIC34.H
 ------------------------------------------------------------------------------*/
 
 time2   ReadTimeCan34(void);
+double2 ReadCntCurr34(void);
--- ek-tm4c1294xl/project1/self/sensors/sensor34/device34.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/device34.h
@@ -7,20 +7,9 @@ DEVICE34.H
 #define MAX_LINE_N34    8
 
 
-/*
-uint    PopInt33(void);
-ulong   PopLong33(void);
 
-void    QueryOpen33(void);
-bool    ReadOpen33(void);
-uchar   GetVersion33(void);
-*/
 void    QueryTime34(void);
 time    ReadTime34(void);
-//time    ReadPackTime33(void);
-//void    QueryControl33(time  ti);
-/*
-void    QueryEngAbs33(uchar  ibTrf);
 
-bool    Checksum33(uchar  bSize);
-*/
+void    QueryEngAbs34(void);
+void    ReadEng34(void);
