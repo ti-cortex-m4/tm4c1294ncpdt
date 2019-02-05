261ff179e8e055030c9464c073dbc121d28e770a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-02-05 19:14:27 +0300

    automatic

--- ek-tm4c1294xl/project1/self/sensors/sensor34/automatic34.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/automatic34.c
@@ -6,57 +6,14 @@ AUTOMATIC34.C
 
 #include "../../main.h"
 #include "../../console.h"
-//#include "../../memory/mem_factors.h"
-//#include "../../hardware/watchdog.h"
-//#include "../../serial/ports.h"
-//#include "../../serial/ports_devices.h"
-//#include "../../serial/monitor.h"
-//#include "../../devices/devices.h"
-//#include "../../digitals/digitals_messages.h"
-//#include "../../digitals/wait_answer.h"
-//#include "../automatic1.h"
-//#include "../sensor31/automatic31.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports_devices.h"
 #include "device34.h"
 #include "automatic34.h"
 
 
 
 #ifndef SKIP_34
-/*
-void    Query33(uint  cwIn, uchar  cbOut)
-{
-  Query31(cwIn,cbOut);
-}
-
-
-serial  Input33(void)
-{
-  return Input31();
-}
-
-
-bool    QueryOpen33_Full(uchar  bPercent)
-{
-  Clear();
-
-  uchar r;
-  for (r=0; r<MaxRepeats(); r++)
-  {
-    DelayOff();
-    QueryOpen33();
-
-    if (Input33() == SER_GOODCHECK) break;
-    if (fKey == true) return false;
-  }
-
-  if (r == MaxRepeats()) return false;
-  ShowPercent(bPercent);
-
-  if (ReadOpen33() == false) return false;
-
-  return true;
-}
-*/
 
 time2   QueryTime34_Full(uchar  bPercent)
 {
@@ -77,18 +34,34 @@ time2   QueryTime34_Full(uchar  bPercent)
 }
 
 
-/*
-bool    Automatic33(void)
+
+double2 QueryEngAbs34_Full(uchar  bPercent)
 {
-  if (QueryOpen33_Full(25) == false) return false;
+  uchar r;
+  for (r=0; r<MaxRepeats(); r++)
+  {
+    DelayOff();
+    QueryEngAbs34();
 
-  ShowPercent(100);
+    if (Input() == SER_GOODCHECK) break;
+    if (fKey == true) return GetDouble2Error();
+  }
 
-  SetupFactors(GetFactors(1, 10000));
+  if (r == MaxRepeats()) return GetDouble2Error();
+  ShowPercent(bPercent);
 
-  return true;
+  ReadEng34();
+
+  uchar i;
+  for (i=0; i<MAX_LINE_N34; i++)
+  {
+    mpdbChannelsC[i] = (double)mpddwChannels34[i] / 1000000; // dbTrans
+    mpboChannelsA[i] = true;
+  }
+
+  return GetDouble2(mpdbChannelsC[diCurr.ibLine], true);
 }
-*/
+
 
 
 time2   ReadTimeCan34(void)
@@ -109,47 +82,23 @@ time2   ReadTimeCan34(void)
 }
 
 
-/*
-double2 ReadCntCurr33(void)
+
+double2 ReadCntCurr34(void)
 {
-  if (QueryOpen33_Full(25) == 0) GetDouble2Error();
+  double2 db2 = QueryEngAbs34_Full(50);
+  if (db2.fValid == false) return GetDouble2Error();
 
-
-  mpdbChannelsC[0] = 0;
-
-
-  uchar t;
-  for (t=0; t<bTARIFFS; t++)
-  {
-    uchar r;
-    for (r=0; r<MaxRepeats(); r++)
-    {
-      DelayOff();
-      QueryEngAbs33(t);
-
-      ShowPercent(50+t);
-
-      if (Input33() == SER_GOODCHECK) break;
-      if (fKey == true) return GetDouble2Error();
-    }
-
-    if (r == MaxRepeats()) return GetDouble2Error();
-    else
-    {
-      if (Checksum33(14) == false) { ShowLo(szBadCRC); Delay(1000); return GetDouble2Error(); }
-
-      InitPop(3);
-      mpdbChannelsC[0] += (double)PopLong33()/1000;
-    }
-  }
-
-
+/*
   double dbTrans = mpdbTransCnt[ibDig];
 
-  mpdbChannelsC[0] *= dbTrans;
-  mpboChannelsA[0] = true;
-
+  uchar i;
+  for (i=0; i<MAX_LINE_N31; i++)
+  {
+    mpdbChannelsC[i] *= dbTrans;
+    mpboChannelsA[i] = true;
+  }
+*/
   return GetDouble2(mpdbChannelsC[diCurr.ibLine], true);
 }
-*/
+
 #endif
