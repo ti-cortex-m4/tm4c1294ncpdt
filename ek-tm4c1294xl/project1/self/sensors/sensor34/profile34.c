a66abca10074479bd6cb23b62775406d8fd049ae
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-02-10 09:33:06 +0300

    profile

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/profile34.c
@@ -0,0 +1,30 @@
+/*------------------------------------------------------------------------------
+PROFILE34.C
+
+
+------------------------------------------------------------------------------*/
+
+//#include "../../main.h"
+//#include "../../memory/mem_factors.h"
+//#include "../../memory/mem_energy_spec.h"
+//#include "../../memory/mem_profile.h"
+//#include "../../serial/ports.h"
+//#include "../../serial/ports_devices.h"
+//#include "../../serial/monitor.h"
+//#include "../../display/display.h"
+//#include "../../keyboard/time/key_timedate.h"
+//#include "../../time/calendar.h"
+//#include "../../time/delay.h"
+//#include "../../time/timedate.h"
+//#include "../../devices/devices.h"
+//#include "../../devices/devices_time.h"
+//#include "../sensor31/automatic31.h"
+//#include "../sensor31/procedure31.h"
+//#include "automatic33.h"
+//#include "device33.h"
+
+
+
+#ifndef SKIP_34
+
+#endif
