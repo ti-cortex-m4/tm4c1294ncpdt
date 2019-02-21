6335993ce63113184bf6d4f26419cc553a082a0c
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-02-21 22:18:58 +0300

    rename

--- ek-tm4c1294xl/project1/self/sensors/sensor34/test_crypto34.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/test_crypto34.c
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------------
-test_crypto.h
+test_crypto34.c
 
 
 ------------------------------------------------------------------------------*/
@@ -10,27 +10,26 @@ test_crypto.h
 
 
 
-void    assert(bool b) {
+static void assert(bool b) {
   while (!b) {
   }
 }
 
 
-uchar *MD5(const uchar *d, size_t n, uchar *md)
+static void _MD5(const uchar *d, size_t size, uchar *md)
 {
-  MD5_CTX c;
-  MD5_Init(&c);
-  MD5_Update(&c, d, n);
-  MD5_Final(md, &c);
-  return md;
+  MD5_CTX ctx;
+  MD5_Init(&ctx);
+  MD5_Update(&ctx, d, size);
+  MD5_Final(md, &ctx);
 }
 
 
-void    TestCrypto(void) {
+void    TestCrypto34(void) {
   const uchar *md5In = "111111";
   uchar md5Out[16];
 
-  MD5(md5In, 6, md5Out);
+  _MD5(md5In, 6, md5Out);
 
   uchar md5Out2[16] = {0x96,0xE7,0x92,0x18,0x96,0x5E,0xB7,0x2C,0x92,0xA5,0x49,0xDD,0x5A,0x33,0x01,0x12};
   uchar i;
@@ -41,9 +40,9 @@ void    TestCrypto(void) {
   uchar aesIn[] = {0x90,0x0B,0x9B,0xEF,0x21,0x4E,0xD4,0xB5,0xCA,0xB6,0xF1,0x93,0xFB,0x31,0x0E,0x84};
   uchar aesOut[16];
 
-  AES_KEY aesKey;
-  AES_set_encrypt_key(md5Out, 128, &aesKey);
-  AES_encrypt(aesIn, aesOut, &aesKey);
+  AES_KEY key;
+  AES_set_encrypt_key(md5Out, 128, &key);
+  AES_encrypt(aesIn, aesOut, &key);
 
   uchar aesOut2[16] = {0xFB,0x41,0x50,0xDF,0x9A,0xC4,0x27,0x31,0xB0,0x44,0x6E,0x48,0x53,0x12,0x1C,0x7F};
   for (i=0; i<16; i++) {
