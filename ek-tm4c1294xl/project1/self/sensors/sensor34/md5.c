e9fe7e542d99077d38c273c3906da321ad0f97d5
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-02-21 07:45:36 +0300

    md5

--- ek-tm4c1294xl/project1/self/sensors/sensor34/md5.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/md5.c
@@ -7,10 +7,7 @@ md5.h
 #include "../../main.h"
 #include "inc/hw_memmap.h"
 #include "driverlib/shamd5.h"
-
-
-
-uint32_t                mpbHash[16];
+#include "md5.h"
 
 
 
@@ -19,5 +16,13 @@ void    MD5(uint32_t *pui32DataSrc, uint32_t ui32DataLength, uint32_t *pui32Hash
   SHAMD5ConfigSet(SHAMD5_BASE, SHAMD5_ALGO_MD5); // MAP_ ?
   SHAMD5DataProcess(SHAMD5_BASE, pui32DataSrc, ui32DataLength, pui32HashResult);
 }
+
+
+void    RunMD5(void) {
+  uint8_t pass[] = {'1','1','1','1','1','1'};
+  uint32_t hash[4];
+  MD5((uint32_t *)pass, 6, &hash);
+}
+
 
 
