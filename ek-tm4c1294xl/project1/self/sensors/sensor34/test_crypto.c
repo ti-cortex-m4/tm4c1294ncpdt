e45d8399deb73a3b1799dc63ff83b1e6f40c5fdd
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-02-21 21:40:40 +0300

    test

--- ek-tm4c1294xl/project1/self/sensors/sensor34/test_crypto.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/test_crypto.c
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------------
-md5.h
+test_crypto.h
 
 
 ------------------------------------------------------------------------------*/
@@ -21,25 +21,35 @@ unsigned char *MD5(const unsigned char *d, size_t n, unsigned char *md)
 }
 
 
-void    RunMD5(void) {
-  const unsigned char* string = "111111";
-  unsigned char digest[16];
-  MD5(string, 6, digest);
+void    assert(bool b) {
+  while (!b) {
+  }
+}
+
+
+void    TestCrypto(void) {
+  const unsigned char* pass = "111111";
+  unsigned char rgbKey[16];
+
+  MD5(pass, 6, rgbKey);
+
+  unsigned char rgbKey2[16] = {0x96,0xE7,0x92,0x18,0x96,0x5E,0xB7,0x2C,0x92,0xA5,0x49,0xDD,0x5A,0x33,0x01,0x12};
+  uchar i;
+  for (i=0;i<16;i++) {
+    assert (rgbKey[i] == rgbKey2[i]);
+  }
 
   unsigned char in[] = {0x90, 0x0B, 0x9B, 0xEF, 0x21, 0x4E, 0xD4, 0xB5, 0xCA, 0xB6, 0xF1, 0x93, 0xFB, 0x31, 0x0E, 0x84};
   unsigned char out[16];
 
   AES_KEY encryptKey;
-  AES_set_encrypt_key(digest, 128, &encryptKey);
-
+  AES_set_encrypt_key(rgbKey, 128, &encryptKey);
   AES_encrypt(in, out, &encryptKey);
-//  aes_encrypt(state,digest);
-//  for (i=0;i<16;i++) {
-//    if (state[i] != ciphertext[i]) {
-//      err_count++;
-//    }
-//  }
 
+  unsigned char out2[16] ={0xFB,0x41,0x50,0xDF,0x9A,0xC4,0x27,0x31,0xB0,0x44,0x6E,0x48,0x53,0x12,0x1C,0x7F};
+  for (i=0;i<16;i++) {
+    assert (out[i] == out2[i]);
+  }
 }
 
 
