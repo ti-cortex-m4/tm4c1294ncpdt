a4f26e06359c848e6f02673f28f5d882b0457caf
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-02-21 06:29:20 +0300

    auth

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/authentication34.c
@@ -0,0 +1,46 @@
+/*------------------------------------------------------------------------------
+authentication34.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+//#include "../../console.h"
+//#include "../../memory/mem_factors.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports_devices.h"
+//#include "../../devices/devices.h"
+//#include "unix_time_gmt34.h"
+//#include "device34.h"
+#include "authentication34.h"
+
+
+
+static  uchar           mpbAuthRequest[16];
+
+
+
+void    QueryAuthRequest(void)
+{
+  InitPush(0);
+
+  PushChar(diCurr.bAddress);
+  PushChar(0x64);
+  PushChar(0x00);
+  PushChar(0x01);
+  PushChar(0x00);
+  PushChar(0x08);
+
+  QueryIO(3+81+2, 6+2);
+}
+
+
+void    ReadAuthRequest(void)
+{
+  InitPop(4);
+
+  uchar i;
+  for (i=0; i<16; i++) {
+    mpbAuthRequest[i] = PopByte();
+  }
+}
