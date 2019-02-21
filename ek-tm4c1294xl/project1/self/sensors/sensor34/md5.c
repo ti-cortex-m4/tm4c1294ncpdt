5259eea35c4a53d3b7e42364d3b6dea21fc2706a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-02-21 07:29:27 +0300

    md5

--- ek-tm4c1294xl/project1/self/sensors/sensor34/md5.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/md5.c
@@ -3,3 +3,22 @@ md5.h
 
 
 ------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/shamd5.h"
+
+
+
+uint32_t                mpbHash[16];
+
+
+
+void    Md5(void)
+{
+  SHAMD5ConfigSet(SHAMD5_BASE, SHAMD5_ALGO_MD5); // MAP_ ?
+  uchar mpbPass[] = {'1', '1', '1', '1', '1', '1'};
+  SHAMD5DataProcess(SHAMD5_BASE, (uint32_t *)mpbPass, 6, (uint32_t *)mpbHash);
+}
+
+
