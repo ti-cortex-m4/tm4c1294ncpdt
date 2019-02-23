3d260489ae4cfa3ccfc442ffe826f944ebd70c55
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-02-23 08:53:52 +0300

    time

--- ek-tm4c1294xl/project1/self/sensors/sensor34/unpack_34.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/unpack_34.c
@@ -7,16 +7,31 @@ UNPACK_34.C
 #include "../../main.h"
 #include "../../serial/ports.h"
 #include "../../serial/ports2.h"
+#include "../../serial/monitor.h"
+#include "device34.h"
 #include "unpack_34.h"
 
 
 
 #ifndef SKIP_34
 
-void    Unpack34(void)
+void    UnpackCorrect34(void)
 {
+  if (IndexInBuff() == 5)
+  {
+#if MONITOR_34
+    MonitorString("\n unpack: time correct");
+#endif
+    SetCountInBuff(IndexInBuff());
+    mpSerial[ibPort] = SER_POSTINPUT_MASTER;
+  }
+}
 
-  if (IndexInBuff() > 6) {
+
+void    UnpackProfile34(void)
+{
+  if (IndexInBuff() > 6)
+  {
     uint wSize = InBuff(4) + 0x100*InBuff(5);
     if (IndexInBuff() == 4+2+2+wSize*24+2)
     {
