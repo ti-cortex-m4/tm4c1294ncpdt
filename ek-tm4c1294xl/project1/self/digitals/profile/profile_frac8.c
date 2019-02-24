e77c83a3c5759b72ec4636690b2fbc9c98b042b7
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-02-24 19:05:38 +0300

    frac8

---
+++ ek-tm4c1294xl/project1/self/digitals/profile/profile_frac8.c
@@ -0,0 +1,46 @@
+/*------------------------------------------------------------------------------
+profile_frac8.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../nvram/cache.h"
+#include "../../nvram/cache2.h"
+#include "profile_frac8.h"
+
+
+
+bool    SaveProfileFrac6(uint  c)
+{
+  ASSERT(c < bCANALS);
+  return SaveArrayX(PROFILE_FRAC_6, sizeof(mpdbEngFracDigCan[0]), c, mpdbEngFracDigCan[c]);
+}
+
+
+bool    LoadProfileFrac6(uint  c)
+{
+  ASSERT(c < bCANALS);
+  return LoadArrayX(PROFILE_FRAC_6, sizeof(mpdbEngFracDigCan[0]), c, mpdbEngFracDigCan[c]);
+}
+
+
+
+void    SaveProfileFrac6_All(void)
+{
+  uchar c;
+  for (c=0; c<bCANALS; c++)
+  {
+    SaveProfileFrac6(c);
+  }
+}
+
+
+void    LoadProfileFrac6_All(void)
+{
+  uchar c;
+  for (c=0; c<bCANALS; c++)
+  {
+    LoadProfileFrac6(c);
+  }
+}
