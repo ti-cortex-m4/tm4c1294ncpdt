989f680fd6ab0318b388eeb0e8af71d17bd0dfda
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-02-25 21:12:33 +0300

    crc

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor34/crc34.c
@@ -0,0 +1,30 @@
+/*------------------------------------------------------------------------------
+crc34.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "crc34.h"
+
+
+
+unsigned short CRC34(unsigned short  *data, int  count) {
+  const unsigned long base = 7;
+
+  unsigned long hash = base;
+  unsigned long crc = count;
+
+  while (count > 1) {
+    crc += hash * (*data++);
+    hash *= base;
+    count -= 2;
+  }
+
+  if (count) {
+    crc += *(unsigned char*)data * hash;
+  }
+
+  crc *= hash;
+  return crc + (crc >> 16);
+}
