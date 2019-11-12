3d895091161562b6ff85de627d160fa01642d013
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-11-12 21:12:20 +0300

    crc35

---
+++ ek-tm4c1294xl/project1/self/kernel/crc_35.h
@@ -0,0 +1,9 @@
+/*------------------------------------------------------------------------------
+CRC_N31.H
+
+------------------------------------------------------------------------------*/
+
+uchar   MakeCrc8Bit31(uchar  *pbData, uint  wSize);
+uint    MakeCrc16Bit31(uchar  *pbData, uint  wSize);
+
+uint    MakeCrc16Bit32(uchar  *pbData, uint  wSize);
