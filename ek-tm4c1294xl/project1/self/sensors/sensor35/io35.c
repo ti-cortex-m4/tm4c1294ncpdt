447561e8abc46cf8f97af7ec7ea5da7d0d0e777f
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-11-16 16:11:27 +0300

    io35

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor35/io35.c
@@ -0,0 +1,376 @@
+/*------------------------------------------------------------------------------
+AUTOMATIC35!C
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+//#include "../../memory/mem_profile.h"
+//#include "../../memory/mem_factors.h"
+#include "../../display/display.h"
+#include "../../keyboard/keyboard.h"
+#include "../../time/delay.h"
+#include "../../time/timedate.h"
+#include "../../hardware/watchdog.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports2.h"
+#include "../../serial/ports_devices.h"
+#include "../../serial/input_wrapper.h"
+#include "../../serial/monitor.h"
+#include "../../devices/devices.h"
+#include "../../digitals/digitals.h"
+#include "../../digitals/wait_answer.h"
+#include "unpack35.h"
+#include "device35.h"
+#include "automatic35.h"
+
+
+
+#ifndef SKIP_35
+
+static void Query35Internal(uchar  cbIn, uchar  cbOut, uchar  bCommand)
+{
+  if (cbOut > 0)
+  {
+    // расчет CRC счетчика
+    uchar bCrc = MakeCrcSOutBuff(1, cbOut-3);
+
+    InitPush(0);
+    PushChar(0xC0);
+
+    uchar i;
+    for (i=0; i<cbOut-3; i++) SkipChar();
+
+    PushChar(bCrc);
+    PushChar(0xC0);
+
+
+    // перенос пакета счетчика внутри пакета концентратора
+    for (i=0; i<cbOut; i++)
+      SetOutBuff(cbOut+12-i, OutBuff(cbOut-1-i));
+  }
+
+
+  // заполнение пакета концентратора
+  InitPush(0);
+  PushChar(0xC0);
+
+  PushChar(0x02);
+
+  PushChar(0);
+  PushChar(0);
+  PushChar(0);
+  PushChar(0);
+
+  PushChar(0x00);
+
+  PushChar(bCommand);
+
+  PushChar(0xB6);   // TODO 35
+  PushChar(0x59);
+  PushChar(0x00);
+  PushChar(0x00);
+
+  PushChar(0x00);
+
+
+  // расчет CRC концентратора
+  InitPush(13+cbOut);
+
+  uint w = MakeCrc35OutBuff(1, 13+cbOut-1);
+  PushChar(w / 0x100);
+  PushChar(w % 0x100);
+
+  PushChar(0xC0);
+
+  cbOut = 13+cbOut+3;
+
+
+  // упаковка по протоколу SLIP
+  uchar i;
+  for (i=0; i<=cbOut-1; i++)
+    mpbOutBuffSave[i] = OutBuff(i);
+
+  uchar j = 0;
+  SetOutBuff(j++, 0xC0);
+  for (i=1; i<=cbOut-2; i++)
+  {
+    if (mpbOutBuffSave[i] == 0xC0)
+    {
+      SetOutBuff(j++, 0xDB);
+      SetOutBuff(j++, 0xDC);
+    }
+    else if (mpbOutBuffSave[i] == 0xDB)
+    {
+      SetOutBuff(j++, 0xDB);
+      SetOutBuff(j++, 0xDD);
+    }
+    else
+    {
+      SetOutBuff(j++, mpbOutBuffSave[i]);
+    }
+  }
+  SetOutBuff(j++, 0xC0);
+
+
+  Query(cbIn,j,true);
+}
+
+
+void    Query35(uchar  cbIn, uchar  cbOut)
+{
+  Query35Internal(cbIn, cbOut, 0x11);
+}
+
+
+
+static serial Input35Internal(void)
+{
+  InputStart();
+  InitWaitAnswer();
+
+  while (1)
+  {
+    if (fKey == true) { mpSerial[ibPort] = SER_BADLINK; break; }
+
+    ResetWatchdog();
+    ShowWaitAnswer(1);
+    if (GetWaitAnswer()) { mpSerial[ibPort] = SER_BADLINK; break; }
+
+    if (mpSerial[ibPort] == SER_INPUT_MASTER)
+      Decompress35();
+
+    if (mpSerial[ibPort] == SER_POSTINPUT_MASTER)
+    {
+      if (Checksum35Router() == 0)
+      {
+        InputGoodCheck();
+        mpSerial[ibPort] = SER_GOODCHECK;
+      }
+      else
+        mpSerial[ibPort] = SER_BADCHECK;
+
+      break;
+    }
+    else if ((mpSerial[ibPort] == SER_OVERFLOW) ||
+             (mpSerial[ibPort] == SER_BADLINK)) break;
+  }
+
+  return mpSerial[ibPort];
+}
+
+
+serial  Input35(void)
+{
+  SaveDisplay();
+
+  uint r = 0;
+
+  bool repeat;
+  do
+  {
+    Input35Internal();
+    repeat = false;
+
+    if (mpSerial[ibPort] == SER_GOODCHECK)
+    {
+       if (InBuff(7) == 0x14)
+       {
+         sprintf(szLo,"  ожидание: %u   ",(r++ / 2) + 1); Delay(500);
+
+         MonitorString("\n NNCL2 repeat");
+
+         Query35Internal(250, 0, 0x12);
+         repeat = true;
+       }
+       else if (InBuff(7) == 0x12)
+       {
+         if (IndexInBuff() < 15)
+         {
+           MonitorString("\n NNCL2 wrong size");
+           MonitorIntDec(IndexInBuff());
+
+           Clear(); sprintf(szLo+1,"ошибка: 35.4.%u",InBuff(7)); // TODO 35
+           DelayInf();
+           mpSerial[ibPort] = SER_BADCHECK;
+         }
+         else
+         {
+           MonitorString("\n NNCL2 success");
+
+           uchar i;
+           for (i=0; i<IndexInBuff()-15; i++)
+             SetInBuff(i, InBuff(12+i));
+
+           // TODO 35 проверить данные
+         }
+       }
+       else
+       {
+         MonitorString("\n NNCL2 failure");
+         MonitorCharDec(InBuff(7));
+
+         Clear(); sprintf(szLo+1,"ошибка: 35.5.%u",InBuff(7));
+         DelayInf();
+         mpSerial[ibPort] = SER_BADCHECK;
+       }
+    }
+  } while (repeat);
+
+  LoadDisplay();
+
+  return mpSerial[ibPort];
+}
+
+
+
+bool    Automatic35(void)
+{
+  Clear();
+
+  if (QueryConfigS_Full(50) == 0) return(0);
+
+  dbKpulse = wDividerS;                 // K преобразовани€
+  dbKtrans = 1;                         // K трансформации
+  SetAllFactors(dbKpulse,dbKtrans);     // сохранение   преобразовани€ и   трансформации
+
+  return(1);
+}
+
+
+
+bool    QueryConfig35_Full(uchar  bPercent)
+{
+  uchar i;
+  for (i=0; i<MaxRepeats(); i++)
+  {
+    DelayOff();
+    QueryConfig35();
+
+    if (Input35() == SER_GOODCHECK) break;
+    if (fKey == true) return(0);
+  }
+
+  if (i == MaxRepeats()) return(0);
+  ShowPercent(bPercent);
+
+  ReadConfig35();
+  return(1);
+}
+
+
+time2   QueryTime35_Full(uchar  bPercent)
+{
+  uchar i;
+  for (i=0; i<MaxRepeats(); i++)
+  {
+    DelayOff();
+    QueryTime35();
+
+    if (Input35() == SER_GOODCHECK) break;
+    if (fKey == true) return GetTime2Error();
+  }
+
+  if (i == MaxRepeats()) return GetTime2Error();
+  ShowPercent(bPercent);
+
+  return GetTime2(ReadTime35(), true);
+}
+
+
+bool    QueryEngDay35_Full(uchar  bTime, uchar  bPercent)
+{
+  uchar i;
+  for (i=0; i<MaxRepeats(); i++)
+  {
+    DelayOff();
+    QueryEngDay35(bTime);
+
+    if (Input35() == SER_GOODCHECK) break;
+    if (fKey == true) return(0);
+  }
+
+  if (i == MaxRepeats()) return(0);
+  ShowPercent(bPercent);
+
+  ReadEnergy35();
+  return(1);
+}
+
+
+bool    QueryEngMon35_Full(uchar  bTime, uchar  bPercent)
+{
+  uchar i;
+  for (i=0; i<MaxRepeats(); i++)
+  {
+    DelayOff();
+    QueryEngMon35(bTime);
+
+    if (Input35() == SER_GOODCHECK) break;
+    if (fKey == true) return(0);
+  }
+
+  if (i == MaxRepeats()) return(0);
+  ShowPercent(bPercent);
+
+  ReadEnergy35();
+  return(1);
+}
+
+
+
+time2   ReadTimeCan35(void)
+{
+  Clear();
+
+  time2 ti2 = QueryTime35_Full(50);
+  if (ti2.fValid == false) return GetTime2Error();
+
+  tiChannelC = ti2.tiValue;
+  mpboChannelsA[0] = true;
+
+  return GetTime2(ti2.tiValue, true);
+}
+
+
+double2 ReadCntCurr35(void)
+{
+  Clear();
+
+  if (QueryConfig35_Full(50) == 0) return GetDouble2Error();
+
+  if (QueryEngMon35_Full(0, 75) == 0) return GetDouble2Error();
+
+  mpdbChannelsC[0] = (double)mpdwChannelsA[0] / GetDivider35();
+  mpboChannelsA[0] = true;
+
+  return GetDouble2(mpdbChannelsC[0], true);
+}
+
+
+double2 ReadCntMonCan35(uchar  ibMon)
+{
+  Clear();
+
+  if (QueryConfig35_Full(25) == 0) return GetDouble2Error();
+
+  time2 ti2 = QueryTime35_Full(50);
+  if (ti2.fValid == false) return GetDouble2Error();
+  time ti = ti2.tiValue;
+
+  if (ti.bMonth != ibMon+1)
+  {
+    if (QueryEngMon35_Full((bMONTHS+ti.bMonth-1-ibMon) % bMONTHS, 75) == 0) return GetDouble2Error();
+  }
+  else
+  {
+    if (QueryEngDay35_Full(1, 75) == 0) return GetDouble2Error();
+  }
+
+  mpdbChannelsC[0] = (double)mpdwChannelsA[0] / wDividerS;
+  mpboChannelsA[0] = true;
+
+  return GetDouble2(mpdbChannelsC[0], true);
+}
+
+#endif
