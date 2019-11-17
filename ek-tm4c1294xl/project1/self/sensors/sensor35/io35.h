e8c1016476b954379b8104aabb1208a53108bfb0
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-11-17 14:09:23 +0300

    io

--- ek-tm4c1294xl/project1/self/sensors/sensor35/io35.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor35/io35.h
@@ -4,5 +4,11 @@ IO35!H
 
 ------------------------------------------------------------------------------*/
 
+extern  uchar                       cbRepeat35;
+
+
+
+void    Query35Internal(uchar  cbIn, uchar  cbOut, uchar  bCommand);
 void    Query35(uchar  cbIn, uchar  cbOut);
+
 serial  Input35(void);
