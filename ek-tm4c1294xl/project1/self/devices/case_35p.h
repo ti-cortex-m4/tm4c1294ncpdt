427b92db95ca850760e2f445b5d9b9c54021c35d
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-11-20 08:00:27 +0300

    profile 35

---
+++ ek-tm4c1294xl/project1/self/devices/case_35p.h
@@ -0,0 +1,204 @@
+
+#ifndef SKIP_35
+
+    case DEV_START_35P:
+      cbRepeat = MaxRepeats();
+      QueryVersion35();
+      SetCurr(DEV_VERSION_35P);
+      break;
+
+    case DEV_VERSION_35P:
+      if (mpSerial[ibPort] == SER_GOODCHECK)
+      {
+        ReadVersion35();
+
+        if (boHideMessages == false)
+          MakeLongPause(DEV_POSTVERSION_35P, 1);
+        else
+          MakePause(DEV_POSTVERSION_35P);
+      }
+      else
+      {
+        if (cbRepeat == 0) ErrorProfile();
+        else
+        {
+          ErrorLink();
+          cbRepeat--;
+
+          QueryVersion35();
+          SetCurr(DEV_VERSION_35P);
+        }
+      }
+      break;
+
+
+    case DEV_POSTVERSION_35P:
+      cbCorrects = 0;
+
+      if (fCurrCtrl == true)
+        MakePause(DEV_POSTOPENCANAL_35P);
+      else
+        MakePause(DEV_POSTCORRECT_35P);
+      break;
+
+    case DEV_POSTOPENCANAL_35P:
+      Clear(); ShowLo(szRepeats);
+      sprintf(szLo+8,"%1u",cbCorrects+1); DelayInf();
+
+      cbRepeat = MaxRepeats();
+      QueryTime35();
+      SetCurr(DEV_TIME_35P);
+      break;
+
+    case DEV_TIME_35P:
+      if (mpSerial[ibPort] == SER_GOODCHECK)
+      {
+        tiDig = ReadTime35();
+        MakePause(DEV_POSTTIME_35P);
+      }
+      else
+      {
+        if (cbRepeat == 0) ErrorProfile();
+        else
+        {
+          ErrorLink();
+          cbRepeat--;
+
+          QueryTime35();
+          SetCurr(DEV_TIME_35P);
+        }
+      }
+      break;
+
+
+    case DEV_POSTTIME_35P:
+      {
+        ulong dwSecond1 = GetSecondIndex(tiDig);
+        ulong dwSecond2 = GetSecondIndex(tiCurr);
+
+        if (DifferentDay(tiDig, tiCurr))
+        { ShowLo(szBadDates); DelayMsg(); ErrorProfile(); }                       // даты не совпадают, коррекция невозможна
+        else
+        {
+          ShowDigitalDeltaTime(ibDig, dwSecond1, dwSecond2);
+
+          if (AbsLong(dwSecond1 - dwSecond2) < GetCorrectLimit())                 // без коррекции
+          { ShowLo(szCorrectNo); DelayInf(); MakePause(DEV_POSTCORRECT_35P); }
+          else if (GetCurrHouIndex() == (tiDig.bHour*2 + tiDig.bMinute/30))       // простая коррекция
+          { ShowLo(szCorrectYes); DelayInf(); MakePause(DEV_CONTROL_35P); }
+          else
+          { ShowLo(szCorrectBig); DelayMsg(); ErrorProfile(); }                   // разница времени слишком велика, коррекция невозможна
+        }
+      }
+      break;
+
+    case DEV_CONTROL_35P:
+      if (++cbCorrects > bCORRECTS)
+        MakePause(DEV_POSTCORRECT_35P);
+      else
+      {
+        cbRepeat = MaxRepeats();
+        QueryControl35(tiCurr);
+        SetCurr(DEV_POSTOPENCANAL_35P);
+      }
+      break;
+
+
+    case DEV_POSTCORRECT_35P:
+      Clear(); ShowPercent(25);
+
+      cbRepeat = MaxRepeats();
+      QueryConfig35();
+      SetCurr(DEV_CONFIG_35P);
+      break;
+
+    case DEV_CONFIG_35P:
+      if (mpSerial[ibPort] == SER_GOODCHECK)
+      {
+        ReadConfig35();
+        MakePause(DEV_POSTCONFIG_35P);
+      }
+      else
+      {
+        if (cbRepeat == 0) ErrorProfile();
+        else
+        {
+          ErrorLink();
+          cbRepeat--;
+
+          QueryConfig35();
+          SetCurr(DEV_CONFIG_35P);
+        }
+      }
+      break;
+
+    case DEV_POSTCONFIG_35P:
+      ShowPercent(50);
+
+      cbRepeat = MaxRepeats();
+      QueryTime35();
+      SetCurr(DEV_VALUE_35P);
+      break;
+
+    case DEV_VALUE_35P:
+      if (mpSerial[ibPort] == SER_GOODCHECK)
+      {
+        tiValueS = ReadTime35();
+        dwValueS = DateToHouIndex(tiValueS);
+        MakePause(DEV_POSTVALUE_35P);
+      }
+      else
+      {
+        if (cbRepeat == 0)
+          ErrorProfile();
+        else
+        {
+          ErrorLink();
+          cbRepeat--;
+
+          QueryTime35();
+          SetCurr(DEV_VALUE_35P);
+        }
+      }
+      break;
+
+    case DEV_POSTVALUE_35P:
+      ShowPercent(75);
+      InitHeader35();
+
+      cbRepeat = MaxRepeats();
+      QueryHeader35();
+      SetCurr(DEV_HEADER_35P);
+      break;
+
+    case DEV_HEADER_35P:
+      if (mpSerial[ibPort] == SER_GOODCHECK)
+        MakePause(DEV_POSTHEADER_35P);
+      else
+      {
+        if (cbRepeat == 0) ErrorProfile();
+        else
+        {
+          ErrorLink();
+          cbRepeat--;
+
+          QueryHeader35();
+          SetCurr(DEV_HEADER_35P);
+        }
+      }
+      break;
+
+    case DEV_POSTHEADER_35P:
+      if (ReadHeader35() == 0)
+        DoneProfile();
+      else
+        MakePause(DEV_DATA_35P);
+      break;
+
+    case DEV_DATA_35P:
+      cbRepeat = MaxRepeats();
+      QueryHeader35();
+      SetCurr(DEV_HEADER_35P);
+      break;
+
+#endif
