54e8931aad86a89f5f6db4a9ffc9ce8703ba88dc
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-11-20 21:55:57 +0300

    timeouts

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor35/timeout35.h
@@ -0,0 +1,10 @@
+/*------------------------------------------------------------------------------
+timeout35.h
+
+
+------------------------------------------------------------------------------*/
+
+void    ResetTimeoutHistogramAll35(void);
+void    ResetTimeoutHistogramDay35(void);
+
+void    TimeoutHistogramDay35(uint  wTimeout);
