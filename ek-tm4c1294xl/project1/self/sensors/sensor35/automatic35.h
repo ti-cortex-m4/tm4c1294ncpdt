b9cf5128f2ad4a1e56d41b47aaeb07dfd02fb8b0
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-11-22 20:22:51 +0300

    rename

--- ek-tm4c1294xl/project1/self/sensors/sensor35/automatic35.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor35/automatic35.h
@@ -6,6 +6,9 @@ AUTOMATIC35.H
 
 bool    Automatic35(void);
 
+bool    QueryConfig35_Full(uchar  bPercent);
+time2   QueryTime35_Full(uchar  bPercent);
+
 time2   ReadTimeCan35(void);
 time2   ReadTimeCan35_Short(void);
 double2 ReadCntCurr35(void);
