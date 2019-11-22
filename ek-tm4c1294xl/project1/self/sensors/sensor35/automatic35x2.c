c52ff8601d35f1140556b9fb9d9366ab3300266b
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-11-22 20:22:51 +0300

    rename

--- ek-tm4c1294xl/project1/self/sensors/sensor35/automatic35x2.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor35/automatic35x2.c
@@ -4,43 +4,44 @@ AUTOMATIC35x2!C
 
 ------------------------------------------------------------------------------*/
 
-#include "../main.h"
-#include "../memory/mem_profile.h"
-#include "../memory/mem_factors.h"
-#include "../display/display.h"
-#include "../keyboard/keyboard.h"
-#include "../time/delay.h"
-#include "../time/timedate.h"
-#include "../hardware/watchdog.h"
-#include "../kernel/crc_s.h"
-#include "../serial/ports.h"
-#include "../serial/ports2.h"
-#include "../serial/ports_devices.h"
-#include "../devices/devices.h"
-#include "../sensors/unpack_s.h"
-#include "../digitals/digitals.h"
-#include "../digitals/wait_answer.h"
-#include "automatic1.h"
+#include "../../main.h"
+#include "../../memory/mem_profile.h"
+#include "../../memory/mem_factors.h"
+#include "../../display/display.h"
+#include "../../keyboard/keyboard.h"
+#include "../../time/delay.h"
+#include "../../time/timedate.h"
+#include "../../hardware/watchdog.h"
+#include "../../kernel/crc_s.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports2.h"
+#include "../../serial/ports_devices.h"
+#include "../../devices/devices.h"
+#include "../../sensors/unpack_s.h"
+#include "../../digitals/digitals.h"
+#include "../../digitals/wait_answer.h"
 #include "device35.h"
+#include "io35.h"
+#include "automatic35.h"
 #include "automatic35x2.h"
 
 
 
-#ifndef SKIP_S
+#ifndef SKIP_35
 
 // ReadTariffValue [0x0130]
 // „тение значени€ энергии по тарифу.
 // UINT8[1] Ц номер тарифа (0 Ц тариф є1, 1 Ц тариф є2, Е, 7 Ц тариф є8);
 // UINT8[2] Ц индекс глубины опроса (0 Ц текущее значение, 1 Ц значение на конец прошедшего мес€ца, 2 Ц значение на конец второго мес€ца назад, Е, 13 Ц значение на конец тринадцатого мес€ца назад)
 // DATA4[1] - значение энергии.
-void    QueryEngMonTariffS(uchar  ibMon, uchar  ibTrf)
+void    QueryEngMonTariff35(uchar  ibMon, uchar  ibTrf)
 {
   InitPush(0);
 
   PushChar(0xC0);
   PushChar(0x48);
 
-  PushAddressS();
+  PushAddress35();
 
   PushChar(0xD2);
   PushChar(0x01);
@@ -48,47 +49,47 @@ void    QueryEngMonTariffS(uchar  ibMon, uchar  ibTrf)
   PushChar(ibTrf);
   PushChar(ibMon);
 
-  QueryS(100+15, 17);
+  Query35(100+15, 17);
 }
 
 
-bool    QueryEngMonTariffS_Full(uchar  ibMon, uchar  ibTrf, uchar  bPercent)
+bool    QueryEngMonTariff35_Full(uchar  ibMon, uchar  ibTrf, uchar  bPercent)
 {
   uchar i;
   for (i=0; i<MaxRepeats(); i++)
   {
     DelayOff();
-    QueryEngMonTariffS(ibMon, ibTrf);
+    QueryEngMonTariff35(ibMon, ibTrf);
 
-    if (InputS() == SER_GOODCHECK) break;
+    if (Input35() == SER_GOODCHECK) break;
     if (fKey == true) return(0);
   }
 
   if (i == MaxRepeats()) return(0);
   ShowPercent(bPercent);
 
-  ReadEnergyS();
+  ReadEnergy35();
   return(1);
 }
 
 
-status  ReadCntMonCanTariffS(uchar  ibMonth, uchar  ibTrf) // на начало мес€ца
+status  ReadCntMonCanTariff35(uchar  ibMonth, uchar  ibTrf) // на начало мес€ца
 {
   Clear();
 
-  if (QueryConfigS_Full(25) == 0) return ST_BADDIGITAL;
+  if (QueryConfig35_Full(25) == 0) return ST_BADDIGITAL;
 
 
-  time2 ti2 = QueryTimeS_Full(50);
+  time2 ti2 = QueryTime35_Full(50);
   if (ti2.fValid == false) return ST_BADDIGITAL;
   time ti = ti2.tiValue;
 
 
   uchar ibMon = ((12 + ti.bMonth - 1 - ibMonth) % 12) + 1;
-  if (QueryEngMonTariffS_Full(ibMon, ibTrf, 75) == 0) return ST_BADDIGITAL;
+  if (QueryEngMonTariff35_Full(ibMon, ibTrf, 75) == 0) return ST_BADDIGITAL;
 
 
-  mpdbChannelsC[0] = (double)mpdwChannelsA[0] / wDividerS;
+  mpdbChannelsC[0] = (double)mpdwChannelsA[0] / GetDivider35();
   mpboChannelsA[0] = true;
 
   return ST_OK;
