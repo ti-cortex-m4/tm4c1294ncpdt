e3d71374b00c8033c8f1e601b2e5941bd830d534
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-11-25 07:40:30 +0300

    status

--- ek-tm4c1294xl/project1/self/sensors/sensor35/status35.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor35/status35.h
@@ -4,11 +4,10 @@ STATUS35!H
 
 ------------------------------------------------------------------------------*/
 
-extern volatile serial         Serial35;
-
 void    SetCurr35Internal(device  de);
 device  GetCurr35Internal(void);
 
 void    SetCurr35(device  de);
 
+void    SetSerial35(serial  se);
 bool    IsSerial35(void);
