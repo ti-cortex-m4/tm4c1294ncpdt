0d2956747e837fbfa9c45a7af41c722eb1597628
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-11-28 20:48:22 +0300

    pack & unpack

--- ek-tm4c1294xl/project1/self/sensors/sensor35/io35.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor35/io35.c
@@ -33,36 +33,6 @@ static uchar                bCommandSave;
 
 
 
-uchar   Pack35(uchar  cbOut)
-{
-  uchar i;
-  for (i=0; i<=cbOut-1; i++)
-    mpbOutBuffSave[i] = OutBuff(i);
-
-  uchar j = 0;
-
-  SetOutBuff(j++, 0xC0);
-  for (i=1; i<=cbOut-2; i++)
-  {
-    if (mpbOutBuffSave[i] == 0xC0)
-    {
-      SetOutBuff(j++, 0xDB);
-      SetOutBuff(j++, 0xDC);
-    }
-    else if (mpbOutBuffSave[i] == 0xDB)
-    {
-      SetOutBuff(j++, 0xDB);
-      SetOutBuff(j++, 0xDD);
-    }
-    else
-    {
-      SetOutBuff(j++, mpbOutBuffSave[i]);
-    }
-  }
-  SetOutBuff(j++, 0xC0);
-
-  return j;
-}
 
 
 void    Query35Internal(uchar  cbIn, uchar  cbOut, uchar  bCommand)
--- ek-tm4c1294xl/project1/self/sensors/sensor35/router35.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor35/router35.c
@@ -22,39 +22,6 @@ ROUTER35!C
 
 #ifndef SKIP_35
 
-void    Unpack35(void)
-{
-  uchar i,j;
-
-  i = 1;
-  j = 1;
-
-  while (i < IndexInBuff()) {
-    if ((InBuff(i) == 0xDB) && (InBuff(i+1) == 0xDD)) {
-#ifdef MONITOR_35
-      MonitorString("\n unpack 0xDB at "); MonitorCharDec(i);
-#endif
-      SetInBuff(j, 0xDB);
-      i++; i++;
-      j++;
-    } else if ((InBuff(i) == 0xDB) && (InBuff(i+1) == 0xDC)) {
-#ifdef MONITOR_35
-      MonitorString("\n unpack 0xC0 at "); MonitorCharDec(i);
-#endif
-      SetInBuff(j, 0xC0);
-      i++; i++;
-      j++;
-    } else {
-      SetInBuff(j, InBuff(i));
-      i++;
-      j++;
-    }
-  }
-
-  SetIndexInBuff(j);
-}
-
-
 void    Decompress35(void)
 {
   uchar i,j;
