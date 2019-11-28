e9c09534428103a227a40d63c49cefb1d6cc174a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-11-28 20:34:40 +0300

    rename

--- ek-tm4c1294xl/project1/self/sensors/sensor35/unpack35.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor35/unpack35.h
@@ -4,6 +4,7 @@ DECOMPRESS35!H
 
 ------------------------------------------------------------------------------*/
 
+void    Unpack35(void);
 void    Decompress35(void);
 
 uchar   ChecksumRouter35(void);
