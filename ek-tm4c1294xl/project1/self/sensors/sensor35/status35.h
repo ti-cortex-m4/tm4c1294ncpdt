fbea0756af009fbdb414e06ef08d9b366f5ad2a4
TI Cortex M4 <ti.cortex.m4@gmail.com>
2019-11-30 09:50:17 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor35/status35.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor35/status35.h
@@ -4,7 +4,6 @@ STATUS35!H
 
 ------------------------------------------------------------------------------*/
 
-void    SetCurr35Internal(device  de);
 device  GetCurr35Internal(void);
 
 void    SetCurr35(device  de);
