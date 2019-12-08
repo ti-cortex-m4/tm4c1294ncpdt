274b9bd930adc004c491ae7f818479ad9c888829
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-12-08 15:29:18 +0300

    update

--- ek-tm4c1294xl/project1/self/isr/serial_flow.h
+++ ek-tm4c1294xl/project1/self/isr/serial_flow.h
@@ -5,3 +5,7 @@ serial_flow.h
 ------------------------------------------------------------------------------*/
 
 void    InitSerialFlow(void);
+
+void    InSerialFlow(uchar  p, uchar  b);
+
+void    InDelaySerialFlow_Timer0(void);
