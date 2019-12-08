38d78e653ba5709ba45a18e55445331e18c764b4
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-12-08 15:14:37 +0300

    fix

--- ek-tm4c1294xl/project1/self/isr/serial_flow.c
+++ ek-tm4c1294xl/project1/self/isr/serial_flow.c
@@ -9,3 +9,47 @@ serial_flow.c
 
 
 
+volatile serial     mpSerialF[2];
+
+volatile uchar      mpbInBuffF[2][wINBUFF_SIZE];
+
+volatile uint       iwInBuffF[2];
+
+volatile uint       cwInDelayF[2];
+
+
+
+void    InitSerialFlow(void)
+{
+  uchar i;
+  for (i=0; i<2; i++)
+  {
+    mpSerialF[i] = SER_BEGIN;
+  }
+}
+
+
+void    InitSerialFlow(uchar  ibPrt, uchar  b)
+{
+  if (mpSerial[0] == SER_BEGIN)
+  {
+    cwInDelay0 = mpwMinorInDelay[0];
+
+    mpSerial[0] = SER_INPUT_SLAVE;
+    iwInBuff0 = 0;
+  }
+
+  if (mpSerial[0] == SER_INPUT_SLAVE)
+  {
+    if (iwInBuff0 >= wINBUFF_SIZE)
+    {
+      InputMode0();
+      mpSerial[0] = SER_BEGIN;
+    }
+    else
+    {
+      cwInDelay0 = mpwMinorInDelay[0];
+      mpbInBuff0[ iwInBuff0++ ] = bT;
+    }
+  }
+}
