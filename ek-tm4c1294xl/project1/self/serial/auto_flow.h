94f367598bc3b912013984c4dd2c1cb0d3a4f08a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-12-11 21:20:31 +0300

    remove

--- ek-tm4c1294xl/project1/self/serial/auto_flow.h
+++ ek-tm4c1294xl/project1/self/serial/auto_flow.h
@@ -4,34 +4,17 @@ auto_flow.h
 
 ------------------------------------------------------------------------------*/
 
-#ifndef __AUTO_FLOW
-#define __AUTO_FLOW
-
-
-typedef struct
-{
-  uchar         bMinuteStart;
-  uchar         bMinuteStop;
-} auto_flow;
-
-
-#endif
-
-
 extern  bool                    fAutoFlowEnbl;
 extern  uchar                   mpibAutoFlowHou[48];
-extern  auto_flow               stAutoFlow;
 
 
 extern  cache const             chAutoFlowEnbl;
 extern  cache const             chAutoFlowHou;
-extern  cache const             chAutoFlow;
 
 
 
 void    InitAutoFlow(void);
 void    ResetAutoFlow(void);
-bool    IsValidAutoFlow(auto_flow  af);
 
 bool    IsAutoFlowTo3(void);
 bool    IsAutoFlowTo4(void);
