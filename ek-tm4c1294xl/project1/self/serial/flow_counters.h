1a2517846441a96a6006bca7d760f38deedd8488
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-12-14 12:17:14 +0300

    cnt

---
+++ ek-tm4c1294xl/project1/self/serial/flow_counters.h
@@ -0,0 +1,16 @@
+/*------------------------------------------------------------------------------
+timeout35.h
+
+
+------------------------------------------------------------------------------*/
+
+void    InitTimeoutHistogram35(void);
+void    NextDayResetTimeoutHistogram35(void);
+
+void    Timeout35(uint  wTimeout);
+
+uchar   GetTimeoutDelta35(void);
+
+void    OutTimeoutHistogramAll35(void);
+void    OutResetTimeoutHistogramAll35(void);
+void    OutResetTimeoutHistogramDay35(void);
