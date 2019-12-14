db79f2812b0a35ec63e6a7d443114236d0c30193
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2019-12-14 12:41:17 +0300

    fix

--- ek-tm4c1294xl/project1/self/isr/serial_flow.c
+++ ek-tm4c1294xl/project1/self/isr/serial_flow.c
@@ -12,6 +12,7 @@ serial_flow.c
 #include "../memory/mem_serial3.h"
 #include "../memory/mem_flow.h"
 #include "../serial/ports.h"
+#include "../serial/flow.h"
 #include "../serial/auto_flow.h"
 #include "serial0.h"
 #include "serial1.h"
@@ -21,7 +22,7 @@ serial_flow.c
 
 
 
-volatile serial     mpSerialF[2];
+volatile serial     mpZerial[2];
 
 uchar               mpbInBuffF[2][wINBUFF_SIZE];
 
@@ -36,7 +37,7 @@ void    InitSerialFlow(void)
   uchar p;
   for (p=0; p<2; p++)
   {
-    mpSerialF[p] = SER_BEGIN;
+    mpZerial[p] = SER_BEGIN;
   }
 }
 
@@ -44,17 +45,17 @@ void    InitSerialFlow(void)
 
 void    InSerialFlow(uchar  p, uchar  b)
 {
-  if (mpSerialF[p] == SER_BEGIN)
+  if (mpZerial[p] == SER_BEGIN)
   {
-    mpSerialF[p] = SER_INPUT_SLAVE;
+    mpZerial[p] = SER_INPUT_SLAVE;
     iwInBuffF[p] = 0;
   }
 
-  if (mpSerialF[p] == SER_INPUT_SLAVE)
+  if (mpZerial[p] == SER_INPUT_SLAVE)
   {
     if (iwInBuffF[p] >= wINBUFF_SIZE)
     {
-      mpSerialF[p] = SER_BEGIN;
+      mpZerial[p] = SER_BEGIN;
     }
     else
     {
@@ -67,7 +68,7 @@ void    InSerialFlow(uchar  p, uchar  b)
 
 void    StopSerialFlow(uchar  p)
 {
-  mpSerialF[p] = SER_BEGIN;
+  mpZerial[p] = SER_BEGIN;
 }
 
 
@@ -76,10 +77,10 @@ void    InDelaySerialFlow_Timer0(void)
   uchar p;
   for (p=0; p<2; p++)
   {
-    if (mpSerialF[p] == SER_INPUT_SLAVE)
+    if (mpZerial[p] == SER_INPUT_SLAVE)
     {
       if (cwInDelayF[p] == 0)
-        mpSerialF[p] = SER_POSTINPUT_SLAVE;
+        mpZerial[p] = SER_POSTINPUT_SLAVE;
       else
         cwInDelayF[p]--;
     }
@@ -90,116 +91,54 @@ void    InDelaySerialFlow_Timer0(void)
 
 void    RunResponseSerialFlow2(void)
 {
-    if (mpSerialF[0] == SER_POSTINPUT_SLAVE)
-    {
-      mpSerialF[0] = SER_BEGIN;
+  if (mpZerial[0] == SER_POSTINPUT_SLAVE)
+  {
+    mpZerial[0] = SER_BEGIN;
 
-      if (!IsAutoFlowTo3()) return;
-      if ((fFlow == 1) && (ibFlowPortFrom == 1)) return;
+    if (!IsAutoFlowTo3()) return;
+    if ((fFlow == 1) && (ibFlowPortFrom == 1)) return;
 
-      fFlow = 1;
-      ibFlowPortFrom = 0;
-
-      cbInFlow0++;
-      iwInFlow0 = iwInBuffF[0];
-
-      memcpy(mpbOutBuff2,mpbInBuffF[0],iwInBuffF[0]);
-      Query2(0xFFFF,iwInBuffF[0],1);
-    }
-    else if (mpSerial[2] == SER_BADLINK)
-    {
-      mpSerial[2] = SER_BEGIN;
-
-      cbOutFlow0++;
-      iwOutFlow0 = iwInBuff2;
-
-      memcpy(mpbOutBuff0,mpbInBuff2,iwInBuff2);
-      Answer0(iwInBuff2,SER_OUTPUT_SLAVE);
-    }
+    ibFlowPortTo = 2+1;
+    RunFlow0();
+  }
 
 
-    if (mpSerialF[1] == SER_POSTINPUT_SLAVE)
-    {
-      mpSerialF[1] = SER_BEGIN;
+  if (mpZerial[1] == SER_POSTINPUT_SLAVE)
+  {
+    mpZerial[1] = SER_BEGIN;
 
-      if (!IsAutoFlowTo3()) return;
-      if ((fFlow == 1) && (ibFlowPortFrom == 0)) return;
+    if (!IsAutoFlowTo3()) return;
+    if ((fFlow == 1) && (ibFlowPortFrom == 0)) return;
 
-      fFlow = 1;
-      ibFlowPortFrom = 1;
-
-      cbInFlow1++;
-      iwInFlow1 = iwInBuffF[1];
-
-      memcpy(mpbOutBuff2,mpbInBuffF[1],iwInBuffF[1]);
-      Query2(0xFFFF,iwInBuffF[1],1);
-    }
-    else if (mpSerial[2] == SER_BADLINK)
-    {
-      mpSerial[2] = SER_BEGIN;
-
-      cbOutFlow1++;
-      iwOutFlow1 = iwInBuff2;
-
-      memcpy(mpbOutBuff1,mpbInBuff2,iwInBuff2);
-      Answer1(iwInBuff2,SER_OUTPUT_SLAVE);
-    }
+    ibFlowPortTo = 2+1;
+    RunFlow1();
+  }
 }
 
 
 void    RunResponseSerialFlow3(void)
 {
-    if (mpSerialF[0] == SER_POSTINPUT_SLAVE)
-    {
-      mpSerialF[0] = SER_BEGIN;
+  if (mpZerial[0] == SER_POSTINPUT_SLAVE)
+  {
+    mpZerial[0] = SER_BEGIN;
 
-      if (!IsAutoFlowTo4()) return;
+    if (!IsAutoFlowTo4()) return;
+    if ((fFlow == 1) && (ibFlowPortFrom == 1)) return;
 
-      fFlow = 1;
-      ibFlowPortFrom = 0;
+    ibFlowPortTo = 3+1;
+    RunFlow0();
+  }
 
-      cbInFlow0++;
-      iwInFlow0 = iwInBuffF[0];
+  if (mpZerial[1] == SER_POSTINPUT_SLAVE)
+  {
+    mpZerial[1] = SER_BEGIN;
 
-      memcpy(mpbOutBuff3,mpbInBuffF[0],iwInBuffF[0]);
-      Query3(0xFFFF,iwInBuffF[0],1);
-    }
-    else if (mpSerial[3] == SER_BADLINK)
-    {
-      mpSerial[3] = SER_BEGIN;
+    if (!IsAutoFlowTo4()) return;
+    if ((fFlow == 1) && (ibFlowPortFrom == 0)) return;
 
-      cbOutFlow0++;
-      iwOutFlow0 = iwInBuff3;
-
-      memcpy(mpbOutBuff0,mpbInBuff3,iwInBuff3);
-      Answer0(iwInBuff3,SER_OUTPUT_SLAVE);
-    }
-
-    if (mpSerialF[1] == SER_POSTINPUT_SLAVE)
-    {
-      mpSerialF[1] = SER_BEGIN;
-
-      if (!IsAutoFlowTo4()) return;
-
-      fFlow = 1;
-      ibFlowPortFrom = 1;
-
-      cbInFlow1++;
-      iwInFlow1 = iwInBuffF[1];
-
-      memcpy(mpbOutBuff3,mpbInBuffF[1],iwInBuffF[1]);
-      Query3(0xFFFF,iwInBuffF[1],1);
-    }
-    else if (mpSerial[3] == SER_BADLINK)
-    {
-      mpSerial[3] = SER_BEGIN;
-
-      cbOutFlow1++;
-      iwOutFlow1 = iwInBuff3;
-
-      memcpy(mpbOutBuff1,mpbInBuff3,iwInBuff3);
-      Answer1(iwInBuff3,SER_OUTPUT_SLAVE);
-    }
+    ibFlowPortTo = 3+1;
+    RunFlow1();
+  }
 }
 
 
