1b4a56b4de76d1884c297bda5342f36b4da4cc34
TI Cortex M4 <ti.cortex.m4@gmail.com>
2020-03-09 20:49:36 +0300

    io

--- ek-tm4c1294xl/project1/self/sensors/sensor36/io36.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/io36.c
@@ -5,18 +5,18 @@ IO36!C
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
-//#include "../../display/display.h"
-//#include "../../keyboard/keyboard.h"
-//#include "../../time/delay.h"
-//#include "../../hardware/watchdog.h"
-//#include "../../serial/ports.h"
-//#include "../../serial/ports2.h"
-//#include "../../serial/ports_devices.h"
-//#include "../../serial/input_wrapper.h"
-//#include "../../serial/monitor.h"
-//#include "../../digitals/wait_answer.h"
+#include "../../display/display.h"
+#include "../../keyboard/keyboard.h"
+#include "../../time/delay.h"
+#include "../../hardware/watchdog.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports2.h"
+#include "../../serial/ports_devices.h"
+#include "../../serial/input_wrapper.h"
+#include "../../serial/monitor.h"
+#include "../../digitals/wait_answer.h"
 //#include "include35.h"
-//#include "pack35.h"
+#include "unpack36.h"
 //#include "router35.h"
 //#include "device35.h"
 //#include "action35.h"
@@ -28,10 +28,79 @@ IO36!C
 
 void    Query36(void)
 {
+  InitPush(0);
+
+  PushChar(0x7E);
+  PushChar(0xA0);
+  PushChar(0x20);
+  PushChar(0x03);
+  PushChar(0x03);
+  PushChar(0x93);
+  PushChar(0xFE);
+  PushChar(0xC9);
+  PushChar(0x81);
+  PushChar(0x80);
+  PushChar(0x14);
+  PushChar(0x05);
+  PushChar(0x02);
+  PushChar(0x00);
+  PushChar(0x80);
+  PushChar(0x06);
+  PushChar(0x02);
+  PushChar(0x00);
+  PushChar(0x80);
+  PushChar(0x07);
+  PushChar(0x04);
+  PushChar(0x00);
+  PushChar(0x00);
+  PushChar(0x00);
+  PushChar(0x01);
+  PushChar(0x08);
+  PushChar(0x04);
+  PushChar(0x00);
+  PushChar(0x00);
+  PushChar(0x00);
+  PushChar(0x01);
+  PushChar(0xCE);
+  PushChar(0x6A);
+  PushChar(0x7E);
+
+  Query(1000,34,true);
 }
+
 
 
 serial  Input36(void)
 {
-  return SER_GOODCHECK;
+  InputStart();
+  InitWaitAnswer();
+
+  while (1)
+  {
+    if (fKey == true) { mpSerial[ibPort] = SER_BADLINK; break; }
+
+    ResetWatchdog();
+    ShowWaitAnswer(1);
+    if (GetWaitAnswer()) { mpSerial[ibPort] = SER_BADLINK; break; }
+
+    if (mpSerial[ibPort] == SER_INPUT_MASTER)
+      Decompress36();
+
+    if (mpSerial[ibPort] == SER_POSTINPUT_MASTER)
+    {
+//      if (ChecksumRouter35() == 0)
+//      {
+//        InputGoodCheck();
+        mpSerial[ibPort] = SER_GOODCHECK;
+//      }
+//      else
+//        mpSerial[ibPort] = SER_BADCHECK;
+
+      break;
+    }
+    else if ((mpSerial[ibPort] == SER_OVERFLOW) ||
+             (mpSerial[ibPort] == SER_BADLINK)) break;
+  }
+
+  return mpSerial[ibPort];
 }
---
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/unpack36.c
@@ -0,0 +1,31 @@
+/*------------------------------------------------------------------------------
+unpack36.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../memory/mem_ports.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports2.h"
+#include "unpack36.h"
+
+
+
+void    Decompress36(void)
+{
+  uchar i,j;
+
+  if (mpSerial[ibPort] != SER_INPUT_MASTER) return;
+
+  j = 0;
+  for (i=0; i<IndexInBuff(); i++) if (InBuff(i) == 0x7E) j++;
+  if (j != 2) return;
+
+  if ((InBuff(0) != 0x7E) || (InBuff(IndexInBuff()-1) != 0x7E))
+    return;
+
+//  Unpack36();
+
+  mpSerial[ibPort] = SER_POSTINPUT_MASTER;
+}
