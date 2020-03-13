8f7a58bc94bfab2ec90cb9cf043da3e65d6dc9f6
xxx <xxx@gmail.com>
2020-03-13 19:32:08 +0300

    rename

--- ek-tm4c1294xl/project1/self/sensors/sensor36/crc16_x25.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/crc16_x25.h
@@ -4,7 +4,5 @@ crc16_x25.h
 
 ------------------------------------------------------------------------------*/
 
-void Crc16_X25(uchar  *pbData, uint  wSize);
-
 uint    MakeCRC16_X25OutBuff(uchar  bOffset, uint  wSize);
 uint    MakeCRC16_X25InBuff(uchar  bOffset, uint  wSize);
