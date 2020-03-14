8a7497477dec50362db250f8d3e75aff270e846a
xxx <xxx@gmail.com>
2020-03-14 17:56:42 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor36/monitor36.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/monitor36.c
@@ -15,14 +15,14 @@ static void MonitorControl(uchar  bControl) {
   MonitorString("\n Control="); MonitorCharHex(bControl);
 
   if ((bControl & 0x01) == 0x00) {
-    MonitorString(" I-frame");
+    MonitorString("I-frame");
     MonitorString(" N(R)=");  MonitorCharDec((bControl & 0xE0) >> 5);
     MonitorString(" N(S)=");  MonitorCharDec((bControl & 0x0E) >> 1);
   } else if ((bControl & 0x03) == 0x01) {
-    MonitorString(" S-frame");
+    MonitorString("S-frame");
     MonitorString(" N(R)=");  MonitorCharDec((bControl & 0xE0) >> 5);
   } else if ((bControl & 0x03) == 0x03) {
-    MonitorString(" U-frame");
+    MonitorString("U-frame");
   }
 
   if ((bControl & 0x10) == 0x00) {
@@ -36,7 +36,9 @@ static void MonitorControl(uchar  bControl) {
 
 void    MonitorOutput36(void)
 {
-  MonitorString("\n Format="); MonitorCharHex(OutBuff(1)); MonitorCharHex(OutBuff(2));
+  MonitorString("\n Output");
+
+  MonitorString(" Format="); MonitorCharHex(OutBuff(1)); MonitorCharHex(OutBuff(2));
   MonitorControl(OutBuff(5));
 }
 
@@ -44,10 +46,11 @@ void    MonitorOutput36(void)
 
 void    MonitorInput36(void)
 {
+  MonitorString("\n Input");
   InitPop(1);
 
   uint wFormat = PopIntLtl();
-  MonitorString("\n Format="); MonitorIntHex(wFormat);
+  MonitorString(" Format="); MonitorIntHex(wFormat);
 
   PopChar();
   PopChar();
