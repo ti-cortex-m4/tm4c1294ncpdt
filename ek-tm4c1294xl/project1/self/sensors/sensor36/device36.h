71bb2cee55d771e678d06c2d658508f472e7a498
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-03-18 19:30:04 +0300

    RR

--- ek-tm4c1294xl/project1/self/sensors/sensor36/device36.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/device36.h
@@ -9,7 +9,7 @@ void    Query36_SNRM(void);
 void    Query36_Open2(uchar  bNS, uchar  bNR);
 void    Query36_Open3(uchar  bNR);
 void    Query36_GetTime(uchar  bNS, uchar  bNR);
-void    Query36_Open5(uchar  bNR);
+//void    Query36_Open5(uchar  bNR);
 
 void    QueryTime36(void);
 time    ReadTime36(void);
