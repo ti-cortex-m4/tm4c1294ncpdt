1b2562d5d9fe609fefe9c37690b762e7b143e2b9
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-03-19 22:00:29 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor36/device36.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/device36.h
@@ -7,7 +7,7 @@ DEVICE36.H
 void    Query36_DISC(void);
 void    Query36_SNRM(void);
 void    Query36_Open2(uchar  bNS, uchar  bNR);
-void    Query36_Open3(uchar  bNR);
+void    Query36_RR(uchar  bNR);
 void    Query36_GetTime(uchar  bNS, uchar  bNR);
 //void    Query36_Open5(uchar  bNR);
 
