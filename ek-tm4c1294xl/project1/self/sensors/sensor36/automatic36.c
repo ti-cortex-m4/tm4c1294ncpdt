72c564b6cf2a4d8703972722339d659a6d9472f9
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-04-19 16:13:31 +0300

    fix 36

--- ek-tm4c1294xl/project1/self/sensors/sensor36/automatic36.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/automatic36.c
@@ -5,12 +5,12 @@ AUTOMATIC36!C
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
-//#include "../../memory/mem_factors.h"
+#include "../../memory/mem_factors.h"
 #include "../../display/display.h"
 #include "../../keyboard/keyboard.h"
 //#include "../../time/delay.h"
 #include "../../serial/ports.h"
-//#include "../../devices/devices.h"
+#include "../../devices/devices.h"
 //#include "../../sensors/automatic1.h"
 #include "../../digitals/digitals.h"
 #include "device36.h"
@@ -105,4 +105,42 @@ time2   ReadTimeCan36_Short(void)
   QueryClose36();
 
   return GetTime2(ti, true);
+}
+
+
+
+double2 ReadCntCurr36(uchar  bMaxLines)
+{
+  Clear();
+
+  uchar i;
+  for (i=0; i<bMaxLines; i++)
+  {
+    if (SkipLine(ibDig, i) == true) { mpdbChannelsC[i] = 0; continue; }
+
+    uchar r;
+    for (r=0; r<MaxRepeats(); r++)
+    {
+      ShowPercent(50 + i);
+      //QueryCloseU();
+      QueryEngAbs36(i);
+
+      if (Input36() == SER_GOODCHECK) break;
+      if (fKey == true) return GetDouble2Error();
+    }
+
+    if (r == MaxRepeats()) return GetDouble2Error();
+    ReadEng36(i);
+  }
+
+  QueryClose36();
+
+
+  for (i=0; i<bMaxLines; i++)
+  {
+    mpdbChannelsC[i] *= mpdbTransCnt[ibDig];
+    mpboChannelsA[i] = true;
+  }
+
+  return GetDouble2(mpdbChannelsC[diCurr.ibLine], true);
 }
--- ek-tm4c1294xl/project1/self/sensors/sensor36/device36.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/device36.h
@@ -10,4 +10,7 @@ time    ReadTime36(void);
 void    QueryDate36(void);
 time    ReadDate36(time  ti);
 
+void    QueryEngAbs36(uchar  ibLine);
+void    ReadEng36(uchar  ibLine);
+
 void    QueryClose36(void);
