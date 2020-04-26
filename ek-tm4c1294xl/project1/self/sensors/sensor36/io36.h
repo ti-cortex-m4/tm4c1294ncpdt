98332ec65ad0e3840301800ecbbf2b84c80071bc
u25jyl2gz0udjw4n <u25jyl2gz0udjw4n@gmail.com>
2020-04-26 12:45:45 +0300

    rename

--- ek-tm4c1294xl/project1/self/sensors/sensor36/io36.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/io36.h
@@ -9,7 +9,7 @@ void    BccQuery36(uint  cwIn, uchar  cbOut, uchar  cbHeaderMax);
 
 void    Query36Repeat(void);
 
-serial  Input36_(bool  fShowChecksumError);
+serial  Input36_(bool  fIgnoreChecksumError);
 serial  Input36(void);
 
 bool    NoData36(void);
