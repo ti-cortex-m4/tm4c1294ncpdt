5e1380a52d8b578308ac2dcd0ea014d29a924136
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-05-06 18:31:47 +0300

    update

--- ek-tm4c1294xl/project1/self/sensors/sensor36/automatic36_2.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/automatic36_2.h
@@ -4,12 +4,4 @@ AUTOMATIC36.H
 
 ------------------------------------------------------------------------------*/
 
-//bool    Automatic36(void);
-//
-//bool    QueryConfig36_Full(uchar  bPercent);
-time2   QueryTime36_Full(uchar  bPercent);
-
-time2   ReadTimeCan36(void);
-//time2   ReadTimeCan36_Short(void);
-double2 ReadCntCurr36(void);
-double2 ReadCntMonCan36(uchar  ibMon);
+double2 ReadCntMonCan36_2(void);
