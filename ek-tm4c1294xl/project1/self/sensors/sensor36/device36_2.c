67c415b3c505e2849e623d46e09109af772d14ce
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-05-08 19:00:00 +0300

    Update device36_2.c

--- ek-tm4c1294xl/project1/self/sensors/sensor36/device36_2.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/device36_2.c
@@ -298,24 +298,37 @@ uint64_t ReadEngMon36(void)
 */
 void    QueryNextBlock36(uchar  bNS, uchar  bNR, uchar  bInvokeId, uchar  bBlockNumber)
 {
-  MonitorString("\n\n RR");
+  MonitorString("\n\n NextBlock"); // TODO ???
 
-  uint wSize = 5 + GetHdlcAddressesSize();
+  uint wSize = 17 + GetHdlcAddressesSize();
 
   InitPush(0);
   PushChar(0x7E);
   PushFormat(wSize);
-//  PushChar(0xA0);
-//  PushChar(0x07);
   PushHdlcAddresses();
-//  PushChar(0x03);
-//  PushChar(0x03);
 
 //MonitorString("Control{R(R)=1} 31 ? "); MonitorCharHex((bNR << 5) | 0x10 | 0x01);
   PushChar((bNR << 5) | 0x10 | 0x01);
 
-  PushIntLtl(MakeCRC16_X25OutBuff(1, 3+GetHdlcAddressesSize())); // 5
+  PushIntLtl(MakeCRC16_X25OutBuff(1, 3+GetHdlcAddressesSize()));
+
+  // DLMS start
+
+  PushChar(0xE6); // LLC
+  PushChar(0xE6);
+  PushChar(0x00);
+
+  PushChar(0xC0); // Get-Request
+  PushChar(0x02); // Get-Request-Normal /// TODO ???
+  PushChar(0x80 | (bInvokeId % 16)); // Invoke-Id-And-Priority
+
+  PushLongLtl(bBlockNumber); // <BlockNumber Value="00000001" /> TODO ???
+
+  // DLMS finish
+
+  PushIntLtl(MakeCRC16_X25OutBuff(1, wSize-2));
+
   PushChar(0x7E);
 
-  Query36(1000, wSize+2); // 9
+  Query36(1000, wSize+2); // 0x13
 }
