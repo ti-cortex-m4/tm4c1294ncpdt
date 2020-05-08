f068c5b2a4ffca6cae4fef3d0b39676c17d4175c
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-05-08 19:44:08 +0300

    print

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/buffers36.c
@@ -0,0 +1,17 @@
+/*------------------------------------------------------------------------------
+buffers36.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../memory/mem_ports.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports2.h"
+#include "../../serial/monitor.h"
+#include "buffers36.h"
+
+
+
+void    PrintInBuff(uint  iwStart, uint  cwSize) {
+}
