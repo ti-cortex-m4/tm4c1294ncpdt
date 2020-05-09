8035798676d54b1082618d9e7f109e63d01c8a7e
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-05-09 09:28:35 +0300

    update

--- ek-tm4c1294xl/project1/self/sensors/sensor36/automatic36_2.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/automatic36_2.c
@@ -12,6 +12,7 @@ AUTOMATIC36!C
 #include "../../serial/ports.h"
 #include "../../devices/devices.h"
 //#include "../../sensors/automatic1.h"
+#include "../../serial/monitor.h" //
 #include "../../digitals/digitals.h"
 #include "device36.h"
 #include "device36_2.h"
@@ -48,49 +49,53 @@ double2 ReadCntMonCan36_2(void)
   DelayOff();
 
 
-  bNS++;
+
+  bNS++; MonitorString("\n\n step1");
   QueryEngCurrDay36(bNS, bNR, bInvokeId++);
   if (Input36() != SER_GOODCHECK) return GetDouble2Error();
 //  if (!ValidateIframe(bNS, bNR)) return GetDouble2Error();
-  PrintInBuff(22, IndexInBuff()-22-2); // input...
+  PrintInBuff(22, IndexInBuff()-22-3); // input...
   DelayOff();
 
-  bNR++;
+  bNR++; MonitorString("\n\n step2");
   Query36_RR(bNR);
   if (Input36() != SER_GOODCHECK) return GetDouble2Error();
 //  if (!ValidateSframe(bNR)) return GetDouble2Error();
-  PrintInBuff(8, IndexInBuff()-8-2); // input...
+  PrintInBuff(8, IndexInBuff()-8-3); // input...
   DelayOff();
 
-  bNR++;
+  bNR++; MonitorString("\n\n step3");
   Query36_RR(bNR);
   if (Input36() != SER_GOODCHECK) return GetDouble2Error();
 //  if (!ValidateSframe(bNR)) return GetDouble2Error();
   DelayOff();
+
 
 
   uchar bBlockNumber = 1;
 
 
-  bNS++;
+
+  bNS++; MonitorString("\n\n step4");
   QueryNextBlock36(bNS, bNR, bInvokeId++, bBlockNumber);
   if (Input36() != SER_GOODCHECK) return GetDouble2Error();
 //  if (!ValidateIframe(bNS, bNR)) return GetDouble2Error();
-  PrintInBuff(22, IndexInBuff()-22-2); // input...
+  PrintInBuff(22, IndexInBuff()-22-3); // input...
   DelayOff();
 
-  bNR++;
+  bNR++; MonitorString("\n\n step5");
   Query36_RR(bNR);
   if (Input36() != SER_GOODCHECK) return GetDouble2Error();
 //  if (!ValidateSframe(bNR)) return GetDouble2Error();
-  PrintInBuff(8, IndexInBuff()-8-2); // input...
+  PrintInBuff(8, IndexInBuff()-8-3); // input...
   DelayOff();
 
-  bNR++;
+  bNR++; MonitorString("\n\n step6");
   Query36_RR(bNR);
   if (Input36() != SER_GOODCHECK) return GetDouble2Error();
 //  if (!ValidateSframe(bNR)) return GetDouble2Error();
   DelayOff();
+
 
 
   uint64_t ddw = ReadEngMon36();
