87eeedfd6b757fe8d98c5212b45079e64bef06fe
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-05-10 14:08:17 +0300

    update

--- ek-tm4c1294xl/project1/self/sensors/sensor36/buffers36.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/buffers36.c
@@ -12,6 +12,42 @@ buffers36.c
 #include "buffers36.h"
 
 
+uchar                       mpbBuff1[3000];
+
+uint                        iwBuff1;
+
+
+
+void    StartBuffer1(void) {
+  memset(&mpbBuff1, 0xFF, sizeof(mpbBuff1));
+  iwBuff1 = 0;
+}
+
+void    AddBuffer1(uint  iwStart, uint  cwSize) {
+  MonitorString("\n buffer size "); MonitorIntDec(cwSize); MonitorString("\n ");
+  if (cwSize > 5000) return;
+
+  InitPop(iwStart);
+
+  uint i;
+  for (i=0; i<cwSize; i++) {
+    uchar b = PopChar();
+    mpbBuff1[iwBuff1++] = b;
+    MonitorCharHex(b);
+  }
+}
+
+void    FinishBuffer1(void) {
+  MonitorString("\n finish size "); MonitorIntDec(iwBuff1); MonitorString("\n ");
+
+  uint i;
+  for (i=0; i<iwBuff1; i++) {
+    MonitorCharHex(mpbBuff1[i]);
+  }
+}
+
+
+
 
 void    PrintInBuff(uint  iwStart, uint  cwSize) {
   MonitorString("\n buffer size "); MonitorIntDec(cwSize);
--- ek-tm4c1294xl/project1/self/sensors/sensor36/buffers36.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor36/buffers36.h
@@ -4,4 +4,8 @@ buffers36.h
 
 ------------------------------------------------------------------------------*/
 
+void    StartBuffer1(void);
+void    AddBuffer1(uint  iwStart, uint  cwSize);
+void    FinishBuffer1(void);
+
 void    PrintInBuff(uint  iwStart, uint  cwSize);
