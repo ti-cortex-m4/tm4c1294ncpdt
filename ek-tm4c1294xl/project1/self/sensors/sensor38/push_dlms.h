f8cbad36e327c080e264297f741e1e63a7948572
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-05-12 08:14:16 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor38/push_dlms.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/push_dlms.h
@@ -1,22 +1,9 @@
 /*------------------------------------------------------------------------------
-DEVICE36.H
+push_dlms.h
 
 
 ------------------------------------------------------------------------------*/
 
 void    PushFormat(uint  wSize);
+void    PushMon36(uchar  bMonth, uchar  bYear);
 void    PushTime36(time  ti);
-
-void    Query36_DISC(void);
-void    Query36_SNRM(void);
-void    Query36_Open2(uchar  bNS, uchar  bNR);
-void    Query36_RR(uchar  bNR);
-
-void    QueryTime36(uchar  bNS, uchar  bNR, uchar  bInvokeId);
-time    ReadTime36(void);
-
-void    QueryEngAbs36(uchar  bNS, uchar  bNR, uchar  bInvokeId, uchar  ibLine);
-uint64_t ReadEngAbs36(void);
-
-void    QueryEngMon36(uchar  bNS, uchar  bNR, uchar  bInvokeId, uchar  bMonth, uchar  bYear);
-uint64_t ReadEngMon36(void);
