9d65658adaeb34486e37bbf314ec68bdbad58d7a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-05-13 08:33:12 +0300

    rename

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/query_profile_38.h
@@ -0,0 +1,9 @@
+/*------------------------------------------------------------------------------
+query_next_block_36.h
+
+
+------------------------------------------------------------------------------*/
+
+void    QueryEngCurrDay36(uchar  bNS, uchar  bNR, uchar  bInvokeId);
+
+void    QueryNextBlock36(uchar  bNS, uchar  bNR, uchar  bInvokeId, uchar  bBlockNumber);
