20951f8360031d858e5d9946761f7a85a3cbf830
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-05-15 21:48:53 +0300

    Update automatic36.c

--- ek-tm4c1294xl/project1/self/sensors/sensor38/automatic36.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/automatic36.c
@@ -20,26 +20,6 @@ AUTOMATIC36!C
 
 
 /*
-bool    QueryConfig36_Full(uchar  bPercent)
-{
-  uchar i;
-  for (i=0; i<MaxRepeats(); i++)
-  {
-    DelayOff();
-    QueryConfig36();
-
-    if (Input36() == SER_GOODCHECK) break;
-    if (fKey == true) return(0);
-  }
-
-  if (i == MaxRepeats()) return(0);
-  ShowPercent(bPercent);
-
-  ReadConfig36();
-  return(1);
-}
-
-
 bool    Automatic36(void)
 {
   Clear();
@@ -97,70 +77,6 @@ bool    QueryEngMon36_Full(uchar  bTime, uchar  bPercent)
 }
 */
 
-double2 ReadCntCurr38(void)
-{
-  Query38_DISC();
-  if (Input38() != SER_GOODCHECK) return GetDouble2Error();
-  DelayOff();
-
-  Query38_SNRM();
-  if (Input38() != SER_GOODCHECK) return GetDouble2Error();
-  DelayOff();
-
-  uchar bNS = 0;
-  uchar bNR = 0;
-  uchar bInvokeId = 0;
-
-  Query38_Open2(bNS, bNR);
-  if (Input38() != SER_GOODCHECK) return GetDouble2Error();
-  if (!ValidateIframe(bNS, bNR)) return GetDouble2Error();
-  DelayOff();
-
-  bNR++;
-  Query38_RR(bNR);
-  if (Input38() != SER_GOODCHECK) return GetDouble2Error();
-  if (!ValidateSframe(bNR)) return GetDouble2Error();
-  DelayOff();
-
-  uchar i;
-  for (i=0; i<4; i++) {
-    bNS++;
-    QueryEngAbs36(bNS, bNR, bInvokeId++, i);
-    if (Input38() != SER_GOODCHECK) return GetDouble2Error();
-    if (!ValidateIframe(bNS, bNR)) return GetDouble2Error();
-
-    uint64_t ddw = ReadEngAbs36();
-    mpdwChannelsA[i] = ddw % 0x100000000;
-    mpdbChannelsC[i] = (double)mpdwChannelsA[i] / 1000;
-    mpboChannelsA[i] = true;
-
-    DelayOff();
-
-    bNR++;
-    Query38_RR(bNR);
-    if (Input38() != SER_GOODCHECK) return GetDouble2Error();
-    if (!ValidateSframe(bNR)) return GetDouble2Error();
-    DelayOff();
-  }
-
-  Query38_DISC(); // TODO always close
-  if (Input38() != SER_GOODCHECK) return GetDouble2Error();
-  DelayOff();
-
-  return GetDouble2(mpdbChannelsC[diCurr.ibLine], true);
-/*
-  Clear();
-
-  if (QueryConfig36_Full(50) == 0) return GetDouble2Error();
-
-  if (QueryEngMon36_Full(0, 75) == 0) return GetDouble2Error();
-
-  mpdbChannelsC[0] = (double)mpdwChannelsA[0] / GetDivider36();
-  mpboChannelsA[0] = true;
-
-  return GetDouble2(mpdbChannelsC[0], true);
-*/
-}
 
 
 double2 ReadCntMonCan36(uchar  ibMon)
