b0d0779f3fec0e31ffea8c1e9c724bb539732b38
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-05-15 20:54:56 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/automatic_get_cntcurr_36.c
@@ -0,0 +1,133 @@
+/*------------------------------------------------------------------------------
+automatic_get_time_36.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../display/display.h"
+#include "../../keyboard/keyboard.h"
+#include "../../time/delay.h"
+#include "../../serial/ports.h"
+#include "../../digitals/digitals.h"
+#include "device36.h"
+#include "io36.h"
+#include "monitor36.h"
+#include "automatic_get_time_36.h"
+
+
+
+time2   QueryTime38_Full(void)
+{
+  Query36_DISC();
+  if (Input36() != SER_GOODCHECK) return GetTime2Error();
+  DelayOff();
+
+  Query36_SNRM();
+  if (Input36() != SER_GOODCHECK) return GetTime2Error();
+  DelayOff();
+
+  uchar bNS = 0;
+  uchar bNR = 0;
+  uchar bInvokeId = 0;
+
+  Query36_Open2(bNS, bNR);
+  if (Input36() != SER_GOODCHECK) return GetTime2Error();
+  if (!ValidateIframe(bNS, bNR)) return GetTime2Error();
+  DelayOff();
+
+  bNR++;
+  Query38_RR(bNR);
+  if (Input36() != SER_GOODCHECK) return GetTime2Error();
+  if (!ValidateSframe(bNR)) return GetTime2Error();
+  DelayOff();
+
+  bNS++;
+  bInvokeId++;
+  QueryTime38(bNS, bNR, bInvokeId);
+  if (Input36() != SER_GOODCHECK) return GetTime2Error();
+  if (!ValidateIframe(bNS, bNR)) return GetTime2Error();
+  time ti = ReadTime36();
+  DelayOff();
+
+  bNR++;
+  Query38_RR(bNR);
+  if (Input36() != SER_GOODCHECK) return GetTime2Error();
+  if (!ValidateSframe(bNR)) return GetTime2Error();
+  DelayOff();
+
+  Query36_DISC();
+  if (Input36() != SER_GOODCHECK) return GetTime2Error();
+  DelayOff();
+
+/*
+  uchar i;
+  for (i=0; i<MaxRepeats(); i++)
+  {
+    DelayOff();
+    QueryTime36();
+
+    if (Input36() == SER_GOODCHECK) break;
+    if (fKey == true) return GetTime2Error();
+  }
+
+  if (i == MaxRepeats()) return GetTime2Error();
+  ShowPercent(bPercent);
+*/
+  return GetTime2(ti, true);
+}
+
+
+time2   ReadTimeCan38(void)
+{
+  Clear();
+
+
+  uchar r;
+  for (r=0; r<MaxRepeats(); r++)
+  {
+    time2 ti2 = QueryTime38_Full();
+    if (fKey == true) break;
+    if (ti2.fValid)
+    {
+      ShowPercent(50);
+
+      tiChannelC = ti2.tiValue;
+      mpboChannelsA[0] = true;
+
+      return GetTime2(ti2.tiValue, true);
+    }
+  }
+
+  Query36_DISC();
+  if (Input36() != SER_GOODCHECK) return GetTime2Error();
+  DelayOff();
+
+  return GetTime2Error();
+}
+
+
+
+time2   ReadTimeCan36_Short(void)
+{
+  Clear();
+
+
+  time2 ti2 = QueryTime38_Full();
+  if (ti2.fValid)
+  {
+    tiChannelC = ti2.tiValue;
+    mpboChannelsA[0] = true;
+
+    return GetTime2(ti2.tiValue, true);
+  }
+
+
+  Query36_DISC();
+  if (Input36() != SER_GOODCHECK) return GetTime2Error();
+  DelayOff();
+
+  return GetTime2Error();
+}
+
+
