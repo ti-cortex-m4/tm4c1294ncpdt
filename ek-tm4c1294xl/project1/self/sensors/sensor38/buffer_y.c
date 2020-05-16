741798a3037daf429b01a7006458cee5b74e8936
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-05-16 10:49:08 +0300

    buff

--- ek-tm4c1294xl/project1/self/sensors/sensor38/buffer_y.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/buffer_y.c
@@ -9,6 +9,7 @@ buffer_y.c
 #include "../../serial/ports.h"
 #include "../../serial/ports2.h"
 #include "../../serial/monitor.h"
+#include "../../time/timedate.h"
 #include "buffer_x.h"
 #include "buffer_y.h"
 
@@ -16,10 +17,6 @@ buffer_y.c
 
 void    StartBufferY(void) {
   InitPushX();
-
-  byValue.uint64_t = 0;
-  byValue.tiValue = tiZero;
-  byValue.fValid = false;
 }
 
 
@@ -33,52 +30,69 @@ void    AddBufferY(uint  iwStart, uint  cwSize) {
 }
 
 
+
+buff_y  GetBufferYError(char  bError)
+{
+  buff_y by;
+
+  by.bError = bError;
+  by.ddwValue = 0;
+  by.tiValue = tiZero;
+  by.fFirst = false;
+
+  return by;
+}
+
+
 buff_y  FinishBufferY(void) {
-  buff_y  byValue;
-  byValue.ddwValue = 0;
-  byValue.tiValue = tiZero;
-  byValue.fValid = false;
+  buff_y  by;
+  by.bError = 0;
+  by.ddwValue = 0;
+  by.tiValue = tiZero;
+  by.fFirst = false;
 
 
   InitPopX();
 
   if (GetPopCapacity() < 2)
-    return 1;
+    return GetBufferYError(1);
 
   if (PopCharX() != 1) // array
-    return 2;
+    return GetBufferYError(2);
 
   uchar bCount = PopCharX();
+
 
   uchar i;
   for (i=0; i<bCount; i++)
   {
     if (GetPopCapacity() < 2 + 2+12 + 1+8)
-      return 3;
+      return GetBufferYError(3);
 
     if (PopCharX() != 0x02) // structure
-      return 4;
+      return GetBufferYError(4);
 
     if (PopCharX() != 2) // structure size
-      return 5;
+      return GetBufferYError(5);
 
     if (PopCharX() != 0x09) // string
-      return 6;
+      return GetBufferYError(6);
 
     if (PopCharX() != 12) // string size
-      return 7;
+      return GetBufferYError(7);
 
     time ti = PopTimeDateX();
 
     if (PopCharX() != 0x15) // unsigned long 64
-      return 8;
+      return GetBufferYError(8);
 
     uint64_t ddw = PopLong64X();
 
-    if (byValue.fValid == false) {
-      byValue.ddwValue = ddw;
-      byValue.tiValue = ti;
-      byValue.fValid = true;
+    if (by.fFirst == false) {
+      by.bError = 0;
+      by.ddwValue = ddw;
+      by.tiValue = ti;
+      by.fFirst = true;
     }
 
     MonitorString("\n");
@@ -87,5 +101,5 @@ buff_y  FinishBufferY(void) {
     MonitorLongDec(ddw % 1000000);
   }
 
-  return byValue;
+  return by;
 }
