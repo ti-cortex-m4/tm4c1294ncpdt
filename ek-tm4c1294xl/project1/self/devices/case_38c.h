2fda05f6e099374aebe466e6c0aea55e8233c67e
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-05-17 10:06:54 +0300

    c38

--- ek-tm4c1294xl/project1/self/devices/case_38c.h
+++ ek-tm4c1294xl/project1/self/devices/case_38c.h
@@ -1,18 +1,18 @@
-#ifndef SKIP_31
+#ifndef SKIP_38
 
-    case DEV_START_31C:
+    case DEV_START_38C:
       cbRepeat = MaxRepeats();
-      QueryOpen31();
-      SetCurr(DEV_OPENCANAL_31C);
+      Query38_DISC();
+      SetCurr(DEV_DISC_38C);
       break;
 
-    case DEV_OPENCANAL_31C:
+    case DEV_DISC_38C:
       if (mpSerial[ibPort] == SER_GOODCHECK)
       {
-        if (ReadOpen31() == false)
-          ErrorCurrent();
-        else
-          MakePause(DEV_POSTOPENCANAL_31C);
+//        if (ReadOpen38() == false)
+//          ErrorCurrent();
+//        else
+//          MakePause(DEV_POSTOPENCANAL_38C);
       }
       else
       {
@@ -22,21 +22,27 @@
           ErrorLink();
           cbRepeat--;
 
-          QueryOpen31();
-          SetCurr(DEV_OPENCANAL_31C);
+          Query38_DISC();
+          SetCurr(DEV_DISC_38C);
         }
       }
       break;
 
-    case DEV_POSTOPENCANAL_31C:
+
+    case DEV_POSTDISC_38C:
       cbRepeat = MaxRepeats();
-      QueryEngAbs31();
-      SetCurr(DEV_ENERGY_31C);
+      Query38_SNRM();
+      SetCurr(DEV_SNRM_38C);
       break;
 
-    case DEV_ENERGY_31C:
+    case DEV_SNRM_38C:
       if (mpSerial[ibPort] == SER_GOODCHECK)
-        ReadCurrent31();
+      {
+//        if (ReadOpen38() == false)
+//          ErrorCurrent();
+//        else
+//          MakePause(DEV_POSTOPENCANAL_38C);
+      }
       else
       {
         if (cbRepeat == 0) ErrorCurrent();
@@ -45,8 +51,126 @@
           ErrorLink();
           cbRepeat--;
 
-          QueryEngAbs31();
-          SetCurr(DEV_ENERGY_31C);
+          Query38_SNRM();
+          SetCurr(DEV_SNRM_38C);
+        }
+      }
+      break;
+
+
+    case DEV_POSTSNRM_38C:
+      cbRepeat = MaxRepeats();
+      Query38_Open2();
+      SetCurr(DEV_OPEN2_38C);
+      break;
+
+    case DEV_OPEN2_38C:
+      if (mpSerial[ibPort] == SER_GOODCHECK)
+      {
+//        if (ReadOpen38() == false)
+//          ErrorCurrent();
+//        else
+//          MakePause(DEV_POSTOPENCANAL_38C);
+      }
+      else
+      {
+        if (cbRepeat == 0) ErrorCurrent();
+        else
+        {
+          ErrorLink();
+          cbRepeat--;
+
+          Query38_Open2();
+          SetCurr(DEV_OPEN2_38C);
+        }
+      }
+      break;
+
+
+    case DEV_POSTOPEN2_38C:
+      cbRepeat = MaxRepeats();
+      Query38_RR();
+      SetCurr(DEV_RR1_38C);
+      break;
+
+    case DEV_RR1_38C:
+      if (mpSerial[ibPort] == SER_GOODCHECK)
+      {
+//        if (ReadOpen38() == false)
+//          ErrorCurrent();
+//        else
+//          MakePause(DEV_POSTOPENCANAL_38C);
+      }
+      else
+      {
+        if (cbRepeat == 0) ErrorCurrent();
+        else
+        {
+          ErrorLink();
+          cbRepeat--;
+
+          Query38_RR();
+          SetCurr(DEV_RR1_38C);
+        }
+      }
+      break;
+
+
+    case DEV_POSTRR_38C:
+      cbRepeat = MaxRepeats();
+      QueryEngAbs38();
+      SetCurr(DEV_ENGABS_38C);
+      break;
+
+    case DEV_ENGABS_38C:
+      if (mpSerial[ibPort] == SER_GOODCHECK)
+      {
+        ReadCurrent38();
+//        if (ReadOpen38() == false)
+//          ErrorCurrent();
+//        else
+//          MakePause(DEV_POSTOPENCANAL_38C);
+      }
+      else
+      {
+        if (cbRepeat == 0) ErrorCurrent();
+        else
+        {
+          ErrorLink();
+          cbRepeat--;
+
+          QueryEngAbs38();
+          SetCurr(DEV_ENGABS_38C);
+        }
+      }
+      break;
+
+
+    case DEV_POSTENGABS_38C:
+      cbRepeat = MaxRepeats();
+      Query38_RR();
+      SetCurr(DEV_RR2_38C);
+      break;
+
+    case DEV_RR2_38C:
+      if (mpSerial[ibPort] == SER_GOODCHECK)
+      {
+        ReadCurrent38();
+//        if (ReadOpen38() == false)
+//          ErrorCurrent();
+//        else
+//          MakePause(DEV_POSTOPENCANAL_38C);
+      }
+      else
+      {
+        if (cbRepeat == 0) ErrorCurrent();
+        else
+        {
+          ErrorLink();
+          cbRepeat--;
+
+          Query38_RR();
+          SetCurr(DEV_RR2_38C);
         }
       }
       break;
