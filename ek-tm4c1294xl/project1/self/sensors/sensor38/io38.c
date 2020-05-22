3b3885d30e51bc93193d3ed70f21c8299cd1b24e
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-05-22 19:55:18 +0300

    rename

--- ek-tm4c1294xl/project1/self/sensors/sensor38/io38.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/io38.c
@@ -4,8 +4,63 @@ io38.c
 
 ------------------------------------------------------------------------------*/
 
+
 #include "../../main.h"
+#include "../../display/display.h"
+#include "../../keyboard/keyboard.h"
+#include "../../time/delay.h"
+#include "../../hardware/watchdog.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports2.h"
+#include "../../serial/ports_devices.h"
+#include "../../serial/input_wrapper.h"
+#include "../../serial/monitor.h"
+#include "../../digitals/wait_answer.h"
+#include "unpack38.h"
+#include "monitor38.h"
 #include "io38.h"
 
 
 
+
+void    Query38(uint  cwIn, uchar  cbOut)
+{
+  Query(cwIn, cbOut, true);
+}
+
+
+
+serial  Input38(void)
+{
+  InputStart();
+  InitWaitAnswer();
+
+  while (1)
+  {
+    if (fKey == true) { mpSerial[ibPort] = SER_BADLINK; break; }
+
+    ResetWatchdog();
+    ShowWaitAnswer(1);
+    if (GetWaitAnswer()) { mpSerial[ibPort] = SER_BADLINK; break; }
+
+    if (mpSerial[ibPort] == SER_INPUT_MASTER)
+      Decompress38();
+
+    if (mpSerial[ibPort] == SER_POSTINPUT_MASTER)
+    {
+      if (ValidInput38())
+      {
+        InputGoodCheck();
+        mpSerial[ibPort] = SER_GOODCHECK;
+      }
+      else
+        mpSerial[ibPort] = SER_BADCHECK;
+
+      break;
+    }
+    else if ((mpSerial[ibPort] == SER_OVERFLOW) ||
+             (mpSerial[ibPort] == SER_BADLINK)) break;
+  }
+
+  return mpSerial[ibPort];
+}
