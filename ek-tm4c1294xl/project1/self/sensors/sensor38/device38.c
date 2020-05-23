a949f6548c0014e45eea08f1d387c7c6d212c8e7
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-05-23 18:29:53 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor38/device38.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/device38.c
@@ -11,12 +11,19 @@ device38.c
 
 
 
+#include "../../time/calendar.h"
+#include "../../serial/monitor.h"
+#include "../../memory/mem_serial3.h"
+uint DFF_Decoder(uchar  *pbData, uchar bS);
+
+
+
 void    PushAddress38(void)
 {
-    PushChar(0xE8);
-    PushChar(0xB7);
-    PushChar(0xC4);
-    PushChar(0x04);
+  PushChar(0xE8);
+  PushChar(0xB7);
+  PushChar(0xC4);
+  PushChar(0x04);
 
 //  PushLongLtl(80001000); // TODO
 }
@@ -44,15 +51,58 @@ void    QueryTime38(void)
 
 time    ReadTime38(void)
 {
-  time ti;
+  uint w = DFF_Decoder(&mpbInBuff3[11], 0);
 
-  ti.bSecond = 0;
-  ti.bMinute = 0;
-  ti.bHour   = 0;
+  MonitorIn();
+  InitPop(11);
+  ulong dw = PopLongLtl();
 
-  ti.bDay    = 0;
-  ti.bMonth  = 0;
-  ti.bYear   = 0;
+  time ti = SecIndexToDate(dw);
+  ti.bYear += 12;
+
+//  ti.bSecond = 0;
+//  ti.bMinute = 0;
+//  ti.bHour   = 0;
+//
+//  ti.bDay    = 0;
+//  ti.bMonth  = 0;
+//  ti.bYear   = 0;
 
   return ti;
+}
+
+
+
+void    QueryCntCurr38(void)
+{
+  InitPush(0);
+
+  PushChar(0xC0);
+  PushChar(0x06);
+
+  PushAddress38();
+
+  PushChar(0x00);
+  PushChar(0x06);
+
+  PushChar(0x0A);
+  PushChar(0x00);
+  PushChar(0x01);
+
+  PushChar(0x02);
+  PushChar(0x00);
+
+  Query38(100+17, 16);
+}
+
+
+ulong   ReadCntCurr38_(void)
+{
+  uint w = DFF_Decoder(&mpbInBuff3[11], 0);
+
+  MonitorIn();
+  InitPop(11);
+  ulong dw = PopChar() + PopChar()*0x100 + PopChar()*0x10000;
+
+  return dw;
 }
--- ek-tm4c1294xl/project1/self/sensors/sensor38/device38.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/device38.h
@@ -6,3 +6,6 @@ device38.h
 
 void    QueryTime38(void);
 time    ReadTime38(void);
+
+void    QueryCntCurr38(void);
+ulong   ReadCntCurr38_(void);
