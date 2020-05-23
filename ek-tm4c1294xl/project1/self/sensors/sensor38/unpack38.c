295b439e2e07cceba162b5791d2dc70ff75dbb3e
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-05-23 12:44:27 +0300

    pack

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/unpack38.c
@@ -0,0 +1,37 @@
+/*------------------------------------------------------------------------------
+unpack35.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../memory/mem_ports.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports2.h"
+#include "unpack35.h"
+
+
+
+void    Unpack35(void)
+{
+  uchar i = 1;
+  uchar j = 1;
+
+  while (i < IndexInBuff()) {
+    if ((InBuff(i) == 0xDB) && (InBuff(i+1) == 0xDD)) {
+      SetInBuff(j, 0xDB);
+      i += 2;
+      j++;
+    } else if ((InBuff(i) == 0xDB) && (InBuff(i+1) == 0xDC)) {
+      SetInBuff(j, 0xC0);
+      i += 2;
+      j++;
+    } else {
+      SetInBuff(j, InBuff(i));
+      i++;
+      j++;
+    }
+  }
+
+  SetIndexInBuff(j);
+}
