fb595153d2bd712dc320000924235871b111b5c2
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-05-27 19:54:54 +0300

    Update dff.h

--- ek-tm4c1294xl/project1/self/sensors/sensor38/dff.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/dff.h
@@ -4,5 +4,7 @@ dff.c
 
 ------------------------------------------------------------------------------*/
 
+uint64_t    DffPopDecodeLong64(uchar  i);
+
 uint64_t    DffDecodeLong64(uchar  *pb);
 ulong       DffDecodeLong(uchar  *pb);
