91abb1b769fe2c959f0af18b9531f7575430c358
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-05-27 20:04:24 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor38/automatic_get_time_38.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/automatic_get_time_38.c
@@ -5,14 +5,12 @@ automatic_get_time_38.c
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
-// #include "../../memory/mem_factors.h"
 #include "../../display/display.h"
 #include "../../keyboard/keyboard.h"
 #include "../../time/timedate.h"
- #include "../../serial/ports.h"
- #include "../../devices/devices.h"
-// #include "../../sensors/automatic1.h"
- #include "../../digitals/digitals.h"
+#include "../../serial/ports.h"
+#include "../../devices/devices.h"
+#include "../../digitals/digitals.h"
 #include "device38.h"
 #include "io38.h"
 #include "automatic_get_time_38.h"
