8757f13acfbb52440875607564914a7bac512c91
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-05-30 13:11:42 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor38/profile38.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/profile38.c
@@ -5,11 +5,67 @@ profile38.c
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
-#include "../../serial/ports.h"
+//#include "../../serial/ports.h"
+//#include "../../serial/ports_stack.h"
+//#include "../../serial/ports_devices.h"
+#include "../../memory/mem_settings.h"
+#include "../../memory/mem_digitals.h"
+#include "../../memory/mem_ports.h"
+//#include "../../memory/mem_current.h"
+#include "../../memory/mem_factors.h"
+#include "../../memory/mem_realtime.h"
+//#include "../../memory/mem_energy_spec.h"
+#include "../../memory/mem_profile.h"
+#include "../../memory/mem_limits.h"
+#include "../../display/display.h"
+//#include "../../keyboard/time/key_timedate.h"
+#include "../../time/timedate.h"
+#include "../../time/calendar.h"
+#include "../../time/delay.h"
+#include "../../serial/ports_stack.h"
+#include "../../serial/ports_devices.h"
+#include "../../serial/ports_common.h"
+#include "../../serial/monitor.h"
+#include "../../devices/devices.h"
+//#include "../../devices/devices_time.h"
+//#include "../../digitals/current/current_run.h"
+#include "../../digitals/digitals.h"
+#include "../../digitals/limits.h"
+//#include "../../special/special.h"
+//#include "../../hardware/watchdog.h"
 #include "io38.h"
 #include "dff.h"
 #include "device38.h"
 #include "profile38.h"
+
+
+
+uint                    wProfile38;
+
+time                    tiValue38;
+ulong                   dwValue38;
+
+
+
+void    InitHeader38(void)
+{
+  if (!UseBounds())
+    wProfile38 = 0;
+  else
+  {
+    wProfile38 = (mpcwStartRelCan[ibDig] / 6) * 6;
+    sprintf(szLo," начало %04u:%02u ",wProfile38,(uchar)(wProfile38/48 + 1));
+    if (boShowMessages == true) DelayMsg();
+  }
+
+  tiDigPrev = tiCurr;
+
+  uchar i = tiDigPrev.bHour*2 + tiDigPrev.bMinute/30;
+  i = (i / 6) * 6;
+
+  tiDigPrev.bHour = i / 2;
+  tiDigPrev.bMinute = (i % 2)*30;
+}
 
 
 
@@ -59,15 +115,15 @@ bool    ReadHeader38(void)
   {
     ulong dw = DateToHouIndex(tiDigPrev);
     dw += 5;
-    dw -= (wProfileC + i);
+    dw -= (wProfile38 + i);
     tiDig = HouIndexToDate(dw);
 
-    if (dw < dwValueC)
-      if (ReadHeaderC(5-i) == 0) return(0);
+    if (dw < dwValue38)
+      if (ReadHeader38(5-i) == 0) return(0);
   }
 
-  wProfileC += 6;
-  if (wProfileC > wHOURS) return(0);
+  wProfile38 += 6;
+  if (wProfile38 > wHOURS) return(0);
 
   MonitorString("\n");
   return(1);
