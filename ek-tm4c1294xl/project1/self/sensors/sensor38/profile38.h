8122ed7b967427b01d7420b6697b3cae4686aa17
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-05-30 13:11:42 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor38/profile38.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/profile38.h
@@ -4,4 +4,10 @@ profile38.h
 
 ------------------------------------------------------------------------------*/
 
+extern  time                    tiValue38;
+extern  ulong                   dwValue38;
+
+
+
+void    InitHeader38(void);
 void    QueryProfile38(uchar  ib30MinRel);
