123b158a430b23b1a481f144e068bae8db7c840c
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-01 21:20:21 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor38/params38.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/params38.c
@@ -58,11 +58,11 @@ float2  ReadParam38(void)
     if (Input38() != SER_GOODCHECK) return GetFloat2Error();
 
     uchar ibIn = 10;
+    ibIn++;
 
     uchar i;
     for (i=0; i<3; i++)
     {
-      ibIn++;
 
       uint64_t ddw = 0;
       uchar delta = pucDecodeBitArr((uchar *) &ddw, InBuffPtr(ibIn));
@@ -72,14 +72,48 @@ float2  ReadParam38(void)
       mpeValues[i] = ddw % 0x100000000;
     }
 
+/*
+    ibIn = 10;
+    ibIn++;
+
+    uchar b10 = InBuff(10);
+    uchar b11 = InBuff(11);
+    uchar b12 = InBuff(12);
+    uchar b13 = InBuff(13);
+    uchar b14 = InBuff(14);
+    uchar b15 = InBuff(15);
+
+    uint64_t ddw1 = 0;
+    uchar i1 = ibIn;
+    uchar delta1 = pucDecodeBitArr((uchar *) &ddw1, InBuffPtr(ibIn));
+    if (delta1 == 0xFF) return GetFloat2Error();
+    ibIn += delta1;
+    ulong dw1 = ddw1 % 0x100000000;
+
+    uint64_t ddw2 = 0;
+    uchar i2 = ibIn;
+    uchar delta2 = pucDecodeBitArr((uchar *) &ddw2, InBuffPtr(ibIn));
+    if (delta2 == 0xFF) return GetFloat2Error();
+    ibIn += delta2;
+    ulong dw2 = ddw2 % 0x100000000;
+
+    uint64_t ddw3 = 0;
+    uchar i3 = ibIn;
+    uchar delta3 = pucDecodeBitArr((uchar *) &ddw3, InBuffPtr(ibIn));
+    if (delta3 == 0xFF) return GetFloat2Error();
+    ibIn += delta3;
+    ulong dw3 = ddw3 % 0x100000000;
+
+    uchar i4 = ibIn;
+*/
     fBeginParam = true;
   }
 
   switch (diCurr.ibLine)
   {
-    case PAR_U1 : return GetFloat2(mpeValues[0], true);
-    case PAR_U2 : return GetFloat2(mpeValues[1], true);
-    case PAR_U3 : return GetFloat2(mpeValues[2], true);
+    case PAR_U1 : return GetFloat2(mpeValues[0]/100, true);
+    case PAR_U2 : return GetFloat2(mpeValues[1]/100, true);
+    case PAR_U3 : return GetFloat2(mpeValues[2]/100, true);
     case PAR_U  : return GetFloat2(-1, true);
 
     case PAR_I1 : return GetFloat2(-1, true);
