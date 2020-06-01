e21c1ffcb58838c61bd9e13d1378d8de8047e2de
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-01 20:20:59 +0300

    Update params38.c

--- ek-tm4c1294xl/project1/self/sensors/sensor38/params38.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/params38.c
@@ -8,10 +8,42 @@ params34.c
 #include "../../memory/mem_digitals.h"
 #include "../../serial/ports.h"
 #include "../../serial/ports_devices.h"
+#include "../../serial/ports2.h"
 #include "../../digitals/params/params2.h"
 #include "../../display/display.h"
 #include "../../time/delay.h"
+#include "io38.h"
+#include "dff.h"
+#include "bits2.h"
+#include "device38.h"
 #include "params38.h"
+
+
+
+static float        mpeValues[4];
+
+
+
+void    QueryParams38(void)
+{
+  InitPush(0);
+
+  PushChar(0xC0);
+  PushChar(0x06);
+
+  PushAddress38();
+
+  PushChar(0x00);
+  PushChar(0x06);
+
+  PushChar(0x0A);
+  PushChar(0x00);
+
+  PushChar(0x18);
+  PushChar(0x1C);
+
+  Query38(250, 16);
+}
 
 
 
@@ -22,14 +54,32 @@ float2  ReadParam38(void)
   if (fBeginParam == false)
   {
 
+    QueryParams38();
+    if (Input38() != SER_GOODCHECK) return GetFloat2Error();
+
+    uchar ibIn = 10;
+
+    uchar i;
+    for (i=0; i<3; i++)
+    {
+      ibIn++;
+
+      uint64_t ddw = 0;
+      uchar delta = pucDecodeBitArr((uchar *) &ddw, InBuffPtr(ibIn));
+      if (delta == 0xFF) return GetFloat2Error();
+      ibIn += delta;
+
+      mpeValues[i] = ddw % 0x100000000;
+    }
+
     fBeginParam = true;
   }
 
   switch (diCurr.ibLine)
   {
-    case PAR_U1 : return GetFloat2(-1, true);
-    case PAR_U2 : return GetFloat2(-1, true);
-    case PAR_U3 : return GetFloat2(-1, true);
+    case PAR_U1 : return GetFloat2(mpeValues[0], true);
+    case PAR_U2 : return GetFloat2(mpeValues[1], true);
+    case PAR_U3 : return GetFloat2(mpeValues[2], true);
     case PAR_U  : return GetFloat2(-1, true);
 
     case PAR_I1 : return GetFloat2(-1, true);
