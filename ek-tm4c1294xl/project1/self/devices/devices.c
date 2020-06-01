0afc2365eae9b138cdfe110364ba20155d1ae08d
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-01 17:01:41 +0300

    fix

--- ek-tm4c1294xl/project1/self/devices/devices.c
+++ ek-tm4c1294xl/project1/self/devices/devices.c
@@ -6592,31 +6592,19 @@ void    RunDevices(void)
 
 #endif
 
-#if false
+#if SKIP_38
 
     case DEV_START_38C:
-      Clear(); ShowPercent(50);
-
-      ibLine38 = 0;
+      Clear();
 
       cbRepeat = MaxRepeats();
-      QueryEngAbs38(ibLine38);
+      QueryEngAbs38();
       SetCurr(DEV_ENERGY_38C);
       break;
 
     case DEV_ENERGY_38C:
       if (mpSerial[ibPort] == SER_GOODCHECK)
-      {
-        ReadEng38(11);
-        if (++ibLine38 < 4)
-        {
-          Clear(); ShowPercent(50+ibLine38);
-          QueryEngAbs38(ibLine38);
-          SetCurr(DEV_ENERGY_38C);
-        }
-        else
-          ReadCurrent38();
-      }
+        ReadCurrent38();
       else
       {
         if (cbRepeat == 0) ErrorCurrent();
@@ -6625,7 +6613,7 @@ void    RunDevices(void)
           ErrorLink();
           cbRepeat--;
 
-          QueryEngAbs38(ibLine38);
+          QueryEngAbs38();
           SetCurr(DEV_ENERGY_38C);
         }
       }
