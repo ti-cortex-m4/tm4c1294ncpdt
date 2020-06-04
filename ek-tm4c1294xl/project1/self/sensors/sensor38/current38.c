312f6dbc555aa386f9c2d9d66961829f3933e5b4
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-04 23:26:40 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor38/current38.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/current38.c
@@ -8,7 +8,6 @@ current38.c
 #include "../../memory/mem_digitals.h"
 #include "../../memory/mem_current.h"
 #include "../../memory/mem_factors.h"
-#include "../../display/messages.h"
 #include "../../time/delay.h"
 #include "../../serial/ports.h"
 #include "../../serial/ports2.h"
@@ -22,17 +21,15 @@ current38.c
 
 void    ReadCurrent38(void)
 {
-  uchar ibIn = 10;
+  uchar* pbIn = InBuffPtr(10);
 
   uchar i;
   for (i=0; i<4; i++)
   {
-    ibIn++;
+    *(pbIn++);
 
-    uint64_t ddw = 0;
-    uchar delta = pucDecodeBitArr((uchar *) &ddw, InBuffPtr(ibIn));
-    if (delta == 0xFF) { Error(); DelayInf(); return; }
-    ibIn += delta;
+    int64_t ddw = 0;
+    pbIn = DffDecodePositive(pbIn, &ddw);
 
     mpdbChannelsC[i] = (double)(ddw % 0x100000000) / 10000;
     mpdwBaseDig[i] = mpdbChannelsC[i] * mpdbPulseMnt[ibDig];
