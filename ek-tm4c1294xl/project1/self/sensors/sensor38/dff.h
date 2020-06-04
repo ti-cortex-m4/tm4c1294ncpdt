030e5347a5e75a332789a3d2f0a9d40415bb698d
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-04 18:57:14 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor38/dff.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/dff.h
@@ -5,9 +5,11 @@ dff.c
 ------------------------------------------------------------------------------*/
 
 uchar   EncodeInt_(uchar*  send_buffer_position, int64_t  value);
+
 int EncodeInt(int64_t value, uint8_t *send_buffer_position);
 
 uint64_t    DffDecodeLong64(uchar  *pb);
-uchar*  DffDecodeLong64_(uchar*  pbIn, int64_t*  pdwOut);
+
+uchar*  DffDecode(uchar  *pbIn, int64_t  *pdwOut);
 
 unsigned char  pucDecodeBitArr(unsigned char *pOut, unsigned char *pIn);
