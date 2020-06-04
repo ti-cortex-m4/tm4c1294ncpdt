bd678fd92a8eff1e08fb04388d9d6e808efe9fbc
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-04 13:48:42 +0300

    fix

--- ek-tm4c1294xl/project1/self/devices/case_38p.h
+++ ek-tm4c1294xl/project1/self/devices/case_38p.h
@@ -2,27 +2,25 @@
 #ifndef SKIP_38
 
     case DEV_START_38P:
-      MakePause(DEV_PREVTIME_38P);
+      if (fCurrCtrl == true)
+        MakePause(DEV_PREVTIME1_38P);
+      else
+        MakePause(DEV_PREVTIME2_38P);    
       break;
 
 
-/*
-    case DEV_START_34P:
-      MakePause(DEV_PREVAUTHKEY_34P);
-      break;
-
-
-    case DEV_PREVAUTHKEY_34P:
+    case DEV_PREVTIME1_38P:
       cbRepeat = MaxRepeats();
-      QueryAuthKey34();
-      SetCurr(DEV_AUTHKEY_34P);
+      QueryTime38();
+      SetCurr(DEV_TIME1_38P);
       break;
 
-    case DEV_AUTHKEY_34P:
+    case DEV_TIME1_38P:
       if (mpSerial[ibPort] == SER_GOODCHECK)
       {
-        ReadAuthKey34();
-        MakePause(DEV_POSTAUTHKEY_34P);
+        tiValue38 = ReadTime38();
+        dwValue38 = DateToHouIndex(tiValue38);
+        MakePause(DEV_POSTTIME1_38P);
       }
       else
       {
@@ -32,24 +30,56 @@
           ErrorLink();
           cbRepeat--;
 
-          QueryAuthKey34();
-          SetCurr(DEV_AUTHKEY_34P);
+          QueryTime38();
+          SetCurr(DEV_TIME1_38P);
+        }
+      }
+      break;
+
+/*
+    case DEV_START_38P:
+      MakePause(DEV_PREVAUTHKEY_38P);
+      break;
+
+
+    case DEV_PREVAUTHKEY_38P:
+      cbRepeat = MaxRepeats();
+      QueryAuthKey38();
+      SetCurr(DEV_AUTHKEY_38P);
+      break;
+
+    case DEV_AUTHKEY_38P:
+      if (mpSerial[ibPort] == SER_GOODCHECK)
+      {
+        ReadAuthKey38();
+        MakePause(DEV_POSTAUTHKEY_38P);
+      }
+      else
+      {
+        if (cbRepeat == 0) ErrorProfile();
+        else
+        {
+          ErrorLink();
+          cbRepeat--;
+
+          QueryAuthKey38();
+          SetCurr(DEV_AUTHKEY_38P);
         }
       }
       break;
 
 
-    case DEV_POSTAUTHKEY_34P:
+    case DEV_POSTAUTHKEY_38P:
       cbRepeat = MaxRepeats();
-      QueryAuthReq34();
-      SetCurr(DEV_AUTHREQ_34P);
+      QueryAuthReq38();
+      SetCurr(DEV_AUTHREQ_38P);
       break;
 
-    case DEV_AUTHREQ_34P:
+    case DEV_AUTHREQ_38P:
       if (mpSerial[ibPort] == SER_GOODCHECK)
       {
-        if (ReadAuthReq34())
-          MakePause(DEV_POSTSTART_34P);
+        if (ReadAuthReq38())
+          MakePause(DEV_POSTSTART_38P);
         else
           ErrorProfile();
       }
@@ -61,53 +91,18 @@
           ErrorLink();
           cbRepeat--;
 
-          QueryAuthReq34();
-          SetCurr(DEV_AUTHREQ_34P);
+          QueryAuthReq38();
+          SetCurr(DEV_AUTHREQ_38P);
         }
       }
       break;
-
-
-    case DEV_POSTSTART_34P:
-      if (fCurrCtrl == true)
-        MakePause(DEV_PREVTIME_34P);
-      else
-        MakePause(DEV_PREVINIT_34P);
-      break;
-
-
-    case DEV_PREVTIME_34P:
-      cbRepeat = MaxRepeats();
-      QueryTime34();
-      SetCurr(DEV_TIME_34P);
-      break;
-
-    case DEV_TIME_34P:
-      if (mpSerial[ibPort] == SER_GOODCHECK)
+*/
+    case DEV_POSTTIME1_38P:
       {
-        SetTimeCurr34(ReadTime34());
-        MakePause(DEV_POSTTIME_34P);
-      }
-      else
-      {
-        if (cbRepeat == 0) ErrorProfile();
-        else
-        {
-          ErrorLink();
-          cbRepeat--;
-
-          QueryTime34();
-          SetCurr(DEV_TIME_34P);
-        }
-      }
-      break;
-
-    case DEV_POSTTIME_34P:
-      {
-        ulong dwSecond1 = GetSecondIndex(GetTimeCurr34());
+        ulong dwSecond1 = GetSecondIndex(GetTimeCurr38());
         ulong dwSecond2 = GetSecondIndex(tiCurr);
 
-        if (DifferentDay(GetTimeCurr34(), tiCurr))
+        if (DifferentDay(GetTimeCurr38(), tiCurr))
         { ShowLo(szBadDates); DelayMsg(); ErrorProfile(); } // даты не совпадают, коррекция невозможна
         else
         {
@@ -116,17 +111,17 @@
           ulong dwDelta = AbsLong(dwSecond1 - dwSecond2);
           if (dwDelta < GetCorrectLimit()) {
             ShowLo(szCorrectNo); DelayInf();
-            MakePause(DEV_PREVINIT_34P); // без коррекции
+            MakePause(DEV_PREVINIT_38P); // без коррекции
           }
-          else if (GetCurrHouIndex() == GetTimeCurrIndex34())
+          else if (GetCurrHouIndex() == GetTimeCurrIndex38())
           {
-            if (dwDelta < CORRECT_LIMIT_34) {
-              SetCorrectSecond34(dwSecond2 - dwSecond1);
+            if (dwDelta < CORRECT_LIMIT_38) {
+              SetCorrectSecond38(dwSecond2 - dwSecond1);
               ShowLo(szCorrectYes); DelayInf();
-              MakePause(DEV_PREVCORRECT_34P); // коррекция времени
+              MakePause(DEV_PREVCORRECT_38P); // коррекция времени
             } else {
               ShowLo(szManageYes); DelayInf();
-              MakePause(DEV_PREVMANAGE_34P); // установка времени
+              MakePause(DEV_PREVMANAGE_38P); // установка времени
             }
           }
           else
@@ -137,21 +132,21 @@
 
 // начало коррекции времени
 
-    case DEV_PREVCORRECT_34P:
+    case DEV_PREVCORRECT_38P:
       cbRepeat = MaxRepeats();
-      QueryCorrect34();
-      SetCurr(DEV_CORRECT_34P);
+      QueryCorrect38();
+      SetCurr(DEV_CORRECT_38P);
       break;
 
-    case DEV_CORRECT_34P:
+    case DEV_CORRECT_38P:
       if (mpSerial[ibPort] == SER_GOODCHECK)
       {
-        ReadCorrect34();
+        ReadCorrect38();
         if (InBuff(2) == 0x05) {
           ShowLo(szManageYes); DelayInf();
-          MakePause(DEV_PREVMANAGE_34P); // нельзя корректировать время, можно установить время
+          MakePause(DEV_PREVMANAGE_38P); // нельзя корректировать время, можно установить время
         } else {
-          MakePause(DEV_PREVINIT_34P);
+          MakePause(DEV_PREVINIT_38P);
         }
       }
       else
@@ -162,8 +157,8 @@
           ErrorLink();
           cbRepeat--;
 
-          QueryCorrect34();
-          SetCurr(DEV_CORRECT_34P);
+          QueryCorrect38();
+          SetCurr(DEV_CORRECT_38P);
         }
       }
       break;
@@ -172,17 +167,17 @@
 
 // начало установки времени
 
-    case DEV_PREVMANAGE_34P:
+    case DEV_PREVMANAGE_38P:
       cbRepeat = MaxRepeats();
-      QueryManage34();
-      SetCurr(DEV_MANAGE_34P);
+      QueryManage38();
+      SetCurr(DEV_MANAGE_38P);
       break;
 
-    case DEV_MANAGE_34P:
+    case DEV_MANAGE_38P:
       if (mpSerial[ibPort] == SER_GOODCHECK)
       {
-        ReadManage34();
-        MakePause(DEV_PREVINIT_34P);
+        ReadManage38();
+        MakePause(DEV_PREVINIT_38P);
       }
       else
       {
@@ -192,8 +187,8 @@
           ErrorLink();
           cbRepeat--;
 
-          QueryManage34();
-          SetCurr(DEV_MANAGE_34P);
+          QueryManage38();
+          SetCurr(DEV_MANAGE_38P);
         }
       }
       break;
@@ -201,18 +196,18 @@
 // конец установки времени
 
 */
-    case DEV_PREVTIME_38P:
+    case DEV_PREVTIME2_38P:
       cbRepeat = MaxRepeats();
       QueryTime38();
-      SetCurr(DEV_TIME_38P);
+      SetCurr(DEV_TIME2_38P);
       break;
 
-    case DEV_TIME_38P:
+    case DEV_TIME2_38P:
       if (mpSerial[ibPort] == SER_GOODCHECK)
       {
         tiValue38 = ReadTime38();
         dwValue38 = DateToHouIndex(tiValue38);
-        MakePause(DEV_POSTTIME_38P);
+        MakePause(DEV_POSTTIME2_38P);
       }
       else
       {
@@ -223,13 +218,13 @@
           cbRepeat--;
 
           QueryTime38();
-          SetCurr(DEV_TIME_38P);
+          SetCurr(DEV_TIME2_38P);
         }
       }
       break;
 
 
-    case DEV_POSTTIME_38P:
+    case DEV_POSTTIME2_38P:
       InitHeader38();
 
       cbRepeat = MaxRepeats();
