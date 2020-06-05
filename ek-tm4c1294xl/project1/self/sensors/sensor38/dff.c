8b6e5edac5f94f1fb9ffc8bfc7b60b5525b84f3c
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-05 19:02:16 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor38/dff.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/dff.c
@@ -8,10 +8,9 @@ dff.c
 #include "../../serial/ports.h"
 #include "dff.h"
 
-#include "../../serial/monitor.h"
 
 
-int EncodeInt(int64_t value, uint8_t *send_buffer_position) {
+int     EncodeInt(int64_t value, uint8_t *send_buffer_position) {
   int num=0; // число записанных байт (лбф + 7 бит)
   char ch;
 
@@ -27,28 +26,30 @@ int EncodeInt(int64_t value, uint8_t *send_buffer_position) {
 
 
 
-uchar   DffEncode(int64_t  dwValue, uchar*  pbOut) {
-  int num = 0; // число записанных байт (лбф + 7 бит)
+uchar   DffEncode(int64_t  ddwIn, uchar*  pbOut) {
+  int bytes = 0;
   int bits = 0;
 
   while (true)
   {
-    int64_t new = dwValue >> (bits - 1);
+    int64_t new = ddwIn >> (bits - 1);
     bool f1 = (new != -1);
     bool f2 = (new != 0);
-    bool f3 = (num == 0);
+    bool f3 = (bytes == 0);
     bool f = ((f1 && f2) || f3);
     if (f == false) break; // условие остановки кодирования: оставшиеся биты и последний записанный либо нули либо единицы
 
-    char ch = (char)(dwValue >> bits); // следующие 7 бит
+    char ch = (char)(ddwIn >> bits); // следующие 7 бит
 
     *pbOut++ = ch | 0x80; // запись с флагом lbf
-    num++;
+
+    bytes++;
     bits += 7; // записали очередные 7 бит
   }
 
   *(pbOut - 1) &= 0x7F; //убрать флаг у последнего байта
-  return num;
+
+  return bytes;
 }
 
 
@@ -83,36 +84,4 @@ uchar*  DffDecodePositive(uchar  *pbIn, int64_t  *pdwOut) {
   while (*(pbIn++) & 0x80);
 
   return pbIn;
-}
-
-
-
-uchar  pucDecodeBitArr(uchar *pOut, uchar *pIn) {
-  uchar  bIn, bOut = 0;
-  unsigned int   boId = 0, i = 0, o = 0;
-
-  for (;;) {
-    bIn = pIn[i++];
-
-    uchar t;
-    for (t=0; t<8; t++) {
-      if (bIn & (0x01 << t)) {
-        if (t != 7)
-          bOut |= (0x01 << boId++);
-      }
-      else {
-        if (t == 7) {
-          pOut[o] = bOut;
-          return i; //&(pIn[i]);
-        }
-        bOut &= ~(0x01 << boId++);
-      }
-      if (boId >=8) {
-        pOut[o++] = bOut;
-        bOut = boId = 0;
-      }
-    }
-    if (i > 256) break;
-  }
-  return 0xFF; //NUL;
 }
--- ek-tm4c1294xl/project1/self/sensors/sensor38/dff.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/dff.h
@@ -5,10 +5,7 @@ dff.c
 ------------------------------------------------------------------------------*/
 
 uchar   DffEncode(int64_t  value, uchar*  send_buffer_position);
-
-int EncodeInt(int64_t value, uint8_t *send_buffer_position);
+int     EncodeInt(int64_t value, uint8_t *send_buffer_position);
 
 uchar*  DffDecode(uchar  *pbIn, int64_t  *pdwOut);
 uchar*  DffDecodePositive(uchar  *pbIn, int64_t  *pdwOut);
-
-uchar  pucDecodeBitArr(uchar *pOut, uchar *pIn);
