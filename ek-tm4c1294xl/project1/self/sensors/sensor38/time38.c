dfe5fe3f728dec161a8c32146c30b1cf5ee5407e
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-05 08:41:06 +0300

    Update time38.c

--- ek-tm4c1294xl/project1/self/sensors/sensor38/time38.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/time38.c
@@ -5,16 +5,8 @@ time38.c
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
-//#include "../../memory/mem_digitals.h"
-//#include "../../memory/mem_current.h"
-//#include "../../memory/mem_factors.h"
-//#include "../../display/messages.h"
-//#include "../../time/delay.h"
 #include "../../serial/ports.h"
 #include "../../serial/ports2.h"
-//#include "../../devices/devices.h"
-//#include "../../digitals/digitals.h"
-//#include "../../digitals/current/current_run.h"
 #include "device38.h"
 #include "io38.h"
 #include "dff.h"
