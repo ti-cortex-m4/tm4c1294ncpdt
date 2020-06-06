14e034b12fb921becc3ce2b8aced72177e38a896
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-06 13:44:11 +0300

    search

--- ek-tm4c1294xl/project1/self/sensors/sensor38/automatic38.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/automatic38.c
@@ -7,7 +7,9 @@ automatic38.c
 #include "../../main.h"
 #include "../../display/display.h"
 #include "../../sensors/automatic1.h"
+#include "../../hardware/beep.h"
 #include "../../time/delay.h"
+#include "../../serial/ports.h"
 #include "device38.h"
 #include "io38.h"
 #include "automatic38.h"
@@ -22,7 +24,6 @@ bool    Automatic38(void)
   if (Input38() != SER_GOODCHECK) return false;
 
   ShowLong(ReadNumber38());
-
   Delay(1000);
 
 
@@ -30,5 +31,33 @@ bool    Automatic38(void)
   dbKtrans = 1;                         // K трансформации
   SetAllFactors(dbKpulse,dbKtrans);     // сохранение К преобразования и К трансформации
 
+  return true;
+}
+
+
+
+bool    ChangeSpeed38(void)
+{
+  InitPush(0);
+
+  PushChar(0xC0);
+  PushChar(0x06);
+
+  PushLongLtl(0);
+
+  PushChar(0x00);
+  PushChar(0x06);
+
+  PushChar(6);
+  PushChar(0);
+  PushChar(5);
+
+  Query38(250, 14);
+
+
+  if (Input38() != SER_GOODCHECK) return false;
+  Beep();
+
+  ShowLong(ReadNumber38());
   return true;
 }
