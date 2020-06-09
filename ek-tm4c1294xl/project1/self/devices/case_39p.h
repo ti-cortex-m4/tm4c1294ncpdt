5b87a0ed1c481d4cf244141b9126ba55fdca8f14
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-09 09:00:24 +0300

    fix

--- ek-tm4c1294xl/project1/self/devices/case_39p.h
+++ ek-tm4c1294xl/project1/self/devices/case_39p.h
@@ -2,23 +2,23 @@
 #ifndef SKIP_38
 
     case DEV_START_P39:
-      MakePause(DEV_POSTVALUE_P39);
+      MakePause(DEV_PREVTIME2_P39);
       break;
 
-    case DEV_POSTCONFIG_P39:
+    case DEV_PREVTIME2_P39:
       ShowPercent(50);
 
       cbRepeat = MaxRepeat39();
       QueryTime39();
-      SetCurr(DEV_VALUE_P39);
+      SetCurr(DEV_TIME2_P39);
       break;
 
-    case DEV_VALUE_P39:
+    case DEV_TIME2_P39:
       if (mpSerial[ibPort] == SER_GOODCHECK)
       {
-        tiValueS = ReadTime39();
-        dwValueS = DateToHouIndex(tiValueS);
-        MakePause(DEV_POSTVALUE_P39);
+        tiValue39 = ReadTime39();
+        dwValue39 = DateToHouIndex(tiValue39);
+        MakePause(DEV_INITHEADER_P39);
       }
       else
       {
@@ -30,12 +30,12 @@
           cbRepeat--;
 
           QueryTime39();
-          SetCurr(DEV_VALUE_P39);
+          SetCurr(DEV_TIME2_P39);
         }
       }
       break;
 
-    case DEV_POSTVALUE_P39:
+    case DEV_INITHEADER_P39:
       ShowPercent(75);
       InitHeader39();
 
@@ -46,7 +46,12 @@
 
     case DEV_HEADER_P39:
       if (mpSerial[ibPort] == SER_GOODCHECK)
-        MakePause(DEV_POSTHEADER_P39);
+      {
+        if (ReadHeader39() == 0)
+          DoneProfile();
+        else 
+          MakePause(DEV_DATA_P39);
+      }  
       else
       {
         if (cbRepeat == 0) ErrorProfile();
@@ -59,13 +64,6 @@
           SetCurr(DEV_HEADER_P39);
         }
       }
-      break;
-
-    case DEV_POSTHEADER_P39:
-      if (ReadHeader39() == 0)
-        DoneProfile();
-      else
-        MakePause(DEV_DATA_P39);
       break;
 
     case DEV_DATA_P39:
