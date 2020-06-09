8c7bb28e9e97e77c3b7153ea20e0026e5317e435
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-09 22:32:50 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor38/buffer_profile_38.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/buffer_profile_38.h
@@ -1,12 +1,12 @@
 /*------------------------------------------------------------------------------
-buffer_z*c
+buffer_profile_38.h
 
 
 ------------------------------------------------------------------------------*/
 
-void    InitBufferZ(void);
-void    AddBufferZ(time  ti, uint64_t  ddw);
+void    InitPrfBuff38(void);
+void    AddPrfBuff38(time  ti, uint64_t  ddw);
 
 profile38 GetPrf38(uchar i);
 uchar   GetPrfSize38(void);
-uchar   GetPrfOveflow38(void);
+bool    GetPrfOveflow38(void);
