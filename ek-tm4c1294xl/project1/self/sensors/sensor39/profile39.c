6daff91fbec95ef44671064ecfdf2a143ae95918
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-10 21:15:45 +0300

    rename

--- ek-tm4c1294xl/project1/self/sensors/sensor39/profile39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/profile39.c
@@ -18,8 +18,8 @@ profile39*c
 #include "device38.h"
 #include "io39.h"
 #include "monitor38.h"
-#include "fragment_open_38.h"
-#include "fragment_profile_38.h"
+#include "fragment_open_39.h"
+#include "fragment_profile_39.h"
 #include "buffer_profile_38.h"
 #include "profile39.h"
 
