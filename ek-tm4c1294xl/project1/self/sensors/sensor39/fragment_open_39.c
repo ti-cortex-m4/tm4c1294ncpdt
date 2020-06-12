bb42d9507eecc53f486aed10b4426beb8e8350ce
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-12 22:25:12 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_open_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_open_39.c
@@ -16,22 +16,22 @@ uchar   FragmentOpen39(runner39  *pr)
 {
   Query38_DISC();
   if (Input39() != SER_GOODCHECK) return 1;
-  DelayOff();
+//  DelayOff();
 
   Query38_SNRM();
   if (Input39() != SER_GOODCHECK) return 2;
-  DelayOff();
+//  DelayOff();
 
   Query38_Open2((*pr).bNS, (*pr).bNR);
   if (Input39() != SER_GOODCHECK) return 3;
   if (!ValidateIframe((*pr).bNS, (*pr).bNR)) return 4;
-  DelayOff();
+//  DelayOff();
 
   (*pr).bNR++;
   Query38_RR((*pr).bNR);
   if (Input39() != SER_GOODCHECK) return 5;
   if (!ValidateSframe((*pr).bNR)) return 6;
-  DelayOff();
+//  DelayOff();
 
   return 0;
 }
