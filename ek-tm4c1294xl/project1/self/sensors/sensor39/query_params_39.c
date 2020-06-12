98dadca6caf5fb27c705a0c07385fcfdbb0e9a54
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-12 17:58:52 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/query_params_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/query_params_39.c
@@ -5,6 +5,7 @@ query_params_39.c
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
+#include "../../kernel/wrappers.h"
 #include "../../serial/ports.h"
 #include "../../serial/ports2.h"
 #include "../../serial/ports_devices.h"
@@ -65,6 +66,53 @@ void    QueryGetRequestDLMS(const obis_t  obis, uchar  bNS, uchar  bNR, uchar  b
 
 
 
+void    QueryGetScalerDLMS(const obis_t  obis, uchar  bNS, uchar  bNR, uchar  bInvokeId)
+{
+#ifdef MONITOR_39
+  MonitorString("\n\n QueryGetScalerDLMS ");
+#endif
+
+  uint wSize = 23 + GetHdlcAddressesSize(); // 0x19 25
+
+  InitPush(0);
+  PushChar(0x7E);
+
+  PushFormatDLMS(wSize);
+  PushHdlcAddresses();
+
+  PushChar((bNR << 5) | 0x10 | (bNS << 1) | 0x00);
+
+  PushIntLtl(MakeCRC16X25OutBuff(1, 3+GetHdlcAddressesSize())); // 5
+
+  // DLMS start
+
+  PushChar(0xE6); // LLC
+  PushChar(0xE6);
+  PushChar(0x00);
+
+  PushChar(0xC0); // Get-Request
+  PushChar(0x01); // Get-Request-Normal
+  PushChar(0x80 | (bInvokeId % 16)); // Invoke-Id-And-Priority
+
+  PushChar(0x00);
+  PushChar(0x03); // class
+
+  PushOBIS_DLMS(obis);
+
+  PushChar(3); // attribute
+  PushChar(0x00);
+
+  // DLMS finish
+
+  PushIntLtl(MakeCRC16X25OutBuff(1, wSize-2));
+
+  PushChar(0x7E);
+
+  Query39(1000, wSize+2); // 27
+}
+
+
+
 uint    ReadType18ULong16(void)
 {
   InitPop(14 + GetHdlcAddressesSize());
@@ -74,7 +122,7 @@ uint    ReadType18ULong16(void)
 
 
 // The Blue Book: 4.1.5 Common data types
-long64_ ReadValueX(void)
+ulong64_ ReadValueX(void)
 {
   InitPop(12 + GetHdlcAddressesSize());
 
@@ -84,7 +132,7 @@ long64_ ReadValueX(void)
 #endif  
   if (bDataAccessResult != 0) {
     // TODO error(no_success, bDataAccessResult)
-    return GetLong64Error();
+    return GetLong64Error(0);
   }
 
   uchar bDataType = PopChar();
@@ -94,11 +142,35 @@ long64_ ReadValueX(void)
   if (bDataType == 18) // long-unsigned [18] Unsigned16 0…65 535
   {
     uint value = PopIntBig();
-    return GetLong64(value, true);  
+    return GetLong64(value, true, 0);
   }
 
   // TODO error(unknown_data_type, bDataType)
-  return GetLong64Error();
+  return GetLong64Error(1);
+}
+
+
+schar2  ReadScaler(void)
+{
+  InitPop(16 + GetHdlcAddressesSize());
+
+//  uchar bDataAccessResult = PopChar();
+//#ifdef MONITOR_39
+//  MonitorString("\n bDataAccessResult="); MonitorCharDec(bDataAccessResult);
+//#endif
+//  if (bDataAccessResult != 0) {
+//    // TODO error(no_success, bDataAccessResult)
+//    return GetLong64Error(0);
+//  }
+
+  schar bScaler = PopChar();
+  uchar bUnit = PopChar();
+#ifdef MONITOR_39
+  MonitorString("\n bScaler="); MonitorCharHex(bScaler);
+  MonitorString("\n bUnit="); MonitorCharDec(bUnit);
+#endif
+
+  return GetSChar2(bScaler, true);
 }
 
 /*
