8ecccc1e8a0138af4bcbbc464323d46a7723318b
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-12 20:44:48 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/query_params_39.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/query_params_39.h
@@ -5,8 +5,9 @@ query_params_39.h
 ------------------------------------------------------------------------------*/
 
 void    QueryGetRegisterValueDLMS(const obis_t  obis, runner39  r);
+void    QueryGetRegisterSDLMS(const obis_t  obis, runner39  r);
 void    QueryGetScalerDLMS(const obis_t  obis, uchar  bNS, uchar  bNR, uchar  bInvokeId);
 uint    ReadType18ULong16(void);
 
 ulong64_ ReadValueX(void);
-schar2  ReadScaler(void);
+schar2  ReadRegisterScaler(void);
