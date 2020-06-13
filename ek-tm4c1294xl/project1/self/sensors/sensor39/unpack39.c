207d0819631df56c0884d7d197a037883529de5f
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-13 09:55:14 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/unpack39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/unpack39.c
@@ -31,3 +31,13 @@ void    Decompress39(void)
 
   mpSerial[ibPort] = SER_POSTINPUT_MASTER;
 }
+
+/*
+ *  Output: out=9 in=1000
+ 7e a0 07 03 03 91 9e 32 7e
+ Input: in=7
+ 7e a0 41 03 03 58 7e
+ Input HDLC:  Format=a041 wSize=0041 wCRCexpected=eac1 wCRCactual=3f00 CRC_error error1=2
+ error 11
+ */
+ */
