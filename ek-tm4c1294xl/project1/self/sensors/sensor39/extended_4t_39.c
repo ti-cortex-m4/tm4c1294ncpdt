96291a7917b1730c1c5ee99e02b04a90922df690
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-14 22:21:03 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/extended_4t_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/extended_4t_39.c
@@ -92,7 +92,7 @@ status  CntMonCanTariff39_Internal(uchar  ibMon, uchar  ibTariff)
   bool present = (IsEngMonPresent39() == 0);
   bool absent = (IsEngMonAbsent39() == 0);
   InitPop(17 + GetHdlcAddressesSize());
-  ulong64_ ddw2 = ReadUnsignedValueDLSM();
+  ulong64_ ddw2 = PopUnsignedValueDLSM();
 
 #ifdef  MONITOR_39
   MonitorString("\n present="); MonitorBool(present);
