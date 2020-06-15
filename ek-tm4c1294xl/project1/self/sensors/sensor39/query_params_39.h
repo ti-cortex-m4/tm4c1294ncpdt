26664f57a22e22c5c9c5a75722b1d1be70a84e56
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-15 14:32:46 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/query_params_39.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/query_params_39.h
@@ -6,12 +6,3 @@ query_params_39.h
 
 void    QueryGetRegisterValueDLMS(const obis_t  obis, runner39  r);
 void    QueryGetRegisterScalerDLMS(const obis_t  obis, runner39  r);
-
-uint    ReadType18ULong16(void);
-
-// ulong64_ PopUnsignedValueDLSM(void);
-// long64_ PopSignedValueDLSM(void);
-
-// ulong64_ ReadUnsignedValueDLSM(void);
-
-
