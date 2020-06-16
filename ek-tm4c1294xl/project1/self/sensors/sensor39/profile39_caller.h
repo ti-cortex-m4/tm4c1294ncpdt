4f4f04363cfba4d219b946d82fc559a28cf374a4
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-16 16:23:10 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/profile39_caller.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/profile39_caller.h
@@ -7,9 +7,10 @@ profile39_caller.h
 void    Query39_DISC_Profile(void);
 void    Query39_AARQ_Profile(void);
 void    Query39_RR_Profile(void);
-void    QueryEngAbs39_Profile(void);
+void    QueryTime39_Profile(void);
 
 bool    ValidateSframe_Profile(void);
 bool    ValidateIframe_Profile(void);
 
-void    InitProfile39(void);
+void    InitProfile39_Profile(void);
+void    QueryProfile39_Profile(void);
