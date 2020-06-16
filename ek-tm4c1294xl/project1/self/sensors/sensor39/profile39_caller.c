e2860ce032209975096eba86b6b84dfd24910d7f
hgzqfrqnntfvennv <hgzqfrqnntfvennv@gmail.com>
2020-06-16 16:38:26 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/profile39_caller.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/profile39_caller.c
@@ -11,6 +11,7 @@ profile39_caller.c
 // #include "../../serial/ports.h"
 // #include "../../devices/devices.h"
 // #include "../../digitals/digitals.h"
+#include "../../kernel/tuples.h"
 // #include "io39.h"
 #include "device39.h"
 #include "hdlc_monitor.h"
@@ -22,36 +23,31 @@ static caller39         c;
 
 
 
-void    InitRunner39_Profile(void)
-{ 
+void    InitRunner39_Profile(void) { 
   caller39 c = InitCaller();
 }
 
 
 
-void    Query39_DISC_Profile(void)
-{
+void    Query39_DISC_Profile(void) {
   InitRunner39_Profile();
 
   Query39_DISC();
 }
 
 
-void    Query39_AARQ_Profile(void)
-{
+void    Query39_AARQ_Profile(void) {
   Query39_AARQ(c.bNS, c.bNR);
 }
 
 
-void    Query39_RR_Profile(void)
-{
+void    Query39_RR_Profile(void) {
   c.bNR++;
   Query39_RR(c.bNR);
 }
 
 
-void    QueryTime39_Profile(void)
-{
+void    QueryTime39_Profile(void) {
   c.bNS++;
   c.bInvokeId++;
   QueryTime39(c.bNS, c.bNR, c.bInvokeId);
@@ -59,14 +55,12 @@ void    QueryTime39_Profile(void)
 
 
 
-bool    ValidateSframe_Profile(void)
-{
+bool    ValidateSframe_Profile(void) {
   return ValidateSframe(c.bNR);
 }
 
 
-bool    ValidateIframe_Profile(void)
-{
+bool    ValidateIframe_Profile(void) {
   return ValidateIframe(c.bNS, c.bNR);
 };
 
@@ -79,5 +73,9 @@ void    InitProfile39_Profile(void) {
 
 
 void    QueryProfile39_Profile(void) {
-  // QueryHeader39(&c);
+  t2time t2 = QueryHeader39(&c);
+
+  c.bNS++;
+  c.bInvokeId++;
+  QueryProfile39(c.bNS, c.bNR, c.bInvokeId, t2.ti1, t2.ti2);
 }
