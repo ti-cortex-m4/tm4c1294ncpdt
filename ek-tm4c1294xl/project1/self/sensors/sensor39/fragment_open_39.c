0ae74f7e06ab7d28dfd5ea5643d8d4bb0f8dd823
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-17 10:45:55 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_open_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_open_39.c
@@ -16,22 +16,18 @@ uchar   FragmentOpen39(caller39  *pc)
 {
   Query39_DISC();
   if (Input39() != SER_GOODCHECK) return 1;
-//  DelayOff();
 
   Query39_SNRM();
   if (Input39() != SER_GOODCHECK) return 2;
-//  DelayOff();
 
   Query39_AARQ((*pc).bNS, (*pc).bNR);
   if (Input39() != SER_GOODCHECK) return 3;
   if (!ValidateIframe((*pc).bNS, (*pc).bNR)) return 4;
-//  DelayOff();
 
   (*pc).bNR++;
   Query39_RR((*pc).bNR);
   if (Input39() != SER_GOODCHECK) return 5;
   if (!ValidateSframe((*pc).bNR)) return 6;
-//  DelayOff();
 
   return 0;
 }
