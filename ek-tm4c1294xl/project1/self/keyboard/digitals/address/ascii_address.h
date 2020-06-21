f644b514c9d55627dd4cbb7778b41812d0e8139a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-21 18:08:06 +0300

    fix

--- ek-tm4c1294xl/project1/self/keyboard/digitals/address/ascii_address.h
+++ ek-tm4c1294xl/project1/self/keyboard/digitals/address/ascii_address.h
@@ -4,9 +4,9 @@ ascii_address.h
 
 ------------------------------------------------------------------------------*/
 
-void    AsciiAddress_Show(line  *address);
-void    AsciiAddress_Init(line  *address);
-void    AsciiAddress_Increment(line  *address);
-void    AsciiAddress_Decrement(line  *address);
-void    AsciiAddress_Enter(line  *address, uchar  idx);
-void    AsciiAddress_Delete(line  *address);
+void    AsciiAddress_Show(line  *pl);
+void    AsciiAddress_Init(line  *pl);
+void    AsciiAddress_Increment(line  *pl);
+void    AsciiAddress_Decrement(line  *pl);
+void    AsciiAddress_Enter(line  *pl, uchar  idx);
+void    AsciiAddress_Delete(line  *pl, uchar  idx);
