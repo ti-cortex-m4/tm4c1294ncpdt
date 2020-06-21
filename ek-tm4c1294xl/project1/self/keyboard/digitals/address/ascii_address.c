b4e4d2b6063e6a450397b268f7ea3f320e5f19b2
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-21 20:14:17 +0300

    Update ascii_address.c

--- ek-tm4c1294xl/project1/self/keyboard/digitals/address/ascii_address.c
+++ ek-tm4c1294xl/project1/self/keyboard/digitals/address/ascii_address.c
@@ -30,11 +30,11 @@ void    AsciiAddress_Init(line  *pl) {
 
 void    AsciiAddress_Increment(line  *pl) {
   uchar i = 0;
-  if ((*pl).szLine[i] >= 'Z')
+  if ((*pl).szLine[i] == 'Z')
     (*pl).szLine[i] = '0';
   else if ((*pl).szLine[i] >= 'A')
     (*pl).szLine[i]++;
-  else if ((*pl).szLine[i] >= 'z')
+  else if ((*pl).szLine[i] == 'z')
     (*pl).szLine[i] = 'A';
   else if ((*pl).szLine[i] >= 'a')
     (*pl).szLine[i]++;
@@ -49,7 +49,7 @@ void    AsciiAddress_Increment(line  *pl) {
 
 void    AsciiAddress_Decrement(line  *pl) {
   uchar i = 0;
-  if ((*pl).szLine[i] <= '0')
+  if ((*pl).szLine[i] == '0')
     (*pl).szLine[i] = 'Z';
   else if ((*pl).szLine[i] <= '9')
     (*pl).szLine[i]--;
@@ -58,7 +58,7 @@ void    AsciiAddress_Decrement(line  *pl) {
   else if ((*pl).szLine[i] <= 'z')
     (*pl).szLine[i]--;
   else if ((*pl).szLine[i] == 'A')
-    (*pl).szLine[i] = '9';
+    (*pl).szLine[i] = 'z';
   else if ((*pl).szLine[i] <= 'Z')
     (*pl).szLine[i]--;
   else
