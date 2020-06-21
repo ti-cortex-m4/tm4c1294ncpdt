e4d2e4d41894504cbac8c5f41fda45a542f316a6
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-21 14:29:08 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/automatic_get_time_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/automatic_get_time_39.c
@@ -22,10 +22,10 @@ time2   ReadTimeCan39_Internal(void)
   caller39 c = InitCaller();
 
   time2 tm2 = FragmentOpenTime39(&c);
-  if (!tm2.fValid) return GetTime2Error1(91);
+  if (!tm2.fValid) return GetTime2Error1(90);
 
   Query39_DISC();
-  if (Input39() != SER_GOODCHECK) return GetTime2Error1(92);
+  if (Input39() != SER_GOODCHECK) return GetTime2Error1(91);
 
   return GetTime0(tm2.tiValue);
 }
@@ -51,9 +51,9 @@ time2   ReadTimeCan39(void)
   }
 
   Query39_DISC();
-  if (Input39() != SER_GOODCHECK) return GetTime2Error1(93);
+  if (Input39() != SER_GOODCHECK) return GetTime2Error1(92);
 
-  return GetTime2Error1(94);
+  return GetTime2Error1(93);
 }
 
 
@@ -72,7 +72,7 @@ time2   ReadTimeCan39_Short(void)
   }
 
   Query39_DISC();
-  if (Input39() != SER_GOODCHECK) return GetTime2Error1(95);
+  if (Input39() != SER_GOODCHECK) return GetTime2Error1(94);
 
-  return GetTime2Error1(96);
+  return GetTime2Error1(95);
 }
