2ebda3a4b008f07043e7d7beffd466df0d4d6aaf
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-21 09:53:55 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_open_time_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_open_time_39.c
@@ -7,27 +7,28 @@ fragment_open_time_39.c
 #include "../../main.h"
 #include "../../time/delay.h"
 #include "device39.h"
+#include "time39.h"
 #include "io39.h"
+#include "fragment_open_39.h"
 #include "fragment_open_time_39.h"
 
 
 
-uchar   FragmentOpenTime39(caller39  *pc)
+time2   FragmentOpenTime39(caller39  *pc)
 {
-  if (FragmentOpen39(&r) != 0) return 191;
+  if (FragmentOpen39(pc) != 0) return GetTime2Error1(191);
 
   (*pc).bNS++;
   (*pc).bInvokeId++;
   QueryTime39((*pc).bNS, (*pc).bNR, (*pc).bInvokeId);
-  if (Input39() != SER_GOODCHECK) return 7;
-  if (!ValidateIframe((*pc).bNS, (*pc).bNR)) return 8;
-  tiValue39 = ReadTime39();
-  dwValue39 = DateToHouIndex(tiValue39);
+  if (Input39() != SER_GOODCHECK) return GetTime2Error1(192);
+  if (!ValidateFrame((*pc).bNS, (*pc).bNR)) return GetTime2Error1(193);
+  time tm = ReadTime39();
 
   (*pc).bNR++;
   Query39_RR((*pc).bNR);
-  if (Input39() != SER_GOODCHECK) return 9;
-  if (!ValidateSframe((*pc).bNR)) return 10;
+  if (Input39() != SER_GOODCHECK) return GetTime2Error1(194);
+  if (!ValidateFrame((*pc).bNS, (*pc).bNR)) return GetTime2Error1(195);
 
-  return 0;
+  return GetTime0(tm);
 }
