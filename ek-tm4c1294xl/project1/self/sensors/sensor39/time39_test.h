97ccad40379e8b2318dff808c15b6f4e40e5afd6
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-22 21:38:04 +0300

    correct

--- ek-tm4c1294xl/project1/self/sensors/sensor39/time39_test.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/time39_test.h
@@ -1,10 +1,6 @@
 /*------------------------------------------------------------------------------
-time39.h
+time39_test.h
 
 
 ------------------------------------------------------------------------------*/
 
-void    QueryTime39(uchar  bNS, uchar  bNR, uchar  bInvokeId);
-time    ReadTime39(void);
-
-void    QueryCorrectTime39(uchar  bNS, uchar  bNR, uchar  bInvokeId, sint  wSeconds);
