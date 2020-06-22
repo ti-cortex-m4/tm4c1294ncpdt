825a8df7f21d7b7f3ac3010cf880171e2dedea70
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-22 12:05:50 +0300

    fix

--- ek-tm4c1294xl/project1/self/devices/case_39c.h
+++ ek-tm4c1294xl/project1/self/devices/case_39c.h
@@ -2,120 +2,121 @@
 #ifndef SKIP_39
 
     case DEV_START_39C:
+      MakePause(DEV_DISC_O_39C);
+      break;
+
+
+    case DEV_DISC_O_39C:
       Clear(); ShowPercent(50);
 
       cbRepeat = MaxRepeats();
       Query39_DISC_Current();
-      SetCurr(DEV_DISC_39C);
+      SetCurr(DEV_DISC_I_39C);
       break;
 
-    case DEV_DISC_39C:
+    case DEV_DISC_I_39C:
       if (mpSerial[ibPort] == SER_GOODCHECK) {
-        MakePause(DEV_POSTDISC_39C);
-      }
-      else {
+        MakePause(DEV_SNRM_O_39C);
+      } else {
         if (cbRepeat == 0) ErrorCurrent();
         else {
           ErrorLink();
           cbRepeat--;
 
           Query39_DISC_Current();
-          SetCurr(DEV_DISC_39C);
+          SetCurr(DEV_DISC_I_39C);
         }
       }
       break;
 
 
-    case DEV_POSTDISC_39C:
+    case DEV_SNRM_O_39C:
       Clear(); ShowPercent(51);
 
       cbRepeat = MaxRepeats();
       Query39_SNRM();
-      SetCurr(DEV_SNRM_39C);
+      SetCurr(DEV_SNRM_I_39C);
       break;
 
-    case DEV_SNRM_39C:
+    case DEV_SNRM_I_39C:
       if (mpSerial[ibPort] == SER_GOODCHECK) {
         MakePause(DEV_POSTSNRM_39C);
-      }
-      else {
+      } else {
         if (cbRepeat == 0) ErrorCurrent();
         else {
           ErrorLink();
           cbRepeat--;
 
           Query39_SNRM();
-          SetCurr(DEV_SNRM_39C);
+          SetCurr(DEV_SNRM_I_39C);
         }
       }
       break;
 
 
-    case DEV_POSTSNRM_39C:
+    case DEV_AARQ_O_39C:
       Clear(); ShowPercent(52);
 
       cbRepeat = MaxRepeats();
       Query39_AARQ_Current();
-      SetCurr(DEV_OPEN2_39C);
+      SetCurr(DEV_AARQ_I_39C);
       break;
 
-    case DEV_OPEN2_39C:
+    case DEV_AARQ_I_39C:
       if (mpSerial[ibPort] == SER_GOODCHECK) {
         if (!ValidateIframe_Current())
           ErrorCurrent();
         else
-          MakePause(DEV_POSTOPEN2_39C);
-      }
-      else {
+          MakePause(DEV_RR_AARQ_O_39C);
+      } else {
         if (cbRepeat == 0) ErrorCurrent();
         else {
           ErrorLink();
           cbRepeat--;
 
           Query39_AARQ_Current();
-          SetCurr(DEV_OPEN2_39C);
+          SetCurr(DEV_AARQ_I_39C);
         }
       }
       break;
 
 
-    case DEV_POSTOPEN2_39C:
+    case DEV_RR_AARQ_O_39C:
       Clear(); ShowPercent(53);
 
       cbRepeat = MaxRepeats();
       Query39_RR_Current();
-      SetCurr(DEV_RR1_39C);
+      SetCurr(DEV_RR_AARQ_I_39C);
       break;
 
-    case DEV_RR1_39C:
+    case DEV_RR_AARQ_I_39C:
       if (mpSerial[ibPort] == SER_GOODCHECK) {
         if (!ValidateSframe_Current())
           ErrorCurrent();
         else
           MakePause(DEV_POSTRR1_39C);
-      }
-      else {
+      } else {
         if (cbRepeat == 0) ErrorCurrent();
         else {
           ErrorLink();
           cbRepeat--;
 
           Query39_RR_Current();
-          SetCurr(DEV_RR1_39C);
+          SetCurr(DEV_RR_AARQ_I_39C);
         }
       }
       break;
 
 
-    case DEV_POSTRR1_39C:
+    case DEV_ENGABS_O_39C:
       Clear(); ShowPercent(54);
 
       cbRepeat = MaxRepeats();
       QueryEngAbs39_Current();
-      SetCurr(DEV_ENGABS_39C);
+      SetCurr(DEV_ENGABS_I_39C);
       break;
 
-    case DEV_ENGABS_39C:
+    case DEV_ENGABS_I_39C:
       if (mpSerial[ibPort] == SER_GOODCHECK)
       {
         if (!ValidateIframe_Current())
@@ -124,15 +125,14 @@
           SaveCurrent39();
           MakePause(DEV_POSTENGABS_39C);
         }
-      }
-      else {
+      } else {
         if (cbRepeat == 0) ErrorCurrent();
         else {
           ErrorLink();
           cbRepeat--;
 
           QueryEngAbs39_Current();
-          SetCurr(DEV_ENGABS_39C);
+          SetCurr(DEV_ENGABS_I_39C);
         }
       }
       break;
