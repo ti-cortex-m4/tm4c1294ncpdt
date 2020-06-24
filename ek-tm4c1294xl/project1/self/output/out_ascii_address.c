4c165dbabf1b773cfc2cab657b7d50dd84729975
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-24 22:15:59 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/output/out_ascii_address.c
@@ -0,0 +1,75 @@
+/*------------------------------------------------------------------------------
+out_ascii_address.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+#include "../memory/mem_ports.h"
+//#include "../memory/mem_settings.h"
+//#include "../memory/mem_phones.h"
+#include "../serial/ports.h"
+#include "../digitals/address/ascii_address.h"
+#include "../nvram/cache.h"
+#include "out_ascii_address.h"
+
+
+
+void    OutGetPhone(void)
+{
+  if (bInBuff6 < bCANALS)
+    Common(&mpphAsciiAddress[ bInBuff6 ], sizeof(line));
+  else
+    Result(bRES_BADADDRESS);
+}
+
+
+void    OutSetPhone(void)
+{
+  if (enGlobal == GLB_PROGRAM)
+  {
+    if (bInBuff6 < bCANALS)
+    {
+      static line ph;
+
+      uchar i;
+      for (i=0; i<bLINE_SIZE; i++)
+      {
+        ph.szLine[i] = InBuff(7+i);
+      }
+
+      if (true)
+      {
+        mpphAsciiAddress[ bInBuff6 ] = ph;
+
+        if (bInBuff6 == bCANALS - 1)
+          SaveCache(&chAsciiAddress);
+
+        LongResult(bRES_OK);
+      }
+      else Result(bRES_BADDATA);
+    }
+    else Result(bRES_BADADDRESS);
+  }
+  else Result(bRES_NEEDPROGRAM);
+}
+
+
+
+void    OutPhonesExt(void)
+{
+  InitPushPtr();
+  uchar wSize = 0;
+
+  uchar c;
+  for (c=0; c<bCANALS; c++)
+  {
+    if ((InBuff(6 + c/8) & (0x80 >> c%8)) != 0)
+    {
+      Push(&mpphAsciiAddress[c], sizeof(line));
+      wSize += sizeof(line);
+    }
+  }
+
+  OutptrOutBuff(wSize);
+}
