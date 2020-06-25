15b3e653d2c9d9b7b25abab798501b74c5485904
hgzqfrqnntfvennv <hgzqfrqnntfvennv@gmail.com>
2020-06-25 14:42:25 +0300

    error

--- ek-tm4c1294xl/project1/self/sensors/sensor39/errors39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/errors39.c
@@ -6,3 +6,14 @@ errors39.c
 
 #include "../../main.h"
 #include "errors39.h"
+
+
+
+uchar   Error39(uchar  bError) {
+  return bError;        
+}
+
+
+uchar   Log39(uchar  bLog) {
+  return bLog;        
+}
--- ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_open_time_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_open_time_39.c
@@ -9,6 +9,7 @@ fragment_open_time_39.c
 #include "device39.h"
 #include "time39.h"
 #include "io39.h"
+#include "error39.h"
 #include "fragment_open_39.h"
 #include "fragment_open_time_39.h"
 
@@ -22,14 +23,14 @@ time2   FragmentOpenTime39(caller39  *pc)
   (*pc).bNS++;
   (*pc).bInvokeId++;
   QueryTime39((*pc).bNS, (*pc).bNR, (*pc).bInvokeId);
-  if (Input39() != SER_GOODCHECK) return GetTime2Error1(190);
-  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return GetTime2Error1(191);
+  if (Input39() != SER_GOODCHECK) return GetTime2Error1(Error39(190));
+  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return GetTime2Error1(Error39(191));
   time tm = ReadTime39();
 
   (*pc).bNR++;
   Query39_RR((*pc).bNR);
-  if (Input39() != SER_GOODCHECK) return GetTime2Error1(192);
-  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return GetTime2Error1(193);
+  if (Input39() != SER_GOODCHECK) return GetTime2Error1(Error39(192));
+  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return GetTime2Error1(Error39(193));
 
   return GetTime0(tm);
 }
