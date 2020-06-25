78121406e1f61a29a19bc531e7cea812588d533f
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-25 14:42:25 +0300

    error

--- ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_open_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_open_39.c
@@ -8,6 +8,7 @@ fragment_open_39.c
 #include "../../time/delay.h"
 #include "device39.h"
 #include "io39.h"
+#include "error39.h"
 #include "fragment_open_39.h"
 
 
@@ -15,19 +16,19 @@ fragment_open_39.c
 uchar   FragmentOpen39(caller39  *pc)
 {
   Query39_DISC();
-  if (Input39() != SER_GOODCHECK) return 180;
+  if (Input39() != SER_GOODCHECK) return Error39(180);
 
   Query39_SNRM();
-  if (Input39() != SER_GOODCHECK) return 181;
+  if (Input39() != SER_GOODCHECK) return Error39(181);
 
   Query39_AARQ((*pc).bNS, (*pc).bNR);
-  if (Input39() != SER_GOODCHECK) return 182;
-  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return 183;
+  if (Input39() != SER_GOODCHECK) return Error39(182);
+  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return Error39(183);
 
   (*pc).bNR++;
   Query39_RR((*pc).bNR);
-  if (Input39() != SER_GOODCHECK) return 184;
-  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return 185;
+  if (Input39() != SER_GOODCHECK) return Error39(184);
+  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return Error39(185);
 
   return 0;
 }
