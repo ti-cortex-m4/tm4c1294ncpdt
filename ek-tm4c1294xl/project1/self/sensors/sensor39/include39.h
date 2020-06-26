d4e43022466a9b8f21752376c67f0b3fd3e286ca
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-26 22:27:06 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/include39.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/include39.h
@@ -18,13 +18,15 @@ typedef struct
 
 
 #define MONITOR_39          1
-//#define MONITOR_39_NAMES    1
-//#define MONITOR_39_MONITOR  1
+#define MONITOR_39_NAMES    1
+#define MONITOR_39_MONITOR  1
+//#define MONITOR_39_CRC      1
 
 
 #ifndef MONITOR_39
 #undef MONITOR_39_NAMES
 #undef MONITOR_39_MONITOR
+#undef MONITOR_39_CRC
 #endif
 
 
