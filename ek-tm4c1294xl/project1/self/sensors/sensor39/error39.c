9211ad44e18dad01b57485755fd61dbbf543afaf
hgzqfrqnntfvennv <hgzqfrqnntfvennv@gmail.com>
2020-06-26 09:15:13 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/error39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/error39.c
@@ -5,16 +5,25 @@ error39*c
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
+#include "../../serial/monitor.h"
+#include "include39.h"
 #include "error39.h"
 
 
 
 uchar   Error39(uchar  bError) {
+#ifdef  MONITOR_39  
+  MonitorString("\n error="); MonitorCharDec(bError);
+#endif  
   return bError;        
 }
 
 
 uchar   Error39_(uchar  bError, uint  wCode) {
+#ifdef  MONITOR_39  
+  MonitorString("\n error="); MonitorCharDec(bError);
+  MonitorString("\n code="); MonitorIntDec(wCode);
+#endif  
   return bError;        
 }
 
