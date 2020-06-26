78d49064f624117ebcbea0289b5bfc6dbb8d7eae
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-26 19:46:24 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_cntmon_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_cntmon_39.c
@@ -1,70 +1,46 @@
 /*------------------------------------------------------------------------------
-extended_4t_39.c
+fragment_cntmon_39.c
 
 
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
-#include "../../memory/mem_digitals.h"
-#include "../../memory/mem_factors.h"
-#include "../../display/display.h"
-#include "../../keyboard/keyboard.h"
-#include "../../time/timedate.h"
-#include "../../time/rtc.h"
+//#include "../../memory/mem_digitals.h"
+//#include "../../memory/mem_factors.h"
+//#include "../../display/display.h"
+//#include "../../keyboard/keyboard.h"
+//#include "../../time/timedate.h"
+//#include "../../time/rtc.h"
 #include "../../serial/monitor.h"
-#include "../../serial/monitor_settings.h"
-#include "../../serial/ports.h"
-#include "../../serial/ports_devices.h"
-#include "../../serial/ports2.h"
-#include "../../devices/devices.h"
-#include "../../digitals/digitals.h"
+//#include "../../serial/monitor_settings.h"
+//#include "../../serial/ports.h"
+//#include "../../serial/ports_devices.h"
+//#include "../../serial/ports2.h"
+//#include "../../devices/devices.h"
+//#include "../../digitals/digitals.h"
 #include "device39.h"
 #include "device39_obis.h"
 #include "error39.h"
-#include "time39.h"
+//#include "time39.h"
 #include "query_engmon_39.h"
 #include "read_engmon_39.h"
 #include "query_register_39.h"
-#include "fragment_open_time_39.h"
+//#include "fragment_open_time_39.h"
 #include "io39.h"
 #include "hdlc_address.h"
 #include "dlms_read_data.h"
 #include "dlms_read_register.h"
-#include "extended_4t_39.h"
+#include "fragment_cntmon_39.h"
 
 
 
-const obis_t *GetOBIS(uchar  ibTariff)
+double2 FragmentCntMonCan(const obis_t  obis, caller39  *pc, uchar  bMonth, uchar  bYear)
 {
-  ASSERT(sizeof(obisEngAbsTariff)/sizeof(obisEngAbsTariff[0]) == 4);
-  ASSERT(ibTariff < 4);
-  return &obisEngAbsTariff[ibTariff];
-}
-
-
-static status BadDigital(uchar  bError) {  
-  Error39(bError);
-  return ST_BADDIGITAL;
-}
-
-
-status  CntMonCanTariff39_Internal(uchar  ibMon, uchar  ibTariff)
-{
-  caller39 c = InitCaller39();
-  
-  time2 tm2 = FragmentOpenTime39(&c);
-  if (!tm2.fValid) return BadDigital(130+0);
-  time tm = tm2.tiValue;
-
-
-  uchar bMonth = ibMon + 1;
-  uchar bYear = (bMonth > tm.bMonth) ? tm.bYear-1 : tm.bYear;
-
-
-  c.bNS++;
-  QueryEngMon39(*GetOBIS(ibTariff), c.bNS, c.bNR, c.bInvokeId++, bMonth, bYear);
-  if (Input39() != SER_GOODCHECK) return BadDigital(130+1);
-  if (ValidateFrame(c.bNS, c.bNR) != 0) return BadDigital(130+2);
+  (*pc).bNS++;
+  (*pc).bInvokeId++;
+  QueryEngMon39(obis, (*pc).bNS, (*pc).bNR, (*pc).bInvokeId, bMonth, bYear);
+  if (Input39() != SER_GOODCHECK) return GetDouble2Error1(Error39(110+0));
+  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return GetDouble2Error1(Error39(110+0));
 
 
   bool present = (IsEngMonPresent39() == 0);
@@ -84,89 +60,35 @@ status  CntMonCanTariff39_Internal(uchar  ibMon, uchar  ibTariff)
 #endif
 
 
-  c.bNR++;
-  Query39_RR(c.bNR);
-  if (Input39() != SER_GOODCHECK) return BadDigital(130+3);
-  if (!ValidateSframe(c.bNR)) return BadDigital(130+4);
+  (*pc).bNR++;
+  Query39_RR((*pc).bNR);
+  if (Input39() != SER_GOODCHECK) return GetDouble2Error1(Error39(110+0));
+  if (!ValidateSframe((*pc).bNR)) return GetDouble2Error1(Error39(110+0));
 
 
-  double2 scaler = ReadRegisterScaler39(*GetOBIS(ibTariff), &c);
-  if (!scaler.fValid) return BadDigital(130+5);
+  double2 scaler = ReadRegisterScaler39(obis, pc);
+  if (!scaler.fValid) return GetDouble2Error1(Error39(110+0));
   double dbScaler = scaler.dbValue;
 
 
   Query39_DISC();
-  if (Input39() != SER_GOODCHECK) return BadDigital(130+6);
+  if (Input39() != SER_GOODCHECK) return GetDouble2Error1(Error39(110+0));
 
 
   if (present) {
-    mpdbChannelsC[0] = ((double)ddwValue.ddwValue * dbScaler / 1000) * mpdbTransCnt[ibDig];
-    mpboChannelsA[0] = true;
+    double db = (double)ddwValue.ddwValue * dbScaler / 1000;
 #ifdef MONITOR_39
-    MonitorString("\n result="); MonitorDouble6(mpdbChannelsC[0]);
+    MonitorString("\n result="); MonitorDouble6(db);
 #endif
-    return ST_OK;
+    return GetDouble0(db);
   }
 
   if (absent) {
 #ifdef MONITOR_39
     MonitorString("\n result absent");
 #endif
-    return ST_NOTPRESENTED;
+    return GetDouble2Error1(1);
   }
 
-  return BadDigital(130+7);
+  return GetDouble2Error1(Error39(110+0));
 }
-
-
-
-status  ReadCntMonCanTariff39(uchar  ibMonAbs, uchar  ibTariff) // на начало мес€ца
-{
-  Clear();
-
-  uchar r;
-  for (r=0; r<MaxRepeats(); r++)
-  {
-    status s = CntMonCanTariff39_Internal(ibMonAbs, ibTariff);
-    if (fKey == true) break;
-
-    if (s == ST_OK) return s;
-    if (s == ST_NOTPRESENTED) return s;
-  }
-
-  Query39_DISC();
-  if (Input39() != SER_GOODCHECK) return BadDigital(130+8);
-
-  return BadDigital(130+9);
-}
-
-
-
-#ifdef  MONITOR_39
-
-double2 TestCntMonCanTariff39(void)
-{
-  fMonitorLogBasic = false;
-  fMonitorLogHex = false;
-
-  MonitorOpen(0);
-
-  uchar bMonth = (*GetCurrTimeDate()).bMonth;
-
-  uchar m;
-  for (m=0; m<10; m++) {
-    uchar ibMonthAbs = (12 + bMonth - 1 - m) % 12;
-    
-    uchar t;
-    for (t=0; t<4; t++) {
-      MonitorString("\n\n month="); MonitorCharDec(ibMonthAbs+1);
-      MonitorString(" tariff="); MonitorCharDec(t);
-      status s = ReadCntMonCanTariff39(ibMonthAbs, t);
-      MonitorString("\n status="); MonitorCharDec(s);
-    }
-  }
-
-  return GetDouble0(0);
-}
-
-#endif
