f6abc435411662d3fdc87fc858affab2126192fe
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-26 20:09:21 +0300

    Update fragment_cntmon_39.c

--- ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_cntmon_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_cntmon_39.c
@@ -1,31 +1,18 @@
 /*------------------------------------------------------------------------------
-fragment_cntmon_39*c
+fragment_cntmon_39.c
 
 
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
-//#include "../../memory/mem_digitals.h"
-//#include "../../memory/mem_factors.h"
-//#include "../../display/display.h"
-//#include "../../keyboard/keyboard.h"
-//#include "../../time/timedate.h"
-//#include "../../time/rtc.h"
 #include "../../serial/monitor.h"
-//#include "../../serial/monitor_settings.h"
 #include "../../serial/ports_pop.h"
-//#include "../../serial/ports_devices.h"
-//#include "../../serial/ports2.h"
-//#include "../../devices/devices.h"
-//#include "../../digitals/digitals.h"
 #include "device39.h"
 #include "device39_obis.h"
 #include "error39.h"
-//#include "time39.h"
 #include "query_engmon_39.h"
 #include "read_engmon_39.h"
 #include "query_register_39.h"
-//#include "fragment_open_time_39.h"
 #include "io39.h"
 #include "hdlc_address.h"
 #include "dlms_read_data.h"
@@ -40,7 +27,7 @@ double2 FragmentCntMonCan(const obis_t  obis, caller39  *pc, uchar  bMonth, ucha
   (*pc).bInvokeId++;
   QueryEngMon39(obis, (*pc).bNS, (*pc).bNR, (*pc).bInvokeId, bMonth, bYear);
   if (Input39() != SER_GOODCHECK) return GetDouble2Error1(Error39(110+0));
-  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return GetDouble2Error1(Error39(110+0));
+  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return GetDouble2Error1(Error39(110+1));
 
 
   bool present = (IsEngMonPresent39() == 0);
@@ -62,17 +49,13 @@ double2 FragmentCntMonCan(const obis_t  obis, caller39  *pc, uchar  bMonth, ucha
 
   (*pc).bNR++;
   Query39_RR((*pc).bNR);
-  if (Input39() != SER_GOODCHECK) return GetDouble2Error1(Error39(110+0));
-  if (!ValidateSframe((*pc).bNR)) return GetDouble2Error1(Error39(110+0));
+  if (Input39() != SER_GOODCHECK) return GetDouble2Error1(Error39(110+2));
+  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return GetDouble2Error1(Error39(110+3));
 
 
   double2 scaler = ReadRegisterScaler39(obis, pc);
-  if (!scaler.fValid) return GetDouble2Error1(Error39(110+0));
+  if (!scaler.fValid) return GetDouble2Error1(Error39(110+4));
   double dbScaler = scaler.dbValue;
-
-
-  Query39_DISC();
-  if (Input39() != SER_GOODCHECK) return GetDouble2Error1(Error39(110+0));
 
 
   if (present) {
@@ -85,10 +68,10 @@ double2 FragmentCntMonCan(const obis_t  obis, caller39  *pc, uchar  bMonth, ucha
 
   if (absent) {
 #ifdef MONITOR_39
-    MonitorString("\n result absent");
+    MonitorString("\n result is absent");
 #endif
-    return GetDouble2(-1, false);
+    return GetDouble2Error1(1);
   }
 
-  return GetDouble2Error1(Error39(110+0));
+  return GetDouble2Error1(Error39(110+5));
 }
