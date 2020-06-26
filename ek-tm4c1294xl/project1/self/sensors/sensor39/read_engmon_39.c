f9d68d303d13d35c5aed164d701e198b8f590760
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-26 18:38:43 +0300

    read

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/read_engmon_39.c
@@ -0,0 +1,242 @@
+/*------------------------------------------------------------------------------
+query_engmon_39*c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports2.h"
+#include "../../serial/ports_devices.h"
+#include "../../serial/monitor.h"
+#include "include39.h"
+#include "crc16x25.h"
+#include "io39.h"
+#include "hdlc_address.h"
+#include "dlms_push.h"
+#include "query_engmon_39.h"
+
+
+
+/*
+<GetRequest>
+  <GetRequestNormal>
+    <!--Priority: HIGH ServiceClass: UN_CONFIRMED invokeID: 6-->
+    <InvokeIdAndPriority Value="86" />
+    <AttributeDescriptor>
+      <!--PROFILE_GENERIC-->
+      <ClassId Value="0007" />
+      <!--0.0.98.1.0.255-->
+      <InstanceId Value="0000620100FF" />
+      <AttributeId Value="02" />
+    </AttributeDescriptor>
+    <AccessSelection>
+      <AccessSelector Value="01" />
+      <AccessParameters>
+        <Structure Qty="04" >
+          <Structure Qty="04" >
+            <UInt16 Value="0008" />
+            <!--0.0.1.0.0.255-->
+            <OctetString Value="0000010000FF" />
+            <Int8 Value="02" />
+            <UInt16 Value="0000" />
+          </Structure>
+          <!--2020-03-01 00:00:01-->
+          <OctetString Value="07E40301FF000001FF8000FF" />
+          <!--2020-04-01 00:00:00-->
+          <OctetString Value="07E40401FF000000FF8000FF" />
+          <Array Qty="01" >
+            <Structure Qty="04" >
+              <UInt16 Value="0003" />
+              <!--1.0.15.8.0.255-->
+              <OctetString Value="01000F0800FF" />
+              <Int8 Value="02" />
+              <UInt16 Value="0000" />
+            </Structure>
+          </Array>
+        </Structure>
+      </AccessParameters>
+    </AccessSelection>
+  </GetRequestNormal>
+</GetRequest>
+*/
+void    QueryEngMon39(const obis_t  obis, uchar  bNS, uchar  bNR, uchar  bInvokeId, uchar  bMonth, uchar  bYear)
+{
+#ifdef MONITOR_39_NAMES
+  MonitorString("\n\n QueryEngMon39 "); MonitorCharDec(bMonth); MonitorString(" "); MonitorCharDec(bYear);
+#endif
+
+  uint wSize = 92 + GetHdlcAddressesSize(); // 0x5E 94
+
+  InitPush(0);
+  PushChar(0x7E);
+
+  PushFormatDLMS(wSize);
+  PushHdlcAddresses();
+
+  PushChar((bNR << 5) | 0x10 | (bNS << 1) | 0x00);
+
+  PushIntLtl(MakeCRC16X25OutBuff(1, 3+GetHdlcAddressesSize())); // 5
+
+  // DLMS start
+
+  PushChar(0xE6); // LLC
+  PushChar(0xE6);
+  PushChar(0x00);
+
+  PushChar(0xC0); // Get-Request
+  PushChar(0x01); // Get-Request-Normal
+  PushChar(0xC0 | (bInvokeId % 16)); // Invoke-Id-And-Priority
+
+  PushChar(0x00); // TODO ???
+
+  PushChar(0x07); // <ClassId Value="0007" /> <!--PROFILE_GENERIC-->
+
+  PushChar(0x00);
+  PushChar(0x00);
+  PushChar(0x62);
+  PushChar(0x01);
+  PushChar(0x00);
+  PushChar(0xFF);
+
+  PushChar(0x02); // <AttributeId Value="02" />
+
+  PushChar(0x01); // <AccessSelector Value="01" />
+  PushChar(0x01);
+
+  PushChar(0x02);
+  PushChar(0x04);
+
+  PushChar(0x02);
+  PushChar(0x04);
+
+  PushChar(0x12); // <UInt16 Value="0008" />
+  PushChar(0x00);
+  PushChar(0x08);
+
+  PushChar(0x09); // <OctetString Value="0000010000FF" />
+  PushChar(0x06);
+  PushChar(0x00); // <!--0.0.1.0.0.255-->
+  PushChar(0x00);
+  PushChar(0x01);
+  PushChar(0x00);
+  PushChar(0x00);
+  PushChar(0xFF);
+
+  PushChar(0x0F); // <Int8 Value="02" />
+  PushChar(0x02);
+
+  PushChar(0x12); // <UInt16 Value="0000" />
+  PushChar(0x00);
+  PushChar(0x00);
+
+  PushChar(0x09); // <OctetString Value="?" />
+  PushChar(0x0C);
+  PushTimeMonthYearDLMS(bMonth, bYear);
+
+  PushChar(0x09); // <OctetString Value="?" />
+  PushChar(0x0C);
+  PushTimeMonthYearDLMS(bMonth, bYear);
+
+  PushChar(0x01); // array
+  PushChar(0x01);
+
+  PushChar(0x02); // structure
+  PushChar(0x04);
+
+  PushChar(0x12); // <UInt16 Value="0003" />
+  PushChar(0x00);
+  PushChar(0x03);
+
+  PushChar(0x09); // <OctetString Value="01000F0800FF" />
+  PushChar(0x06);
+  PushOBIS_DLMS(obis); // <!--1.0.15.8.0.255-->
+
+  PushChar(0x0F); // <Int8 Value="02" />
+  PushChar(0x02);
+
+  PushChar(0x12); // <UInt16 Value="0000" />
+  PushChar(0x00);
+  PushChar(0x00);
+
+  // DLMS finish
+
+  PushIntLtl(MakeCRC16X25OutBuff(1, wSize-2));
+
+  PushChar(0x7E);
+
+  Query39(1000, wSize+2);
+}
+
+
+
+/*
+<PDU>
+<GetResponse>
+  <GetResponseNormal>
+    <!--Priority: HIGH ServiceClass: UN_CONFIRMED invokeID: 6-->
+    <InvokeIdAndPriority Value="86" />
+    <Result>
+      <Data>
+        <Array Qty="01" >
+          <Structure Qty="01" >
+            <UInt64 Value="00000000000030B3" />
+          </Structure>
+        </Array>
+      </Data>
+    </Result>
+  </GetResponseNormal>
+</GetResponse>
+</PDU>
+*/
+uint64_t ReadEngMon39(void)
+{
+  InitPop(18 + GetHdlcAddressesSize());
+  return PopLongBig()*0x100000000 + PopLongBig();
+}
+
+
+
+uchar   IsEngMonPresent39(void)
+{
+  InitPop(12 + GetHdlcAddressesSize());
+
+  if (PopChar() != 0) return 1; // !success
+  if (PopChar() != 1) return 2; // !array
+  if (PopChar() != 1) return 3; // array size != 1
+  if (PopChar() != 2) return 4; // !structure
+  if (PopChar() != 1) return 5; // structure size != 1
+
+  return 0;
+}
+
+
+uchar   IsEngMonAbsent39(void)
+{
+  InitPop(12 + GetHdlcAddressesSize());
+
+  if (PopChar() != 0) return 1; // !success
+  if (PopChar() != 1) return 2; // !array
+  if (PopChar() != 0) return 3; // array size != 0
+
+  return 0;
+}
+
+
+
+/*
+7e a0 1d 03 03 74 32 9d e6 e7 00 c4 01 81  00 01 01 02 01 15  00 00 00 00 00 00 0f 31  1d 75 7e
+00 success
+01 array
+01 array size
+02 structure
+01 structure size
+15 [21] Unsigned64
+value=3889
+
+7e a0 12 03 03 74 cb 2f e6 e7 00 c4 01 81  00 01 00  3d ce 7e
+00 success
+01 array
+00 array size
+no value
+*/
