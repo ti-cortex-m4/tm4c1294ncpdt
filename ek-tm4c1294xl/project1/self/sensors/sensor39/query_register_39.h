5b5334ac980620bbf01590764872366c21bd3d93
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-27 16:36:26 +0300

    rename

--- ek-tm4c1294xl/project1/self/sensors/sensor39/query_register_39.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/query_register_39.h
@@ -4,10 +4,5 @@ query_register_39.h
 
 ------------------------------------------------------------------------------*/
 
-#include "caller39.h"
-#include "device39_obis.h"
-
-
-
 void    QueryGetRegisterValueDLMS(const obis_t  obis, caller39  c);
 void    QueryGetRegisterScalerDLMS(const obis_t  obis, caller39  c);
