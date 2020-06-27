d3435f813fd11e17ea4b0c152e72a9db4e9cf677
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-27 09:15:48 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/automatic2.c
+++ ek-tm4c1294xl/project1/self/sensors/automatic2.c
@@ -1930,7 +1930,7 @@ double2 ReadCntCurrCan(uchar  ibCan)
 #endif
 
 #ifndef SKIP_39
-    case 39: return TestCntMonCanTariff39(); //TestTimeCorrect39(); //ReadCntCurr39();
+    case 39: return TestProfile39(); //TestTimeCorrect39(); //ReadCntCurr39();
 #endif
 
     default: return GetDouble2Error();
