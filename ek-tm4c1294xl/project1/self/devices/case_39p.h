d677a0c25a6c67aeb7dfde27384ce6f681fba19d
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-06-28 17:16:16 +0300

    fix

--- ek-tm4c1294xl/project1/self/devices/case_39p.h
+++ ek-tm4c1294xl/project1/self/devices/case_39p.h
@@ -52,7 +52,7 @@
 
     case DEV_AARQ_I_39P:
       if (mpSerial[ibPort] == SER_GOODCHECK) {
-        if (!ValidateFrame_Profile()) {}
+        if (!ValidateFrame_Profile()) {
           Error39(220+0);
           ErrorProfile();
         } else {
