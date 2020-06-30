d85abaec28c802f058df5671a89e76eac2d5b1e4
9by1q4wjhrcld2r4 <9by1q4wjhrcld2r4@gmail.com>
2020-06-30 08:02:19 +0300

    fix

--- ek-tm4c1294xl/project1/self/devices/case_39p3.h
+++ ek-tm4c1294xl/project1/self/devices/case_39p3.h
@@ -1,14 +1,14 @@
 
 
-    case DEV_TIME_O_39P:
+    case DEV_SCALER_O_39P:
       Clear(); ShowPercent(54);
 
       cbRepeat = MaxRepeats();
-      QueryTime39_Profile();
-      SetCurr(DEV_TIME_I_39P);
+      QueryScaler39_Profile();
+      SetCurr(DEV_SCALER_I_39P);
       break;
 
-    case DEV_TIME_I_39P:
+    case DEV_SCALER_I_39P:
       if (mpSerial[ibPort] == SER_GOODCHECK) {
         if (!ValidateFrame_Profile()) {
           Error39(220+6);
@@ -16,12 +16,12 @@
           if (cbRepeat == 0) ErrorProfile();
           else {
             ErrorLink_RepeatDecrement();
-            QueryTime39_Profile();
-            SetCurr(DEV_TIME_I_39P);
+            QueryScaler39_Profile();
+            SetCurr(DEV_SCALER_I_39P);
           }
         } else {
-          ReadTime39_Profile();
-          MakePause(DEV_RR_TIME_O_39P);
+          ReadScaler39_Profile();
+          MakePause(DEV_RR_SCALER_O_39P);
         }
       } else {
         Error39(220+7);
@@ -29,22 +29,22 @@
         if (cbRepeat == 0) ErrorProfile();
         else {
           ErrorLink_RepeatDecrement();
-          QueryTime39_Profile();
-          SetCurr(DEV_TIME_I_39P);
+          QueryScaler39_Profile();
+          SetCurr(DEV_SCALER_I_39P);
         }
       }
       break;
 
 
-    case DEV_RR_TIME_O_39P:
+    case DEV_RR_SCALER_O_39P:
       Clear(); ShowPercent(55);
 
       cbRepeat = MaxRepeats();
       Query39_RR_Profile();
-      SetCurr(DEV_RR_TIME_I_39P);
+      SetCurr(DEV_RR_SCALER_I_39P);
       break;
 
-    case DEV_RR_TIME_I_39P:
+    case DEV_RR_SCALER_I_39P:
       if (mpSerial[ibPort] == SER_GOODCHECK) {
         if (!ValidateFrame_Profile()) {
           Error39(220+0);
@@ -53,7 +53,7 @@
           else {
             ErrorLink_RepeatDecrement();
             Query39_RR_Profile();
-            SetCurr(DEV_RR_TIME_I_39P);
+            SetCurr(DEV_RR_SCALER_I_39P);
           }
         } else {
           MakePause(DEV_INIT_39P);
@@ -65,7 +65,7 @@
         else {
           ErrorLink_RepeatDecrement();
           Query39_RR_Profile();
-          SetCurr(DEV_RR_TIME_I_39P);
+          SetCurr(DEV_RR_SCALER_I_39P);
         }
       }
       break;
