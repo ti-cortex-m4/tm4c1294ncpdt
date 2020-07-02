0f3b17a3a86439f42699cb7bf47a4bbb0e4720c9
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-07-02 22:29:31 +0300

    Update buffer_record_39.c

--- ek-tm4c1294xl/project1/self/sensors/sensor39/buffer_record_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/buffer_record_39.c
@@ -90,6 +90,12 @@ static record39 Fault(uchar  bError)
 }
 
 
+static record39 Fault_(uchar  bError, uint  wData)
+{
+  return GetRecordError39(Error39_(bError, wData));
+}
+
+
 
 record39 FinishRecord39(void) {
   record39 r = GetRecordError39(0);
@@ -97,11 +103,13 @@ record39 FinishRecord39(void) {
 
   InitPop39();
 
-  if (GetPopCapacity39() < 2)
-    return Fault(140+1);
+  uint wCapacity1 = GetPopCapacity39();
+  if (wCapacity1 < 2)
+    return Fault_(140+1, wCapacity1);
 
-  if (PopChar39() != 1) // array
-    return Fault(140+2);
+  uchar bTypeArray = PopChar39();
+  if (bTypeArray != 1) // array
+    return Fault_(140+2, bTypeArray);
 
   uchar bCount = PopChar39();
 
@@ -109,70 +117,50 @@ record39 FinishRecord39(void) {
   MonitorString("\n count="); MonitorCharDec(bCount); MonitorString("\n");
 #endif
 
-  InitProfile39();
-
   uchar i;
   for (i=0; i<bCount; i++)
   {
-    if (GetPopCapacity39() < 2 + 2+12 + 1+8)
-      return Fault(140+3);
+    MonitorString("\n idx "); MonitorCharDec(i); MonitorString("\n");
 
-    if (PopChar39() != 0x02) // structure
-      return Fault(140+4);
+    uint wCapacity2 = GetPopCapacity39();
+    if (wCapacity2 < 2 + 2+12 + 1+8)
+      return Fault_(140+3, wCapacity2);
 
-    if (PopChar39() != 2) // structure size
-      return Fault(140+5);
+    uchar bTypeStructure = PopChar39();
+    if (bTypeStructure != 0x02) // structure
+      return Fault_(140+4, bTypeStructure);
 
-    if (PopChar39() != 0x09) // string
-      return Fault(140+6);
+    uchar bSizeStructure = PopChar39();
+    if (bSizeStructure != 2) // structure size
+      return Fault_(140+5, bSizeStructure);
 
-    if (PopChar39() != 12) // string size
-      return Fault(140+7);
+    uchar bTypeString = PopChar39();
+    if (bTypeString != 0x09) // string
+      return Fault_(140+6, bTypeString);
+
+    uchar bSizeString = PopChar39();
+    if (bSizeString != 12) // string size
+      return Fault_(140+7, bSizeString);
 
     time tm = PopTimeDate39();
+    MonitorString("\n date "); MonitorTime(tm); MonitorString("\n");
 
-    if (PopChar39() != 0x15) // unsigned long 64
-      return Fault(140+8);
+    uchar bTypeLong64 = PopChar39();
+    if (bTypeLong64 != 0x15) // unsigned long 64
+      return Fault_(140+8, bTypeLong64); // ???
 
-    uint64_t ddw = PopLongLong39();
+    uint64_t ddwValue = PopLongLong39();
+    MonitorString("\n value "); MonitorLong64Hex(ddwValue); MonitorString("\n");
 
     if (r.fFirst == false) {
       r.bError = 0;
-      r.ddwValue = ddw;
+      r.ddwValue = ddwValue;
       r.tmValue = tm;
       r.fFirst = true;
-
-      FirstPrev = FirstCurr;
-
-      FirstCurr.fExists = true;
-      FirstCurr.tmTime = tm;
-      FirstCurr.ddwValue = ddw;
     }
-
-#if BUFFER_RECORD_39
-    MonitorString("\n");
-    MonitorTime(tm);
-    MonitorLongDec(ddw / 1000000);
-    MonitorLongDec(ddw % 1000000);
-#endif
-    AddProfile39(tm, ddw);
   }
 
-  if (FirstPrev.fExists == true) {
-#if BUFFER_RECORD_39
-    MonitorString("\n");
-    MonitorTime(FirstPrev.tmTime);
-    MonitorLongDec(FirstPrev.ddwValue / 1000000);
-    MonitorLongDec(FirstPrev.ddwValue % 1000000);
-    MonitorString(" previous");
-#endif    
-    AddProfile39(FirstPrev.tmTime, FirstPrev.ddwValue);
-  }
-
-  if (IsProfileOveflow39()) 
-    return Fault(140+9);
-  else  
-    return r;
+  return r;
 }
 
 
