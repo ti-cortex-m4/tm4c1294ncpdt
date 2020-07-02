c9233fd5b3a067062171d2da38b5e1fe5bcbffc2
hgzqfrqnntfvennv <hgzqfrqnntfvennv@gmail.com>
2020-07-02 09:04:21 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/dlms_read_register.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/dlms_read_register.c
@@ -21,6 +21,13 @@ Blue Book: 4.3.2 Register (class_id = 3, version = 0)
 
 
 
+static double2 Fault(uchar  bError)
+{
+  return GetDouble2Error1(Error39(bError));
+}
+
+
+
 double2 ReadRegisterScalerDLMS(void)
 {
   InitPop(12 + GetHdlcAddressesSize());
@@ -30,17 +37,17 @@ double2 ReadRegisterScalerDLMS(void)
   MonitorString("\n DataAccessResult="); MonitorCharDec(bDataAccessResult);
 #endif
   if (bDataAccessResult != 0) {
-    return GetDouble2Error1(Error39_(20+0, bDataAccessResult));
+    return GetDouble2Error1(Error39(20+0, bDataAccessResult));
   }
 
-  if (PopChar() != 2) return GetDouble2Error1(Error39(20+1)); // !structure
-  if (PopChar() != 2) return GetDouble2Error1(Error39(20+2)); // structure size != 1
+  if (PopChar() != 2) return Fault(20+1); // !structure
+  if (PopChar() != 2) return Fault(20+2); // structure size != 1
 
   slong64_ scaler = PopSignedValueDLSM();
-  if (!scaler.fValid) return GetDouble2Error1(Error39(20+3));
+  if (!scaler.fValid) return Fault(20+3);
 
   ulong64_ unit = PopUnsignedValueDLSM();
-  if (!unit.fValid) return GetDouble2Error1(Error39(20+4));
+  if (!unit.fValid) return Fault(20+4);
 
   double dbScaler = pow(10, scaler.ddwValue);
 #ifdef MONITOR_39
@@ -59,15 +66,15 @@ double2 ReadRegisterValue39(const obis_t  obis, caller39*  pc)
   (*pc).bNS++;
   (*pc).bInvokeId++;
   QueryGetRegisterValueDLMS(obis, (*pc));
-  if (Input39() != SER_GOODCHECK) return GetDouble2Error1(Error39(20+5));
-  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return GetDouble2Error1(Error39(20+6));
+  if (Input39() != SER_GOODCHECK) return Fault(20+5);
+  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return Fault(20+6);
   ulong64_ ddw2 = ReadUnsignedValueDLSM();
-  if (!ddw2.fValid) return GetDouble2Error1(Error39(20+7));
+  if (!ddw2.fValid) return Fault(20+7);
 
   (*pc).bNR++;
   Query39_RR((*pc).bNR);
-  if (Input39() != SER_GOODCHECK) return GetDouble2Error1(Error39(20+8));
-  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return GetDouble2Error1(Error39(20+9));
+  if (Input39() != SER_GOODCHECK) return Fault(20+8);
+  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return Fault(20+9);
 
   return GetDouble0(ddw2.ddwValue);
 }
@@ -78,15 +85,15 @@ double2 ReadRegisterScaler39(const obis_t  obis, caller39*  pc)
   (*pc).bNS++;
   (*pc).bInvokeId++;
   QueryGetRegisterScalerDLMS(obis, (*pc));
-  if (Input39() != SER_GOODCHECK) return GetDouble2Error1(Error39(20+10));
-  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return GetDouble2Error1(Error39(20+11));
+  if (Input39() != SER_GOODCHECK) return Fault(20+10);
+  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return Fault(20+11);
   double2 scaler = ReadRegisterScalerDLMS();
-  if (!scaler.fValid) return GetDouble2Error1(Error39(20+12));
+  if (!scaler.fValid) return Fault(20+12);
 
   (*pc).bNR++;
   Query39_RR((*pc).bNR);
-  if (Input39() != SER_GOODCHECK) return GetDouble2Error1(Error39(20+13));
-  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return GetDouble2Error1(Error39(20+14));
+  if (Input39() != SER_GOODCHECK) return Fault(20+13);
+  if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return Fault(20+14);
 
   return GetDouble0(scaler.dbValue);
 }
@@ -96,10 +103,10 @@ double2 ReadRegisterScaler39(const obis_t  obis, caller39*  pc)
 double2 ReadRegisterValueWithScaler39(const obis_t  obis, caller39*  pc)
 {
   double2 value = ReadRegisterValue39(obis, pc);
-  if (!value.fValid) return GetDouble2Error1(Error39(20+15));
+  if (!value.fValid) return Fault(20+15);
 
   double2 scaler = ReadRegisterScaler39(obis, pc);
-  if (!scaler.fValid) return GetDouble2Error1(Error39(20+16));
+  if (!scaler.fValid) return Fault(20+16);
 
   return GetDouble0(value.dbValue * scaler.dbValue);
 }
