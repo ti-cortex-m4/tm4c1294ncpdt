3e2e7734521f8cd08a949e02b32e26ef64e5cb99
s7dy1jmtxd9loas6 <s7dy1jmtxd9loas6@gmail.com>
2020-09-19 19:27:35 +0300

    fix

--- ek-tm4c1294xl/project1/self/keyboard/sensors/key_ignore_status38.c
+++ ek-tm4c1294xl/project1/self/keyboard/sensors/key_ignore_status38.c
@@ -13,7 +13,7 @@ key_ignore_status38.c
 //                                         0123456789ABCDEF
 static char const       szMessage1[]    = "  Игнорировать  ",
                         szMessage2[]    = " статус ответа  ",
-                        szMessage3[]    = "   для СЕ318    ";
+                        szMessage3[]    = "  для СЕ318 ?   ";
 
 static char const       *pszMessages[]  = { szMessage1, szMessage2, szMessage3, "" };
 
