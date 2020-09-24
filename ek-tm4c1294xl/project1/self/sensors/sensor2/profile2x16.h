bfead38546e0f93737f8149469b5d45acedc697d
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-09-24 08:49:58 +0300

    create

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor2/profile2x16.h
@@ -0,0 +1,9 @@
+/*------------------------------------------------------------------------------
+DEVICE_B2!H
+
+
+------------------------------------------------------------------------------*/
+
+void    ReadTopBNew(void);
+void    QueryHeaderBNew(void);
+bool    ReadHeaderBNew(uchar  ibBlock, bool  fDelay);
