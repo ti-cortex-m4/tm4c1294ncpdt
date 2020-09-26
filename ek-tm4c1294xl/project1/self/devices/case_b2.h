5afa087e9875aabfab1e822099148011666f4561
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2020-09-26 14:00:19 +0300

    Revert "fix"
    This reverts commit 8fcbedfb4dd4aa82e61b5129aebab820389a4194.

--- ek-tm4c1294xl/project1/self/devices/case_b2.h
+++ ek-tm4c1294xl/project1/self/devices/case_b2.h
@@ -195,14 +195,9 @@
 
       if (bShortProfileB == PROFILE2X16_16)
       {
-        MakePause(DEV_PREVTOP_B2x16);
-      }
-      else if (bShortProfileB == PROFILE2X16_AUTO)
-      {
-        wBaseCurr = 0;
-
-        QueryHeaderBx16();
-        SetCurr(DEV_AUTOMATIC_B2x16);
+        cbRepeat = MaxRepeats();
+        QueryTopBx16();
+        SetCurr(DEV_TOP_B2x16);
       }
       else
       {
@@ -433,29 +428,6 @@
 
 
     // Меркурий-230 блоками по 16 получасов
-    case DEV_AUTOMATIC_B2x16:
-      if (mpSerial[ibPort] == SER_GOODCHECK)
-      {
-        ShowLo(szProfile2x16); DelayMsg(); // DelayInf
-
-        MakePause(DEV_PREVTOP_B2x16);
-      }
-      else
-      {
-        ShowLo(szProfile2x1); DelayMsg(); // DelayInf
-
-        cbRepeat = MaxRepeats();
-        QueryTopB();
-        SetCurr(DEV_TOP_B2);
-      }
-      break;
-
-    case DEV_PREVTOP_B2x16:
-      cbRepeat = MaxRepeats();
-      QueryTopBx16();
-      SetCurr(DEV_TOP_B2x16);
-      break;
-
     case DEV_TOP_B2x16:
       if (mpSerial[ibPort] == SER_GOODCHECK)
       {
--- ek-tm4c1294xl/project1/self/sensors/sensor2/profile2x16.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor2/profile2x16.c
@@ -36,7 +36,7 @@ PROFILE2X16!C
 
 
 //                                         0123456789ABCDEF
-char const              szProfile2x1[]  = " 1 получас      ",
+static char const       szProfile2x1[]  = " 1 получас      ",
                         szProfile2x16[] = " 16 получасов   ";
 
 
