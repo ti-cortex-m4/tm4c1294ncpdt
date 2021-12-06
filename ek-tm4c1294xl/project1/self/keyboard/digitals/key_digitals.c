28a5728041814446d295a32ca856719288a106ba
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2021-12-06 19:07:11 +0300

    Update key_digitals.c

--- ek-tm4c1294xl/project1/self/keyboard/digitals/key_digitals.c
+++ ek-tm4c1294xl/project1/self/keyboard/digitals/key_digitals.c
@@ -69,6 +69,7 @@ static void MakeInDelays(uchar  ibPort, uchar  ibPhone, uchar  bDevice)
       case 36:
       case 37:
       case 38:
+      case 39:
         mpwMajorInDelay[ibPort] = (uint)(wFREQUENCY_T0*0.4);
         mpwMinorInDelay[ibPort] = (uint)(wFREQUENCY_T0*0.4); break;
 
