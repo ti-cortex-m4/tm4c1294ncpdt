473d7760115e5b8ea34d4bb536802e1e401142fe
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2021-12-06 19:30:11 +0300

    39

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/profile39.h
@@ -0,0 +1,19 @@
+/*------------------------------------------------------------------------------
+profile38.h
+
+
+------------------------------------------------------------------------------*/
+
+extern  time                    tiValue38;
+extern  ulong                   dwValue38;
+
+extern  uint                    cwShutdown38;
+
+
+
+void    InitHeader38(void);
+void    QueryHeader38(void);
+void    QueryProfile38(uint  iw30MinRelStart, uint  iw30MinRelEnd);
+bool    ReadData38(void);
+
+void    RunProfile38(void);
