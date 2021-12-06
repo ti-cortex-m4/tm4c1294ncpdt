ade07d233e0e1145412283ce93163327b13077c0
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2021-12-06 21:52:00 +0300

    profiles 39

--- ek-tm4c1294xl/project1/self/devices/devices_header.h
+++ ek-tm4c1294xl/project1/self/devices/devices_header.h
@@ -79,6 +79,7 @@ DEVICES.C
 #include "../sensors/sensor38/device38.h"
 #include "../sensors/sensor38/current38.h"
 #include "../sensors/sensor38/profile38.h"
+#include "../sensors/sensor38/profile39.h"
 #include "../sensors/sensor38/auth38.h"
 #include "../sensors/sensor38/time38.h"
 #include "../serial/ports.h"
