616b67dbfd68922697167f43d524d05aedf78b90
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2021-12-07 17:36:12 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor38/profile39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor38/profile39.c
@@ -241,12 +241,7 @@ bool    ReadData39(void)
     MonitorString(" #"); MonitorCharDec(mpPrf38[h].bStatus); MonitorString(" ");
 #endif
 
-//    if ((difference == true) && (mpPrf38[h].bStatus == 2))
-      tiDig = tiVirtual;
-//    else if ((mpPrf38[h].tiTime.bMinute % 30 != 0) && (mpPrf38[h].bStatus == 0))
-//      tiDig = tiVirtual;
-//    else
-//      tiDig = mpPrf38[h].tiTime;
+    tiDig = tiVirtual;
 
     if (ReadBlock39(h) == false) return false;
   }
