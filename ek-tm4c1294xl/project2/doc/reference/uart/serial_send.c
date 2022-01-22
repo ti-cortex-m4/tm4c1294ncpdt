d514df8c9eab112ee7c0058064f6dbdbdfe946bd
Aliaksandr <liakh.aliaksandr@gmail.com>
2022-01-22 14:02:43 +0300

    send

--- ek-tm4c1294xl/project2/doc/reference/uart/serial_send.c
+++ ek-tm4c1294xl/project2/doc/reference/uart/serial_send.c
@@ -1,3 +1,4 @@
+#if 1
 /*------------------------------------------------------------------------------
 serial_send.c
 
@@ -17,7 +18,7 @@ serial_send.c
 //*****************************************************************************
 //! Checks the availability of the serial port output buffer.
 //!
-//! \param ucPort is the UART port number to be accessed.
+//! \param ulPort is the UART port number to be accessed.
 //!
 //! This function checks to see if there is room on the UART transmit buffer
 //! for additional data.
@@ -25,21 +26,20 @@ serial_send.c
 //! \return Returns the number of bytes available in the serial transmit ring
 //! buffer.
 //*****************************************************************************
-bool SerialSendFull(uint8_t ucPort)
+bool
+SerialSendFull(uint32_t ulPort)
 {
     // Check the arguments.
-    ASSERT(ucPort < UART_COUNT);
+    ASSERT(ulPort < MAX_S2E_PORTS);
 
     // Return the number of bytes available in the tx ring buffer.
-    return(RingBufFull(&g_sTxBuf[ucPort]));
+    return(RingBufFull(&g_sTxBuf[ulPort]));
 }
-
-
 
 //*****************************************************************************
 //! Sends a character to the UART.
 //!
-//! \param ucPort is the UART port number to be accessed.
+//! \param ulPort is the UART port number to be accessed.
 //! \param ucChar is the character to be sent.
 //!
 //! This function sends a character to the UART.  The character will either be
@@ -48,35 +48,35 @@ bool SerialSendFull(uint8_t ucPort)
 //!
 //! \return None.
 //*****************************************************************************
-void SerialSend(uint8_t ucPort, uint8_t ucChar)
+void
+SerialSend(uint32_t ulPort, uint8_t ucChar)
 {
     // Check the arguments.
-    ASSERT(ucPort < UART_COUNT);
+    ASSERT(ulPort < MAX_S2E_PORTS);
 
     // Disable the UART transmit interrupt while determining how to handle this
     // character.  Failure to do so could result in the loss of this character,
     // or stalled output due to this character being placed into the UART
     // transmit buffer but never transferred out into the UART FIFO.
-    UARTIntDisable(g_ulUARTBase[ucPort], UART_INT_TX);
+    UARTIntDisable(g_ulUARTBase[ulPort], UART_INT_TX);
 
-#if 0
     // See if the transmit buffer is empty and there is space in the FIFO.
-    if(RingBufEmpty(&g_sTxBuf[ucPort]) && (UARTSpaceAvail(g_ulUARTBase[ucPort])))
+    if(RingBufEmpty(&g_sTxBuf[ulPort]) &&
+       (UARTSpaceAvail(g_ulUARTBase[ulPort])))
     {
         // Write this character directly into the FIFO.
-        UARTCharPut(g_ulUARTBase[ucPort], ucChar);
+        UARTCharPut(g_ulUARTBase[ulPort], ucChar);
     }
 
     // See if there is room in the transmit buffer.
-    else
-#endif
-    if(!RingBufFull(&g_sTxBuf[ucPort]))
+    else if(!RingBufFull(&g_sTxBuf[ulPort]))
     {
         // Put this character into the transmit buffer.
-        RingBufWriteOne(&g_sTxBuf[ucPort], ucChar);
+        RingBufWriteOne(&g_sTxBuf[ulPort], ucChar);
     }
 
     // Enable the UART transmit interrupt.
-    UARTIntEnable(g_ulUARTBase[ucPort], UART_INT_TX);
-    IntPendSet(g_ulIntUART[ucPort]);
+    UARTIntEnable(g_ulUARTBase[ulPort], UART_INT_TX);
 }
+
+#endif
