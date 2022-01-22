4a532aa4a9436b43282fad070aec1930dade62f6
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2022-01-22 13:47:22 +0300

    send & poll

--- ek-tm4c1294xl/project2/doc/reference/tcp/telnet_poll.c
+++ ek-tm4c1294xl/project2/doc/reference/tcp/telnet_poll.c
@@ -1,3 +1,4 @@
+#if 1
 /*------------------------------------------------------------------------------
 telnet_poll.c
 
@@ -27,62 +28,51 @@ telnet_poll.c
 //!
 //! \return This function will return an lwIP defined error code.
 //*****************************************************************************
-err_t TelnetPoll(void *arg, struct tcp_pcb *pcb)
+static err_t
+TelnetPoll(void *arg, struct tcp_pcb *pcb)
 {
-    tState *pState = arg;
+    err_t eError;
+    struct ip_addr sIPAddr;
+    tTelnetSessionData *pState = arg;
 
-    CONSOLE("%u: poll 0x%08x, 0x%08x %u/%u\n", pState->ucSerialPort, arg, pcb, pState->ulConnectionTimeout, pState->ulMaxTimeout);
+    DEBUG_MSG("TelnetPoll 0x%08x, 0x%08x\n", arg, pcb);
 
     // Are we operating as a server or a client?
     if(!pState->pListenPCB)
     {
-        // We are operating as a client.  Are we currently trying to reconnect to the server?
+        // We are operating as a client.  Are we currently trying to reconnect
+        // to the server?
         if(pState->eTCPState == STATE_TCP_CONNECTING)
         {
-            if (pcb->state != CLOSED)
-            {
-              CONSOLE("%u: poll state=%d\n", pState->ucSerialPort, pcb->state);
-            }
-            else
-            {
-                // We are trying to reconnect but can't have received the connection
-                // callback in the last 3 seconds so we try connecting again.
-                pState->ucReconnectCount++;
+            // We are trying to reconnect but can't have received the connection
+            // callback in the last 3 seconds so we try connecting again.
+            pState->ucReconnectCount++;
+            sIPAddr.addr = htonl(pState->ulTelnetRemoteIP);
+            eError = tcp_connect(pcb, &sIPAddr, pState->usTelnetRemotePort,
+                                 TelnetConnected);
 
-                struct ip4_addr sIPAddr;
-                sIPAddr.addr = htonl(pState->ulTelnetRemoteIP);
-
-                err_t err = tcp_connect(pcb, &sIPAddr, pState->usTelnetRemotePort, TelnetConnected);
-                if(err != ERR_OK)
-                {
-                    ERROR("%u: poll.tcp_connect failed, error=%d\n", pState->ucSerialPort, err);
-                    ErrorTCPOperation(pState->ucSerialPort, err, TCP_CONNECT_POLL);
-                }
-            }
-        }
-        else if(pState->eTCPState == STATE_TCP_CONNECTED)
-        {
-            pState->ulConnectionTimeout++;
-            if ((pState->ulMaxTimeout != 0) && (pState->ulConnectionTimeout > pState->ulMaxTimeout))
+            if(eError != ERR_OK)
             {
-               ModemDisconnectedByTimeout(pState->ucSerialPort);
-
-               CONSOLE("%u: poll - close client connection by timeout\n", pState->ucSerialPort);
-               return TelnetCloseClient(pState->ucSerialPort);
+                // Remember the error for later.
+                pState->eLastErr = eError;
             }
         }
     }
     else
     {
-        // We are operating as a server. Increment the timeout value and close the telnet connection if the configured timeout has been exceeded.
+        // We are operating as a server. Increment the timeout value and close
+        // the telnet connection if the configured timeout has been exceeded.
         pState->ulConnectionTimeout++;
-        if ((pState->ulMaxTimeout != 0) && (pState->ulConnectionTimeout > pState->ulMaxTimeout))
+        if((pState->ulMaxTimeout != 0) &&
+           (pState->ulConnectionTimeout > pState->ulMaxTimeout))
         {
-            CONSOLE("%u: poll - close server connection by timeout\n", pState->ucSerialPort);
-            TelnetCloseServer(pcb, pState->ucSerialPort);
-            StartServerConnection(pState->ucSerialPort);
+            // Close the telnet connection.
+            tcp_abort(pcb);
         }
     }
 
+    // Return OK.
     return(ERR_OK);
 }
+
+#endif
