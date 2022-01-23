188ea4294df0524d991ca3f452d5fdf2b83df2c2
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-01-23 12:44:13 +0300

    fix ACCEPT & CLOSE

--- ek-tm4c1294xl/project2/self/tcp/telnet_close.c
+++ ek-tm4c1294xl/project2/self/tcp/telnet_close.c
@@ -91,8 +91,8 @@ void TelnetCloseServer(struct tcp_pcb *pcb, uint8_t ucSerialPort)
 
     // Reset the session data for this port.
     pState->pConnectPCB = NULL;
-//    pState->pListenPCB = NULL; TODO ???
-//    pState->eTCPState = STATE_TCP_IDLE; TODO ???
+    pState->pListenPCB = NULL;
+    pState->eTCPState = STATE_TCP_IDLE;
     pState->ulConnectionTimeout = 0;
     pState->ulMaxTimeout = 0;
     pState->ucSerialPort = ucSerialPort;
