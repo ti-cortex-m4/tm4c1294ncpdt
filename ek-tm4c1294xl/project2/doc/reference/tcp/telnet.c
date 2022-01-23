2e9fd94622ff54d55150d239ab2c23091b25b3ec
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2022-01-23 13:05:14 +0300

    Update telnet.c

--- ek-tm4c1294xl/project2/doc/reference/tcp/telnet.c
+++ ek-tm4c1294xl/project2/doc/reference/tcp/telnet.c
@@ -1,3 +1,4 @@
+#if 0
 /*------------------------------------------------------------------------------
 TELNET.C
 
@@ -146,3 +147,4 @@ void TelnetNotifyLinkStatus(bool bLinkStatusUp)
         }
     }
 }
+#endif
