a8930322926c9023ef240957f94e83f1eebd9268
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-01-26 20:07:31 +0300

    Update entities_debug.c

--- ek-tm4c1294xl/project2/self/kernel/entities_debug.c
+++ ek-tm4c1294xl/project2/self/kernel/entities_debug.c
@@ -97,14 +97,14 @@ const entity enLED1Mode = {
 
 const entity enPowerUpResetCount = {
   EEPROM_POWER_UP_RESET_COUNT, &cwPowerUpResetCount, sizeof(uint), INT, 0,
-  0, 0x10000, 0,
+  0, 0xFFFF, 0,
   "",
   "",
 };
 
 const entity enWatchdogResetCount = {
   EEPROM_WATCHDOG_RESET_COUNT, &cwWatchdogResetCount, sizeof(uint), INT, 0,
-  0, 0x10000, 0,
+  0, 0xFFFF, 0,
   "",
   "",
 };
