f46d5054a79892c344accb6318777e2e74cdda73
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2022-01-29 18:11:00 +0300

    rename

---
+++ ek-tm4c1294xl/project2/self/hardware/internal_temperature.c
@@ -0,0 +1,99 @@
+/*------------------------------------------------------------------------------
+internal_temperature.c
+
+
+------------------------------------------------------------------------------*/
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/adc.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+
+
+
+int32_t GetInternalTemperature(void)
+{
+    //
+    // This array is used for storing the data read from the ADC FIFO. It
+    // must be as large as the FIFO for the sequencer in use.  This example
+    // uses sequence 3 which has a FIFO depth of 1.  If another sequence
+    // was used with a deeper FIFO, then the array size must be changed.
+    //
+    uint32_t pui32ADC0Value[1];
+
+    //
+    // These variables are used to store the temperature conversions for
+    // Celsius and Fahrenheit.
+    //
+    int32_t ui32TempValueC;
+
+    //
+    // The ADC0 peripheral must be enabled for use.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+
+    //
+    // Enable sample sequence 3 with a processor signal trigger.  Sequence 3
+    // will do a single sample when the processor sends a singal to start the
+    // conversion.  Each ADC module has 4 programmable sequences, sequence 0
+    // to sequence 3.  This example is arbitrarily using sequence 3.
+    //
+    ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
+
+    //
+    // Configure step 0 on sequence 3.  Sample the temperature sensor
+    // (ADC_CTL_TS) and configure the interrupt flag (ADC_CTL_IE) to be set
+    // when the sample is done.  Tell the ADC logic that this is the last
+    // conversion on sequence 3 (ADC_CTL_END).  Sequence 3 has only one
+    // programmable step.  Sequence 1 and 2 have 4 steps, and sequence 0 has
+    // 8 programmable steps.  Since we are only doing a single conversion using
+    // sequence 3 we will only configure step 0.  For more information on the
+    // ADC sequences and steps, reference the datasheet.
+    //
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_TS | ADC_CTL_IE |
+                             ADC_CTL_END);
+
+    //
+    // Since sample sequence 3 is now configured, it must be enabled.
+    //
+    ADCSequenceEnable(ADC0_BASE, 3);
+
+    //
+    // Clear the interrupt status flag.  This is done to make sure the
+    // interrupt flag is cleared before we sample.
+    //
+    ADCIntClear(ADC0_BASE, 3);
+
+        //
+        // Trigger the ADC conversion.
+        //
+        ADCProcessorTrigger(ADC0_BASE, 3);
+
+        //
+        // Wait for conversion to be completed.
+        //
+        while(!ADCIntStatus(ADC0_BASE, 3, false))
+        {
+        }
+
+        //
+        // Clear the ADC interrupt flag.
+        //
+        ADCIntClear(ADC0_BASE, 3);
+
+        //
+        // Read ADC Value.
+        //
+        ADCSequenceDataGet(ADC0_BASE, 3, pui32ADC0Value);
+
+        //
+        // Use non-calibrated conversion provided in the data sheet.  Make
+        // sure you divide last to avoid dropout.
+        //
+        ui32TempValueC = 147.5 - ((75 * 3.3 * pui32ADC0Value[0]) / 4096);
+
+        return ui32TempValueC;
+}
