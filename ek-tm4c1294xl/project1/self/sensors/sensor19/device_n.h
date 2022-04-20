e90b6421e3307090ca9580cc8a3c982ff7c0f232
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-04-20 18:59:04 +0300

    add #19

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor19/device_n.h
@@ -0,0 +1,18 @@
+/*------------------------------------------------------------------------------
+DEVICE_N.H
+
+
+------------------------------------------------------------------------------*/
+
+#include        "main.h"
+
+
+
+#ifndef SKIP_N
+
+void    QueryEnergyAbsN(void);
+void    ReadEnergyN(void);
+void    ReadCurrentN(void);
+
+#endif
+
