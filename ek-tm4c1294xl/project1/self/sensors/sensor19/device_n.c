49bf9d79fa5d0aa6a11247f2870df80a5711bbca
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2022-04-20 18:59:04 +0300

    add #19

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor19/device_n.c
@@ -0,0 +1,69 @@
+/*------------------------------------------------------------------------------
+DEVICE_N.C
+
+
+------------------------------------------------------------------------------*/
+
+#include        "main.h"
+#include        "xdata.h"
+#include        "ports.h"
+#include        "engine.h"
+#include        "energy.h"
+#include        "digitals.h"
+
+
+
+#ifndef SKIP_N
+
+void    QueryEnergyAbsN(void)
+{
+  InitPush();
+  PushChar(0);   
+  PushChar(diCurr.bAddress);
+
+  PushChar(1);
+
+  PushChar(0);
+  PushChar(0);
+  PushChar(0);
+  PushChar(0);
+  PushChar(0);
+  PushChar(0);
+  PushChar(0);
+  PushChar(0);
+
+  QueryIO(5+8, 5+8);
+}
+
+
+
+void    ReadEnergyN(void)
+{
+  InitPop(3);  
+
+  coEnergy.mpbBuff[0] = PopChar();
+  coEnergy.mpbBuff[1] = PopChar();
+  coEnergy.mpbBuff[2] = PopChar();
+  coEnergy.mpbBuff[3] = PopChar();
+
+  dwBuffC = coEnergy.dwBuff;
+  SetCanLong(mpdwChannelsA, 0);
+
+  mpboChannelsA[0] = boTrue;
+}
+
+
+
+void    ReadCurrentN(void)
+{
+  ReadEnergyN();
+
+  dwBuffC = *PGetCanLong(mpdwChannelsA, 0);
+  SetCanLong(mpdwBaseDig, 0);
+
+  MakeCurrent();
+}
+
+#endif
+
+
