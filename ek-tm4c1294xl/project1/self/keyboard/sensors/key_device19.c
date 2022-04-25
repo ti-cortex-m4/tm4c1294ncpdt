83b4cbeb72a6a519003ed2f8e62a5569657df244
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-04-25 10:35:56 +0300

    Update key_device19.c

--- ek-tm4c1294xl/project1/self/keyboard/sensors/key_device19.c
+++ ek-tm4c1294xl/project1/self/keyboard/sensors/key_device19.c
@@ -5,19 +5,23 @@ KEY_DEVICE19.C
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
+#include "../../memory/mem_factors.h"
 #include "../../console.h"
 #include "../../sensors/sensor19/automatic19.h"
+#include "../../digitals/digitals.h"
+#include "../../digitals/digitals_messages.h"
+#include "key_device19.h"
 
 
 
 //                                         0123456789ABCDEF
-static char const       szDevicesInt[]  = "Импульсы спец.  ",
-                        szDevicesReal[] = "Счетчики спец.  ",
+static char const       szDevicesInt[]  = "Импульсы ПИ-1   ",
+                        szDevicesReal[] = "Счетчики ПИ-1   ",
                         szNoDevices[]   = "      нет       ";
 
 
 
-static void ShowLong(void)
+static void ShowDevice19Long(uchar c)
 {
   if (GetDigitalDevice(c) != 19)
     ShowLo(szNoDevices); 
@@ -28,16 +32,16 @@ static void ShowLong(void)
     LoadCurrDigital(c);
     ibPort = diCurr.ibPort;
 
-    if (mpboEnblCan[c] == boFalse)
-      ShowLo(szBlocking); 
+    if (mpboEnblCan[c] == false)
+      ShowLo(szBlocked);
     else 
     {
-      long2 dw2 = ReadSensorN();
-      (dw2.fValid) ? ShowLong(dw2.dbValue) : Error();
+      ulong2 dw2 = ReadSensorN();
+      (dw2.fValid) ? ShowDevice19Long(dw2.dwValue) : Error();
     }
   }
 
-  sprintf(szLo+14,"%2bu",c+1);
+  sprintf(szLo+14,"%2u",c+1);
 }
 
 
@@ -59,21 +63,21 @@ static uchar c;
       enKeyboard = KBD_POSTENTER;
 
       c = 0;
-      ShowLong();
+      ShowDevice19Long(c);
     }
     else if (enKeyboard == KBD_POSTINPUT1)
     {
       if ((c = GetCharLo(10,11) - 1) < bCANALS)
       {
         enKeyboard = KBD_POSTENTER;
-        ShowLong();
+        ShowDevice19Long(c);
       }
       else Beep();
     }
     else if (enKeyboard == KBD_POSTENTER)
     {
       if (++c >= bCANALS) c = 0;
-      ShowLong();
+      ShowDevice19Long(c);
     }
     else Beep();
   }
@@ -84,7 +88,7 @@ static uchar c;
     if (enKeyboard == KBD_POSTENTER)
     {
       if (c > 0) c--; else c = bCANALS-1;
-      ShowLong();
+      ShowDevice19Long(c);
     }
     else Beep();
   }
@@ -94,7 +98,7 @@ static uchar c;
   {
     if (enKeyboard == KBD_POSTENTER)
     {
-      ShowLong();
+      ShowDevice19Long(c);
     }
     else Beep();
   }
@@ -114,7 +118,7 @@ static uchar c;
 
 
 
-static void ShowDouble(void)
+static void ShowDevice19Double(uchar c)
 {
   if (GetDigitalDevice(c) != 19)
     ShowLo(szNoDevices); 
@@ -125,16 +129,16 @@ static void ShowDouble(void)
     LoadCurrDigital(c);
     ibPort = diCurr.ibPort;
 
-    if (mpboEnblCan[c] == boFalse)
-      ShowLo(szBlocking); 
+    if (mpboEnblCan[c] == false)
+      ShowLo(szBlocked);
     else 
     {
-      long2 dw2 = ReadSensorN();
-      (dw2.fValid) ? ShowDouble((dw2.dwValue * mpdbValueCntHou[c]) + mpdbCount[c]) : Error();
+      ulong2 dw2 = ReadSensorN();
+      (dw2.fValid) ? ShowDevice19Double(c(dw2.dwValue * mpdbValueCntHou[c]) + mpdbCount[c]) : Error();
     }
   }
 
-  sprintf(szLo+14,"%2bu",c+1);
+  sprintf(szLo+14,"%2u",c+1);
 }
 
 
@@ -156,21 +160,21 @@ static uchar c;
       enKeyboard = KBD_POSTENTER;
 
       c = 0;
-      ShowDouble();
+      ShowDevice19Double(c);
     }
     else if (enKeyboard == KBD_POSTINPUT1)
     {
       if ((c = GetCharLo(10,11) - 1) < bCANALS)
       {
         enKeyboard = KBD_POSTENTER;
-        ShowDouble();
+        ShowDevice19Double(c);
       }
       else Beep();
     }
     else if (enKeyboard == KBD_POSTENTER)
     {
       if (++c >= bCANALS) c = 0;
-      ShowDouble();
+      ShowDevice19Double(c);
     }
     else Beep();
   }
@@ -181,7 +185,7 @@ static uchar c;
     if (enKeyboard == KBD_POSTENTER)
     {
       if (c > 0) c--; else c = bCANALS-1;
-      ShowDouble();
+      ShowDevice19Double(c);
     }
     else Beep();
   }
@@ -191,7 +195,7 @@ static uchar c;
   {
     if (enKeyboard == KBD_POSTENTER)
     {
-      ShowDouble();
+      ShowDevice19Double(c);
     }
     else Beep();
   }
