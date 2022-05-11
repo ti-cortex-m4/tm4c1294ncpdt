8554c87a5815ee5bec52d3dfd2bb86b7800e8019
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-05-11 18:55:34 +0300

    fix

---
+++ ek-tm4c1294xl/project2/self/kernel/periodic_reset.h
@@ -0,0 +1,10 @@
+/*------------------------------------------------------------------------------
+periodic_reset.h
+------------------------------------------------------------------------------*/
+
+void PeriodicReset_1Hz(void);
+
+void PeriodicReset_SerialReceiveTCPSend(void);
+void PeriodicReset_TCPReceiveSerialSend(void);
+
+void RunPeriodicReset(void);
