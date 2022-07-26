343e200377715194f7c17aaaa00b02acb7086630
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2022-07-26 19:01:41 +0300

    Update hardware_restart.c

--- ek-tm4c1294xl/project2/self/hardware/hardware_restart.c
+++ ek-tm4c1294xl/project2/self/hardware/hardware_restart.c
@@ -10,8 +10,6 @@ hardware_restart.c
 #include "inc/hw_sysctl.h"
 #include "inc/hw_gpio.h"
 #include "../hardware/delay.h"
-//#include "../kernel/settings.h"
-//#include "../kernel/log.h"
 #include "hardware_restart.h"
 #include "driverlib/gpio.h"
 #include "driverlib/pin_map.h"
@@ -20,12 +18,12 @@ hardware_restart.c
 
 static void SetPL6To0(void)
 {
-  HWREG(GPIO_PORTL_BASE + GPIO_O_DATA + 0x0020) = ~0x0008;
+  HWREG(GPIO_PORTL_BASE + GPIO_O_DATA + 0x0020) = ~0x0040;
 }
 
 static void SetPL6To1(void)
 {
-  HWREG(GPIO_PORTL_BASE + GPIO_O_DATA + 0x0020) = 0x0008;
+  HWREG(GPIO_PORTL_BASE + GPIO_O_DATA + 0x0020) = 0x0040;
 }
 
 
@@ -34,9 +32,9 @@ static void InitPL6(void)
 {
   HWREG(SYSCTL_RCGCGPIO) |= SYSCTL_RCGCGPIO_R10; // GPIO Port L Run Mode Clock Gating Control
   DelayGPIO();
-  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DIR) |= 0x0008; // GPIO Direction
-  HWREG(GPIO_PORTF_AHB_BASE + GPIO_O_DEN) |= 0x0008; // GPIO Digital Enable
-  GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_STRENGTH_8MA, GPIO_PIN_TYPE_STD_WPU);
+  HWREG(GPIO_PORTL_BASE + GPIO_O_DIR) |= 0x0040; // GPIO Direction
+  HWREG(GPIO_PORTL_BASE + GPIO_O_DEN) |= 0x0040; // GPIO Digital Enable
+  GPIOPadConfigSet(GPIO_PORTL_BASE, GPIO_PIN_6, GPIO_STRENGTH_8MA, GPIO_PIN_TYPE_STD_WPU);
 }
 
 
