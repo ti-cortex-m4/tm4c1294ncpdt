298fbd3f9e3236832c75306417b2bf61622432fe
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-07-28 19:34:42 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/dlms_obis.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/dlms_obis.c
@@ -13,7 +13,12 @@ const obis_t        obisId      = {0, 0, 96, 1, 4, 255}; // Ch. 0; Device ID; #5
 
 const obis_t        obisTime    = {0, 0, 1, 0, 0, 255}; // Ch. 0; Clock object; #1
 
-const obis_t        obisEngAbs  = {1, 0, 15, 8, 0, 255}; // Ch. 0; Sum Li Active power (abs(QI+QIV)+abs(QII+QIII)); Time integral 1; Rate 0 (0 is total); Unspecified number of most recent billing periods
+const obis_t        obisEngAbs[4] = {
+  {1, 0, 1, 8, 0, 255}, // Ch. 0; Sum Li Active power+   (QI+QIV);   Time integral 1; Rate 0 (0 is total); Unspecified number of most recent billing periods
+  {1, 0, 2, 8, 0, 255}, // Ch. 0; Sum Li Active power-   (QII+QIII); Time integral 1; Rate 0 (0 is total); Unspecified number of most recent billing periods
+  {1, 0, 3, 8, 0, 255}, // Ch. 0; Sum Li Reactive power+ (QI+QII);   Time integral 1; Rate 0 (0 is total); Unspecified number of most recent billing periods
+  {1, 0, 4, 8, 0, 255}, // Ch. 0; Sum Li Reactive power- (QIII+QIV); Time integral 1; Rate 0 (0 is total); Unspecified number of most recent billing periods
+};
 
 const obis_t        obisEngAbsTariff[4] = {
   {1, 0, 15, 8, 1, 255}, // Ch. 0; Sum Li Active power (abs(QI+QIV)+abs(QII+QIII)); Time integral 1; Rate 1 (0 is total); Unspecified number of most recent billing periods
