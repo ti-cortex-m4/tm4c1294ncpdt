5043eb0e768e6b65f551bdff6333950ffcc7caa2
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-08-03 22:28:01 +0300

    Update include39.h

--- ek-tm4c1294xl/project1/self/sensors/sensor39/include39.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/include39.h
@@ -10,7 +10,7 @@ include39*h
 
 #define MONITOR_39          1
 #define MONITOR_39_NAMES    1
-#define MONITOR_39_MONITOR  1
+//#define MONITOR_39_MONITOR  1
 //#define MONITOR_39_CRC      1
 //#define BUFFER_ARRAY_39     1
 #define BUFFER_RECORD_39    1
