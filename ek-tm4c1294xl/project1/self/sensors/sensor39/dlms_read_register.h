0bf583d1b8a1c15c9efdc1e850918afeae6f99f8
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2022-08-05 00:24:32 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/dlms_read_register.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/dlms_read_register.h
@@ -6,7 +6,7 @@ dlms_read_register.h
 
 double2 ReadRegisterScalerDLMS(void);
 
-double2 ReadRegisterValue39(const obis_t  obis, caller39*  pc);
+double2 ReadRegisterUnsignedValue39(const obis_t  obis, caller39*  pc);
 double2 ReadRegisterScaler39(const obis_t  obis, caller39*  pc);
 
 double2 ReadRegisterValueWithScaler39(const obis_t  obis, caller39*  pc);
