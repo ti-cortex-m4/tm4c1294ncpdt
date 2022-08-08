02f78371d44539af9db8c386365f303d2b5db5e9
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2022-08-08 20:16:47 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_cntmon_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/fragment_cntmon_39.c
@@ -7,6 +7,7 @@ fragment_cntmon_39.c
 #include "../../main.h"
 #include "../../serial/monitor.h"
 #include "../../serial/ports_pop.h"
+#include "../../digitals/digitals.h"
 #include "dlms.h"
 #include "dlms_obis.h"
 #include "error39.h"
@@ -19,7 +20,8 @@ fragment_cntmon_39.c
 #include "dlms_read_register.h"
 #include "fragment_cntmon_39.h"
 
-#include "../../digitals/digitals.h" // TODO
+
+
 
 static double2 Fault(uchar  bError)
 {
@@ -28,37 +30,32 @@ static double2 Fault(uchar  bError)
 
 
 
-double2 FragmentCntMonCan(const obis_t  obisBillingPeriod, const obis_t  obis, caller39  *pc, date  dt)
+double2 FragmentCntMonCan(const obis_t  obisBillingPeriod, const obis_t  obisScaler, caller39  *pc, date  dt)
 {
   (*pc).bNS++;
   (*pc).bInvokeId++;
-  QueryBillingPeriod39(obisBillingPeriod, obis, (*pc).bNS, (*pc).bNR, (*pc).bInvokeId, dt);
+  QueryBillingPeriod39(obisBillingPeriod, (*pc).bNS, (*pc).bNR, (*pc).bInvokeId, dt);
   if (Input39() != SER_GOODCHECK) return Fault(125+0);
   if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return Fault(125+1);
 
 
   bool present = (EngMonPresent39() == 0);
-  bool absent = (EngMonAbsent39() == 0);
 
-  //ulong64_ counter = present ? ReadEngMon39() : GetULong64Error(0);
   ulong64_ counter[4];
-  if (present) {
-      InitPop(12 + 5 + GetHdlcAddressesSize() + 1 + 1 + 12); // skip date/time TODO
-
-      counter[0] = PopUnsignedValueDLSM();
-      PopUnsignedValueDLSM();
-      PopUnsignedValueDLSM();
-      PopUnsignedValueDLSM();
-      PopUnsignedValueDLSM();
-      counter[1] = PopUnsignedValueDLSM();
-      counter[2] = PopUnsignedValueDLSM();
-      counter[3] = PopUnsignedValueDLSM();
+  if (present)
+  {
+    counter[0] = PopUnsignedValueDLSM();
+    PopUnsignedValueDLSM();
+    PopUnsignedValueDLSM();
+    PopUnsignedValueDLSM();
+    PopUnsignedValueDLSM();
+    counter[1] = PopUnsignedValueDLSM();
+    counter[2] = PopUnsignedValueDLSM();
+    counter[3] = PopUnsignedValueDLSM();
   }
 
 #ifdef MONITOR_39
   MonitorString("\n present="); MonitorBool(present);
-  MonitorString("\n absent="); MonitorBool(absent);
-//  MonitorString("\n valid="); MonitorBool(counter.fValid);
   MonitorString("\n counter[0]="); MonitorLongDec(counter[0].ddwValue);
   MonitorString("\n counter[1]="); MonitorLongDec(counter[1].ddwValue);
   MonitorString("\n counter[2]="); MonitorLongDec(counter[2].ddwValue);
@@ -71,24 +68,23 @@ double2 FragmentCntMonCan(const obis_t  obisBillingPeriod, const obis_t  obis, c
   if (Input39() != SER_GOODCHECK) return Fault(125+2);
   if (ValidateFrame((*pc).bNS, (*pc).bNR) != 0) return Fault(125+3);
 
-/*
-  double2 scaler = ReadRegisterScaler39(obis, pc);
+
+  double2 scaler = ReadRegisterScaler39(obisScaler, pc);
   if (!scaler.fValid) return Fault(125+4);
-*/
+
 
   if (present) {
-      mpdbChannelsC[0] = (double)counter[0].ddwValue / 1000;
-      mpdbChannelsC[1] = (double)counter[1].ddwValue / 1000;
-      mpdbChannelsC[2] = (double)counter[2].ddwValue / 1000;
-      mpdbChannelsC[3] = (double)counter[3].ddwValue / 1000;
-//    double db = (double)counter.ddwValue * scaler.dbValue;
+      mpdbChannelsC[0] = (double)counter[0].ddwValue* scaler.dbValue / 1000;
+      mpdbChannelsC[1] = (double)counter[1].ddwValue* scaler.dbValue / 1000;
+      mpdbChannelsC[2] = (double)counter[2].ddwValue* scaler.dbValue / 1000;
+      mpdbChannelsC[3] = (double)counter[3].ddwValue* scaler.dbValue / 1000;
 #ifdef MONITOR_39
     //MonitorString("\n result="); MonitorDouble6(db);
 #endif
     return GetDouble0(-1);
   }
 
-  if (absent) {
+  if (!present) {
 #ifdef MONITOR_39
     MonitorString("\n result is absent");
 #endif
