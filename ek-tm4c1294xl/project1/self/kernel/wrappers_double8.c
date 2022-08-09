880aa555ab12cf719c5f63155768d7baa0facb65
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2022-08-09 21:49:03 +0300

    Update wrappers_double8.c

--- ek-tm4c1294xl/project1/self/kernel/wrappers_double8.c
+++ ek-tm4c1294xl/project1/self/kernel/wrappers_double8.c
@@ -13,8 +13,8 @@ double8 GetDouble8(double  mdbValue[8], bool  fValid)
 {
   double8 db8;
 
-  ASSERT(sizeof(db8.mdbValue) == sizeof(mdbValue));
-  memcpy(&db8.mdbValue, &mdbValue, sizeof(mdbValue));
+  uchar i;
+  for (i=0; i<8; i++) db8.mdbValue[i] = mdbValue[i];
 
   db8.fValid = fValid;
   db8.bError = 0;
@@ -33,7 +33,9 @@ double8 GetDouble8Error(void)
 {
   double8 db8;
 
-  memset(db8.mdbValue, -1, sizeof(db8.mdbValue));
+  uchar i;
+  for (i=0; i<8; i++) db8.mdbValue[i] = -1;
+
   db8.fValid = false;
   db8.bError = 0;
 
@@ -45,7 +47,9 @@ double8 GetDouble8Error1(uchar  bError)
 {
   double8 db8;
 
-  memset(db8.mdbValue, -1, sizeof(db8.mdbValue));
+  uchar i;
+  for (i=0; i<8; i++) db8.mdbValue[i] = -1;
+
   db8.fValid = false;
   db8.bError = bError;
 
