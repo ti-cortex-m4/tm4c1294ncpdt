17815454609966370f915095519e7f47a5622ad1
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-08-11 18:46:29 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/query_id_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/query_id_39.c
@@ -15,53 +15,6 @@ query_id_39.c
 
 
 
-void    QueryId39_Single(uchar  bNS, uchar  bNR, uchar  bInvokeId)
-{
-#ifdef MONITOR_39_NAMES
-  MonitorString("\n\n QueryId39_Single ");
-#endif
-
-  uint wSize = 23 + GetHdlcAddressesSize_Single(); // 0x19 25
-
-  InitPush(0);
-  PushChar(0x7E);
-
-  PushFormatDLMS(wSize);
-  PushHdlcAddresses_Single();
-
-  PushChar((((bNR & 0x07) << 5) | 0x10 | ((bNS & 0x07) << 1) | 0x00));
-
-  PushIntLtl(MakeCRC16X25OutBuff(1, 3+GetHdlcAddressesSize_Single())); // 5
-
-  // DLMS start
-
-  PushChar(0xE6); // LLC
-  PushChar(0xE6);
-  PushChar(0x00);
-
-  PushChar(0xC0); // Get-Request
-  PushChar(0x01); // Get-Request-Normal
-  PushChar(0xC0 | (bInvokeId % 16)); // Invoke-Id-And-Priority
-
-  PushChar(0);
-  PushChar(1); // class
-
-  PushOBIS_DLMS(obisId);
-
-  PushChar(2); // attribute
-  PushChar(0);
-
-  // DLMS finish
-
-  PushIntLtl(MakeCRC16X25OutBuff(1, wSize-2));
-
-  PushChar(0x7E);
-
-  Query39(1000, wSize+2); // 27
-}
-
-
-
 void    QueryId39(uchar  bNS, uchar  bNR, uchar  bInvokeId)
 {
 #ifdef MONITOR_39_NAMES
--- ek-tm4c1294xl/project1/self/sensors/sensor39/query_id_39.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/query_id_39.h
@@ -4,5 +4,4 @@ query_id_39.h
 
 ------------------------------------------------------------------------------*/
 
-void    QueryId39_Single(uchar  bNS, uchar  bNR, uchar  bInvokeId);
 void    QueryId39(uchar  bNS, uchar  bNR, uchar  bInvokeId);
