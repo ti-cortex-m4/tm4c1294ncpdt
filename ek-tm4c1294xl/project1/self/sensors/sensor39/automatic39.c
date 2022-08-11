d9d28cbb5ae443b9d34f6ffaf09bdabf2468c4ee
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2022-08-11 18:30:01 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/automatic39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/automatic39.c
@@ -12,6 +12,7 @@ automatic39.c
 #include "../../sensors/automatic1.h"
 #include "dlms.h"
 #include "error39.h"
+#include "crc16x25.h"
 #include "io39.h"
 #include "hdlc_address.h"
 #include "dlms_push.h"
@@ -85,102 +86,18 @@ bool    Automatic39(void)
 
 
 
-bool    ChangeSpeed39(void)
+static void DISC0(uchar  bNumber)
 {
-  caller39 c = InitCaller39();
-
-  uchar bError = FragmentOpen39(&c);
-  if (bError != 0) return Fault(80+6);
-
-  c.bNS++;
-  c.bInvokeId++;
-  QueryId39_Single(c.bNS, c.bNR, c.bInvokeId);
-  if (Input39() != SER_GOODCHECK) return Fault(80+7);
-  if (ValidateFrame(c.bNS, c.bNR) != 0) return Fault(80+8);
-
-
-  Beep();
-
-
-  InitPop(12 + GetHdlcAddressesSize_Single());
-
-  uchar bResult = PopChar();
-  if (bResult != 0) return Fault(80+9);
-
-  uchar bType = PopChar();
-  if (bType != 10) return Fault(80+10); // visible-string
-
-  uchar bSize = PopChar();
-  Show(bSize);
-
-
-  DISC();
-  if (Input39() != SER_GOODCHECK) return Fault(80+11);
-
-
-  return true;
-}
-
-
-
-uchar   GetHdlcAddressesSize_(uchar  wAddr)
-{
-  if (wAddr == 0) {
-    return 1+1;
-  } else if (wAddr <= 0x7F) {
-    return 2+1;
-  } else {
-    return 4+1;
-  }
-}
-
-
-void    PushHdlcAddresses_(uchar  wAddr)
-{
-  if (wAddr == 0) {
-    PushChar(0x03);
-  } else if (wAddr <= 0x7F) {
-    PushChar(0x02);
-  } else {
-    PushChar(0x00);
-    PushChar(0x02);
-  }
-
-  uchar bHi = wAddr / 0x100;
-  uchar bLo = wAddr % 0x100;
-
-  bHi = (bHi << 2) & 0xFC;
-  if (bLo & 0x80) bHi |= 0x02;
-  bLo = (bLo << 1) | 0x01;
-
-  if (wAddr == 0) {
-  } else if (wAddr <= 0x7F) {
-    PushChar(bLo);
-  } else {
-    PushChar(bHi);
-    PushChar(bLo);
-  }
-
-  PushChar((bLogical << 1) + 0x01);
-}
-
-
-void    DISC_(uchar  bNumber)
-{
-#ifdef MONITOR_39_NAMES
-  MonitorString("\n\n DISC ");
-#endif
-
   InitPush(0);
 
-  uint wSize = 5+GetHdlcAddressesSize_(bNumber); // 7
+  uint wSize = 5+GetHdlcAddressesSize0(bNumber); // 7
 
   PushChar(0x7E);
   PushFormatDLMS(wSize);
-  PushHdlcAddresses_(bNumber);
+  PushHdlcAddresses0(bNumber);
   PushChar(0x53); // DISC
 
-  PushIntLtl(MakeCRC16X25OutBuff(1, 3+GetHdlcAddressesSize_(bNumber))); // 5
+  PushIntLtl(MakeCRC16X25OutBuff(1, 3+GetHdlcAddressesSize0(bNumber))); // 5
   PushChar(0x7E);
 
   Query39(1000, wSize+2); // 9
@@ -189,7 +106,7 @@ void    DISC_(uchar  bNumber)
 
 bool    ChangeNumber39(uchar  bNumber)
 {
-  DISC_(bNumber);
+  DISC0(bNumber);
   if (Input39() != SER_GOODCHECK) return(0);
   Beep();
 
--- ek-tm4c1294xl/project1/self/sensors/sensor39/automatic39.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/automatic39.h
@@ -5,5 +5,4 @@ automatic39.h
 ------------------------------------------------------------------------------*/
 
 bool    Automatic39(void);
-bool    ChangeSpeed39(void);
 bool    ChangeNumber39(uchar  bNumber);
