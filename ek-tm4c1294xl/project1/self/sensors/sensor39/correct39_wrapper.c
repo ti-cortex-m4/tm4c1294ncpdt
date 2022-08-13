d36481931ba2f98cc7edee507ff3751a773bcbb9
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-08-13 17:20:51 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/correct39_wrapper.c
@@ -0,0 +1,28 @@
+/*------------------------------------------------------------------------------
+correct39_wrapper.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+//#include "../../kernel/tuples.h"
+//#include "../../serial/monitor.h"
+//#include "../../serial/monitor_settings.h"
+//#include "include39.h"
+//#include "dlms.h"
+//#include "dlms_obis.h"
+//#include "caller39.h"
+//#include "time39.h"
+//#include "hdlc_address.h"
+//#include "hdlc_monitor.h"
+//#include "buffer_record_39.h"
+//#include "query_profile_39.h"
+//#include "query_next_block_39.h"
+//#include "dlms_read_data.h"
+//#include "dlms_read_register.h"
+//#include "query_register_39.h"
+//#include "profile39.h"
+#include "correct39_wrapper.h"
+
+
+
