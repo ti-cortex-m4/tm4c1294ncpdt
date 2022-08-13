b5be23a20a46e247b1b872da92b9ee91dec19f1a
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-08-13 17:41:03 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor39/correct39_wrapper.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/correct39_wrapper.c
@@ -5,29 +5,22 @@ correct39_wrapper.c
 ------------------------------------------------------------------------------*/
 
 #include "../../main.h"
-//#include "../../kernel/tuples.h"
-//#include "../../serial/monitor.h"
-//#include "../../serial/monitor_settings.h"
-//#include "include39.h"
-//#include "dlms.h"
-//#include "dlms_obis.h"
-//#include "caller39.h"
-//#include "time39.h"
-//#include "hdlc_address.h"
-//#include "hdlc_monitor.h"
-//#include "buffer_record_39.h"
-//#include "query_profile_39.h"
-//#include "query_next_block_39.h"
-//#include "dlms_read_data.h"
-//#include "dlms_read_register.h"
-//#include "query_register_39.h"
-//#include "profile39.h"
 #include "correct39_wrapper.h"
+
 
 
 static uchar            cbCorrects;
 static uint             wDeltaOld;
 static uint             wDeltaNew;
+
+
+
+void    Init_Current39(void)
+{
+  cbCorrects = 0;
+  wDeltaOld = 0;
+  wDeltaNew = 0;
+}
 
 
 
@@ -38,33 +31,23 @@ uchar   GetCounter_Correct39(void)
 
 
 
-void    Init_Current39(void)
-{
-  cbCorrects = 0; // TODO
-  wDeltaOld = 0;
-  wDeltaNew = 0;
-}
-
-
-
 void    Write_Current39(uint  wDelta)
 {
   cbCorrects++;
-  wDeltaOld = (uint)dwDelta;
+  wDeltaOld = wDelta;
 }
 
 
 
-void    Read1_Current39(uint  wDelta)
+bool    Read2_Current39(uint  wDelta)
+{
+  wDeltaNew = wDelta;
+  return (wDeltaNew < wDeltaOld) && (cbCorrects < 100);
+}
+
+
+void    Read3_Current39(void)
 {
   cbCorrects++;
   wDeltaOld = wDeltaNew;
-}
-
-void    Read2_Current39(uint  wDelta)
-{
-}
-
-void    Read3_Current39(uint  wDelta)
-{
 }
--- ek-tm4c1294xl/project1/self/sensors/sensor39/correct39_wrapper.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor39/correct39_wrapper.h
@@ -4,11 +4,11 @@ correct39_wrapper.h
 
 ------------------------------------------------------------------------------*/
 
-uchar   GetCounter_Correct39(void);
-
 void    Init_Current39(void);
 
+uchar   GetCounter_Correct39(void);
+
 void    Write_Current39(uint  wDelta);
-void    Read1_Current39(uint  wDelta);
-void    Read2_Current39(uint  wDelta);
-void    Read3_Current39(uint  wDelta);
+
+bool    Read2_Current39(uint  wDelta);
+void    Read3_Current39(void);
