c4e6239ac3d839253d6f9a58e96433981786f094
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-08-21 19:29:21 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor40/query_billing_period_39.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor40/query_billing_period_39.h
@@ -4,4 +4,4 @@ query_billing_period_39.h
 
 ------------------------------------------------------------------------------*/
 
-void    QueryBillingPeriod39(const obis_t  obisBillingPeriod, uchar  bNS, uchar  bNR, uchar  bInvokeId, date  dt);
+void    QueryBillingPeriod40(const obis_t  obisBillingPeriod, uchar  bNS, uchar  bNR, uchar  bInvokeId, date  dt);
