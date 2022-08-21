c744360d2978ca344cac20d089307d07fed0be9a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2022-08-21 18:10:46 +0300

    Update include39.h

--- ek-tm4c1294xl/project1/self/sensors/sensor40/include39.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor40/include39.h
@@ -1,29 +1,29 @@
 /*------------------------------------------------------------------------------
-include39*h
+include40.h
 
 
 ------------------------------------------------------------------------------*/
 
-#ifndef __INCLUDE39
-#define __INCLUDE39
+#ifndef __INCLUDE40
+#define __INCLUDE40
 
 
-#define MONITOR_40          1
+#define MONITOR_40          1 // TODO
 #define MONITOR_40_NAMES    1
 //#define MONITOR_40_MONITOR  1
 //#define MONITOR_40_CRC      1
-//#define BUFFER_ARRAY_39     1
-#define BUFFER_RECORD_39    1
-#define PROFILE_39_NAMES    1
+//#define BUFFER_ARRAY_40     1
+#define BUFFER_RECORD_40    1
+#define PROFILE_40_NAMES    1
 
 
 #ifndef MONITOR_40
 #undef MONITOR_40_NAMES
 #undef MONITOR_40_MONITOR
 #undef MONITOR_40_CRC
-#undef BUFFER_ARRAY_39
-#undef BUFFER_RECORD_39
-#undef PROFILE_39_NAMES
+#undef BUFFER_ARRAY_40
+#undef BUFFER_RECORD_40
+#undef PROFILE_40_NAMES
 #endif
 
 
