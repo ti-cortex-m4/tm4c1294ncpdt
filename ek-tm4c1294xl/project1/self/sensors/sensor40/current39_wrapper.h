70f6ae96b66403094455c71827e93383e7e3ec8c
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-08-22 18:38:21 +0300

    current40

--- ek-tm4c1294xl/project1/self/sensors/sensor40/current39_wrapper.h
+++ ek-tm4c1294xl/project1/self/sensors/sensor40/current39_wrapper.h
@@ -1,35 +1,35 @@
 /*------------------------------------------------------------------------------
-current39_wrapper.h
+current40_wrapper.h
 
 
 ------------------------------------------------------------------------------*/
 
-#ifndef __CURRENT39_WRAPPER
-#define __CURRENT39_WRAPPER
+#ifndef __CURRENT40_WRAPPER
+#define __CURRENT40_WRAPPER
 
 
-#define CURRENT39_REPEAT_OR_ERROR(error, method, action) { Error40(error); if (cbRepeat == 0) ErrorCurrent(); else { ErrorLink(); cbRepeat--; method; SetCurr(action); } }
+#define CURRENT40_REPEAT_OR_ERROR(error, method, action) { Error40(error); if (cbRepeat == 0) ErrorCurrent(); else { ErrorLink(); cbRepeat--; method; SetCurr(action); } }
 
 
 #endif
 
 
 
-void    ClearLine39(void);
-uchar   IncLine39(void);
-uchar   GetLine39(void);
+void    ClearLine40(void);
+uchar   IncLine40(void);
+uchar   GetLine40(void);
 
-void    DISC_Current39(void);
-void    SNRM_Current39(void);
-void    AARQ_Current39(void);
-void    RR_Current39(void);
+void    DISC_Current40(void);
+void    SNRM_Current40(void);
+void    AARQ_Current40(void);
+void    RR_Current40(void);
 
-bool    ValidateFrame_Current39(void);
+bool    ValidateFrame_Current40(void);
 
-void    QueryValue_Current39(void);
-bool    ReadValue_Current39(void);
+void    QueryValue_Current40(void);
+bool    ReadValue_Current40(void);
 
-void    QueryScaler_Current39(void);
-bool    ReadScaler_Current39(void);
+void    QueryScaler_Current40(void);
+bool    ReadScaler_Current40(void);
 
-void    ReadCurrent39(void);
+void    ReadCurrent40(void);
