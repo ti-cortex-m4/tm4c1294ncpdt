936cad1b18b6b42faf2c412ce9485dfa70d03f38
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-08-23 19:47:47 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor40/buffer_profile_39.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor40/buffer_profile_39.c
@@ -13,60 +13,62 @@ buffer_profile_39*c
 
 
 
-#define PROFILE39_SIZE  (6+2)
+#define PROFILE40_SIZE  (6+6)
 
 
 
-static profile40    mpProfile39[PROFILE39_SIZE];
-static uchar        cbProfileSize39;
-static bool         fProfileOveflow39;
+static profile40    mpProfile[PROFILE40_SIZE];
+
+static uchar        cbProfileSize;
+static bool         fProfileOveflow;
 
 
 
 void    InitProfile40(void)
 {
 #ifdef MONITOR_40
-  MonitorString("\n init profile ");
+  MonitorString("\n InitProfile40");
 #endif
 
-  memset(&mpProfile39, 0, sizeof(mpProfile39));
-  cbProfileSize39 = 0;
-  fProfileOveflow39 = false;
+  memset(&mpProfile, 0, sizeof(mpProfile));
+
+  cbProfileSize = 0;
+  fProfileOveflow = false;
 }
 
 
-void    AddProfile40(time  tmTime, ulong  mdwValue[4])
+void    AddProfile40(time  tmHhrEnd, ulong  mdwValue[4])
 {
 #ifdef MONITOR_40
   MonitorString("\n add profile (end time) ");
-  MonitorString(" "); MonitorIntDec(cbProfileSize39);
-  MonitorString(" "); MonitorTime(tmTime);
+  MonitorString(" "); MonitorIntDec(cbProfileSize);
+  MonitorString(" "); MonitorTime(tmHhrEnd);
   MonitorString(" "); MonitorLongDec(mdwValue[0]);
   MonitorString(" "); MonitorLongDec(mdwValue[1]);
   MonitorString(" "); MonitorLongDec(mdwValue[2]);
   MonitorString(" "); MonitorLongDec(mdwValue[3]);
 #endif
 
-  if (cbProfileSize39 < PROFILE39_SIZE)
+  if (cbProfileSize < PROFILE40_SIZE)
   {
-    uchar i = cbProfileSize39;
-    ASSERT(i < PROFILE39_SIZE);
+    uchar p = cbProfileSize;
+    ASSERT(p < PROFILE40_SIZE);
 
-    mpProfile39[i].fExists = true;
-    mpProfile39[i].tmTime = tmTime;
+    mpProfile[p].fExists = true;
+    mpProfile[p].tmHhrEnd = tmHhrEnd;
 
     uchar c;
     for (c=0; c<4; c++)
-      mpProfile39[i].mdwValue[c] = mdwValue[c];
+      mpProfile[p].mdwValue[c] = mdwValue[c];
 
-    cbProfileSize39++;
+    cbProfileSize++;
   }
   else
   {
     MonitorString("\n add profile overflow ");
     Error40(160+0);
 
-    fProfileOveflow39 = true;
+    fProfileOveflow = true;
   }
 }
 
@@ -75,12 +77,12 @@ void    AddProfile40(time  tmTime, ulong  mdwValue[4])
 void    MonitorBuffPrf38(void)
 {
 #ifdef MONITOR_40
-  uchar i;
-  for (i=0; i<PROFILE39_SIZE; i++)
+  uchar p;
+  for (p=0; p<PROFILE40_SIZE; p++)
   {
-    profile40 prf = GetProfile40(i);
+    profile40 prf = GetProfile40(p);
 
-    MonitorString("\n "); MonitorTime(prf.tmTime);
+    MonitorString("\n "); MonitorTime(prf.tmHhrEnd);
     MonitorString(" "); MonitorLongDec(prf.mdwValue[0]);
     MonitorString(" "); MonitorLongDec(prf.mdwValue[1]);
     MonitorString(" "); MonitorLongDec(prf.mdwValue[2]);
@@ -91,20 +93,20 @@ void    MonitorBuffPrf38(void)
 }
 
 
-profile40 GetProfile40(uchar  i)
+profile40 GetProfile40(uchar  p)
 {
-  ASSERT(i < PROFILE39_SIZE);
-  return mpProfile39[i];
+  ASSERT(p < PROFILE40_SIZE);
+  return mpProfile[p];
 }
 
 
 uchar   GetProfileSize40(void)
 {
-  return cbProfileSize39;
+  return cbProfileSize;
 }
 
 
 bool    IsProfileOveflow40(void)
 {
-  return fProfileOveflow39;
+  return fProfileOveflow;
 }
