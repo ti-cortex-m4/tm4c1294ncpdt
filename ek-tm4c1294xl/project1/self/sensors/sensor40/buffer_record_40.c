a2a02689a4362cdbc6f3c74348a2e1a1e8b7542a
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2022-08-23 20:55:19 +0300

    fix

--- ek-tm4c1294xl/project1/self/sensors/sensor40/buffer_record_40.c
+++ ek-tm4c1294xl/project1/self/sensors/sensor40/buffer_record_40.c
@@ -75,79 +75,6 @@ static bool FaultData(uchar  bError, uint  wData)
 
 
 
-bool    FinishRecord40_Monitor(void) {
-#if BUFFER_RECORD_40
-  MonitorString("\n FinishRecord40_Monitor");
-#endif
-
-  InitPop40();
-
-  uint wCapacity = GetPopCapacity40();
-  if (wCapacity < 2)
-    return FaultData(150+1, wCapacity);
-
-  uchar bType = PopChar40();
-  if (bType != 0x01) // array
-    return FaultData(150+2, bType);
-
-  uchar bCount = PopChar40();
-
-#ifdef BUFFER_RECORD_40
-  MonitorString("\n Count="); MonitorCharDec(bCount); MonitorString("\n");
-#endif
-
-  uchar i;
-  for (i=0; i<bCount; i++)
-  {
-    wCapacity = GetPopCapacity40();
-    if (wCapacity < 2 + 2+12 + 4*(1+4))
-      return FaultData(150+3, wCapacity);
-
-    bType = PopChar40();
-    if (bType != 0x02) // structure
-      return FaultData(150+4, bType);
-
-    uchar bSize = PopChar40();
-    if (bSize != 5) // structure size
-      return FaultData(150+5, bSize);
-
-    bType = PopChar40();
-    if (bType != 0x09) // string
-      return FaultData(150+6, bType);
-
-    bSize = PopChar40();
-    if (bSize != 12) // string size
-      return FaultData(150+7, bSize);
-
-
-    time tmHhrEnd = PopTimeDate40();
-
-    ulong mdwValue[4];
-
-    uchar c;
-    for (c=0; c<4; c++)
-    {
-      bType = PopChar40();
-      if (bType != 0x06) // double-long-unsigned 32
-        return FaultData(150+8, bType);
-
-       mdwValue[c] = PopLong40();
-    }
-
-#if BUFFER_RECORD_40
-    MonitorString("\n "); MonitorTime(tmHhrEnd);
-    MonitorString(" "); MonitorLongDec(mdwValue[0]);
-    MonitorString(" "); MonitorLongDec(mdwValue[1]);
-    MonitorString(" "); MonitorLongDec(mdwValue[2]);
-    MonitorString(" "); MonitorLongDec(mdwValue[3]);
-#endif
-  }
-
-  return true;
-}
-
-
-
 bool    FinishRecord40_AddProfile40(void) {
 #if BUFFER_RECORD_40
   MonitorString("\n FinishRecord40_AddProfile40");
@@ -217,3 +144,79 @@ bool    FinishRecord40_AddProfile40(void) {
   else
     return true;
 }
+
+
+#ifdef  MONITOR_40
+
+bool    FinishRecord40_Monitor(void) {
+#if BUFFER_RECORD_40
+  MonitorString("\n FinishRecord40_Monitor");
+#endif
+
+  InitPop40();
+
+  uint wCapacity = GetPopCapacity40();
+  if (wCapacity < 2)
+    return FaultData(150+1, wCapacity);
+
+  uchar bType = PopChar40();
+  if (bType != 0x01) // array
+    return FaultData(150+2, bType);
+
+  uchar bCount = PopChar40();
+
+#ifdef BUFFER_RECORD_40
+  MonitorString("\n Count="); MonitorCharDec(bCount); MonitorString("\n");
+#endif
+
+  uchar r;
+  for (r=0; r<bCount; r++)
+  {
+    wCapacity = GetPopCapacity40();
+    if (wCapacity < 2 + 2+12 + 4*(1+4))
+      return FaultData(150+3, wCapacity);
+
+    bType = PopChar40();
+    if (bType != 0x02) // structure
+      return FaultData(150+4, bType);
+
+    uchar bSize = PopChar40();
+    if (bSize != 5) // structure size
+      return FaultData(150+5, bSize);
+
+    bType = PopChar40();
+    if (bType != 0x09) // string
+      return FaultData(150+6, bType);
+
+    bSize = PopChar40();
+    if (bSize != 12) // string size
+      return FaultData(150+7, bSize);
+
+
+    time tmHhrEnd = PopTimeDate40();
+
+    ulong mdwValue[4];
+
+    uchar i;
+    for (i=0; i<4; i++)
+    {
+      bType = PopChar40();
+      if (bType != 0x06) // double-long-unsigned 32
+        return FaultData(150+8, bType);
+
+      mdwValue[i] = PopLong40();
+    }
+
+#if BUFFER_RECORD_40
+    MonitorString("\n "); MonitorTime(tmHhrEnd);
+    MonitorString(" "); MonitorLongDec(mdwValue[0]);
+    MonitorString(" "); MonitorLongDec(mdwValue[1]);
+    MonitorString(" "); MonitorLongDec(mdwValue[2]);
+    MonitorString(" "); MonitorLongDec(mdwValue[3]);
+#endif
+  }
+
+  return true;
+}
+
+#endif
