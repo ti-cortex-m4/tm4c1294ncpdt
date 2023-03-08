c994023d74d9b2834c6400cb4d262a9ba36de89e
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2023-03-08 10:41:57 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor41/automatic_get_cntmon_41.c
@@ -0,0 +1,144 @@
+/*------------------------------------------------------------------------------
+automatic_get_cntmon_38.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+#include "../../memory/mem_factors.h"
+#include "../../display/display.h"
+#include "../../keyboard/keyboard.h"
+#include "../../serial/ports.h"
+#include "../../serial/ports2.h"
+#include "../../devices/devices.h"
+#include "../../digitals/digitals.h"
+#include "device38.h"
+#include "io38.h"
+#include "dff.h"
+#include "automatic_get_time_38.h"
+#include "automatic_get_cntmon_38.h"
+
+
+
+bool    ReadEngDay38_Full(uchar  ibDayRel)
+{
+  Clear();
+
+  uchar r;
+  for (r=0; r<MaxRepeats(); r++)
+  {
+    QueryEngDay38(0);
+
+    if (Input38() == SER_GOODCHECK) break;
+    if (fKey == true) return false;
+  }
+
+  if (r == MaxRepeats()) return false;
+
+
+  uchar* pbIn = InBuffPtr(10);
+
+  uchar i;
+  for (i=0; i<4; i++)
+  {
+    *(pbIn++);
+
+    int64_t ddw = 0;
+    pbIn = DffDecodePositive(pbIn, &ddw);
+
+    uchar bStatus = (ddw % 0x100) & 0x03;
+    ulong dw = (ddw >> 3) % 0x100000000;
+
+    if (!GoodStatus38(bStatus)) {
+      Clear();
+      sprintf(szLo+1, "сутки -%u (%u) ?", ibDayRel, bStatus);
+      Delay(1000);
+      return false;
+    }
+
+    mpdwChannelsA[i] = dw;
+    mpdbChannelsC[i] = (double)mpdwChannelsA[i] / 10000;
+  }
+
+
+  for (i=0; i<4; i++)
+  {
+    mpdbChannelsC[i] *= mpdbTransCnt[ibDig];
+    mpboChannelsA[i] = true;
+  }
+
+  return true;
+}
+
+
+
+bool    ReadEngMon38_Full(uchar  ibMonRel)
+{
+  Clear();
+
+  uchar r;
+  for (r=0; r<MaxRepeats(); r++)
+  {
+    QueryEngMon38(ibMonRel);
+
+    if (Input38() == SER_GOODCHECK) break;
+    if (fKey == true) return false;
+  }
+
+
+  uchar* pbIn = InBuffPtr(10);
+
+  uchar i;
+  for (i=0; i<4; i++)
+  {
+    *(pbIn++);
+
+    int64_t ddw = 0;
+    pbIn = DffDecodePositive(pbIn, &ddw);
+
+    uchar bStatus = (ddw % 0x100) & 0x03;
+    ulong dw = (ddw >> 3) % 0x100000000;
+
+    if (!GoodStatus38(bStatus)) {
+      Clear();
+      sprintf(szLo+1, "мес€ц -%u (%u) ?", ibMonRel, bStatus);
+      Delay(1000);
+      return false;
+    }
+
+    mpdwChannelsA[i] = dw;
+    mpdbChannelsC[i] = (double)mpdwChannelsA[i] / 10000;
+  }
+
+
+  for (i=0; i<4; i++)
+  {
+    mpdbChannelsC[i] *= mpdbTransCnt[ibDig];
+    mpboChannelsA[i] = true;
+  }
+
+  return true;
+}
+
+
+
+double2 ReadCntMonCan38(uchar  ibMonAbs)
+{
+  Clear();
+
+  time2 ti2 = ReadTimeCan38();
+  if (ti2.fValid == false) return GetDouble2Error();
+  time ti = ti2.tiValue;
+
+  if (ti.bMonth != ibMonAbs+1)
+  {
+    uchar ibMonRel = (bMONTHS+ti.bMonth-2-ibMonAbs) % bMONTHS;
+    if (ReadEngMon38_Full(ibMonRel) == false) return GetDouble2Error();
+  }
+  else
+  {
+    if (ReadEngDay38_Full(0) == false) return GetDouble2Error();
+  }
+
+  return GetDouble2(mpdbChannelsC[diCurr.ibLine], true);
+}
