590ffe0f6f6d5a7498c99ee1cfcb388180c47999
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2023-05-27 18:50:17 +0300

    fix

--- ek-tm4c1294xl/project1/self/digitals/profile/profile_interval.h
+++ ek-tm4c1294xl/project1/self/digitals/profile/profile_interval.h
@@ -6,6 +6,15 @@ profile_interval.h
 
 extern  bool                    boProfileInterval;
 extern  cache const             chProfileInterval;
+
+extern  uchar                   bProfileIntervalDirect;
+extern  cache const             chProfileIntervalDirect;
+
+extern  uchar                   bProfileIntervalModem;
+extern  cache const             chProfileIntervalModem;
+
+
+
 extern  uchar                   ibProfileIntervalDig;
 
 
