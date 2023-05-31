27d77d9f016041ebeb46a4dcea3cb82bd08e2009
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2023-05-31 19:38:48 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/realtime/realtime_indices.c
@@ -0,0 +1,113 @@
+/*------------------------------------------------------------------------------
+timeout35.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "realtime_indices.h"
+
+#include "../../main.h"
+#include "../../memory/mem_settings.h"
+#include "../../serial/ports.h"
+
+
+
+static uint             mwTimeoutHistogramAbs35[0x100],
+                        mwTimeoutHistogramDay35[0x100];
+
+
+
+#define DELTA_SIZE      10
+
+static uint             cwTimeoutDelta35;
+static uchar            mbTimeoutDelta35[DELTA_SIZE];
+
+
+
+void    InitTimeoutHistogram35(void)
+{
+  memset(&mwTimeoutHistogramAbs35, 0, sizeof(mwTimeoutHistogramAbs35));
+  memset(&mwTimeoutHistogramDay35, 0, sizeof(mwTimeoutHistogramDay35));
+
+  cwTimeoutDelta35 = 0;
+  memset(&mbTimeoutDelta35, 0, sizeof(mbTimeoutDelta35));
+}
+
+
+void    NextDayResetTimeoutHistogram35(void)
+{
+  memset(&mwTimeoutHistogramDay35, 0, sizeof(mwTimeoutHistogramDay35));
+}
+
+
+
+void    Timeout35(uint  wTimeout)
+{
+  uchar i = wTimeout < 0x100 ? wTimeout : 0xFF;
+
+  mwTimeoutHistogramAbs35[i]++;
+  mwTimeoutHistogramDay35[i]++;
+
+  mbTimeoutDelta35[cwTimeoutDelta35++ % DELTA_SIZE] = i;
+}
+
+
+
+uchar   GetTimeoutDelta35(void)
+{
+  uint  w = 0;
+  uchar c = 0;
+
+  uchar i;
+  for (i=0; i<DELTA_SIZE; i++)
+  {
+    uchar j = mbTimeoutDelta35[(DELTA_SIZE + cwTimeoutDelta35 - i) % DELTA_SIZE];
+    if (j != 0)
+    {
+      w += j;
+      c++;
+    }
+  }
+
+  return w/c;
+}
+
+
+
+void    OutTimeoutHistogramAll35(void)
+{
+  InitPushCRC();
+
+  PushIntLtl(cwTimeoutDelta35);
+  Push(&mbTimeoutDelta35, sizeof(mbTimeoutDelta35));
+  PushChar(GetTimeoutDelta35());
+
+  Push(&mwTimeoutHistogramAbs35, sizeof(mwTimeoutHistogramAbs35));
+  Push(&mwTimeoutHistogramDay35, sizeof(mwTimeoutHistogramDay35));
+
+  Output(2+sizeof(mbTimeoutDelta35)+1+sizeof(mwTimeoutHistogramAbs35)+sizeof(mwTimeoutHistogramDay35));
+}
+
+
+void    OutResetTimeoutHistogramAll35(void)
+{
+  if (enGlobal == GLB_REPROGRAM)
+  {
+    InitTimeoutHistogram35();
+    Result(bRES_OK);
+  }
+  else
+    Result(bRES_NEEDREPROGRAM);
+}
+
+
+void    OutResetTimeoutHistogramDay35(void)
+{
+  if (enGlobal == GLB_REPROGRAM)
+  {
+    NextDayResetTimeoutHistogram35();
+    Result(bRES_OK);
+  }
+  else
+    Result(bRES_NEEDREPROGRAM);
+}
---
+++ ek-tm4c1294xl/project1/self/realtime/realtime_indices.h
@@ -0,0 +1,17 @@
+/*------------------------------------------------------------------------------
+realtime_indices.h
+
+
+------------------------------------------------------------------------------*/
+/*
+void    InitTimeoutHistogram35(void);
+void    NextDayResetTimeoutHistogram35(void);
+
+void    Timeout35(uint  wTimeout);
+
+uchar   GetTimeoutDelta35(void);
+
+void    OutTimeoutHistogramAll35(void);
+void    OutResetTimeoutHistogramAll35(void);
+void    OutResetTimeoutHistogramDay35(void);
+*/
