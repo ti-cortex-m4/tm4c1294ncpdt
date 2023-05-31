b8e638dbcde833d56b2c18dd785d719d42d7c5bc
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2023-05-31 19:59:47 +0300

    fix

--- ek-tm4c1294xl/project1/self/realtime/realtime_indices.h
+++ ek-tm4c1294xl/project1/self/realtime/realtime_indices.h
@@ -31,15 +31,6 @@ typedef struct
 
 #endif
 
-/*
-void    InitTimeoutHistogram35(void);
-void    NextDayResetTimeoutHistogram35(void);
 
-void    Timeout35(uint  wTimeout);
-
-uchar   GetTimeoutDelta35(void);
-
-void    OutTimeoutHistogramAll35(void);
-void    OutResetTimeoutHistogramAll35(void);
-void    OutResetTimeoutHistogramDay35(void);
-*/
+void    InitRealtimeIndices(void);
+void    OutRealtimeIndices(void);
