6e25568b9bd1c11c1ca502dcb359ddae2d1f152b
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2023-06-01 18:27:12 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/time/bcd_time.c
@@ -0,0 +1,41 @@
+/*------------------------------------------------------------------------------
+bcd_time.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+#include "bcd_time.h"
+
+
+
+time        BcdTimeToTime(uint32_t  t)
+{
+  time tm;
+
+  tm.bSecond = 0x3F & (t);
+  tm.bMinute = 0x3F & (t >> 6);
+  tm.bHour   = 0x1F & (t >> 6+6);
+
+  tm.bDay    = 0x1F & (t >> 6+6+5);
+  tm.bMonth  = 0x0F & (t >> 6+6+5+5);
+  tm.bYear  = (0x3F & (t >> 6+6+5+5+4)) + 20;
+
+  return tm;  
+}
+
+
+uint32_t    TimeToBcdTime(time  tm)
+{
+  uint32_t dw = 0;
+
+  dw |= (tm.bSecond      & 0x3F);
+  dw |= (tm.bMinute      & 0x3F) << 6;
+  dw |= (tm.bHour        & 0x1F) << 6+6;
+
+  dw |= (tm.bDay         & 0x1F) << 6+6+5;
+  dw |= (tm.bMonth       & 0x0F) << 6+6+5+5;
+  dw |= ((tm.bYear - 20) & 0x3F) << 6+6+5+5+4;
+
+  return dw;
+}
