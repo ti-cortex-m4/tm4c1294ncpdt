eb926a65bf804556f3e8b52232dc1ee541b45a6a
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2023-06-02 18:36:32 +0300

    Update bcd_time.c

--- ek-tm4c1294xl/project1/self/time/bcd_time.c
+++ ek-tm4c1294xl/project1/self/time/bcd_time.c
@@ -15,11 +15,11 @@ time    BcdTimeToTime(ulong  dw)
 
   tm.bSecond = 0x3F & (dw);
   tm.bMinute = 0x3F & (dw >> 6);
-  tm.bHour   = 0x1F & (dw >> 6+6);
+  tm.bHour   = 0x1F & (dw >> (6+6));
 
-  tm.bDay    = 0x1F & (dw >> 6+6+5);
-  tm.bMonth  = 0x0F & (dw >> 6+6+5+5);
-  tm.bYear  = (0x3F & (dw >> 6+6+5+5+4)) + 20;
+  tm.bDay    = 0x1F & (dw >> (6+6+5));
+  tm.bMonth  = 0x0F & (dw >> (6+6+5+5));
+  tm.bYear  = (0x3F & (dw >> (6+6+5+5+4))) + 20;
 
   return tm;  
 }
@@ -31,11 +31,11 @@ ulong   TimeToBcdTime(time  tm)
 
   dw |= (tm.bSecond      & 0x3F);
   dw |= (tm.bMinute      & 0x3F) << 6;
-  dw |= (tm.bHour        & 0x1F) << 6+6;
+  dw |= (tm.bHour        & 0x1F) << (6+6);
 
-  dw |= (tm.bDay         & 0x1F) << 6+6+5;
-  dw |= (tm.bMonth       & 0x0F) << 6+6+5+5;
-  dw |= ((tm.bYear - 20) & 0x3F) << 6+6+5+5+4;
+  dw |= (tm.bDay         & 0x1F) << (6+6+5);
+  dw |= (tm.bMonth       & 0x0F) << (6+6+5+5);
+  dw |= ((tm.bYear - 20) & 0x3F) << (6+6+5+5+4);
 
   return dw;
 }
