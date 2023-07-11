2bac2fac0162f265682bca1570491c04e966b293
Aliaksandr Liakh <ti.cortex.m4@gmail.com>
2023-07-11 19:29:53 +0300

    fix

---
+++ ek-tm4c1294xl/project1/self/sensors/sensor2/profile2mode.c
@@ -0,0 +1,29 @@
+/*------------------------------------------------------------------------------
+profile2mode.h
+
+
+------------------------------------------------------------------------------*/
+
+#include "../../main.h"
+
+
+
+bool    UseProfile2x1(void) {
+}
+
+
+bool    UseProfile2x16(void) {
+}
+
+
+bool    UseProfile2x17(void) {
+}
+
+
+
+bool    UseProfile2Top(void) {
+}
+
+
+bool    UseProfile2Top17(void) {
+}
---
+++ ek-tm4c1294xl/project1/self/sensors/sensor2/profile2mode.h
@@ -0,0 +1,13 @@
+/*------------------------------------------------------------------------------
+profile2mode.h
+
+
+------------------------------------------------------------------------------*/
+
+
+bool    UseProfile2x1(void);
+bool    UseProfile2x16(void);
+bool    UseProfile2x17(void);
+
+bool    UseProfile2Top(void);
+bool    UseProfile2Top17(void);
