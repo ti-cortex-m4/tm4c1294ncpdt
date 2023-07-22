3c005a482ae1474ee5c248d8f7e45758bac71157
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2023-07-22 15:15:45 +0300

    fix

--- ek-tm4c1294xl/project1/self/devices/devices.c
+++ ek-tm4c1294xl/project1/self/devices/devices.c
@@ -1669,6 +1669,50 @@ void    RunDevices(void)
 
 #ifndef SKIP_B
 
+    // Меркурий-233 блоками по 12 получасов
+    case DEV_HEADER_B2x12:
+      if (mpSerial[ibPort] == SER_GOODCHECK)
+        MakePause(DEV_POSTHEADER_B2x12);
+      else
+      {
+        if (cbRepeat == 0) ErrorProfile();
+        else
+        {
+          ErrorLink();
+          cbRepeat--;
+
+          QueryHeaderBx12();
+          SetCurr(DEV_HEADER_B2x12);
+        }
+      }
+      break;
+
+    case DEV_POSTHEADER_B2x12:
+      {
+        NewBoundsAbs32(dwBaseCurr);
+
+        uchar i;
+        for (i=0; i<12; i++)
+        {
+          if (ReadHeaderBx12(i,1) == 0) break;
+          (dwBaseCurr == 0) ? (dwBaseCurr = 0x1FFF0) : (dwBaseCurr -= 0x0010);
+        }
+
+        if (i != 12)
+          DoneProfile();
+        else
+        {
+          cbRepeat = MaxRepeats();
+          QueryHeaderBx12();
+          SetCurr(DEV_HEADER_B2x12);
+        }
+      }
+      break;
+
+#endif
+
+#ifndef SKIP_B
+
     case DEV_START_B3:
       cbRepeat = MaxRepeats();
       QueryOpenB();
