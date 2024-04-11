66705da38778b9ebdb19fe58ec6a8c40777873a9
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2024-04-11 21:32:57 +0300

    add

---
+++ ek-tm4c1294xl/project2/self/kernel/link_status_up.c
@@ -0,0 +1,39 @@
+/*------------------------------------------------------------------------------
+link_status_up.c
+
+
+------------------------------------------------------------------------------*/
+
+#include "../main.h"
+//#include "../hardware/delay.h"
+//#include "../hardware/hardware_restart.h"
+//#include "../kernel/settings.h"
+//#include "../kernel/log.h"
+//#include "clock.h"
+#include "link_status_up.h"
+
+
+
+void InitLinkStatusUp(void) {
+//  if ((fPeriodicResetFlag == 1) && (wPeriodicResetPeriod >= 1) && (wPeriodicResetPeriod <= 1440))
+//  {
+//    uint minutes = GetClockSeconds() / 60;
+//    if (minutes >= wPeriodicResetPeriod)
+//    {
+//      CONSOLE("periodic restart \n");
+//      DelaySecond(1);
+//
+//      //Restart();
+//      HardwareRestart();
+//    }
+//  }
+}
+
+
+void    LinkStatusUp_1Hz(void) {
+
+}
+
+
+void RunLinkStatusUp(void) {
+}
