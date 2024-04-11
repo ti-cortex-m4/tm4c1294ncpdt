41fbd7516c0b387ff40c1fc4d75c277a6bf8dc24
ti-cortex-m4 <ti-cortex-m4@users.noreply.github.com>
2024-04-11 21:43:07 +0300

    add

--- ek-tm4c1294xl/project2/self/kernel/link_up_reset.h
+++ ek-tm4c1294xl/project2/self/kernel/link_up_reset.h
@@ -1,9 +1,10 @@
 /*------------------------------------------------------------------------------
-link_status_up.h
+link_up_reset.h
 
 
 ------------------------------------------------------------------------------*/
 
 void InitLinkUpReset(void);
+void LinkUpReset_Start(void);
 void LinkUpReset_1Hz(void);
 void RunLinkUpReset(void);
